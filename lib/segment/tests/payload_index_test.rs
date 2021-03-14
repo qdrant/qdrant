0a91d3ae06fe1e7381a42599730256e3a7b5964a
Andrey Vasnetsov <andrey@vasnetsov.com>
2021-03-14 19:39:41 +0100

    struct payload index segment test

--- lib/segment/tests/payload_index_test.rs
+++ lib/segment/tests/payload_index_test.rs
@@ -2,10 +2,12 @@
 mod tests {
     use rand::prelude::ThreadRng;
     use rand::seq::SliceRandom;
-    use segment::types::{PayloadType, VectorElementType, SegmentConfig, Indexes, PayloadIndexType, Distance, StorageType};
+    use segment::types::{PayloadType, VectorElementType, SegmentConfig, Indexes, PayloadIndexType, Distance, StorageType, TheMap, PayloadKeyType, Filter, Condition, FieldCondition, Match, Range};
     use rand::Rng;
     use tempdir::TempDir;
     use segment::segment_constructor::segment_constructor::build_segment;
+    use segment::entry::entry_point::SegmentEntry;
+    use itertools::Itertools;
 
     const ADJECTIVE: &'static [&'static str] = &[
         "jobless",
@@ -21,7 +23,7 @@ mod tests {
         "broad",
     ];
 
-    const NOUN: &'static [&'static str] =&[
+    const NOUN: &'static [&'static str] = &[
         "territory",
         "jam",
         "neck",
@@ -49,18 +51,75 @@ mod tests {
     fn random_int_payload(rnd_gen: &mut ThreadRng) -> PayloadType {
         let val1: i64 = rnd_gen.gen_range(0..500);
         let val2: i64 = rnd_gen.gen_range(0..500);
-        let val3: i64 = rnd_gen.gen_range(0..500);
-
-        PayloadType::Integer(vec![val1, val2, val3])
+        PayloadType::Integer(vec![val1, val2])
     }
 
     fn random_vector(rnd_gen: &mut ThreadRng, size: usize) -> Vec<VectorElementType> {
         (0..size).map(|_| rnd_gen.gen()).collect()
     }
 
+    fn random_field_condition(rnd_gen: &mut ThreadRng) -> Condition {
+        let kv_or_int: bool = rnd_gen.gen();
+        match kv_or_int {
+            true => Condition::Field(FieldCondition {
+                key: "kvd".to_string(),
+                r#match: Some(Match {
+                    keyword: Some(random_keyword(rnd_gen)),
+                    integer: None,
+                }),
+                range: None,
+                geo_bounding_box: None,
+                geo_radius: None,
+            }),
+            false => Condition::Field(FieldCondition {
+                key: "int".to_string(),
+                r#match: None,
+                range: Some(Range {
+                    lt: None,
+                    gt: None,
+                    gte: Some(rnd_gen.gen_range(0..500) as f64),
+                    lte: Some(rnd_gen.gen_range(0..500) as f64),
+                }),
+                geo_bounding_box: None,
+                geo_radius: None,
+            })
+        }
+    }
+
+    fn random_filter(rnd_gen: &mut ThreadRng) -> Filter {
+        let mut rnd1 = rand::thread_rng();
+
+        let should_conditions = (0..=2)
+            .take_while(|_| rnd1.gen::<f64>() > 0.6)
+            .map(|_| random_field_condition(rnd_gen))
+            .collect_vec();
+
+        let should_conditions_opt = match should_conditions.is_empty() {
+            false => Some(should_conditions),
+            true => None,
+        };
+
+        let must_conditions = (0..=2)
+            .take_while(|_| rnd1.gen::<f64>() > 0.6)
+            .map(|_| random_field_condition(rnd_gen))
+            .collect_vec();
+
+        let must_conditions_opt = match must_conditions.is_empty() {
+            false => Some(must_conditions),
+            true => None,
+        };
+
+        Filter {
+            should: should_conditions_opt,
+            must: must_conditions_opt,
+            must_not: None,
+        }
+    }
+
     #[test]
     fn test_struct_payload_index() {
         // Compare search with plain and struct indexes
+        let mut rnd = rand::thread_rng();
 
         let dir1 = TempDir::new("segment1_dir").unwrap();
         let dir2 = TempDir::new("segment2_dir").unwrap();
@@ -79,7 +138,36 @@ mod tests {
         config.payload_index = Some(PayloadIndexType::Struct);
         let mut struct_segment = build_segment(dir2.path(), &config).unwrap();
 
-        // ToDo: Init both segments with same data
-        // ToDo: Compare indexed and un-indexed search results
+        let str_key = "kvd".to_string();
+        let int_key = "int".to_string();
+
+        for idx in 0..1000 {
+            let vector = random_vector(&mut rnd, dim);
+            let mut payload: TheMap<PayloadKeyType, PayloadType> = Default::default();
+            payload.insert(str_key.clone(), random_keyword_payload(&mut rnd));
+            payload.insert(int_key.clone(), random_int_payload(&mut rnd));
+
+            plain_segment.upsert_point(idx, idx, &vector).unwrap();
+            struct_segment.upsert_point(idx, idx, &vector).unwrap();
+
+            plain_segment.set_full_payload(idx, idx, payload.clone()).unwrap();
+            struct_segment.set_full_payload(idx, idx, payload.clone()).unwrap();
+        }
+
+        for _i in 0..20 {
+            let query_vector = random_vector(&mut rnd, dim);
+            let query_filter = random_filter(&mut rnd);
+
+            let plain_result = plain_segment.search(&query_vector, Some(&query_filter), 5, None).unwrap();
+            let struct_result = struct_segment.search(&query_vector, Some(&query_filter), 5, None).unwrap();
+
+            plain_result
+                .iter()
+                .zip(struct_result.iter())
+                .for_each(|(r1, r2)| {
+                    assert_eq!(r1.id, r2.id);
+                    assert!((r1.score - r2.score) < 0.0001)
+                });
+        }
     }
 }
