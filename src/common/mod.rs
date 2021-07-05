45f3743fe5347621083674b52b83c31116fec010
Andrey Vasnetsov <andrey@vasnetsov.com>
2021-07-06 00:55:34 +0200

    fix fmt

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,3 +1,3 @@
-#[allow(dead_code)]  // May contain functions used in different binaries. Not actually dead
+#[allow(dead_code)] // May contain functions used in different binaries. Not actually dead
 pub mod helpers;
 pub mod models;
