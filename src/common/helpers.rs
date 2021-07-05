7f7d75c33f1c668e521a12f0e2d6d27f28390c70
Konstantin G <kgrech@mail.ru>
2021-07-05 22:32:23 +0100

    Own search runtime out of the async scope

--- src/common/helpers.rs
+++ src/common/helpers.rs
@@ -4,6 +4,21 @@ use actix_web::{HttpResponse, Responder};
 use serde::Serialize;
 use std::fmt::Debug;
 use storage::content_manager::errors::StorageError;
+use tokio::runtime;
+use tokio::runtime::Runtime;
+
+pub fn create_search_runtime(max_search_threads: usize) -> std::io::Result<Runtime> {
+    let mut search_threads = max_search_threads;
+
+    if search_threads == 0 {
+        let num_cpu = num_cpus::get();
+        search_threads = std::cmp::max(1, num_cpu - 1);
+    }
+
+    runtime::Builder::new_multi_thread()
+        .worker_threads(search_threads)
+        .build()
+}
 
 pub fn process_response<D>(response: Result<D, StorageError>, timing: Instant) -> impl Responder
 where
