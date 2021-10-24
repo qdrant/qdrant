74b71a152d421f48b68f43b051d40123749c2153
Andrey Vasnetsov <andrey@vasnetsov.com>
2021-10-24 15:38:06 +0200

    fail recovery test

--- lib/segment/tests/fail_recovery_test.rs
+++ lib/segment/tests/fail_recovery_test.rs
@@ -3,7 +3,7 @@ mod fixtures;
 #[cfg(test)]
 mod tests {
     use crate::fixtures::segment::empty_segment;
-    use segment::entry::entry_point::SegmentEntry;
+    use segment::entry::entry_point::{OperationError, SegmentEntry, SegmentFailedState};
     use segment::types::PayloadType;
     use tempdir::TempDir;
 
@@ -16,14 +16,56 @@ mod tests {
         let mut segment = empty_segment(dir.path());
 
         segment.upsert_point(1, 1, &vec1).unwrap();
+        segment.upsert_point(1, 2, &vec1).unwrap();
 
-        segment
+        segment.error_status = Some(SegmentFailedState {
+            version: 2,
+            point_id: Some(1),
+            error: OperationError::ServiceError{ description: "test error".to_string() }
+        });
+
+        // op_num is greater than errored. Skip because not recovered yet
+        let fail_res = segment
+            .set_payload(
+                3,
+                1,
+                &"color".to_string(),
+                PayloadType::Keyword(vec!["red".to_string()]),
+            );
+        assert!(fail_res.is_err());
+
+        // Also skip even with another point operation
+        let fail_res = segment
+            .set_payload(
+                3,
+                2,
+                &"color".to_string(),
+                PayloadType::Keyword(vec!["red".to_string()]),
+            );
+        assert!(fail_res.is_err());
+
+        // Perform operation, but keep error status: operation is not fully recovered yet
+        let ok_res = segment
+            .set_payload(
+                2,
+                2,
+                &"color".to_string(),
+                PayloadType::Keyword(vec!["red".to_string()]),
+            );
+        assert!(ok_res.is_ok());
+        assert!(segment.error_status.is_some());
+
+
+        // Perform operation anf recover the error - operation is fixed now
+        let recover_res = segment
             .set_payload(
                 2,
                 1,
                 &"color".to_string(),
                 PayloadType::Keyword(vec!["red".to_string()]),
-            )
-            .unwrap();
+            );
+
+        assert!(recover_res.is_ok());
+        assert!(segment.error_status.is_none());
     }
 }
