64b3f9e41730e08d54008402cacf688eb9884285
Andrey Vasnetsov <andrey@vasnetsov.com>
2021-11-20 16:04:53 +0100

    extend comments for strorage crate

--- lib/storage/src/lib.rs
+++ lib/storage/src/lib.rs
@@ -1,10 +1,7 @@
+//! Storage - is a crate which contains all service functions, abstracted from the external interface
+//!
+//! It provides all functions, which could be used from REST (or any other interface), but do not
+//! implement any concrete interface.
+
 pub mod content_manager;
 pub mod types;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
