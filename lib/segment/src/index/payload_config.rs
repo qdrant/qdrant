01330b6d517c6f2e0ab171be4b33269624164c53
Andrey Vasnetsov <andrey@vasnetsov.com>
2021-12-05 19:34:12 +0100

    cargo fmt

--- lib/segment/src/index/payload_config.rs
+++ lib/segment/src/index/payload_config.rs
@@ -6,7 +6,6 @@ use std::path::{Path, PathBuf};
 
 pub const PAYLOAD_INDEX_CONFIG_FILE: &str = "config.json";
 
-
 /// Keeps information of which field should be index
 #[derive(Debug, Default, Deserialize, Serialize, Clone)]
 pub struct PayloadConfig {
