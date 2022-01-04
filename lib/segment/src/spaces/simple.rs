a0138600319f981f023a9067c8d321600be3fe28
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-01-05 01:17:49 +0300

    add sse support

--- lib/segment/src/spaces/simple.rs
+++ lib/segment/src/spaces/simple.rs
@@ -25,6 +25,9 @@ impl Metric for EuclidMetric {
             if is_x86_feature_detected!("avx2") {
                 return unsafe { euclid_similarity_avx2(v1, v2) };
             }
+            if is_x86_feature_detected!("sse") {
+                return unsafe { euclid_similarity_sse(v1, v2) };
+            }
         }
         euclid_similarity(v1, v2)
     }
@@ -44,6 +47,9 @@ impl Metric for DotProductMetric {
         {
             if is_x86_feature_detected!("avx2") {
                 return unsafe { dot_similarity_avx2(v1, v2) };
+            }
+            if is_x86_feature_detected!("sse") {
+                return unsafe { dot_similarity_sse(v1, v2) };
             }
         }
         dot_similarity(v1, v2)
@@ -65,6 +71,9 @@ impl Metric for CosineMetric {
             if is_x86_feature_detected!("avx2") {
                 return unsafe { dot_similarity_avx2(v1, v2) };
             }
+            if is_x86_feature_detected!("sse") {
+                return unsafe { dot_similarity_sse(v1, v2) };
+            }
         }
         dot_similarity(v1, v2)
     }
@@ -74,6 +83,9 @@ impl Metric for CosineMetric {
         {
             if is_x86_feature_detected!("avx2") {
                 return Some(unsafe { cosine_preprocess_avx2(vector) });
+            }
+            if is_x86_feature_detected!("sse") {
+                return Some(unsafe { cosine_preprocess_sse(vector) });
             }
         }
         Some(cosine_preprocess(vector))
@@ -102,7 +114,7 @@ fn dot_similarity(v1: &[VectorElementType], v2: &[VectorElementType]) -> ScoreTy
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 #[target_feature(enable = "avx2")]
-unsafe fn hsum256_ps_avx(x: __m256) -> f32 {
+unsafe fn hsum256_ps_avx2(x: __m256) -> f32 {
     /* ( x3+x7, x2+x6, x1+x5, x0+x4 ) */
     let x128: __m128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));
     /* ( -, -, x1+x3+x5+x7, x0+x2+x4+x6 ) */
@@ -123,7 +135,7 @@ unsafe fn euclid_similarity_avx2(v1: &[VectorElementType], v2: &[VectorElementTy
         let sub256: __m256 = _mm256_sub_ps(_mm256_loadu_ps(&v1[i]), _mm256_loadu_ps(&v2[i]));
         sum256 = _mm256_fmadd_ps(sub256, sub256, sum256);
     }
-    let mut res = hsum256_ps_avx(sum256);
+    let mut res = hsum256_ps_avx2(sum256);
     for i in m..n {
         res += (v1[i] - v2[i]).powi(2);
     }
@@ -143,7 +155,7 @@ unsafe fn cosine_preprocess_avx2(vector: &[VectorElementType]) -> Vec<VectorElem
             sum256,
         );
     }
-    let mut length = hsum256_ps_avx(sum256);
+    let mut length = hsum256_ps_avx2(sum256);
     for v in vector.iter().take(n).skip(m) {
         length += v.powi(2);
     }
@@ -160,7 +172,71 @@ unsafe fn dot_similarity_avx2(v1: &[VectorElementType], v2: &[VectorElementType]
     for i in (0..m).step_by(8) {
         sum256 = _mm256_fmadd_ps(_mm256_loadu_ps(&v1[i]), _mm256_loadu_ps(&v2[i]), sum256);
     }
-    let mut res = hsum256_ps_avx(sum256);
+    let mut res = hsum256_ps_avx2(sum256);
+    for i in m..n {
+        res += v1[i] * v2[i];
+    }
+    res
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "sse")]
+unsafe fn hsum128_ps_sse(x: __m128) -> f32 {
+    let x64: __m128 = _mm_add_ps(x, _mm_movehl_ps(x, x));
+    let x32: __m128 = _mm_add_ss(x64, _mm_shuffle_ps(x64, x64, 0x55));
+    _mm_cvtss_f32(x32)
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "sse")]
+unsafe fn euclid_similarity_sse(v1: &[VectorElementType], v2: &[VectorElementType]) -> ScoreType {
+    let n = v1.len();
+    let m = n - (n % 4);
+    let mut sum128: __m128 = _mm_setzero_ps();
+    for i in (0..m).step_by(4) {
+        let sub128: __m128 = _mm_sub_ps(_mm_loadu_ps(&v1[i]), _mm_loadu_ps(&v2[i]));
+        let a = _mm_mul_ps(sub128, sub128);
+        sum128 = _mm_add_ps(a, sum128);
+    }
+    let mut res = hsum128_ps_sse(sum128);
+    for i in m..n {
+        res += (v1[i] - v2[i]).powi(2);
+    }
+    -res.sqrt()
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "sse")]
+unsafe fn cosine_preprocess_sse(vector: &[VectorElementType]) -> Vec<VectorElementType> {
+    let n = vector.len();
+    let m = n - (n % 4);
+    let mut sum128: __m128 = _mm_setzero_ps();
+    for i in (0..m).step_by(4) {
+        let a = _mm_loadu_ps(&vector[i]);
+        let b = _mm_mul_ps(a, a);
+        sum128 = _mm_add_ps(b, sum128);
+    }
+    let mut length = hsum128_ps_sse(sum128);
+    for v in vector.iter().take(n).skip(m) {
+        length += v.powi(2);
+    }
+    length = length.sqrt();
+    vector.iter().map(|x| x / length).collect()
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "sse")]
+unsafe fn dot_similarity_sse(v1: &[VectorElementType], v2: &[VectorElementType]) -> ScoreType {
+    let n = v1.len();
+    let m = n - (n % 4);
+    let mut sum128: __m128 = _mm_setzero_ps();
+    for i in (0..m).step_by(4) {
+        let a = _mm_loadu_ps(&v1[i]);
+        let b = _mm_loadu_ps(&v2[i]);
+        let c = _mm_mul_ps(a, b);
+        sum128 = _mm_add_ps(c, sum128);
+    }
+    let mut res = hsum128_ps_sse(sum128);
     for i in m..n {
         res += v1[i] * v2[i];
     }
@@ -180,28 +256,53 @@ mod tests {
 
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     #[test]
-    fn test_avx2() {
-        if is_x86_feature_detected!("avx2") {
+    fn test_simd() {
+        if is_x86_feature_detected!("sse") {
             let v1: Vec<f32> = vec![
                 10., 11., 12., 13., 14., 15., 16., 17., 18., 19., 20., 21., 22., 23., 24., 25.,
-                26., 27., 28., 29., 30.,
+                26., 27., 28., 29., 30., 31.,
             ];
             let v2: Vec<f32> = vec![
                 40., 41., 42., 43., 44., 45., 46., 47., 48., 49., 50., 51., 52., 53., 54., 55.,
-                56., 57., 58., 59., 60.,
+                56., 57., 58., 59., 60., 61.
             ];
 
-            let euclid_avx2 = unsafe { euclid_similarity_avx2(&v1, &v2) };
+            let euclid_simd = unsafe { euclid_similarity_sse(&v1, &v2) };
             let euclid = euclid_similarity(&v1, &v2);
-            assert_eq!(euclid_avx2, euclid);
+            assert_eq!(euclid_simd, euclid);
 
-            let dot_avx2 = unsafe { dot_similarity_avx2(&v1, &v2) };
+            let dot_simd = unsafe { dot_similarity_sse(&v1, &v2) };
             let dot = dot_similarity(&v1, &v2);
-            assert_eq!(dot_avx2, dot);
+            assert_eq!(dot_simd, dot);
 
-            let cosine_avx2 = unsafe { cosine_preprocess_avx2(&v1) };
+            let cosine_simd = unsafe { cosine_preprocess_sse(&v1) };
             let cosine = cosine_preprocess(&v1);
-            assert_eq!(cosine_avx2, cosine);
+            assert_eq!(cosine_simd, cosine);
+        } else {
+            println!("SSE test skiped");
+        }
+
+        if is_x86_feature_detected!("avx2") {
+            let v1: Vec<f32> = vec![
+                10., 11., 12., 13., 14., 15., 16., 17., 18., 19., 20., 21., 22., 23., 24., 25.,
+                26., 27., 28., 29., 30., 31.,
+            ];
+            let v2: Vec<f32> = vec![
+                40., 41., 42., 43., 44., 45., 46., 47., 48., 49., 50., 51., 52., 53., 54., 55.,
+                56., 57., 58., 59., 60., 61.,
+            ];
+
+            let euclid_simd = unsafe { euclid_similarity_avx2(&v1, &v2) };
+            let euclid = euclid_similarity(&v1, &v2);
+            assert_eq!(euclid_simd, euclid);
+
+            let dot_simd = unsafe { dot_similarity_avx2(&v1, &v2) };
+            let dot = dot_similarity(&v1, &v2);
+            assert_eq!(dot_simd, dot);
+
+            let cosine_simd = unsafe { cosine_preprocess_avx2(&v1) };
+            let cosine = cosine_preprocess(&v1);
+            assert_eq!(cosine_simd, cosine);
         } else {
             println!("AVX2 test skiped");
         }
