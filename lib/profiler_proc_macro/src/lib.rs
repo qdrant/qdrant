47bcd17aeb3254a0115a69bb01d41cd4268fddf9
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-02-08 10:57:05 +0300

    change function name format

--- lib/profiler_proc_macro/src/lib.rs
+++ lib/profiler_proc_macro/src/lib.rs
@@ -8,8 +8,7 @@ use ::syn::{ ItemFn, parse_macro_input };
 pub fn trace(_: TokenStream, input: TokenStream) -> TokenStream {
     let mut input_string = input.to_string();
     let input_fn = parse_macro_input!(input as ItemFn);
-    
-    let string_to_insert = format!("let _span = tracy_client::span!(\"{:#?}\");", input_fn);
+    let string_to_insert = format!("let _span = tracy_client::span!(\"{}\");", input_fn.sig.ident);
 
     if let Some(idx) = input_string.find("{") {
         input_string.insert_str(idx+1, &string_to_insert);
