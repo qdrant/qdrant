3944cc0b732f936f37e2e3d4ddfde7dc2449b4c7
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-02-11 02:12:31 +0100

    wip: re-implement http tests using openapi validation library

---
+++ openapi/tests/tests/test_delete_points.py
@@ -0,0 +1,40 @@
+import pytest
+
+from ..openapi_integration.helpers import request_with_validation
+from ..openapi_integration.collection_setup import basic_collection_setup, drop_collection
+
+collection_name = 'test_collection_delete'
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    basic_collection_setup(collection_name=collection_name)
+    yield
+    drop_collection(collection_name=collection_name)
+
+
+def test_points_retrieve():
+    # delete point by filter (has_id)
+    response = request_with_validation(
+        api='/collections/{name}/points/delete',
+        method="POST",
+        path_params={'name': collection_name},
+        query_params={'wait': 'true'},
+        body={
+            "filter": {
+                "must": [
+                    {"has_id": [5]}
+                ]
+            }
+        }
+    )
+    assert response.ok
+
+    # quantity check if the above point id was deleted
+    response = request_with_validation(
+        api='/collections/{name}',
+        method="GET",
+        path_params={'name': collection_name},
+    )
+    assert response.ok
+    assert response.json()['result']['vectors_count'] == 5
