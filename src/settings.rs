3d76cc87b0cf00895047cc8cf61b16406869fc34
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2022-02-22 13:31:36 +0100

    draft migration

--- src/settings.rs
+++ src/settings.rs
@@ -23,27 +23,27 @@ pub struct Settings {
 impl Settings {
     #[allow(dead_code)]
     pub fn new() -> Result<Self, ConfigError> {
-        let mut s = Config::new();
+        let mut builder = Config::builder();
 
         // Start off by merging in the "default" configuration file
-        s.merge(File::with_name("config/config"))?;
+        builder.add_source(File::with_name("config/config"));
 
         // Add in the current environment file
         // Default to 'development' env
         // Note that this file is _optional_
         let env = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
-        s.merge(File::with_name(&format!("config/{}", env)).required(false))?;
+        builder.add_source(File::with_name(&format!("config/{}", env)).required(false));
 
         // Add in a local configuration file
         // This file shouldn't be checked in to git
-        s.merge(File::with_name("config/local").required(false))?;
+        builder.add_source(File::with_name("config/local").required(false));
 
         // Add in settings from the environment (with a prefix of APP)
         // Eg.. `QDRANT_DEBUG=1 ./target/app` would set the `debug` key
-        s.merge(Environment::with_prefix("QDRANT"))?;
+        builder.add_source(Environment::with_prefix("QDRANT"));
 
         // You can deserialize (and thus freeze) the entire configuration as
-        s.try_into()
+        builder.build()?.try_into()
     }
 }
 
