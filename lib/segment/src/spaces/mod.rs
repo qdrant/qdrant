b6bf803c3ae6ef6dd0166d96d21fbe7b212723a0
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-03-16 12:26:32 +0400

    fix build

--- lib/segment/src/spaces/mod.rs
+++ lib/segment/src/spaces/mod.rs
@@ -8,8 +8,5 @@ pub mod simple_sse;
 #[cfg(target_arch = "x86_64")]
 pub mod simple_avx;
 
-#[cfg(target_arch = "x86_64")]
-pub mod simple_avx512;
-
 #[cfg(target_arch = "aarch64")]
 pub mod simple_neon;
