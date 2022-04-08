b260cad53db412630e5fcb9fd504f559f3cdb246
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2022-04-08 17:04:48 +0200

    cleaner script

--- .github/scripts/protoc.sh
+++ .github/scripts/protoc.sh
@@ -2,5 +2,5 @@
 
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 curl -LO $PB_REL/download/v3.12.4/protoc-3.12.4-linux-x86_64.zip
-
-sudo unzip protoc-3.12.4-linux-x86_64.zip -d /usr/bin/
+unzip protoc-3.12.4-linux-x86_64.zip -d /tmp/protoc
+sudo mv /tmp/protoc/bin/protoc /usr/bin/
