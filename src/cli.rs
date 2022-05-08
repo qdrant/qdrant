710fe19fae956d36676a510ed569de2ea7ad2b0d
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-05-09 01:11:26 +0200

    add cli with mem profiler

--- src/cli.rs
+++ src/cli.rs
@@ -1,20 +1,32 @@
 mod common;
 
-use std::path::Path;
 use clap::Parser;
-use tokio::runtime::Runtime;
+use collection::collection_manager::optimizers::indexing_optimizer::IndexingOptimizer;
+use collection::collection_manager::optimizers::segment_optimizer::{
+    OptimizerThresholds, SegmentOptimizer,
+};
+use collection::optimizers_builder::OptimizersConfig;
+use collection::shard::Shard;
 use collection::Collection;
+use std::path::Path;
+use std::sync::atomic::AtomicBool;
+use std::thread;
+use std::time::Duration;
+use log::{info};
+use tokio::runtime::Runtime;
+use collection::collection_manager::optimizers::vacuum_optimizer::VacuumOptimizer;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
 
 #[derive(Parser, Debug)]
 #[clap(version, about)]
 struct Args {
-
     /// Path to the collection
     #[clap(short, long)]
     pub collection: String,
 }
-
 
 fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "DEBUG");
@@ -32,13 +44,16 @@ fn main() -> std::io::Result<()> {
         .expect("A filename of one of the collection files is not a valid UTF-8")
         .to_string();
 
-    eprintln!("collection_name = {:#?}", collection_name);
+    {
+        #[cfg(feature = "dhat-heap")]
+            let _profiler = dhat::Profiler::new_heap();
+        {
+            eprintln!("collection_name = {:#?}", collection_name);
 
-    let mut collection = rt
-        .block_on(Collection::load(collection_name.clone(), &collection_path));
+            let mut collection = rt.block_on(Collection::load(collection_name.clone(), &collection_path));
+            rt.block_on(collection.before_drop());
+        }
+    }
 
-    eprintln!("collection.info() = {:#?}", rt.block_on(collection.info(None)));
-
-    rt.block_on(collection.before_drop());
     Ok(())
 }
