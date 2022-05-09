59ddb81330f2f59a2e7e80fea361b88f1481f808
Gabriel Velo <gabriel.velo@gmail.com>
2022-05-09 14:43:57 -0300

    add key encoding for numeric index. (#570)
    add key encoding for numeric index.

--- lib/segment/src/index/mod.rs
+++ lib/segment/src/index/mod.rs
@@ -1,6 +1,7 @@
 mod field_index;
 pub mod hnsw_index;
 mod index_base;
+mod key_encoding;
 mod payload_config;
 pub mod plain_payload_index;
 pub mod query_estimator;
@@ -9,5 +10,4 @@ mod sample_estimation;
 mod struct_filter_context;
 pub mod struct_payload_index;
 mod visited_pool;
-
 pub use index_base::*;
