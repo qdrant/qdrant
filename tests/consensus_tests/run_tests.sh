55ccf1fff5aa334439f3f172dda9d39edb6c9cda
Egor Ivkov <e.o.ivkov@gmail.com>
2022-05-18 19:28:17 +0300

    Run consensus in docker-compose (#609)
    Co-authored-by: Andrey Vasnetsov <andrey@vasnetsov.com>

---
+++ tests/consensus_tests/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+# Ensure current path script dir
+cd "$(dirname "$0")/"
+
+function clear_after_tests()
+{
+  docker-compose down
+}
+
+# Prevent double building in docker-compose
+docker build ../../ --tag=qdrant_consensus
+docker-compose up -d --force-recreate
+trap clear_after_tests EXIT
+
+# Wait for service to start
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:6433)" != "200" ]]; do
+  sleep 1;
+done
+
