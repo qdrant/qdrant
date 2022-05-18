1f98bc5a7209e6cd52a770f4f440ccec55bb05d9
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-05-18 08:38:45 +0200

    add tests and error handling

--- lib/segment/src/index/field_index/on_disk_map_index.rs
+++ lib/segment/src/index/field_index/on_disk_map_index.rs
@@ -5,7 +5,7 @@ use std::iter;
 use serde_json::Value;
 
 use crate::common::rocksdb_operations::db_write_options;
-use crate::entry::entry_point::OperationResult;
+use crate::entry::entry_point::{OperationError, OperationResult};
 use crate::index::field_index::PayloadFieldIndex;
 use crate::index::field_index::{
     CardinalityEstimation, PayloadBlockCondition, PrimaryCondition, ValueIndexer,
@@ -30,16 +30,53 @@ pub struct OnDiskMapIndex<N: Hash + Eq + Clone + Display> {
 }
 
 impl<N: Hash + Eq + Clone + Display + FromStr> OnDiskMapIndex<N> {
+    #[allow(dead_code)]
     pub fn new(store: Arc<AtomicRefCell<DB>>, store_cf_name: &str) -> OnDiskMapIndex<N> {
-        let mut index = OnDiskMapIndex {
+        OnDiskMapIndex {
             map: HashMap::new(),
             point_to_values: Vec::new(),
             total_points: 0,
             store_cf_name: String::from(store_cf_name),
             store,
-        };
-        index.load_from_db();
-        index
+        }
+    }
+
+    #[allow(dead_code)]
+    fn load(&mut self) -> OperationResult<()> {
+        let store_ref = self.store.borrow();
+        let cf_handle = store_ref.cf_handle(&self.store_cf_name).ok_or_else(|| {
+            OperationError::service_error(&format!(
+                "Index load error: column family {} not found",
+                self.store_cf_name
+            ))
+        })?;
+        for (record, _) in store_ref.iterator_cf(cf_handle, IteratorMode::Start) {
+            let record = std::str::from_utf8(&record).map_err(|_| {
+                OperationError::service_error("Index load error: UTF8 error while DB parsing")
+            })?;
+            let (value, idx) = Self::decode_db_record(record)?;
+            if self.point_to_values.len() <= idx as usize {
+                self.point_to_values.resize(idx as usize + 1, Vec::new())
+            }
+            if self.point_to_values[idx as usize].is_empty() {
+                self.total_points += 1;
+            }
+            self.point_to_values[idx as usize].push(value.clone());
+            self.map.entry(value).or_default().push(idx);
+        }
+        Ok(())
+    }
+
+    #[allow(dead_code)]
+    pub fn flush(&self) -> OperationResult<()> {
+        let store_ref = self.store.borrow();
+        let cf_handle = store_ref.cf_handle(&self.store_cf_name).ok_or_else(|| {
+            OperationError::service_error(&format!(
+                "Index flush error: column family {} not found",
+                self.store_cf_name
+            ))
+        })?;
+        Ok(store_ref.flush_cf(cf_handle)?)
     }
 
     pub fn match_cardinality(&self, value: &N) -> CardinalityEstimation {
@@ -56,13 +93,19 @@ impl<N: Hash + Eq + Clone + Display + FromStr> OnDiskMapIndex<N> {
         }
     }
 
+    #[allow(dead_code)]
     pub fn get_values(&self, idx: PointOffsetType) -> Option<&Vec<N>> {
         self.point_to_values.get(idx as usize)
     }
 
-    fn add_many_to_map(&mut self, idx: PointOffsetType, values: Vec<N>) {
+    fn add_many_to_map(&mut self, idx: PointOffsetType, values: Vec<N>) -> OperationResult<()> {
         let store_ref = self.store.borrow();
-        let cf_handle = store_ref.cf_handle(&self.store_cf_name).unwrap();
+        let cf_handle = store_ref.cf_handle(&self.store_cf_name).ok_or_else(|| {
+            OperationError::service_error(&format!(
+                "Index add error: column family {} not found",
+                self.store_cf_name
+            ))
+        })?;
 
         if self.point_to_values.len() <= idx as usize {
             self.point_to_values.resize(idx as usize + 1, Vec::new())
@@ -77,11 +120,14 @@ impl<N: Hash + Eq + Clone + Display + FromStr> OnDiskMapIndex<N> {
             let db_record = Self::encode_db_record(value, idx);
             store_ref
                 .put_cf_opt(cf_handle, &db_record, &[], &db_write_options())
-                .unwrap();
+                .map_err(|e| {
+                    OperationError::service_error(&format!("Index db update error: {}", e))
+                })?;
         }
         if !empty {
             self.total_points += 1;
         }
+        Ok(())
     }
 
     fn get_iterator(&self, value: &N) -> Box<dyn Iterator<Item = PointOffsetType> + '_> {
@@ -91,43 +137,25 @@ impl<N: Hash + Eq + Clone + Display + FromStr> OnDiskMapIndex<N> {
             .unwrap_or_else(|| Box::new(iter::empty::<PointOffsetType>()))
     }
 
-    fn load_from_db(&mut self) {
-        let store_ref = self.store.borrow();
-        let cf_handle = store_ref.cf_handle(&self.store_cf_name).unwrap();
-        for (record, _) in store_ref.iterator_cf(cf_handle, IteratorMode::Start) {
-            let record = std::str::from_utf8(&record).unwrap();
-            let (value, idx) = Self::decode_db_record(record);
-            if self.point_to_values.len() <= idx as usize {
-                self.point_to_values.resize(idx as usize + 1, Vec::new())
-            }
-            if self.point_to_values[idx as usize].is_empty() {
-                self.total_points += 1;
-            }
-            self.point_to_values[idx as usize].push(value.clone());
-            self.map.entry(value).or_default().push(idx);
-        }
-    }
-
-    fn flush(&self) -> OperationResult<()> {
-        let store_ref = self.store.borrow();
-        let cf_handle = store_ref.cf_handle(&self.store_cf_name).unwrap();
-        Ok(store_ref.flush_cf(cf_handle)?)
-    }
-
     fn encode_db_record(value: &N, idx: PointOffsetType) -> String {
         format!("{}/{}", value, idx)
     }
 
-    fn decode_db_record(s: &str) -> (N, PointOffsetType) {
-        let separator_pos = s.rfind('/').unwrap();
+    fn decode_db_record(s: &str) -> OperationResult<(N, PointOffsetType)> {
+        const DECODE_ERR: &str = "Index db parsing error: wrong data format";
+        let separator_pos = s
+            .rfind('/')
+            .ok_or_else(|| OperationError::service_error(DECODE_ERR))?;
         if separator_pos == s.len() - 1 {
-            panic!("");
+            return Err(OperationError::service_error(DECODE_ERR));
         }
         let value_str = &s[..separator_pos];
-        let value = N::from_str(value_str).map_err(|_| {}).unwrap();
+        let value =
+            N::from_str(value_str).map_err(|_| OperationError::service_error(DECODE_ERR))?;
         let idx_str = &s[separator_pos + 1..];
-        let idx = PointOffsetType::from_str(idx_str).unwrap();
-        (value, idx)
+        let idx = PointOffsetType::from_str(idx_str)
+            .map_err(|_| OperationError::service_error(DECODE_ERR))?;
+        Ok((value, idx))
     }
 }
 
@@ -231,7 +259,7 @@ impl PayloadFieldIndex for OnDiskMapIndex<IntPayloadType> {
 
 impl ValueIndexer<String> for OnDiskMapIndex<String> {
     fn add_many(&mut self, id: PointOffsetType, values: Vec<String>) {
-        self.add_many_to_map(id, values);
+        self.add_many_to_map(id, values).unwrap();
     }
 
     fn get_value(&self, value: &Value) -> Option<String> {
@@ -244,7 +272,7 @@ impl ValueIndexer<String> for OnDiskMapIndex<String> {
 
 impl ValueIndexer<IntPayloadType> for OnDiskMapIndex<IntPayloadType> {
     fn add_many(&mut self, id: PointOffsetType, values: Vec<IntPayloadType>) {
-        self.add_many_to_map(id, values);
+        self.add_many_to_map(id, values).unwrap();
     }
 
     fn get_value(&self, value: &Value) -> Option<IntPayloadType> {
@@ -252,5 +280,102 @@ impl ValueIndexer<IntPayloadType> for OnDiskMapIndex<IntPayloadType> {
             return num.as_i64();
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::common::rocksdb_operations::db_options;
+    use std::collections::HashSet;
+    use std::fmt::Debug;
+    use std::iter::FromIterator;
+    use std::path::Path;
+    use tempdir::TempDir;
+
+    use super::*;
+
+    const CF_NAME: &str = "test_cf";
+
+    fn open_db(path: &Path) -> Arc<AtomicRefCell<DB>> {
+        let db = DB::open_cf(&db_options(), path, &[CF_NAME]).unwrap();
+        Arc::new(AtomicRefCell::new(db))
+    }
+
+    fn save_map_index<N: Hash + Eq + Clone + Display + FromStr + Debug>(
+        data: &Vec<Vec<N>>,
+        path: &Path,
+    ) {
+        let mut index = OnDiskMapIndex::<N>::new(open_db(path), CF_NAME);
+        for idx in 0..data.len() {
+            index
+                .add_many_to_map(idx as PointOffsetType, data[idx].clone())
+                .unwrap();
+        }
+        index.flush().unwrap();
+    }
+
+    fn load_map_index<N: Hash + Eq + Clone + Display + FromStr + Debug>(
+        data: &Vec<Vec<N>>,
+        path: &Path,
+    ) {
+        let mut index = OnDiskMapIndex::<N>::new(open_db(path), CF_NAME);
+        index.load().unwrap();
+        for idx in 0..data.len() {
+            let index_values: HashSet<N> = HashSet::from_iter(
+                index
+                    .get_values(idx as PointOffsetType)
+                    .unwrap()
+                    .iter()
+                    .cloned(),
+            );
+            let check_values: HashSet<N> = HashSet::from_iter(data[idx].iter().cloned());
+            assert_eq!(index_values, check_values);
+        }
+    }
+
+    #[test]
+    fn test_int_disk_map_index() {
+        let data = vec![
+            vec![1, 2, 3, 4, 5, 6],
+            vec![1, 2, 3, 4, 5, 6],
+            vec![13, 14, 15, 16, 17, 18],
+            vec![19, 20, 21, 22, 23, 24],
+            vec![25],
+        ];
+
+        let tmp_dir = TempDir::new("store_dir").unwrap();
+        save_map_index(&data, tmp_dir.path());
+        load_map_index(&data, tmp_dir.path());
+    }
+
+    #[test]
+    fn test_string_disk_map_index() {
+        let data = vec![
+            vec![
+                String::from("AABB"),
+                String::from("UUFF"),
+                String::from("IIBB"),
+            ],
+            vec![
+                String::from("PPMM"),
+                String::from("QQXX"),
+                String::from("YYBB"),
+            ],
+            vec![
+                String::from("FFMM"),
+                String::from("IICC"),
+                String::from("IIBB"),
+            ],
+            vec![
+                String::from("AABB"),
+                String::from("UUFF"),
+                String::from("IIBB"),
+            ],
+            vec![String::from("PPGG")],
+        ];
+
+        let tmp_dir = TempDir::new("store_dir").unwrap();
+        save_map_index(&data, tmp_dir.path());
+        load_map_index(&data, tmp_dir.path());
     }
 }
