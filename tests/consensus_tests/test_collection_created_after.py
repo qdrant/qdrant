5bebd075202241ff906e2d7b40b140cd36b5cf5b
Egor Ivkov <e.o.ivkov@gmail.com>
2022-05-25 19:19:16 +0300

    Move out consensus test setup into util (#629)

--- tests/consensus_tests/test_collection_created_after.py
+++ tests/consensus_tests/test_collection_created_after.py
@@ -1,59 +1,28 @@
-import os
 import pathlib
-import shutil
 import time
-
 import requests
-
 from .utils import *
-from . import conftest
-from subprocess import Popen
 
 N_PEERS = 5
 
+
 def test_collection_after_peers_added(tmp_path: pathlib.Path):
-    # Ensure current path is project root
-    directory_path = os.getcwd()
-    folder_name = os.path.basename(directory_path)
-    assert folder_name == "qdrant"
-
-    qdrant_exec = directory_path + "/target/debug/qdrant"
-
-    # Make peer folders
-    peer_dirs = []
-    for i in range(N_PEERS):
-        peer_dir = tmp_path / f"peer{i}"
-        peer_dir.mkdir()
-        peer_dirs.append(peer_dir)
-        shutil.copytree("config", peer_dir / "config")
+    assert_project_root()
+    peer_dirs = make_peer_folders(tmp_path, N_PEERS)
 
     # Gathers REST API uris
     peer_api_uris = []
 
-    # Start bootstrap
-    p2p_port = get_port()
-    http_port = get_port()
-    env = get_env(p2p_port, http_port)
-    bootstrap_uri = get_uri(p2p_port)
-    peer_api_uris.append(get_uri(http_port))
-    log_file = open("peer_0_0.log", "w")
-    conftest.processes.append(
-        Popen([qdrant_exec, "--uri", bootstrap_uri], env=env, cwd=peer_dirs[0], stderr=log_file))
-    time.sleep(5)
+    (bootstrap_api_uri, bootstrap_uri) = start_first_peer(
+        peer_dirs[0], "peer_0_0.log")
+    peer_api_uris.append(bootstrap_api_uri)
 
-    # Start other peers
     for i in range(1, len(peer_dirs)):
-        p2p_port = get_port()
-        http_port = get_port()
-        env = get_env(p2p_port, http_port)
-        peer_api_uris.append(get_uri(http_port))
-        log_file = open(f"peer_0_{i}.log", "w")
-        conftest.processes.append(
-            Popen([qdrant_exec, "--bootstrap", bootstrap_uri], env=env, cwd=peer_dirs[i], stderr=log_file))
-        time.sleep(3)
+        peer_api_uris.append(start_peer(
+            peer_dirs[i], f"peer_0_{i}.log", bootstrap_uri))
 
     # Wait
-    time.sleep(3)
+    time.sleep(5)
 
     # Check that there are no collections on all peers
     for uri in peer_api_uris:
--- tests/consensus_tests/utils.py
+++ tests/consensus_tests/utils.py
@@ -1,7 +1,13 @@
 import os
+import shutil
+from subprocess import Popen
+import time
+from typing import Tuple
 import requests
 import socket
 from contextlib import closing
+from . import conftest
+from pathlib import Path
 
 
 def get_port() -> int:
@@ -27,3 +33,50 @@ def assert_http_ok(response: requests.Response):
     if response.status_code != 200:
         raise Exception(
             f"Http request failed with status {response.status_code} and contents: {response.json()}")
+
+
+def assert_project_root():
+    directory_path = os.getcwd()
+    folder_name = os.path.basename(directory_path)
+    assert folder_name == "qdrant"
+
+
+def get_qdrant_exec() -> str:
+    directory_path = os.getcwd()
+    qdrant_exec = directory_path + "/target/debug/qdrant"
+    return qdrant_exec
+
+
+# Starts a peer and returns its api_uri
+def start_peer(peer_dir: Path, log_file: str, bootstrap_uri: str) -> str:
+    p2p_port = get_port()
+    http_port = get_port()
+    env = get_env(p2p_port, http_port)
+    log_file = open(log_file, "w")
+    conftest.processes.append(
+        Popen([get_qdrant_exec(), "--bootstrap", bootstrap_uri], env=env, cwd=peer_dir, stderr=log_file))
+    time.sleep(3)
+    return get_uri(http_port)
+
+
+# Starts a peer and returns its api_uri and p2p_uri
+def start_first_peer(peer_dir: Path, log_file: str) -> Tuple[str, str]:
+    p2p_port = get_port()
+    http_port = get_port()
+    env = get_env(p2p_port, http_port)
+    log_file = open(log_file, "w")
+    bootstrap_uri = get_uri(p2p_port)
+    conftest.processes.append(
+        Popen([get_qdrant_exec(), "--uri", bootstrap_uri], env=env, cwd=peer_dir, stderr=log_file))
+    time.sleep(5)
+    return (get_uri(http_port), bootstrap_uri)
+
+
+def make_peer_folders(base_path: Path, n_peers: int) -> list[Path]:
+    peer_dirs = []
+    for i in range(n_peers):
+        peer_dir = base_path / f"peer{i}"
+        peer_dir.mkdir()
+        peer_dirs.append(peer_dir)
+        shutil.copytree("config", peer_dir / "config")
+    return peer_dirs
