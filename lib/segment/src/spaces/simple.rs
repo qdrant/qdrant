5dff34f8ccef8354672163917876d82364889766
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-05-26 20:50:59 +0200

    arm unused const warning

--- lib/segment/src/spaces/simple.rs
+++ lib/segment/src/spaces/simple.rs
@@ -14,7 +14,11 @@ use super::simple_neon::*;
 #[cfg(target_arch = "x86_64")]
 const MIN_DIM_SIZE_AVX: usize = 32;
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64"))]
+#[cfg(any(
+    target_arch = "x86",
+    target_arch = "x86_64",
+    all(target_arch = "aarch64", target_feature = "neon")
+))]
 const MIN_DIM_SIZE_SIMD: usize = 16;
 
 #[derive(Clone)]
