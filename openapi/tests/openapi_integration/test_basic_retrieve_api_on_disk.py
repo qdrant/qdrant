6fab45440e0b890b0120662c05484d41dfcccca5
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-05-30 00:48:20 +0200

    add integration test with on-disk payload

---
+++ openapi/tests/openapi_integration/test_basic_retrieve_api_on_disk.py
@@ -0,0 +1,35 @@
+import pytest
+
+from .helpers.collection_setup import basic_collection_setup, drop_collection
+from .helpers.helpers import request_with_validation
+from .test_basic_retrieve_api import points_retrieve, exclude_payload, is_empty_condition, \
+    recommendation, query_nested
+
+collection_name = 'test_collection'
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    basic_collection_setup(collection_name=collection_name, on_disk_payload=True)
+    yield
+    drop_collection(collection_name=collection_name)
+
+
+def test_points_retrieve_on_disk():
+    points_retrieve()
+
+
+def test_exclude_payload_on_disk():
+    exclude_payload()
+
+
+def test_is_empty_condition_on_disk():
+    is_empty_condition()
+
+
+def test_recommendation_on_disk():
+    recommendation()
+
+
+def test_query_nested_on_disk():
+    query_nested()
