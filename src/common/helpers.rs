88d14732ea9d332e4d2d2fa92f8c0c6f6ad8ab46
Egor Ivkov <e.o.ivkov@gmail.com>
2022-06-03 15:58:07 +0300

    Wait for leader to be established (#658)
    * Wait for leader to be established
    
    * use condwar instead of channel (#659)
    
    Co-authored-by: Andrey Vasnetsov <andrey@vasnetsov.com>

--- src/common/helpers.rs
+++ src/common/helpers.rs
@@ -1,3 +1,4 @@
+use parking_lot::{Condvar, Mutex};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use tokio::runtime;
 use tokio::runtime::Runtime;
@@ -18,4 +19,71 @@ pub fn create_search_runtime(max_search_threads: usize) -> std::io::Result<Runti
             format!("search-{}", id)
         })
         .build()
+}
+
+pub struct IsReady {
+    condvar: Condvar,
+    value: Mutex<bool>,
+}
+
+impl Default for IsReady {
+    fn default() -> Self {
+        Self {
+            condvar: Condvar::new(),
+            value: Mutex::new(false),
+        }
+    }
+}
+
+impl IsReady {
+    pub fn make_ready(&self) {
+        let mut is_ready = self.value.lock();
+        if !*is_ready {
+            *is_ready = true;
+            self.condvar.notify_all();
+        }
+    }
+
+    pub fn make_not_ready(&self) {
+        *self.value.lock() = false;
+    }
+
+    pub fn check_ready(&self) -> bool {
+        *self.value.lock()
+    }
+
+    pub fn await_ready(&self) {
+        let mut is_ready = self.value.lock();
+        if !*is_ready {
+            self.condvar.wait(&mut is_ready);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+    use std::thread;
+    use std::thread::sleep;
+    use std::time::Duration;
+
+    #[test]
+    fn test_is_ready() {
+        let is_ready = Arc::new(IsReady::default());
+        let is_ready_clone = is_ready.clone();
+        let join = thread::spawn(move || {
+            is_ready_clone.await_ready();
+            eprintln!(
+                "is_ready_clone.check_ready() = {:#?}",
+                is_ready_clone.check_ready()
+            );
+        });
+
+        sleep(Duration::from_millis(500));
+        eprintln!("Making ready");
+        is_ready.make_ready();
+        sleep(Duration::from_millis(500));
+        join.join().unwrap()
+    }
 }
