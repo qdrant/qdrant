73c81b45d49a8ab4e7183993e263d480c8d59f7d
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2022-06-22 11:25:13 +0200

    acquire read lock only once and ensure isolation (#726)

--- src/consensus.rs
+++ src/consensus.rs
@@ -375,10 +375,15 @@ fn handle_committed_entries(
     state: &ConsensusStateRef,
     raw_node: &mut RawNode<ConsensusStateRef>,
 ) -> raft::Result<()> {
-    if state.persistent.read().unapplied_entities_count() > 0 {
-        panic!("Preconditon broken - all committed entries must be applied before this fn call")
-    }
-    let last_applied = state.persistent.read().last_applied_entry();
+    let last_applied = {
+        let persistent = state.persistent.read();
+        if persistent.unapplied_entities_count() > 0 {
+            panic!(
+                "Precondition broken - all committed entries must be applied before this fn call"
+            )
+        }
+        persistent.last_applied_entry()
+    };
     if let Some(last_applied) = last_applied {
         entries = entries
             .into_iter()
