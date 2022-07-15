289d0d53174eb91fc6b8bea7c3ccb2d55d5b84d9
Egor Ivkov <e.o.ivkov@gmail.com>
2022-07-15 17:34:59 +0300

    Default value for apply_progress_queue

--- lib/storage/src/content_manager/consensus_state.rs
+++ lib/storage/src/content_manager/consensus_state.rs
@@ -581,6 +581,7 @@ pub struct Persistent {
     state: RaftState,
     #[serde(default)] // TODO quick fix to avoid breaking the compat. with 0.8.1
     latest_snapshot_meta: SnapshotMetadataSer,
+    #[serde(default)]
     apply_progress_queue: EntryApplyProgressQueue,
     #[serde(with = "serialize_peer_addresses")]
     pub peer_address_by_id: Arc<RwLock<PeerAddressById>>,
