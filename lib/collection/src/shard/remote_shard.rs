1d617618260b51790ae4604a3d40cdd4e413042c
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-08-03 15:19:24 +0200

    await for index fixes

--- lib/collection/src/shard/remote_shard.rs
+++ lib/collection/src/shard/remote_shard.rs
@@ -36,7 +36,6 @@ use crate::telemetry::ShardTelemetry;
 /// RemoteShard
 ///
 /// Remote Shard is a representation of a shard that is located on a remote peer.
-#[derive(Clone)]
 pub struct RemoteShard {
     pub(crate) id: ShardId,
     pub(crate) collection_id: CollectionId,
