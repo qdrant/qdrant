1d907723b4d3bcd72f0a37b100e12801fa474653
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-08-16 10:36:46 +0200

    enable jemallocator

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,13 @@ use crate::settings::Settings;
 use crate::snapshots::{recover_full_snapshot, recover_snapshots};
 use crate::startup::setup_logger;
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 /// Qdrant (read: quadrant ) is a vector similarity search engine.
 /// It provides a production-ready service with a convenient API to store, search, and manage points - vectors with an additional payload.
 ///
