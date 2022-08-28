9a04e0d3466ff0addad43ee9bc1b0378607fda83
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-08-28 19:39:00 +0200

    full text index persitance and loading

--- lib/segment/src/index/field_index/full_text_index/mod.rs
+++ lib/segment/src/index/field_index/full_text_index/mod.rs
@@ -1,3 +1,4 @@
 pub mod text_index;
 mod tokenizers;
 mod postings_iterator;
+mod inverted_index;
--- lib/segment/src/index/field_index/full_text_index/text_index.rs
+++ lib/segment/src/index/field_index/full_text_index/text_index.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, BTreeSet, HashSet};
+use std::collections::HashSet;
 use std::sync::Arc;
 
 use parking_lot::RwLock;
@@ -8,50 +8,44 @@ use serde_json::Value;
 use crate::common::rocksdb_wrapper::DatabaseColumnWrapper;
 use crate::common::Flusher;
 use crate::data_types::text_index::TextIndexParams;
-use crate::entry::entry_point::OperationResult;
-use crate::index::field_index::full_text_index::postings_iterator::intersect_btree_iterator;
+use crate::entry::entry_point::{OperationError, OperationResult};
+use crate::index::field_index::full_text_index::inverted_index::{
+    Document, InvertedIndex, ParsedQuery,
+};
 use crate::index::field_index::full_text_index::tokenizers::Tokenizer;
 use crate::index::field_index::{
-    CardinalityEstimation, PayloadBlockCondition, PayloadFieldIndex, PrimaryCondition, ValueIndexer,
+    CardinalityEstimation, PayloadBlockCondition, PayloadFieldIndex, ValueIndexer,
 };
 use crate::telemetry::PayloadIndexTelemetry;
 use crate::types::{FieldCondition, Match, MatchText, PayloadKeyType, PointOffsetType};
 
-type PostingList = BTreeSet<PointOffsetType>;
-
-#[derive(Default, Clone)]
-pub struct Document {
-    pub tokens: BTreeSet<String>,
-}
-
-impl Document {
-    pub fn is_empty(&self) -> bool {
-        self.tokens.is_empty()
-    }
-}
-
-pub struct ParsedQuery {
-    pub tokens: BTreeSet<String>,
-}
-
-impl ParsedQuery {
-    pub fn check_match(&self, document: &Document) -> bool {
-        // Check that all tokens are in document
-        self.tokens
-            .iter()
-            .all(|query_token| document.tokens.contains(query_token))
-    }
-}
-
 pub struct FullTextIndex {
-    postings: BTreeMap<String, PostingList>,
-    point_to_docs: Vec<Document>,
-    points_count: usize,
+    inverted_index: InvertedIndex,
     db_wrapper: DatabaseColumnWrapper,
     config: TextIndexParams,
 }
 
 impl FullTextIndex {
+    fn store_key(id: &PointOffsetType) -> Vec<u8> {
+        bincode::serialize(&id).unwrap()
+    }
+
+    fn restore_key(data: &[u8]) -> PointOffsetType {
+        bincode::deserialize(data).unwrap()
+    }
+
+    fn serialize_document(document: &Document) -> OperationResult<Vec<u8>> {
+        serde_cbor::to_vec(document).map_err(|e| {
+            OperationError::service_error(&format!("Failed to serialize document: {}", e))
+        })
+    }
+
+    fn deserialize_document(data: &[u8]) -> OperationResult<Document> {
+        serde_cbor::from_slice(data).map_err(|e| {
+            OperationError::service_error(&format!("Failed to deserialize document: {}", e))
+        })
+    }
+
     fn storage_cf_name(field: &str) -> String {
         format!("{field}_fts")
     }
@@ -60,22 +54,20 @@ impl FullTextIndex {
         let store_cf_name = Self::storage_cf_name(field);
         let db_wrapper = DatabaseColumnWrapper::new(db, &store_cf_name);
         FullTextIndex {
-            postings: BTreeMap::new(),
-            point_to_docs: Vec::new(),
-            points_count: 0,
+            inverted_index: InvertedIndex::new(),
             db_wrapper,
             config,
         }
     }
 
     pub fn get_doc(&self, idx: PointOffsetType) -> Option<&Document> {
-        self.point_to_docs.get(idx as usize)
+        self.inverted_index.point_to_docs.get(idx as usize)
     }
 
     pub fn get_telemetry_data(&self) -> PayloadIndexTelemetry {
         PayloadIndexTelemetry {
-            points_values_count: self.points_count,
-            points_count: self.points_count,
+            points_values_count: self.inverted_index.points_count,
+            points_count: self.inverted_index.points_count,
             histogram_bucket_size: None,
         }
     }
@@ -107,13 +99,6 @@ impl ValueIndexer<String> for FullTextIndex {
             return Ok(());
         }
 
-        if self.point_to_docs.len() <= idx as usize {
-            self.point_to_docs
-                .resize(idx as usize + 1, Default::default());
-        }
-
-        self.points_count += 1;
-
         let mut tokens: HashSet<String> = HashSet::new();
 
         for value in values {
@@ -122,19 +107,17 @@ impl ValueIndexer<String> for FullTextIndex {
             });
         }
 
-        self.point_to_docs[idx as usize] = Document {
-            tokens: tokens.iter().cloned().collect(),
+        let document = Document {
+            tokens: tokens.into_iter().collect(),
         };
 
-        for token in tokens {
-            let posting = self
-                .postings
-                .entry(token)
-                .or_insert_with(|| BTreeSet::new());
-            posting.insert(idx);
-        }
+        self.inverted_index.index_document(idx, document);
 
-        // ToDo: Persist tokens to db
+        let db_idx = Self::store_key(&idx);
+        let db_document =
+            Self::serialize_document(&self.inverted_index.point_to_docs[idx as usize])?;
+
+        self.db_wrapper.put(&db_idx, &db_document)?;
 
         Ok(())
     }
@@ -147,40 +130,35 @@ impl ValueIndexer<String> for FullTextIndex {
     }
 
     fn remove_point(&mut self, id: PointOffsetType) -> OperationResult<()> {
-        if self.point_to_docs.len() <= id as usize {
-            return Ok(()); // Already removed or never actually existed
-        }
+        let removed_doc = self.inverted_index.remove_document(id);
 
-        let removed_doc = std::mem::take(&mut self.point_to_docs[id as usize]);
-
-        if removed_doc.is_empty() {
+        if removed_doc.is_none() {
             return Ok(());
         }
 
-        self.points_count -= 1;
+        let db_doc_id = Self::store_key(&id);
+        self.db_wrapper.remove(&db_doc_id)?;
 
-        for removed_token in removed_doc.tokens {
-            let posting = self.postings.get_mut(&removed_token);
-            if let Some(posting) = posting {
-                posting.remove(&id);
-                if posting.is_empty() {
-                    self.postings.remove(&removed_token);
-                }
-            }
-        }
-
-        // ToDo: Save changes in DB
         Ok(())
     }
 }
 
 impl PayloadFieldIndex for FullTextIndex {
     fn indexed_points(&self) -> usize {
-        self.points_count
+        self.inverted_index.points_count
     }
 
     fn load(&mut self) -> OperationResult<bool> {
-        todo!()
+        if !self.db_wrapper.has_column_family()? {
+            return Ok(false);
+        };
+
+        for (key, value) in self.db_wrapper.lock_db().iter()? {
+            let idx = Self::restore_key(&key);
+            let document = Self::deserialize_document(&value)?;
+            self.inverted_index.index_document(idx, document);
+        }
+        Ok(true)
     }
 
     fn clear(self) -> OperationResult<()> {
@@ -197,20 +175,7 @@ impl PayloadFieldIndex for FullTextIndex {
     ) -> Option<Box<dyn Iterator<Item = PointOffsetType> + '_>> {
         if let Some(Match::Text(text_match)) = &condition.r#match {
             let parsed_query = self.parse_query(&text_match.text);
-            let postings_opt: Option<Vec<_>> = parsed_query
-                .tokens
-                .iter()
-                .map(|token| self.postings.get(token))
-                .collect();
-            if postings_opt.is_none() {
-                // There are unseen tokens -> no matches
-                return Some(Box::new(vec![].into_iter()));
-            }
-            let postings = postings_opt.unwrap();
-            if postings.is_empty() {
-                return None; // No tokens -> all points are matched
-            }
-            return Some(intersect_btree_iterator(postings));
+            return Some(self.inverted_index.filter(&parsed_query));
         }
         None
     }
@@ -218,47 +183,10 @@ impl PayloadFieldIndex for FullTextIndex {
     fn estimate_cardinality(&self, condition: &FieldCondition) -> Option<CardinalityEstimation> {
         if let Some(Match::Text(text_match)) = &condition.r#match {
             let parsed_query = self.parse_query(&text_match.text);
-            let postings_opt: Option<Vec<_>> = parsed_query
-                .tokens
-                .iter()
-                .map(|token| self.postings.get(token))
-                .collect();
-            if postings_opt.is_none() {
-                // There are unseen tokens -> no matches
-                return Some(CardinalityEstimation {
-                    primary_clauses: vec![PrimaryCondition::Condition(condition.clone())],
-                    min: 0,
-                    exp: 0,
-                    max: 0,
-                });
-            }
-            let postings = postings_opt.unwrap();
-            if postings.is_empty() {
-                return None;
-            }
-            // Smallest posting is the largest possible cardinality
-            let smallest_posting = postings.iter().map(|posting| posting.len()).min().unwrap();
-
-            return if postings.len() == 1 {
-                Some(CardinalityEstimation {
-                    primary_clauses: vec![PrimaryCondition::Condition(condition.clone())],
-                    min: smallest_posting,
-                    exp: smallest_posting,
-                    max: smallest_posting,
-                })
-            } else {
-                let expected_frac: f64 = postings
-                    .iter()
-                    .map(|posting| posting.len() as f64 / self.points_count as f64)
-                    .product();
-                let exp = (expected_frac * self.points_count as f64) as usize;
-                Some(CardinalityEstimation {
-                    primary_clauses: vec![PrimaryCondition::Condition(condition.clone())],
-                    min: 0, // ToDo: make better estimation
-                    exp,
-                    max: smallest_posting,
-                })
-            };
+            return Some(
+                self.inverted_index
+                    .estimate_cardinality(&parsed_query, condition),
+            );
         }
         None
     }
@@ -268,30 +196,11 @@ impl PayloadFieldIndex for FullTextIndex {
         threshold: usize,
         key: PayloadKeyType,
     ) -> Box<dyn Iterator<Item = PayloadBlockCondition> + '_> {
-        // It might be very hard to predict possible combinations of conditions,
-        // so we only build it for individual tokens
-        Box::new(
-            self.postings
-                .iter()
-                .filter(move |(_token, posting)| posting.len() >= threshold)
-                .map(move |(token, posting)| PayloadBlockCondition {
-                    condition: FieldCondition {
-                        key: key.clone(),
-                        r#match: Some(Match::Text(MatchText {
-                            text: token.to_owned(),
-                        })),
-                        range: None,
-                        geo_bounding_box: None,
-                        geo_radius: None,
-                        values_count: None,
-                    },
-                    cardinality: posting.len(),
-                }),
-        )
+        self.inverted_index.payload_blocks(threshold, key)
     }
 
     fn count_indexed_points(&self) -> usize {
-        self.points_count
+        self.inverted_index.points_count
     }
 }
 
@@ -302,6 +211,19 @@ mod tests {
     use super::*;
     use crate::common::rocksdb_wrapper::open_db_with_existing_cf;
     use crate::data_types::text_index::TokenizerType;
+
+    fn filter_request(text: &str) -> FieldCondition {
+        FieldCondition {
+            key: "text".to_owned(),
+            r#match: Some(Match::Text(MatchText {
+                text: text.to_owned(),
+            })),
+            range: None,
+            geo_bounding_box: None,
+            geo_radius: None,
+            values_count: None,
+        }
+    }
 
     #[test]
     fn test_full_text_indexing() {
@@ -317,9 +239,6 @@ mod tests {
         ];
 
         let tmp_dir = Builder::new().prefix("test_dir").tempdir().unwrap();
-
-        let db = open_db_with_existing_cf(&tmp_dir.path().join("test_db")).unwrap();
-
         let config = TextIndexParams {
             tokenizer: TokenizerType::Word,
             min_token_len: None,
@@ -327,48 +246,74 @@ mod tests {
             lowercase: None,
         };
 
-        let mut index = FullTextIndex::new(db, config, "text");
+        {
+            let db = open_db_with_existing_cf(&tmp_dir.path().join("test_db")).unwrap();
 
-        for (idx, payload) in payloads.iter().enumerate() {
-            index.add_point(idx as PointOffsetType, payload).unwrap();
+            let mut index = FullTextIndex::new(db, config, "text");
+
+            index.recreate().unwrap();
+
+            for (idx, payload) in payloads.iter().enumerate() {
+                index.add_point(idx as PointOffsetType, payload).unwrap();
+            }
+
+            assert_eq!(index.count_indexed_points(), payloads.len());
+
+            let filter_condition = filter_request("multivac");
+            let search_res: Vec<_> = index.filter(&filter_condition).unwrap().collect();
+            assert_eq!(search_res, vec![0, 4]);
+
+            let filter_condition = filter_request("giant computer");
+            let search_res: Vec<_> = index.filter(&filter_condition).unwrap().collect();
+            assert_eq!(search_res, vec![2]);
+
+            let filter_condition = filter_request("the great time");
+            let search_res: Vec<_> = index.filter(&filter_condition).unwrap().collect();
+            assert_eq!(search_res, vec![4]);
+
+            index.remove_point(2).unwrap();
+            index.remove_point(3).unwrap();
+
+            let filter_condition = filter_request("giant computer");
+            let search_res: Vec<_> = index.filter(&filter_condition).unwrap().collect();
+            assert!(search_res.is_empty());
+
+            assert_eq!(index.count_indexed_points(), payloads.len() - 2);
+
+            index
+                .add_point(
+                    3,
+                    &serde_json::json!([
+                "The last question was asked for the first time, half in jest, on May 21, 2061,",
+                "at a time when humanity first stepped into the light."
+            ]),
+                )
+                .unwrap();
+
+            index.add_point(4, &serde_json::json!(
+                "The question came about as a result of a five dollar bet over highballs, and it happened this way: "
+            )).unwrap();
+
+            assert_eq!(index.count_indexed_points(), payloads.len() - 1);
+
+            index.flusher()().unwrap();
         }
 
-        assert_eq!(index.count_indexed_points(), payloads.len());
+        {
+            let db = open_db_with_existing_cf(&tmp_dir.path().join("test_db")).unwrap();
+            let mut index = FullTextIndex::new(db, config, "text");
+            let loaded = index.load().unwrap();
+            assert!(loaded);
 
-        let filter_condition = FieldCondition {
-            key: "text".to_string(),
-            r#match: Some(Match::Text(MatchText {
-                text: "multivac".to_owned(),
-            })),
-            range: None,
-            geo_bounding_box: None,
-            geo_radius: None,
-            values_count: None
-        };
+            assert_eq!(index.count_indexed_points(), 4);
 
-        let search_res: Vec<_> = index.filter(&filter_condition).unwrap().collect();
-        assert_eq!(search_res, vec![0, 4]);
+            let filter_condition = filter_request("multivac");
+            let search_res: Vec<_> = index.filter(&filter_condition).unwrap().collect();
+            assert_eq!(search_res, vec![0]);
 
-
-        index.remove_point(2).unwrap();
-        index.remove_point(3).unwrap();
-
-        assert_eq!(index.count_indexed_points(), payloads.len() - 2);
-
-        index
-            .add_point(
-                3,
-                &serde_json::json!([
-            "The last question was asked for the first time, half in jest, on May 21, 2061,",
-            "at a time when humanity first stepped into the light."
-        ]),
-            )
-            .unwrap();
-
-        index.add_point(4, &serde_json::json!(
-            "The question came about as a result of a five dollar bet over highballs, and it happened this way: "
-        )).unwrap();
-
-        assert_eq!(index.count_indexed_points(), payloads.len() - 1);
+            let filter_condition = filter_request("the");
+            let search_res: Vec<_> = index.filter(&filter_condition).unwrap().collect();
+            assert_eq!(search_res, vec![0, 1, 3, 4]);
+        }
     }
 }
