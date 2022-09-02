120bae36b71359bbc26ee7d08e5087b3d5f58c02
Egor Ivkov <e.o.ivkov@gmail.com>
2022-09-02 19:40:09 +0300

    Introduces ReplicaSet

--- lib/collection/src/shard/mod.rs
+++ lib/collection/src/shard/mod.rs
@@ -5,6 +5,8 @@ pub mod local_shard;
 pub mod local_shard_operations;
 pub mod proxy_shard;
 pub mod remote_shard;
+#[allow(dead_code)]
+pub mod replica_set;
 pub mod shard_config;
 pub mod shard_holder;
 pub mod shard_versioning;
@@ -34,11 +36,13 @@ use crate::shard::remote_shard::RemoteShard;
 use crate::shard::shard_versioning::suggest_next_version_path;
 use crate::telemetry::ShardTelemetry;
 
+use self::replica_set::ReplicaSet;
+
 pub type ShardId = u32;
 
 /// Shard
 ///
-/// A shard can either be local or remote
+/// Contains a part of the collection's points
 ///
 #[allow(clippy::large_enum_variant)]
 pub enum Shard {
@@ -46,6 +50,7 @@ pub enum Shard {
     Remote(RemoteShard),
     Proxy(ProxyShard),
     ForwardProxy(ForwardProxyShard),
+    ReplicaSet(ReplicaSet),
 }
 
 impl Shard {
@@ -55,6 +60,7 @@ impl Shard {
             Shard::Remote(remote_shard) => remote_shard,
             Shard::Proxy(proxy_shard) => proxy_shard,
             Shard::ForwardProxy(proxy_shard) => proxy_shard,
+            Shard::ReplicaSet(replica_set) => replica_set,
         }
     }
 
@@ -64,6 +70,7 @@ impl Shard {
             Shard::Remote(_) => (),
             Shard::Proxy(proxy_shard) => proxy_shard.before_drop().await,
             Shard::ForwardProxy(proxy_shard) => proxy_shard.before_drop().await,
+            Shard::ReplicaSet(_) => todo!(),
         }
     }
 
@@ -73,6 +80,7 @@ impl Shard {
             Shard::Remote(remote) => remote.peer_id,
             Shard::Proxy(_) => this_peer_id,
             Shard::ForwardProxy(_) => this_peer_id,
+            Shard::ReplicaSet(_) => todo!(),
         }
     }
 
@@ -82,6 +90,7 @@ impl Shard {
             Shard::Remote(remote_shard) => remote_shard.get_telemetry_data(),
             Shard::Proxy(proxy_shard) => proxy_shard.get_telemetry_data(),
             Shard::ForwardProxy(proxy_shard) => proxy_shard.get_telemetry_data(),
+            Shard::ReplicaSet(_) => todo!(),
         }
     }
 }
