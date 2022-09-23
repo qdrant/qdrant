1a72b24be2bdb76a3ea921b387ae286c669fadff
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-09-23 11:06:28 +0200

    Revert "Persist replica state (#1035)"
    This reverts commit 31c3ae482d7adb314382842caccfba8016c833fc.

--- lib/collection/src/collection.rs
+++ lib/collection/src/collection.rs
@@ -2,7 +2,6 @@ use std::cmp::max;
 use std::collections::{HashMap, HashSet};
 use std::future::Future;
 use std::num::NonZeroU32;
-use std::ops::Deref;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
@@ -1243,7 +1242,7 @@ impl Collection {
                 .map(|(shard_id, shard)| {
                     let shard_info = match shard {
                         Shard::ReplicaSet(replicas) => ShardInfo::ReplicaSet {
-                            replicas: replicas.replica_state.deref().clone(),
+                            replicas: replicas.replica_state.clone(),
                         },
                         shard => ShardInfo::Single(
                             *shard
