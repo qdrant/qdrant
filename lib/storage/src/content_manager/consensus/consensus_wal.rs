843e9d0c1f790b4f370ad80b53e026418ee82a1e
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-10-08 20:31:21 +0200

    ensure consequent entries in consensus wal (#1108)
    * ensure consequent entries in consensus wal
    
    * rollback consensus docker-compose
    
    * rollback consensus docker-compose
    
    * clippy
    
    * add wal index debug assert

--- lib/storage/src/content_manager/consensus/consensus_wal.rs
+++ lib/storage/src/content_manager/consensus/consensus_wal.rs
@@ -8,11 +8,11 @@ use raft::util::limit_size;
 use wal::Wal;
 
 use crate::content_manager::consensus_state;
-use crate::StorageError;
+use crate::{ConsensusOperations, StorageError};
 
 const COLLECTIONS_META_WAL_DIR: &str = "collections_meta_wal";
 
-pub struct ConsensusOpWal(pub Wal);
+pub struct ConsensusOpWal(Wal);
 
 impl ConsensusOpWal {
     pub fn new(storage_path: &str) -> Self {
@@ -20,6 +20,10 @@ impl ConsensusOpWal {
         create_dir_all(&collections_meta_wal_path)
             .expect("Can't create Collections meta Wal directory");
         ConsensusOpWal(Wal::open(collections_meta_wal_path).unwrap())
+    }
+
+    pub fn clear(&mut self) -> Result<(), StorageError> {
+        Ok(self.0.clear()?)
     }
 
     pub fn entry(&self, id: u64) -> raft::Result<RaftEntry> {
@@ -74,13 +78,174 @@ impl ConsensusOpWal {
         Ok(entry.transpose()?)
     }
 
+    /// Difference between raft index and WAL record number.
+    /// Difference might be different because of consensus snapshot.
+    fn index_offset(&self) -> Result<Option<u64>, StorageError> {
+        let last_known_index = self.0.first_index();
+        let first_entry = self.first_entry()?;
+        let offset = first_entry.map(|entry| entry.index - last_known_index);
+        Ok(offset)
+    }
+
     pub fn append_entries(&mut self, entries: Vec<RaftEntry>) -> Result<(), StorageError> {
         for entry in entries {
-            log::debug!("Appending entry: {entry:?}");
+            let operation_opt = ConsensusOperations::try_from(&entry).ok();
+
+            let index = entry.index;
+            let current_index = self.0.last_index();
+            let index_offset = self.index_offset()?;
+
+            if let Some(offset) = index_offset {
+                // Assume we can't skip index numbers in WAL except for snapshot
+                // Example: 2 <= 0 + 1 + 1
+                debug_assert!(
+                    index <= current_index + offset + 1,
+                    "Expected no index skip: {} <= {} + {}",
+                    index,
+                    current_index,
+                    offset
+                );
+
+                if index < current_index + offset {
+                    // If there is a conflict, example:
+                    // Offset = 1
+                    // raft index = 10
+                    // wal index = 11
+                    // expected_wal_index = 10 - 1 = 9
+                    // 10 < 11 + 1
+                    if index < offset {
+                        return Err(StorageError::service_error(&format!(
+                            "Wal index conflict, raft index: {}, wal index: {}, offset: {}",
+                            index, current_index, offset
+                        )));
+                    }
+                    log::debug!(
+                        "Truncate conflicting WAL entries from index {}, raft: {}",
+                        index - offset,
+                        index
+                    );
+                    self.0.truncate(index - offset)?;
+                } // else:
+                  // Offset = 1
+                  // raft index = 11
+                  // wal index = 10
+                  // expected_wal_index = 11 - 1 = 10
+                  // 11 < 10 + 1
+            } else {
+                // There is no offset => there are no records in WAL
+                // If there are no records, conflict is impossible
+                // So we do nothing
+            }
+
+            if let Some(operation) = operation_opt {
+                let term = entry.term;
+                log::debug!(
+                    "Appending operation: term: {term}, index: {index} {operation:?}, entry: "
+                );
+            } else {
+                log::debug!("Appending entry: {entry:?}");
+            }
+
             let mut buf = vec![];
             entry.encode(&mut buf)?;
-            self.0.append(&buf)?;
+            let wal_index = self.0.append(&buf)?;
+            #[cfg(debug_assertions)]
+            if let Some(offset) = index_offset {
+                debug_assert!(wal_index == index - offset);
+            } else {
+                debug_assert!(wal_index == 0)
+            }
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use raft::eraftpb::Entry;
+
+    use super::*;
+
+    #[test]
+    fn test_log_rewrite() {
+        let entries_orig = vec![
+            Entry {
+                entry_type: 0,
+                term: 1,
+                index: 1,
+                data: vec![1, 2, 3],
+                context: vec![],
+                sync_log: false,
+            },
+            Entry {
+                entry_type: 0,
+                term: 1,
+                index: 2,
+                data: vec![1, 2, 3],
+                context: vec![],
+                sync_log: false,
+            },
+            Entry {
+                entry_type: 0,
+                term: 1,
+                index: 3,
+                data: vec![1, 2, 3],
+                context: vec![],
+                sync_log: false,
+            },
+        ];
+
+        let entries_new = vec![
+            Entry {
+                entry_type: 0,
+                term: 1,
+                index: 2,
+                data: vec![2, 2, 2],
+                context: vec![],
+                sync_log: false,
+            },
+            Entry {
+                entry_type: 0,
+                term: 1,
+                index: 3,
+                data: vec![3, 3, 3],
+                context: vec![],
+                sync_log: false,
+            },
+        ];
+
+        let temp_dir = tempfile::tempdir().unwrap();
+
+        let mut wal = ConsensusOpWal::new(temp_dir.path().to_str().unwrap());
+        wal.append_entries(entries_orig).unwrap();
+        wal.append_entries(entries_new.clone()).unwrap();
+
+        let result_entries = wal.entries(1, 4, None).unwrap();
+        assert_eq!(result_entries.len(), 3);
+
+        assert_eq!(result_entries[0].data, vec![1, 2, 3]);
+        assert_eq!(result_entries[1].data, vec![2, 2, 2]);
+        assert_eq!(result_entries[2].data, vec![3, 3, 3]);
+
+        wal.clear().unwrap();
+
+        wal.append_entries(entries_new).unwrap();
+
+        assert_eq!(wal.index_offset().unwrap(), Some(2));
+
+        let broken_entry = vec![Entry {
+            entry_type: 0,
+            term: 1,
+            index: 1, // Index 1 can't be overwritten, because it is already compacted
+            data: vec![5, 5, 5],
+            context: vec![],
+            sync_log: false,
+        }];
+
+        // Some errors can't be corrected
+        assert!(matches!(
+            wal.append_entries(broken_entry),
+            Err(StorageError::ServiceError { .. })
+        ));
     }
 }
