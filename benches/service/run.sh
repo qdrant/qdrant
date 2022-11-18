e83bfef8b26308271700b0616240e446397f80db
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2022-11-18 13:34:49 +0100

    enable compression for gRPC (#1186)
    * enable compression for gRPC
    
    * add benchmarks
    
    * fix grpc benchmark
    
    * discardResponseBodies to reduce memory usage
    
    * improve & split benchmarks
    
    * do not use gzip for inter node communication as benchmarks are not conclusive

--- benches/service/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-
-docker run --network=host --rm -i loadimpact/k6 run - <"$DIR/collection_stress.js"
-
-
