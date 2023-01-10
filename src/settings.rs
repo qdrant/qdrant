5c4a133451b0d24661d24ce76556b2bb24643da7
Hozan <119854621+hozan23@users.noreply.github.com>
2023-01-10 19:34:57 +0300

    Add the option to load alternative configuration file (#1328)
    * add the option to load alternative configuration file
    
    * minor fix & pass cargo clippy warning

--- src/settings.rs
+++ src/settings.rs
@@ -116,12 +116,13 @@ fn default_connection_pool_size() -> usize {
 
 impl Settings {
     #[allow(dead_code)]
-    pub fn new() -> Result<Self, ConfigError> {
+    pub fn new(config_path: Option<String>) -> Result<Self, ConfigError> {
+        let config_path = config_path.unwrap_or_else(|| "config/config".into());
         let env = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
 
         let s = Config::builder()
             // Start off by merging in the "default" configuration file
-            .add_source(File::with_name("config/config"))
+            .add_source(File::with_name(&config_path))
             // Add in the current environment file
             // Default to 'development' env
             // Note that this file is _optional_
@@ -162,6 +163,6 @@ mod tests {
     fn test_read_default_config() {
         let key = "RUN_MODE";
         env::set_var(key, "TEST");
-        Settings::new().unwrap();
+        Settings::new(None).unwrap();
     }
 }
