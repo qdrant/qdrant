09bf8bbfffb709d685058abcfa8f30d387d332ef
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-01-12 16:32:22 +0100

    Add logging to node recovery

--- tests/consensus_tests/assertions.py
+++ tests/consensus_tests/assertions.py
@@ -3,8 +3,9 @@ import requests
 
 def assert_http_ok(response: requests.Response):
     if response.status_code != 200:
+        base_msg = f"HTTP request to {response.url} failed with status code {response.status_code} after {response.elapsed.total_seconds()}s"
         if not response.content:
-            raise Exception(f"Http request failed with status {response.status_code} and no content")
+            raise Exception(f"{base_msg} and without response body")
         else:
             raise Exception(
-                f"Http request failed with status {response.status_code} and content:\n{response.json()}")
+                f"{base_msg} with response body:\n{response.json()}")
