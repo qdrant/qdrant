f41a0b649cd45ca4c06eaca5564ada0db3489fdc
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-02-08 18:00:29 +0100

    Add UUID to storage compatibility tests

--- tests/basic_grpc_test.sh
+++ tests/basic_grpc_test.sh
@@ -45,8 +45,8 @@ $docker_grpcurl -d '{
     {"id": { "num": 2 }, "vectors": {"vector": {"data": [0.19, 0.81, 0.75, 0.11]}}, "payload": {"city": {"list_value": {"values": [{ "string_value": "Berlin" }, { "string_value": "London" }]}}}},
     {"id": { "num": 3 }, "vectors": {"vector": {"data": [0.36, 0.55, 0.47, 0.94]}}, "payload": {"city": {"list_value": {"values": [{ "string_value": "Berlin" }, { "string_value": "Moscow" }]}}}},
     {"id": { "num": 4 }, "vectors": {"vector": {"data": [0.18, 0.01, 0.85, 0.80]}}, "payload": {"city": {"list_value": {"values": [{ "string_value": "London" }, { "string_value": "Moscow" }]}}}},
-    {"id": { "num": 5 }, "vectors": {"vector": {"data": [0.24, 0.18, 0.22, 0.44]}}, "payload": {"count":{"list_value": {"values": [{ "integer_value": 0 }]}}}},
-    {"id": { "num": 6 }, "vectors": {"vector": {"data": [0.35, 0.08, 0.11, 0.44]}}}
+    {"id": { "uuid": "98a9a4b1-4ef2-46fb-8315-a97d874fe1d7" }, "vectors": {"vector": {"data": [0.24, 0.18, 0.22, 0.44]}}, "payload": {"count":{"list_value": {"values": [{ "integer_value": 0 }]}}}},
+    {"id": { "uuid": "f0e09527-b096-42a8-94e9-ea94d342b925" }, "vectors": {"vector": {"data": [0.35, 0.08, 0.11, 0.44]}}}
   ]
 }' $QDRANT_HOST qdrant.Points/Upsert
 
