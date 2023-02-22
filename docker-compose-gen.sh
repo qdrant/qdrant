602c199a939e1857a1f3b3b052b4f829a1d94316
Roman Titov <ffuugoo@users.noreply.github.com>
2023-02-22 15:43:10 +0100

    Add a simple script to generate `docker-compose.yaml` for a cluster of specified number of nodes

---
+++ docker-compose-gen.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+declare NODES="${1:-5}"
+declare BASE_PORT="${2:-6330}"
+
+echo 'version: "3.7"'
+echo 'services:'
+
+for ((node=0; node<NODES; node++))
+do
+	declare SERVICE_NAME=qdrant_node_$node
+
+	declare HTTP_PORT=$((BASE_PORT + node * 10 + 3))
+	declare GRPC_PORT=$((BASE_PORT + node * 10 + 4))
+
+	if ((node == 0))
+	then
+		declare COMMAND="./qdrant --uri 'http://$SERVICE_NAME:6335'"
+	else
+		declare COMMAND="bash -c \"sleep $((5 + node)) && ./qdrant --bootstrap 'http://qdrant_node_0:6335' --uri 'http://$SERVICE_NAME:6335'\""
+	fi
+
+	cat <<-EOF
+	  $SERVICE_NAME:
+	    image: qdrant/qdrant:latest
+	    command: $COMMAND
+	    restart: always
+	    environment:
+	      - QDRANT__SERVICE__GRPC_PORT=6334
+	      - QDRANT__CLUSTER__ENABLED=true
+	      - QDRANT__CLUSTER__P2P__PORT=6335
+	      - QDRANT__CLUSTER__CONSENSUS__MAX_MESSAGE_QUEUE_SIZE=5000
+	      - QDRANT__LOG_LEVEL=debug,raft=info
+	    ports:
+	      - "$HTTP_PORT:6333"
+	      - "$GRPC_PORT:6334"
+
+	EOF
+done
