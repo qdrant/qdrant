d2c68913d0a756a46c9a88581fb92a51abf5a9a9
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-03-08 21:02:59 +0000

    vector index as enum

--- lib/segment/src/index/vector_index_base.rs
+++ lib/segment/src/index/vector_index_base.rs
@@ -1,12 +1,13 @@
 use std::path::PathBuf;
 use std::sync::atomic::AtomicBool;
 
+use super::hnsw_index::graph_links::{GraphLinksMmap, GraphLinksRam};
+use super::hnsw_index::hnsw::HNSWIndex;
+use super::plain_payload_index::PlainIndex;
 use crate::data_types::vectors::VectorElementType;
 use crate::entry::entry_point::OperationResult;
 use crate::telemetry::VectorIndexSearchesTelemetry;
-use crate::types::{
-    Filter, SearchParams,
-};
+use crate::types::{Filter, SearchParams};
 use crate::vector_storage::ScoredPointOffset;
 
 /// Trait for vector searching
@@ -28,4 +29,48 @@ pub trait VectorIndex {
     fn files(&self) -> Vec<PathBuf>;
 }
 
-pub type VectorIndexSS = dyn VectorIndex + Sync + Send;
+pub enum VectorIndexEnum {
+    Plain(PlainIndex),
+    HnswRam(HNSWIndex<GraphLinksRam>),
+    HnswMmap(HNSWIndex<GraphLinksMmap>),
+}
+
+impl VectorIndex for VectorIndexEnum {
+    fn search(
+        &self,
+        vectors: &[&[VectorElementType]],
+        filter: Option<&Filter>,
+        top: usize,
+        params: Option<&SearchParams>,
+    ) -> Vec<Vec<ScoredPointOffset>> {
+        match self {
+            VectorIndexEnum::Plain(index) => index.search(vectors, filter, top, params),
+            VectorIndexEnum::HnswRam(index) => index.search(vectors, filter, top, params),
+            VectorIndexEnum::HnswMmap(index) => index.search(vectors, filter, top, params),
+        }
+    }
+
+    fn build_index(&mut self, stopped: &AtomicBool) -> OperationResult<()> {
+        match self {
+            VectorIndexEnum::Plain(index) => index.build_index(stopped),
+            VectorIndexEnum::HnswRam(index) => index.build_index(stopped),
+            VectorIndexEnum::HnswMmap(index) => index.build_index(stopped),
+        }
+    }
+
+    fn get_telemetry_data(&self) -> VectorIndexSearchesTelemetry {
+        match self {
+            VectorIndexEnum::Plain(index) => index.get_telemetry_data(),
+            VectorIndexEnum::HnswRam(index) => index.get_telemetry_data(),
+            VectorIndexEnum::HnswMmap(index) => index.get_telemetry_data(),
+        }
+    }
+
+    fn files(&self) -> Vec<PathBuf> {
+        match self {
+            VectorIndexEnum::Plain(index) => index.files(),
+            VectorIndexEnum::HnswRam(index) => index.files(),
+            VectorIndexEnum::HnswMmap(index) => index.files(),
+        }
+    }
+}
