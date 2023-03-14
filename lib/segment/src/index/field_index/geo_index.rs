608367193d3932e07c867d4bffea0541e5918199
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-03-14 12:10:54 +0000

    fix values count loading in geoindex

--- lib/segment/src/index/field_index/geo_index.rs
+++ lib/segment/src/index/field_index/geo_index.rs
@@ -104,6 +104,7 @@ impl GeoMapIndex {
 
             self.point_to_values[idx as usize].push(geo_point);
             self.points_map.entry(geo_hash).or_default().insert(idx);
+            self.values_count += 1;
         }
         Ok(true)
     }
