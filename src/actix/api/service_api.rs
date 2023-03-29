91a0200c1b242326d66d374152e3e80f4063c554
Tim Visée <tim+github@visee.me>
2023-03-29 13:08:06 +0200

    Improve metrics building performance, limit endpoints to whitelist (#1616)
    * Fix incorrect metrics value for cluster commit
    
    * Rewrite metrics logic, don't use registry, write values directly
    
    * Only report REST timings for requests having HTTP 200 response
    
    * Limit metrics reporting of endpoints to whitelist
    
    The whitelist contains a selection of search, recommend and upsert endpoints.
    
    * Add MetricsParam, remove detail level, keep anonymize
    
    * Request metrics in basic API test
    
    * Specify content type for metrics endpoint
    
    * Add OpenAPI test for metrics endpoint, remove from basic API test
    
    This test probes for some strings that must exist in the output
    
    * Add note that metrics endpoint whitelist must be sorted

--- src/actix/api/service_api.rs
+++ src/actix/api/service_api.rs
@@ -1,3 +1,4 @@
+use actix_web::http::header::ContentType;
 use actix_web::rt::time::Instant;
 use actix_web::web::Query;
 use actix_web::{get, post, web, HttpResponse, Responder};
@@ -36,23 +37,28 @@ async fn telemetry(
     process_response(Ok(telemetry_data), timing)
 }
 
+#[derive(Deserialize, Serialize, JsonSchema)]
+pub struct MetricsParam {
+    pub anonymize: Option<bool>,
+}
+
 #[get("/metrics")]
 async fn metrics(
     telemetry_collector: web::Data<Mutex<TelemetryCollector>>,
-    params: Query<TelemetryParam>,
+    params: Query<MetricsParam>,
 ) -> impl Responder {
     let anonymize = params.anonymize.unwrap_or(false);
-    let details_level = params.details_level.unwrap_or(0);
     let telemetry_collector = telemetry_collector.lock().await;
-    let telemetry_data = telemetry_collector.prepare_data(details_level).await;
+    let telemetry_data = telemetry_collector.prepare_data(0).await;
     let telemetry_data = if anonymize {
         telemetry_data.anonymize()
     } else {
         telemetry_data
     };
 
-    let metrics_data = MetricsData::from(telemetry_data);
-    HttpResponse::Ok().body(metrics_data.format_metrics())
+    HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .body(MetricsData::from(telemetry_data).format_metrics())
 }
 
 #[post("/locks")]
