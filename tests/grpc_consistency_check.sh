d756483e072ede447e76083b02d7c79a940c6187
timvisee <tim@visee.me>
2023-04-05 15:31:05 +0200

    Add CI task to test gRPC file consistency

---
+++ tests/grpc_consistency_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+# Ensure current path is project root
+cd "$(dirname "$0")/../"
+
+# Keep current version of files to check
+cp ./lib/api/src/grpc/qdrant.rs ./lib/api/src/grpc/.repo.qdrant.rs
+
+# Regenerate gRPC files
+cargo build --manifest-path lib/api/Cargo.toml
+
+# Ensure generated files are the same as files in this repository
+if diff -Zwa ./lib/api/src/grpc/qdrant.rs ./lib/api/src/grpc/.repo.qdrant.rs
+then
+    set +x
+    echo "No diff found."
+else
+    set +x
+    echo "ERROR: Generated gRPC file is not consistent with files in this repository, see diff above."
+    echo "ERROR: See: https://github.com/qdrant/qdrant/blob/master/docs/DEVELOPMENT.md#grpc"
+    exit 1
+fi
+
+# Cleanup
+rm -f ./lib/api/src/grpc/.repo.qdrant.rs
