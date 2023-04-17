bebeace52a09e3b358b0283d1d998d47766c1612
timvisee <tim@visee.me>
2023-04-17 13:36:14 +0200

    Add docs and stabilize issue link to dev_ceil

--- lib/segment/src/vector_storage/mod.rs
+++ lib/segment/src/vector_storage/mod.rs
@@ -9,7 +9,19 @@ mod vector_storage_base;
 pub use raw_scorer::*;
 pub use vector_storage_base::*;
 
-// We can replace this by `div_ceil` from the standard library once it stabilizes.
+/// Calculates the quotient of self and rhs, rounding the result towards positive infinity.
+///
+/// # Panics
+///
+/// This function will panic if rhs is zero.
+///
+/// # Overflow behavior
+///
+/// On overflow, this function will panic if overflow checks are enabled (default in debug mode)
+/// and wrap if overflow checks are disabled (default in release mode).
+// Can be replaced with stdlib once it stabilizes:
+// - <https://github.com/rust-lang/rust/issues/88581>
+// - <https://doc.rust-lang.org/std/primitive.usize.html#method.div_ceil>
 #[inline]
 const fn div_ceil(a: usize, b: usize) -> usize {
     (a + b - 1) / b
