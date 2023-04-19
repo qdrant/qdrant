e8fcc132516276d8edd3ae15eacc6d26b3608b97
Roman Titov <ffuugoo@users.noreply.github.com>
2023-04-19 23:09:17 +0200

    fixup! Refactor `segment/src/common/file_operations.rs` module and `FileStorageError` type
    Move stuff in `segment/src/common/file_operations.rs` around

--- lib/segment/src/common/file_operations.rs
+++ lib/segment/src/common/file_operations.rs
@@ -7,8 +7,32 @@ use atomicwrites::{AtomicFile, OverwriteBehavior};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
-pub type FileStorageError = Error;
+pub fn atomic_save_bin<T: Serialize>(path: &Path, object: &T) -> Result<()> {
+    let af = AtomicFile::new(path, OverwriteBehavior::AllowOverwrite);
+    af.write(|f| bincode::serialize_into(BufWriter::new(f), object))?;
+    Ok(())
+}
+
+pub fn atomic_save_json<T: Serialize>(path: &Path, object: &T) -> Result<()> {
+    let af = AtomicFile::new(path, OverwriteBehavior::AllowOverwrite);
+    af.write(|f| serde_json::to_writer(BufWriter::new(f), object))?;
+    Ok(())
+}
+
+pub fn read_json<T: DeserializeOwned>(path: &Path) -> Result<T> {
+    Ok(serde_json::from_reader(BufReader::new(File::open(path)?))?)
+}
+
+pub fn read_bin<T: DeserializeOwned>(path: &Path) -> Result<T> {
+    Ok(bincode::deserialize_from(BufReader::new(File::open(
+        path,
+    )?))?)
+}
+
 pub type FileOperationResult<T> = Result<T>;
+pub type FileStorageError = Error;
+
+pub type Result<T, E = Error> = result::Result<T, E>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -47,28 +71,4 @@ impl From<bincode::Error> for Error {
     fn from(err: bincode::Error) -> Self {
         Self::Bincode(*err)
     }
-}
-
-pub type Result<T, E = Error> = result::Result<T, E>;
-
-pub fn atomic_save_bin<T: Serialize>(path: &Path, object: &T) -> Result<()> {
-    let af = AtomicFile::new(path, OverwriteBehavior::AllowOverwrite);
-    af.write(|f| bincode::serialize_into(BufWriter::new(f), object))?;
-    Ok(())
-}
-
-pub fn atomic_save_json<T: Serialize>(path: &Path, object: &T) -> Result<()> {
-    let af = AtomicFile::new(path, OverwriteBehavior::AllowOverwrite);
-    af.write(|f| serde_json::to_writer(BufWriter::new(f), object))?;
-    Ok(())
-}
-
-pub fn read_json<T: DeserializeOwned>(path: &Path) -> Result<T> {
-    Ok(serde_json::from_reader(BufReader::new(File::open(path)?))?)
-}
-
-pub fn read_bin<T: DeserializeOwned>(path: &Path) -> Result<T> {
-    Ok(bincode::deserialize_from(BufReader::new(File::open(
-        path,
-    )?))?)
 }
