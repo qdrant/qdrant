d0508a4bdc15be9b905e0e40738c707e38460cc9
nilhex <nilhex@pm.me>
2023-04-24 22:19:20 +0700

    Conversations fix; map_err fix

--- src/tonic/api/collections_api.rs
+++ src/tonic/api/collections_api.rs
@@ -2,16 +2,13 @@ use std::sync::Arc;
 use std::time::{Duration, Instant};
 
 use api::grpc::qdrant::collections_server::Collections;
-use api::grpc::qdrant::update_collection_cluster_setup_request::Operation;
 use api::grpc::qdrant::{
     AliasDescription, ChangeAliases, CollectionClusterInfoRequest, CollectionClusterInfoResponse,
     CollectionOperationResponse, CreateCollection, DeleteCollection, GetCollectionInfoRequest,
     GetCollectionInfoResponse, ListAliasesRequest, ListAliasesResponse,
-    ListCollectionAliasesRequest, ListCollectionsRequest, ListCollectionsResponse, LocalShardInfo,
-    RemoteShardInfo, ShardTransferInfo, UpdateCollection, UpdateCollectionClusterSetupRequest,
-    UpdateCollectionClusterSetupResponse,
+    ListCollectionAliasesRequest, ListCollectionsRequest, ListCollectionsResponse,
+    UpdateCollection, UpdateCollectionClusterSetupRequest, UpdateCollectionClusterSetupResponse,
 };
-use collection::operations::cluster_ops;
 use storage::content_manager::conversions::error_to_status;
 use storage::dispatcher::Dispatcher;
 use tonic::{Request, Response, Status};
@@ -176,45 +173,14 @@ impl Collections for CollectionsService {
         request: Request<CollectionClusterInfoRequest>,
     ) -> Result<Response<CollectionClusterInfoResponse>, Status> {
         validate(request.get_ref())?;
-        let cluster_info = do_get_collection_cluster(
+        let response = do_get_collection_cluster(
             self.dispatcher.toc(),
             request.into_inner().collection_name.as_str(),
         )
         .await
-        .map_err(|e| Status::new(tonic::Code::Unknown, e.to_string()))?;
+        .map_err(error_to_status)?
+        .into();
 
-        let response = CollectionClusterInfoResponse {
-            peer_id: cluster_info.peer_id,
-            shard_count: cluster_info.shard_count as u64,
-            local_shards: cluster_info
-                .local_shards
-                .iter()
-                .map(|shard| LocalShardInfo {
-                    shard_id: shard.shard_id,
-                    points_count: shard.points_count as u64,
-                    state: shard.state as i32,
-                })
-                .collect(),
-            remote_shards: cluster_info
-                .remote_shards
-                .iter()
-                .map(|shard| RemoteShardInfo {
-                    shard_id: shard.shard_id,
-                    peer_id: shard.peer_id,
-                    state: shard.state as i32,
-                })
-                .collect(),
-            shard_transfers: cluster_info
-                .shard_transfers
-                .iter()
-                .map(|shard| ShardTransferInfo {
-                    shard_id: shard.shard_id,
-                    from: shard.from,
-                    to: shard.to,
-                    sync: shard.sync,
-                })
-                .collect(),
-        };
         Ok(Response::new(response))
     }
 
@@ -232,36 +198,14 @@ impl Collections for CollectionsService {
         let result = do_update_collection_cluster(
             self.dispatcher.toc(),
             collection_name,
-            match operation.ok_or(Status::new(tonic::Code::Unknown, "empty operation"))? {
-                Operation::MoveShard(op) => {
-                    cluster_ops::ClusterOperations::MoveShard(cluster_ops::MoveShardOperation {
-                        move_shard: op.into(),
-                    })
-                }
-                Operation::ReplicateShard(op) => cluster_ops::ClusterOperations::ReplicateShard(
-                    cluster_ops::ReplicateShardOperation {
-                        replicate_shard: op.into(),
-                    },
-                ),
-                Operation::AbortTransfer(op) => cluster_ops::ClusterOperations::AbortTransfer(
-                    cluster_ops::AbortTransferOperation {
-                        abort_transfer: op.into(),
-                    },
-                ),
-                Operation::DropReplica(op) => {
-                    cluster_ops::ClusterOperations::DropReplica(cluster_ops::DropReplicaOperation {
-                        drop_replica: cluster_ops::Replica {
-                            shard_id: op.shard_id,
-                            peer_id: op.peer_id,
-                        },
-                    })
-                }
-            },
+            operation
+                .ok_or(Status::new(tonic::Code::InvalidArgument, "empty operation"))?
+                .into(),
             self.dispatcher.as_ref(),
             timeout.map(std::time::Duration::from_secs),
         )
         .await
-        .map_err(|e| Status::new(tonic::Code::Unknown, e.to_string()))?;
+        .map_err(error_to_status)?;
         Ok(Response::new(UpdateCollectionClusterSetupResponse {
             result,
         }))
