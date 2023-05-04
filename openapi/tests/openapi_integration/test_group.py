d3576751c1a0296750585a8fb16f8d825ad0d4c9
Luis Cossío <luis.cossio@outlook.com>
2023-05-04 15:46:19 -0400

    tests: fix test_group.py

--- openapi/tests/openapi_integration/test_group.py
+++ openapi/tests/openapi_integration/test_group.py
@@ -5,14 +5,14 @@ from .helpers.collection_setup import basic_collection_setup, drop_collection
 
 collection_name = 'test_collection_reco'
 
-def upsert_chunked_docs(collection_name, docs=20, chunks=5):
+def upsert_chunked_docs(collection_name, docs=50, chunks=5):
     points = []
     for doc in range(docs):
         for chunk in range(chunks):
             doc_id = f"doc_{doc}"
             i = doc * chunks + chunk
             p = {"id": i, "vector": [0.5, 0.5, 0.5, 0.5], "payload": {"docId": doc_id}} 
-            points += p
+            points.append(p)
             
     response = request_with_validation(
         api='/collections/{collection_name}/points',
@@ -40,7 +40,7 @@ def test_search():
         path_params={'collection_name': collection_name},
         body={
             "search": {
-                "vector": [1.0, 0.0, 0.0, 0.0, 0.0],
+                "vector": [1.0, 0.0, 0.0, 0.0],
                 "limit": 10,
                 "with_payload": True,
             },
@@ -54,9 +54,9 @@ def test_search():
     
     assert len(groups) == 10
     for g in groups:
-        assert len(g.hits) == 3
-        for h in g.hits:
-            assert h["payload"]["docId"] == g["group_id"]
+        assert len(g["hits"]) == 3
+        for h in g["hits"]:
+            assert h["payload"]["docId"] == g["group_id"]["docId"]
             
 def test_recommend():
     response = request_with_validation(
@@ -65,7 +65,8 @@ def test_recommend():
         path_params={'collection_name': collection_name},
         body={
             "recommend": {
-                "positive": [1, 2, 3, 4, 5],
+                "positive": [10, 20, 30],
+                "negative": [4, 5, 6],
                 "limit": 10,
                 "with_payload": True,
             },
@@ -79,9 +80,9 @@ def test_recommend():
     
     assert len(groups) == 10
     for g in groups:
-        assert len(g.hits) == 3
-        for h in g.hits:
-            assert h["payload"]["docId"] == g["group_id"]
+        assert len(g["hits"]) == 3
+        for h in g["hits"]:
+            assert h["payload"]["docId"] == g["group_id"]["docId"]
             
 def test_groups_overrides_limit():
     response = request_with_validation(
@@ -90,7 +91,7 @@ def test_groups_overrides_limit():
         path_params={'collection_name': collection_name},
         body={
             "search": {
-                "vector": [1.0, 0.0, 0.0, 0.0, 0.0],
+                "vector": [1.0, 0.0, 0.0, 0.0],
                 "limit": 10,
             },
             "group_by": "docId",
@@ -104,7 +105,7 @@ def test_groups_overrides_limit():
     
     assert len(groups) == 4
     for g in groups:
-        assert len(g.hits) == 3
+        assert len(g["hits"]) == 3
 
 def test_with_vectors():
     response = request_with_validation(
@@ -113,7 +114,7 @@ def test_with_vectors():
         path_params={'collection_name': collection_name},
         body={
             "search": {
-                "vector": [1.0, 0.0, 0.0, 0.0, 0.0],
+                "vector": [1.0, 0.0, 0.0, 0.0],
                 "limit": 5,
                 "with_payload": True,
                 "with_vector": True,
@@ -128,7 +129,7 @@ def test_with_vectors():
     
     assert len(groups) == 5
     for g in groups:
-        assert len(g.hits) == 3
-        for h in g.hits:
-            assert h["payload"]["docId"] == g["group_id"]
-            assert h["vector"] == [0.5, 0.5, 0.5, 0.5, 0.5]
+        assert len(g["hits"]) == 3
+        for h in g["hits"]:
+            assert h["payload"]["docId"] == g["group_id"]["docId"]
+            assert h["vector"] == [0.5, 0.5, 0.5, 0.5]
