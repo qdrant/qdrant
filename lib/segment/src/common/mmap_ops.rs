0a777a0291fe8859d78d8f62df1925ae2d86f04c
Andrey Vasnetsov <andrey@vasnetsov.com>
2023-05-06 15:19:25 +0200

    remove unused unsafe functions

--- lib/segment/src/common/mmap_ops.rs
+++ lib/segment/src/common/mmap_ops.rs
@@ -1,8 +1,7 @@
 use std::fs::OpenOptions;
+use std::mem;
 use std::mem::size_of;
-use std::ops::DerefMut;
 use std::path::Path;
-use std::{mem, slice};
 
 use memmap2::{Mmap, MmapMut, MmapOptions};
 
@@ -43,12 +42,6 @@ pub fn open_write_mmap(path: &Path) -> OperationResult<MmapMut> {
     let mmap = unsafe { MmapMut::map_mut(&file)? };
     madvise::madvise(&mmap, madvise::get_global())?;
     Ok(mmap)
-}
-
-pub fn transmute_from_u8<T>(data: &[u8]) -> &T {
-    debug_assert!(data.len() == size_of::<T>());
-    let ptr = data.as_ptr() as *const T;
-    unsafe { &*ptr }
 }
 
 pub fn transmute_from_u8_to_mut<'a, T>(data: &mut [u8]) -> &'a mut T {
@@ -102,36 +95,4 @@ pub fn write_to_mmap<T>(mmap: &mut MmapMut, offset: usize, data: T) {
         let byte_data = transmute_to_u8(&data);
         std::ptr::copy_nonoverlapping(byte_data.as_ptr(), ptr.add(offset), byte_data.len());
     }
-}
-
-pub fn write_slice_to_mmap<T>(mmap: &mut MmapMut, offset: usize, data: &[T]) {
-    debug_assert!(offset + data.len() * size_of::<T>() <= mmap.len());
-    let ptr = mmap.as_mut_ptr();
-    unsafe {
-        let byte_data = transmute_to_u8_slice(data);
-        std::ptr::copy_nonoverlapping(byte_data.as_ptr(), ptr.add(offset), byte_data.len());
-    }
-}
-
-pub fn read_from_mmap<T>(mmap: &MmapMut, offset: usize) -> &T {
-    debug_assert!(offset + size_of::<T>() <= mmap.len());
-    let ptr = mmap.as_ptr();
-    unsafe { &*(ptr.add(offset) as *const T) }
-}
-
-pub fn read_slice_from_mmap<T>(mmap: &MmapMut, offset: usize, len: usize) -> &[T] {
-    debug_assert!(offset + len * size_of::<T>() <= mmap.len());
-    let byte_len = len * size_of::<T>();
-    let slice = &mmap[offset..offset + byte_len];
-    transmute_from_u8_to_slice(slice)
-}
-
-pub fn mmap_to_mutable_slice<T>(mmap: &mut MmapMut) -> &mut T {
-    // Obtain static slice into mmap
-    let slice: &'static mut [u8] = unsafe {
-        let slice = mmap.deref_mut();
-        slice::from_raw_parts_mut(slice.as_mut_ptr(), slice.len())
-    };
-
-    transmute_from_u8_to_mut(slice)
 }
