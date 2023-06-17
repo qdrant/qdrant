ae39860427b75476c95ac6f65ed21c91a4b9e157
Amr Hassan <amr.hassan@gmail.com>
2023-06-17 22:20:47 +0200

    Drop allow dead-code

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -13,8 +13,6 @@ pub mod telemetry_ops;
 #[allow(dead_code)] // May contain functions used in different binaries. Not actually dead
 pub mod telemetry_reporting;
 
-#[allow(dead_code)]
 pub mod auth;
 
-#[allow(dead_code)]
 pub mod strings;
