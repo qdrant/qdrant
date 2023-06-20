4509e5926abd2b78fc757cfb0dee4f35b594e57a
Andrey Vasnetsov <andrey@vasnetsov.com>
2023-06-20 13:11:48 +0200

    Download snapshot location validation (#2109)
    * check that downloaded snapshot is within snapshot location
    
    * fmt

--- lib/collection/src/collection.rs
+++ lib/collection/src/collection.rs
@@ -1369,6 +1369,27 @@ impl Collection {
 
     pub async fn get_snapshot_path(&self, snapshot_name: &str) -> CollectionResult<PathBuf> {
         let snapshot_path = self.snapshots_path.join(snapshot_name);
+
+        let absolute_snapshot_path =
+            snapshot_path
+                .canonicalize()
+                .map_err(|_| CollectionError::NotFound {
+                    what: format!("Snapshot {snapshot_name}"),
+                })?;
+
+        let absolute_snapshot_dir =
+            self.snapshots_path
+                .canonicalize()
+                .map_err(|_| CollectionError::NotFound {
+                    what: format!("Snapshot directory: {}", self.snapshots_path.display()),
+                })?;
+
+        if !absolute_snapshot_path.starts_with(absolute_snapshot_dir) {
+            return Err(CollectionError::NotFound {
+                what: format!("Snapshot {snapshot_name}"),
+            });
+        }
+
         if !snapshot_path.exists() {
             return Err(CollectionError::NotFound {
                 what: format!("Snapshot {snapshot_name}"),
