59ab0cf9db94cef93ee31403a58abdfac4d01a4c
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-06-26 10:51:10 +0200

    name scoped thread for troubleshooting

--- lib/collection/src/shards/local_shard.rs
+++ lib/collection/src/shards/local_shard.rs
@@ -808,11 +808,14 @@ impl LocalShard {
 impl Drop for LocalShard {
     fn drop(&mut self) {
         thread::scope(|s| {
-            s.spawn(|| {
-                // Needs dedicated thread to avoid `Cannot start a runtime from within a runtime` error.
-                self.update_runtime
-                    .block_on(async { self.stop_gracefully().await })
-            });
-        });
+            let handle = thread::Builder::new()
+                .name("drop-shard".to_string())
+                .spawn_scoped(s, || {
+                    // Needs dedicated thread to avoid `Cannot start a runtime from within a runtime` error.
+                    self.update_runtime
+                        .block_on(async { self.stop_gracefully().await })
+                });
+            handle.expect("Failed to create thread for shard drop");
+        })
     }
 }
