c8a7011f84d3f1d1f1603d4a339fcdb8b05c835b
timvisee <tim@visee.me>
2023-07-04 15:00:16 +0200

    Fix deadlock in collection state config update

--- lib/collection/src/collection_state.rs
+++ lib/collection/src/collection_state.rs
@@ -76,10 +76,12 @@ impl State {
             .update_optimizer_params(new_config.optimizer_config)
             .await?;
 
-        // updating replication factor
-        let mut config = collection.collection_config.write().await;
-        config.params.replication_factor = new_config.params.replication_factor;
-        config.params.write_consistency_factor = new_config.params.write_consistency_factor;
+        // Update replication factor
+        {
+            let mut config = collection.collection_config.write().await;
+            config.params.replication_factor = new_config.params.replication_factor;
+            config.params.write_consistency_factor = new_config.params.write_consistency_factor;
+        }
 
         collection.recreate_optimizers_blocking().await?;
 
