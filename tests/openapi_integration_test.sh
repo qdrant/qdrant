569546a8bf549fd756676bbb88e5a4c8f71d9660
generall <andrey@vasnetsov.com>
2023-07-14 13:38:03 +0200

    allow to run tests without docker

--- tests/openapi_integration_test.sh
+++ tests/openapi_integration_test.sh
@@ -7,8 +7,17 @@ cd "$(dirname "$0")/../"
 
 cp docs/redoc/master/openapi.json openapi/tests/openapi.json
 
-docker run --rm \
-            --network=host \
-            -e OPENAPI_FILE='openapi.json' \
-            -v "${PWD}"/openapi/tests:/code \
-            "$(docker buildx build --load -q ./openapi/tests)" sh -c /code/run_docker.sh
+WITH_DOCKER=${WITH_DOCKER:-"true"}
+
+if [ "$WITH_DOCKER" == "true" ]; then
+    docker run --rm \
+      --network=host \
+      -e OPENAPI_FILE='openapi.json' \
+      -v "${PWD}"/openapi/tests:/code \
+      "$(docker buildx build --load -q ./openapi/tests)" sh -c /code/run_docker.sh
+
+else
+  cd openapi/tests
+  pip install -r requirements-freeze.txt
+  bash -x run_docker.sh
+fi
