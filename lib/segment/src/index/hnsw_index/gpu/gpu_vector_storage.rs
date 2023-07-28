a68d26d5f90955afb67ff170fab1917d3854ccab
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-07-28 22:52:23 +0200

    fix unit test

--- lib/segment/src/index/hnsw_index/gpu/gpu_vector_storage.rs
+++ lib/segment/src/index/hnsw_index/gpu/gpu_vector_storage.rs
@@ -100,33 +100,6 @@ impl GpuVectorStorage {
             descriptor_set,
         })
     }
-
-    pub fn download_all(&self, dim: usize, count: usize) -> OperationResult<Vec<f32>> {
-        let mut result = Vec::with_capacity(self.vectors_buffer.size);
-
-        let mut download_context = gpu::Context::new(self.vectors_buffer.device.clone());
-        let staging_buffer = Arc::new(gpu::Buffer::new(
-            self.vectors_buffer.device.clone(),
-            gpu::BufferType::GpuToCpu,
-            dim * std::mem::size_of::<f32>(),
-        ));
-        let mut vector = vec![0.0; dim];
-        for i in 0..count {
-            download_context.copy_gpu_buffer(
-                self.vectors_buffer.clone(),
-                staging_buffer.clone(),
-                i * dim * std::mem::size_of::<f32>(),
-                0,
-                dim * std::mem::size_of::<f32>(),
-            );
-            download_context.run();
-            download_context.wait_finish();
-            staging_buffer.download_slice(&mut vector, 0);
-            result.extend_from_slice(&vector);
-        }
-
-        Ok(result)
-    }
 }
 
 #[cfg(test)]
@@ -143,60 +116,9 @@ mod tests {
     use crate::vector_storage::simple_vector_storage::open_simple_vector_storage;
 
     #[test]
-    fn test_gpu_vector_storage_upload() {
-        let num_vectors = 1000;
-        let dim = 256;
-        println!("Data size = {} MB", num_vectors * dim * 4 / 1024 / 1024);
-
-        let timer = std::time::Instant::now();
-        let mut rnd = StdRng::seed_from_u64(42);
-        let points = (0..num_vectors)
-            .map(|_| random_vector(&mut rnd, dim))
-            .collect::<Vec<_>>();
-        println!("Random generate time = {:?}", timer.elapsed());
-
-        let timer = std::time::Instant::now();
-        let dir2 = tempfile::Builder::new().prefix("db_dir").tempdir().unwrap();
-        let db = open_db(dir2.path(), &[DB_VECTOR_CF]).unwrap();
-        let storage2 = open_simple_vector_storage(db, DB_VECTOR_CF, dim, Distance::Dot).unwrap();
-        {
-            let mut borrowed_storage2 = storage2.borrow_mut();
-            points.iter().enumerate().for_each(|(i, vec)| {
-                borrowed_storage2
-                    .insert_vector(i as PointOffsetType, vec)
-                    .unwrap();
-            });
-        }
-        println!("Storage inserting time = {:?}", timer.elapsed());
-
-        let debug_messenger = gpu::PanicIfErrorMessenger {};
-        let instance =
-            Arc::new(gpu::Instance::new("qdrant", Some(&debug_messenger), false).unwrap());
-        let device =
-            Arc::new(gpu::Device::new(instance.clone(), instance.vk_physical_devices[0]).unwrap());
-
-        let timer = std::time::Instant::now();
-        let gpu_vector_storage = GpuVectorStorage::new(device, &storage2.borrow()).unwrap();
-        println!("Upload time = {:?}", timer.elapsed());
-
-        let downloaded = gpu_vector_storage
-            .download_all(
-                storage2.borrow().vector_dim(),
-                storage2.borrow().total_vector_count(),
-            )
-            .unwrap();
-        assert_eq!(downloaded.len(), num_vectors * dim);
-        for i in 0..num_vectors {
-            let vec = &points[i];
-            let downloaded_vec = &downloaded[i * dim..(i + 1) * dim];
-            assert_eq!(vec, downloaded_vec);
-        }
-    }
-
-    #[test]
     fn test_gpu_vector_storage_scoring() {
         let num_vectors = 1000;
-        let dim = 256;
+        let dim = 64;
 
         let mut rnd = StdRng::seed_from_u64(42);
         let points = (0..num_vectors)
@@ -253,7 +175,7 @@ mod tests {
             pipeline,
             &[descriptor_set, gpu_vector_storage.descriptor_set.clone()],
         );
-        context.dispatch(num_vectors, 0, 0);
+        context.dispatch(num_vectors, 1, 1);
         context.run();
         context.wait_finish();
 
@@ -292,7 +214,7 @@ mod tests {
 
         for i in 0..num_vectors {
             let score = DotProductMetric::similarity(&points[0], &points[i]);
-            assert_eq!(score, scores[i]);
+            assert!((score - scores[i]).abs() < 1e-5);
         }
     }
 }
