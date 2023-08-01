69ba27a72b7b07ff59e2cd453cefb2e442c7fa89
zodman <zodman@gmail.com>
2023-08-01 18:18:15 -0400

    on git-bash windows doesnt exist the /usr/local/bin fixing that

--- tests/basic_grpc_test.sh
+++ tests/basic_grpc_test.sh
@@ -8,7 +8,6 @@ cd "$(dirname "$0")/../"
 
 QDRANT_HOST='localhost:6334'
 
-OPT="-plaintext -import-path /proto -proto qdrant.proto"
 if [ "$WITH_DOCKER" == "false" ]; then
 	VER=1.8.7
 	ARCH=x86_64
@@ -16,8 +15,10 @@ if [ "$WITH_DOCKER" == "false" ]; then
 	aria2c "https://github.com/fullstorydev/grpcurl/releases/download/v${VER}/grpcurl_${VER}_windows_${ARCH}.zip"
 	7z x -y "grpcurl_${VER}_windows_${ARCH}.zip"
 	install grpcurl /usr/bin/
+	OPT="-plaintext -import-path ${PWD}/lib/api/src/grpc/proto -proto qdrant.proto"
 	docker_grpcurl="grpcurl ${OPT}"
 else
+	OPT="-plaintext -import-path /proto -proto qdrant.proto"
 	docker_grpcurl="docker run --rm --network=host -v ${PWD}/lib/api/src/grpc/proto:/proto fullstorydev/grpcurl ${OPT}"
 fi
 
