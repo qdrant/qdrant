ddfd3048bea14595de170df6d67851ebda3c6bc9
Roman Titov <ffuugoo@users.noreply.github.com>
2023-08-04 11:16:49 +0200

    fixup! fixup! Replace `env_logger` with `tracing`/`tracing-subscriber`/`tracing-log`
    `tracing_subscriber::fmt::init` initializes `tracing_log::LogTracer`
    automatically with default `tracing-subscriber` features enabled 💁‍♀️

--- src/consensus.rs
+++ src/consensus.rs
@@ -1028,7 +1028,6 @@ mod tests {
         let mut settings = crate::Settings::new(None).expect("Can't read config.");
         settings.storage.storage_path = storage_dir.path().to_str().unwrap().to_string();
         std::env::set_var("RUST_LOG", log::Level::Debug.as_str());
-        tracing_log::LogTracer::init().unwrap();
         tracing_subscriber::fmt::init();
         let search_runtime =
             crate::create_search_runtime(settings.storage.performance.max_search_threads)
