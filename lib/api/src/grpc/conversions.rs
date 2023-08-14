84911a21d7581b293c536e6d6288320ab0ae2c44
Di Zhao <diz@twitter.com>
2023-08-13 19:14:14 -0700

    add ignore_plain_index to speed up search

--- lib/api/src/grpc/conversions.rs
+++ lib/api/src/grpc/conversions.rs
@@ -368,6 +368,7 @@ impl From<SearchParams> for segment::types::SearchParams {
             hnsw_ef: params.hnsw_ef.map(|x| x as usize),
             exact: params.exact.unwrap_or(false),
             quantization: params.quantization.map(|q| q.into()),
+            ignore_plain_index: params.ignore_plain_index.unwrap_or(false),
         }
     }
 }
@@ -378,6 +379,7 @@ impl From<segment::types::SearchParams> for SearchParams {
             hnsw_ef: params.hnsw_ef.map(|x| x as u64),
             exact: Some(params.exact),
             quantization: params.quantization.map(|q| q.into()),
+            ignore_plain_index: Some(params.ignore_plain_index),
         }
     }
 }
