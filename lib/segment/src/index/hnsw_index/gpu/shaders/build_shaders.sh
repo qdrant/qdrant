5803cb522fce0d7794697d907f34ea46f2030a54
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-08-16 19:02:01 +0200

    glslc with o flag

--- lib/segment/src/index/hnsw_index/gpu/shaders/build_shaders.sh
+++ lib/segment/src/index/hnsw_index/gpu/shaders/build_shaders.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-glslc test_vector_storage.comp -o test_vector_storage.spv
-glslc test_links.comp -o test_links.spv
-glslc test_searcher.comp -o test_searcher.spv
-glslc test_greedy_searcher.comp -o test_greedy_searcher.spv
-glslc test_heuristic.comp -o test_heuristic.spv
-glslc apply_responses.comp -o apply_responses.spv
-glslc run_requests.comp -o run_requests.spv
-glslc update_entries.comp -o update_entries.spv
+glslc test_vector_storage.comp -o test_vector_storage.spv -O
+glslc test_links.comp -o test_links.spv -O
+glslc test_searcher.comp -o test_searcher.spv -O
+glslc test_greedy_searcher.comp -o test_greedy_searcher.spv -O
+glslc test_heuristic.comp -o test_heuristic.spv -O
+glslc apply_responses.comp -o apply_responses.spv -O
+glslc run_requests.comp -o run_requests.spv -O
+glslc update_entries.comp -o update_entries.spv -O
