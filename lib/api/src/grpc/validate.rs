3bd8bf71155a21f69af0af01e5a183e1216ca917
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-08-21 13:51:39 +0200

    Fix lints for Clippy 1.72

--- lib/api/src/grpc/validate.rs
+++ lib/api/src/grpc/validate.rs
@@ -42,6 +42,7 @@ where
     V: Validate,
 {
     #[inline]
+    #[allow(clippy::manual_try_fold)] // `try_fold` can't be used because it shortcuts on Err
     fn validate(&self) -> Result<(), ValidationErrors> {
         let errors = self
             .iter()
@@ -59,6 +60,7 @@ where
     V: Validate,
 {
     #[inline]
+    #[allow(clippy::manual_try_fold)] // `try_fold` can't be used because it shortcuts on Err
     fn validate(&self) -> Result<(), ValidationErrors> {
         let errors = self
             .values()
