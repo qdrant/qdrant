dd917c180f2a74ba98e78cc5417ddabccb64041e
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-08-30 10:57:09 +0200

    Support consensus WAL format in wal_inspector (#2244)
    * Support consensus WAL format in wal_inspector
    
    * add basic raft_state printing
    
    * deserialize consensus operations
    
    * fix display last element & improve rendering
    
    * remove obsolete visibility change
    
    ---------
    
    Co-authored-by: generall <andrey@vasnetsov.com>

--- src/wal_inspector.rs
+++ src/wal_inspector.rs
@@ -3,13 +3,56 @@ use std::path::Path;
 
 use collection::operations::CollectionUpdateOperations;
 use collection::wal::SerdeWal;
+use storage::content_manager::consensus::consensus_wal::ConsensusOpWal;
+use storage::content_manager::consensus_ops::ConsensusOperations;
 use wal::WalOptions;
 
-/// Executable to inspect the content of a write ahead log folder.
-/// e.g `cargo run --bin wal_inspector storage/collections/test-collection/0/wal/`
+/// Executable to inspect the content of a write ahead log folder (collection OR consensus WAL).
+/// e.g:
+/// `cargo run --bin wal_inspector storage/collections/test-collection/0/wal/ collection`
+/// `cargo run --bin wal_inspector -- storage/node4/wal/ consensus` (expects `collections_meta_wal` folder as first child)
 fn main() {
     let args: Vec<String> = env::args().collect();
     let wal_path = Path::new(&args[1]);
+    let wal_type = args[2].as_str();
+    match wal_type {
+        "collection" => print_collection_wal(wal_path),
+        "consensus" => print_consensus_wal(wal_path),
+        _ => eprintln!("Unknown wal type: {}", wal_type),
+    }
+}
+
+fn print_consensus_wal(wal_path: &Path) {
+    // must live within a folder named `collections_meta_wal`
+    let wal = ConsensusOpWal::new(wal_path.to_str().unwrap());
+    println!("==========================");
+    let first_index = wal.first_entry().unwrap();
+    println!("First entry: {:?}", first_index);
+    let last_index = wal.last_entry().unwrap();
+    println!("Last entry: {:?}", last_index);
+    println!("Offset of first entry: {:?}", wal.index_offset().unwrap());
+    let entries = wal
+        .entries(
+            first_index.map(|f| f.index).unwrap_or(1),
+            last_index.map(|f| f.index).unwrap_or(0) + 1,
+            None,
+        )
+        .unwrap();
+    for entry in entries {
+        println!("==========================");
+        let command = ConsensusOperations::try_from(&entry);
+        let data = match command {
+            Ok(command) => format!("{:?}", command),
+            Err(_) => format!("{:?}", entry.data),
+        };
+        println!(
+            "Entry ID:{}\nterm:{}\nentry_type:{}\ndata:{:?}",
+            entry.index, entry.term, entry.entry_type, data
+        )
+    }
+}
+
+fn print_collection_wal(wal_path: &Path) {
     let wal: Result<SerdeWal<CollectionUpdateOperations>, _> =
         SerdeWal::new(wal_path.to_str().unwrap(), WalOptions::default());
 
