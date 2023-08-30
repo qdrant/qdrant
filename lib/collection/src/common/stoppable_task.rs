c8d2d33d5de92dbf60fa0f50e8cd8b6c39313f2a
Tim Visée <tim+github@visee.me>
2023-08-30 10:13:17 +0200

    Catch optimizer panics (#2485)
    * Add panic handler to stoppable task
    
    * Define panic handler for optimizer task, report panic to segment holder
    
    * Report panic payload message when optimizer panics
    
    * Add function to handle and clean finished optimizer tasks
    
    * Rewrite optimizer worker, periodically clean finished optimizer tasks
    
    * Minor improvements and codespell fixes
    
    * Add simple test to ensure panic callback is called
    
    * Explicitly handle elapsed timeout errors in optimizer handle handling

--- lib/collection/src/common/stoppable_task.rs
+++ lib/collection/src/common/stoppable_task.rs
@@ -1,12 +1,16 @@
+use std::any::Any;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Weak};
 
 use tokio::task::JoinHandle;
 
+type PanicPayload = Box<dyn Any + Send + 'static>;
+
 pub struct StoppableTaskHandle<T> {
     pub join_handle: JoinHandle<Option<T>>,
     started: Arc<AtomicBool>,
     stopped: Weak<AtomicBool>,
+    panic_handler: Option<Box<dyn Fn(PanicPayload) + Sync + Send>>,
 }
 
 impl<T> StoppableTaskHandle<T> {
@@ -28,9 +32,48 @@ impl<T> StoppableTaskHandle<T> {
         self.ask_to_stop();
         self.is_started().then_some(self.join_handle)
     }
+
+    /// Join this stoppable task and handle any panics
+    ///
+    /// Any panics are propagated through the configured panic handler. If no handler is
+    /// configured, nothing happens.
+    ///
+    /// To call this, the task must already be finished. Otherwise it panics in development, or
+    /// blocks in release.
+    pub async fn join_and_handle_panic(self) {
+        debug_assert!(
+            self.join_handle.is_finished(),
+            "Task must be finished, we cannot block here on awaiting the join handle",
+        );
+
+        match self.join_handle.await {
+            Ok(_) => {}
+            Err(err) if err.is_cancelled() => {}
+            // Propagate panic
+            Err(err) if err.is_panic() && self.panic_handler.is_some() => {
+                log::trace!("Handling stoppable task panic through custom panic handler");
+                let panic = err.into_panic();
+                let panic_handler = self.panic_handler.unwrap();
+                panic_handler(panic);
+            }
+            Err(err) if err.is_panic() => {
+                log::debug!("Stoppable task panicked without panic handler");
+            }
+            // Log error on unknown error
+            Err(err) => {
+                log::error!("Stoppable task handle error for unknown reason: {err}");
+            }
+        }
+    }
 }
 
-pub fn spawn_stoppable<F, T>(f: F) -> StoppableTaskHandle<T>
+/// Spawn stoppable task `f`
+///
+/// An optional `panic_handler` may be given, eventually called if the task panicked.
+pub fn spawn_stoppable<F, T>(
+    f: F,
+    panic_handler: Option<Box<dyn Fn(PanicPayload) + Sync + Send>>,
+) -> StoppableTaskHandle<T>
 where
     F: FnOnce(&AtomicBool) -> T + Send + 'static,
     T: Send + 'static,
@@ -57,11 +100,25 @@ where
         }),
         started: started_c,
         stopped: stopped_w,
+        panic_handler,
     }
+}
+
+/// Convert a panic payload into a string
+///
+/// This converts `String` and `&str` panic payloads into a string.
+/// Other payload types are formatted as is, and may be non descriptive.
+pub(crate) fn panic_payload_into_string(payload: PanicPayload) -> String {
+    payload
+        .downcast::<&str>()
+        .map(|msg| msg.to_string())
+        .or_else(|payload| payload.downcast::<String>().map(|msg| msg.to_string()))
+        .unwrap_or_else(|payload| format!("{payload:?}"))
 }
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Mutex;
     use std::thread;
     use std::time::{Duration, Instant};
 
@@ -91,7 +148,7 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn test_task_stop() {
-        let handle = spawn_stoppable(counting_task);
+        let handle = spawn_stoppable(counting_task, None);
 
         // Signal task to stop after ~20 steps
         sleep(STEP * 20).await;
@@ -117,7 +174,7 @@ mod tests {
         const TASKS: usize = 64;
 
         let handles = (0..TASKS)
-            .map(|_| spawn_stoppable(counting_task))
+            .map(|_| spawn_stoppable(counting_task, None))
             .collect::<Vec<_>>();
 
         // Signal tasks to stop after ~20 steps
@@ -138,5 +195,32 @@ mod tests {
                 }
             }
         }
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_task_panic() {
+        let panic_payload = Arc::new(Mutex::new(String::new()));
+        let handle = spawn_stoppable(
+            |_| {
+                thread::sleep(STEP * 50);
+                panic!("stoppable task panicked");
+            },
+            Some(Box::new({
+                let panic_payload = panic_payload.clone();
+                move |payload| {
+                    *panic_payload.lock().unwrap() = panic_payload_into_string(payload);
+                }
+            })),
+        );
+
+        sleep(STEP * 20).await;
+        assert!(!handle.is_finished());
+        sleep(STEP * 100).await;
+        assert!(handle.is_finished());
+
+        // Join handle to call back panic
+        handle.join_and_handle_panic().await;
+
+        assert_eq!(*panic_payload.lock().unwrap(), "stoppable task panicked");
     }
 }
