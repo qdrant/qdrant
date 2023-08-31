93cdd5a36013e0db2d73ebca86aed3a9d56845f7
Roman Titov <ffuugoo@users.noreply.github.com>
2023-08-31 15:50:21 +0200

    Add `lib/common/download-file`

---
+++ lib/common/download-file/src/lib.rs
@@ -0,0 +1,58 @@
+use std::path::{Path, PathBuf};
+use std::{io, result};
+
+use futures::TryStreamExt as _;
+use tokio::io::AsyncWriteExt as _;
+use url::Url;
+
+#[derive(Debug)]
+pub struct DownloadedFile {
+    pub file: async_tempfile::NamedTempFile,
+    pub url: Url,
+    pub file_name: Option<String>,
+}
+
+pub async fn download_file(url: Url, dir: impl Into<PathBuf>) -> Result<DownloadedFile> {
+    let response = reqwest::get(url).await?;
+
+    response.error_for_status_ref()?;
+
+    let mut file = async_tempfile::NamedTempFile::new_in(dir).await?;
+
+    let url = response.url().clone();
+
+    // TODO: Parse file name from `Content-Disposition` header!?
+    let file_name = Path::new(url.path())
+        .file_name()
+        .and_then(|str| str.to_str())
+        .map(|str| str.to_string());
+
+    {
+        let mut bytes_stream = response.bytes_stream();
+        let mut writer = tokio::io::BufWriter::new(file.as_file_mut());
+
+        while let Some(bytes) = bytes_stream.try_next().await? {
+            writer.write_all(&bytes).await?;
+        }
+
+        writer.shutdown().await?;
+    }
+
+    let downloaded_file = DownloadedFile {
+        file,
+        url,
+        file_name,
+    };
+
+    Ok(downloaded_file)
+}
+
+pub type Result<T, E = Error> = result::Result<T, E>;
+
+#[derive(Debug, thiserror::Error)]
+#[error("{0}")]
+pub enum Error {
+    Io(#[from] io::Error),
+    Reqwest(#[from] reqwest::Error),
+    AsyncTempfile(#[from] async_tempfile::Error),
+}
