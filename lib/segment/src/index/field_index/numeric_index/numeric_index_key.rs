e74dc4a29ffa03be169511f72170f0651e044633
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-09-20 19:55:43 +0200

    test inf float

--- lib/segment/src/index/field_index/numeric_index/numeric_index_key.rs
+++ lib/segment/src/index/field_index/numeric_index/numeric_index_key.rs
@@ -30,7 +30,7 @@ impl<T: PartialEq + PartialOrd> PartialOrd for NumericIndexKey<T> {
             None => {}
             ord => return ord,
         }
-        self.idx.partial_cmp(&other.idx)
+        Some(self.idx.cmp(&other.idx))
     }
 }
 
@@ -38,12 +38,7 @@ impl<T: PartialEq + PartialOrd> Eq for NumericIndexKey<T> {}
 
 impl<T: PartialEq + PartialOrd> Ord for NumericIndexKey<T> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match self.key.partial_cmp(&other.key) {
-            Some(core::cmp::Ordering::Equal) => {}
-            None => {}
-            ord => return ord.unwrap(),
-        }
-        self.idx.cmp(&other.idx)
+        self.partial_cmp(other).unwrap()
     }
 }
 
@@ -60,17 +55,19 @@ mod tests {
         let pairs = [
             NumericIndexKey::new(0.0, 1),
             NumericIndexKey::new(0.0, 3),
-            //NumericIndexKey::new(-0.0, 2),
-            //NumericIndexKey::new(-0.0, 4),
+            NumericIndexKey::new(-0.0, 2),
+            NumericIndexKey::new(-0.0, 4),
             NumericIndexKey::new(0.4, 2),
             NumericIndexKey::new(-0.4, 3),
             NumericIndexKey::new(5.0, 1),
             NumericIndexKey::new(-5.0, 1),
             //NumericIndexKey::new(f64::NAN, 0),
-            //NumericIndexKey::new(f64::INFINITY, 0),
-            //NumericIndexKey::new(f64::NEG_INFINITY, 1),
-            //NumericIndexKey::new(f64::NEG_INFINITY, 2),
-            //NumericIndexKey::new(f64::NEG_INFINITY, 3),
+            //NumericIndexKey::new(f64::NAN, 2),
+            //NumericIndexKey::new(f64::NAN, 3),
+            NumericIndexKey::new(f64::INFINITY, 0),
+            NumericIndexKey::new(f64::NEG_INFINITY, 1),
+            NumericIndexKey::new(f64::NEG_INFINITY, 2),
+            NumericIndexKey::new(f64::NEG_INFINITY, 3),
         ];
 
         let encoded: Vec<Vec<u8>> = pairs
@@ -84,7 +81,7 @@ mod tests {
 
         for (b, k) in set_byte.iter().zip(set_keys.iter()) {
             let (decoded_id, decoded_float) = FloatPayloadType::decode_key(b.as_slice());
-            //println!("{}:{} \t\t {}:{}", k.key, k.idx, decoded_float, decoded_id);
+            println!("{}:{} \t\t {}:{}", k.key, k.idx, decoded_float, decoded_id);
             assert_eq!(decoded_id, k.idx);
             assert_eq!(decoded_float, k.key);
         }
