a3ca2a99236326bfb2639af617cd6757de547ec1
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-09-25 17:25:25 +0200

    build shaders for vulkan v1.3

--- lib/segment/src/index/hnsw_index/gpu/shaders/build_shaders.sh
+++ lib/segment/src/index/hnsw_index/gpu/shaders/build_shaders.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-glslc test_vector_storage.comp -o test_vector_storage.spv -O
-glslc test_links.comp -o test_links.spv -O
-glslc test_searcher.comp -o test_searcher.spv -O
-glslc test_greedy_searcher.comp -o test_greedy_searcher.spv -O
-glslc test_heuristic.comp -o test_heuristic.spv -O
-glslc run_requests.comp -o run_requests.spv -O
-glslc update_entries.comp -o update_entries.spv -O
-glslc builder_profile_helper.comp -o builder_profile_helper.spv -O
+glslc test_vector_storage.comp -o test_vector_storage.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc test_links.comp -o test_links.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc test_searcher.comp -o test_searcher.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc test_greedy_searcher.comp -o test_greedy_searcher.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc test_heuristic.comp -o test_heuristic.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc run_requests.comp -o run_requests.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc update_entries.comp -o update_entries.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc builder_profile_helper.comp -o builder_profile_helper.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
