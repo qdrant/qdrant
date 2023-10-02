8bf0d48bbfa0cf72ef5e8cee5ec9fb7a754976e6
generall <andrey@vasnetsov.com>
2023-10-02 14:37:44 +0200

    remove outdated comment

--- lib/collection/src/shards/remote_shard.rs
+++ lib/collection/src/shards/remote_shard.rs
@@ -154,7 +154,6 @@ impl RemoteShard {
         wait: bool,
         ordering: WriteOrdering,
     ) -> CollectionResult<UpdateResult> {
-        // the target shard is None because the operation is forwarded as if it came from the client
         self.execute_update_operation(
             Some(self.id),
             self.collection_id.clone(),
