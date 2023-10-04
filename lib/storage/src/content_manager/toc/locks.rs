a6a86819fbdab71288afdd8caa567e67c1060fce
Roman Titov <ffuugoo@users.noreply.github.com>
2023-10-04 16:16:09 +0200

    Add explicit imports to `toc/locks.rs`

--- lib/storage/src/content_manager/toc/locks.rs
+++ lib/storage/src/content_manager/toc/locks.rs
@@ -1,10 +1,13 @@
-use super::*;
+use std::sync::atomic;
+
+use super::TableOfContent;
+use crate::content_manager::errors::StorageError;
 
 pub const DEFAULT_WRITE_LOCK_ERROR_MESSAGE: &str = "Write operations are forbidden";
 
 impl TableOfContent {
     pub fn is_write_locked(&self) -> bool {
-        self.is_write_locked.load(Ordering::Relaxed)
+        self.is_write_locked.load(atomic::Ordering::Relaxed)
     }
 
     pub fn get_lock_error_message(&self) -> Option<String> {
@@ -13,7 +16,7 @@ impl TableOfContent {
 
     /// Returns an error if the write lock is set
     pub fn check_write_lock(&self) -> Result<(), StorageError> {
-        if self.is_write_locked.load(Ordering::Relaxed) {
+        if self.is_write_locked.load(atomic::Ordering::Relaxed) {
             return Err(StorageError::Locked {
                 description: self
                     .lock_error_message
@@ -27,7 +30,7 @@ impl TableOfContent {
 
     pub fn set_locks(&self, is_write_locked: bool, error_message: Option<String>) {
         self.is_write_locked
-            .store(is_write_locked, Ordering::Relaxed);
+            .store(is_write_locked, atomic::Ordering::Relaxed);
         *self.lock_error_message.lock() = error_message;
     }
 }
