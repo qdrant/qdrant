27be199f5aa9abf782f9de7be1e86c47fd7c6541
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-10-06 12:27:30 +0200

    Allow JsonPath for PayloadSelector (#2756)
    * Allow JsonPath for PayloadSelector
    
    * code review
    
    * mention Entry API in performance note
    
    * pre-process exclude list to avoid incorrect side-effects
    
    * remove support for array paths
    
    * review refactoring
    
    * add test for implicit/explicit array notation
    
    * [skip ci] panic -> unreachable
    
    ---------
    
    Co-authored-by: generall <andrey@vasnetsov.com>

--- lib/segment/src/common/utils.rs
+++ lib/segment/src/common/utils.rs
@@ -150,16 +150,16 @@ fn parse_array_path(path: &str) -> Option<(&str, Option<u32>)> {
 fn focus_array_path<'a>(
     array_path: &str,
     array_index: Option<u32>,
-    rest_path: Option<&str>,
-    value: &'a serde_json::Map<String, Value>,
+    rest_of_path: Option<&str>,
+    json_map: &'a serde_json::Map<String, Value>,
 ) -> MultiValue<&'a Value> {
-    match value.get(array_path) {
+    match json_map.get(array_path) {
         Some(Value::Array(array)) => {
             let mut values: MultiValue<_> = MultiValue::default();
             for (i, value) in array.iter().enumerate() {
                 if let Some(array_index) = array_index {
                     if i == array_index as usize {
-                        match rest_path {
+                        match rest_of_path {
                             Some(rest_path) => {
                                 // expect an Object if there is a rest path
                                 if let Value::Object(map) = value {
@@ -170,7 +170,7 @@ fn focus_array_path<'a>(
                         }
                     }
                 } else {
-                    match rest_path {
+                    match rest_of_path {
                         Some(rest_path) => {
                             // expect an Object if there is a rest path
                             if let Value::Object(map) = value {
@@ -201,9 +201,10 @@ fn focus_array_path<'a>(
 /// path: "arr[].a"   => Vec![Value::Array[ 1, 2, 3], 4]
 /// path: "arr[].a[]" => Vec![ 1, 2, 3, 4]
 ///
+/// performance: the function could be improved by using the Entry API instead of BTreeMap.get
 pub fn get_value_from_json_map<'a>(
     path: &str,
-    value: &'a serde_json::Map<String, Value>,
+    json_map: &'a serde_json::Map<String, Value>,
 ) -> MultiValue<&'a Value> {
     // check if leaf path element
     match path.split_once('.') {
@@ -211,11 +212,11 @@ pub fn get_value_from_json_map<'a>(
             // check if targeting array
             match parse_array_path(element) {
                 Some((array_element_path, array_index)) => {
-                    focus_array_path(array_element_path, array_index, Some(rest_path), value)
+                    focus_array_path(array_element_path, array_index, Some(rest_path), json_map)
                 }
                 None => {
                     // no array notation
-                    match value.get(element) {
+                    match json_map.get(element) {
                         Some(Value::Object(map)) => get_value_from_json_map(rest_path, map),
                         Some(value) => match rest_path.is_empty() {
                             true => MultiValue::one(value),
@@ -228,9 +229,9 @@ pub fn get_value_from_json_map<'a>(
         }
         None => match parse_array_path(path) {
             Some((array_element_path, array_index)) => {
-                focus_array_path(array_element_path, array_index, None, value)
+                focus_array_path(array_element_path, array_index, None, json_map)
             }
-            None => match value.get(path) {
+            None => match json_map.get(path) {
                 Some(value) => MultiValue::one(value),
                 None => MultiValue::default(),
             },
@@ -244,11 +245,11 @@ pub fn get_value_from_json_map<'a>(
 fn delete_array_path(
     array_path: &str,
     array_index: Option<u32>,
-    rest_path: Option<&str>,
-    value: &mut serde_json::Map<String, Value>,
+    rest_of_path: Option<&str>,
+    json_map: &mut serde_json::Map<String, Value>,
 ) -> MultiValue<Value> {
-    if let Some(Value::Array(array)) = value.get_mut(array_path) {
-        match rest_path {
+    if let Some(Value::Array(array)) = json_map.get_mut(array_path) {
+        match rest_of_path {
             None => {
                 // end of path - delete and collect
                 if let Some(array_index) = array_index {
@@ -281,9 +282,12 @@ fn delete_array_path(
     MultiValue::default()
 }
 
+/// Remove value at a given JSON path from JSON map
+///
+/// performance: the function could be improved by using the Entry API instead of BTreeMap.get_mut
 pub fn remove_value_from_json_map(
     path: &str,
-    value: &mut serde_json::Map<String, Value>,
+    json_map: &mut serde_json::Map<String, Value>,
 ) -> MultiValue<Value> {
     // check if leaf path element
     match path.split_once('.') {
@@ -291,14 +295,14 @@ pub fn remove_value_from_json_map(
             // check if targeting array
             match parse_array_path(element) {
                 Some((array_element_path, array_index)) => {
-                    delete_array_path(array_element_path, array_index, Some(rest_path), value)
+                    delete_array_path(array_element_path, array_index, Some(rest_path), json_map)
                 }
                 None => {
                     // no array notation
                     if rest_path.is_empty() {
-                        MultiValue::option(value.remove(element))
+                        MultiValue::option(json_map.remove(element))
                     } else {
-                        match value.get_mut(element) {
+                        match json_map.get_mut(element) {
                             None => MultiValue::default(),
                             Some(Value::Object(map)) => remove_value_from_json_map(rest_path, map),
                             Some(_value) => MultiValue::default(),
@@ -309,10 +313,115 @@ pub fn remove_value_from_json_map(
         }
         None => match parse_array_path(path) {
             Some((array_element_path, array_index)) => {
-                delete_array_path(array_element_path, array_index, None, value)
+                delete_array_path(array_element_path, array_index, None, json_map)
             }
-            None => MultiValue::option(value.remove(path)),
+            None => MultiValue::option(json_map.remove(path)),
         },
+    }
+}
+
+/// Check if a path is included in a list of patterns
+///
+/// Basically, it checks if either the pattern or path is a prefix of the other.
+/// Examples:
+/// ```
+/// assert!(segment::common::utils::check_include_pattern("a.b.c", "a.b.c"));
+/// assert!(segment::common::utils::check_include_pattern("a.b.c", "a.b"));
+/// assert!(!segment::common::utils::check_include_pattern("a.b.c", "a.b.d"));
+/// assert!(segment::common::utils::check_include_pattern("a.b.c", "a"));
+/// assert!(segment::common::utils::check_include_pattern("a", "a.d"));
+/// ```
+pub fn check_include_pattern(pattern: &str, path: &str) -> bool {
+    pattern
+        .split(['.', '['])
+        .zip(path.split(['.', '[']))
+        .all(|(p, v)| p == v)
+}
+
+/// Check if a path should be excluded by a pattern
+///
+/// Basically, it checks if pattern is a prefix of path, but not the other way around.
+///
+/// ```
+/// assert!(segment::common::utils::check_exclude_pattern("a.b.c", "a.b.c"));
+/// assert!(!segment::common::utils::check_exclude_pattern("a.b.c", "a.b"));
+/// assert!(!segment::common::utils::check_exclude_pattern("a.b.c", "a.b.d"));
+/// assert!(!segment::common::utils::check_exclude_pattern("a.b.c", "a"));
+/// assert!(segment::common::utils::check_exclude_pattern("a", "a.d"));
+/// ```
+
+pub fn check_exclude_pattern(pattern: &str, path: &str) -> bool {
+    if pattern.len() > path.len() {
+        return false;
+    }
+    pattern
+        .split(['.', '['])
+        .zip(path.split(['.', '[']))
+        .all(|(p, v)| p == v)
+}
+
+fn _filter_json_values<'a>(
+    mut path: String,
+    value: &'a Value,
+    filter: &dyn Fn(&str, &Value) -> bool,
+) -> (String, Value) {
+    let value = match &value {
+        Value::Null => value.clone(),
+        Value::Bool(_) => value.clone(),
+        Value::Number(_) => value.clone(),
+        Value::String(_) => value.clone(),
+        Value::Array(array) => {
+            let mut new_array = Vec::new();
+            path.push_str("[]");
+            for value in array.iter() {
+                if filter(&path, value) {
+                    let (path_, value) = _filter_json_values(path, value, filter);
+                    path = path_;
+                    new_array.push(value);
+                }
+            }
+            path.truncate(path.len() - 2);
+            Value::Array(new_array)
+        }
+        Value::Object(object) => {
+            let mut new_object = serde_json::Map::new();
+            for (key, value) in object.iter() {
+                if !path.is_empty() {
+                    path.push('.');
+                }
+                path.push_str(key);
+                if filter(&path, value) {
+                    let (path_, value) = _filter_json_values(path, value, filter);
+                    path = path_;
+                    new_object.insert(key.clone(), value);
+                }
+                path.truncate(path.len() - key.len());
+                if !path.is_empty() {
+                    path.pop();
+                }
+            }
+            Value::Object(new_object)
+        }
+    };
+    (path, value)
+}
+
+/// Filter json map based on external filter function
+///
+/// Filter function takes path and value as input and returns true if the value should be kept
+pub fn filter_json_values(
+    json_map: &serde_json::Map<String, Value>,
+    filter: impl Fn(&str, &Value) -> bool,
+) -> serde_json::Map<String, Value> {
+    let path = "".to_string();
+    let (_, res) = _filter_json_values(path, &Value::Object(json_map.clone()), &filter);
+
+    if let Value::Object(map) = res {
+        map
+    } else {
+        // This should never happen, because _filter_json_values always returns same
+        // type as input
+        unreachable!("Unexpected value type")
     }
 }
 
@@ -624,6 +733,48 @@ mod tests {
                 &Value::Number(2.into()),
                 &Value::Number(3.into()),
             ]
+        );
+    }
+
+    #[test]
+    fn test_filter_json() {
+        let map = serde_json::from_str::<serde_json::Map<String, Value>>(
+            r#"
+            {
+                "a": {
+                    "b": [
+                        { "c": 1 },
+                        { "c": 2 },
+                        { "d": { "e": 3 } }
+                    ]
+                },
+                "f": 3,
+                "g": ["g0", "g1", "g2"]
+            }
+            "#,
+        )
+        .unwrap();
+
+        let res = filter_json_values(&map, |path, _value| {
+            path.starts_with("a.b[].c") || "a.b[].c".starts_with(path)
+        });
+
+        assert_eq!(
+            res,
+            serde_json::from_str::<serde_json::Map<String, Value>>(
+                r#"
+                {
+                    "a": {
+                        "b": [
+                            { "c": 1 },
+                            { "c": 2 },
+                            {}
+                        ]
+                    }
+                }
+                "#,
+            )
+            .unwrap()
         );
     }
 }
