b5987e9acb1c3b283760d692213ac45c6c1cdb96
timvisee <tim@visee.me>
2023-10-10 09:31:56 +0200

    Run OpenAPI integration test in interactive mode if TTY
    If currently in a TTY, run the OpenAPI integration test Docker image
    with -it flags.
    
    This allows users to abort the test with SIGINT and shows fancy output
    colors.

--- tests/openapi_integration_test.sh
+++ tests/openapi_integration_test.sh
@@ -9,8 +9,10 @@ cp docs/redoc/master/openapi.json openapi/tests/openapi.json
 
 docker buildx build --load -q ./openapi/tests --tag=qdrant_openapi_test
 
-docker run --rm \
-            --network=host \
-            -e OPENAPI_FILE='openapi.json' \
-            -v "${PWD}"/openapi/tests:/code \
-            qdrant_openapi_test sh -c /code/run_docker.sh
+DOCKER_ARGS=$([ -t 0 ] && echo "-it" || echo "")
+docker run $DOCKER_ARGS \
+    --rm \
+    --network=host \
+    -e OPENAPI_FILE='openapi.json' \
+    -v "${PWD}"/openapi/tests:/code \
+    qdrant_openapi_test sh -c /code/run_docker.sh
