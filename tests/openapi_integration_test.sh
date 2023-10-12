4f6ad916e0a0728fe3470586cbb9c57b7f2ea461
Tim Visée <tim+github@visee.me>
2023-10-12 15:45:09 +0200

    Tag OpenAPI integration test image, run in interactive mode (#2784)
    * Tag OpenAPI test image to add to Docker engine images on some platforms
    
    * Run OpenAPI integration test in interactive mode if TTY
    
    If currently in a TTY, run the OpenAPI integration test Docker image
    with -it flags.
    
    This allows users to abort the test with SIGINT and shows fancy output
    colors.

--- tests/openapi_integration_test.sh
+++ tests/openapi_integration_test.sh
@@ -7,8 +7,12 @@ cd "$(dirname "$0")/../"
 
 cp docs/redoc/master/openapi.json openapi/tests/openapi.json
 
-docker run --rm \
-            --network=host \
-            -e OPENAPI_FILE='openapi.json' \
-            -v "${PWD}"/openapi/tests:/code \
-            "$(docker buildx build --load -q ./openapi/tests)" sh -c /code/run_docker.sh
+docker buildx build --load -q ./openapi/tests --tag=qdrant_openapi_test
+
+DOCKER_ARGS=$([ -t 0 ] && echo "-it" || echo "")
+docker run $DOCKER_ARGS \
+    --rm \
+    --network=host \
+    -e OPENAPI_FILE='openapi.json' \
+    -v "${PWD}"/openapi/tests:/code \
+    qdrant_openapi_test sh -c /code/run_docker.sh
