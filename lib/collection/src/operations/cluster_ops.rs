c1949308bc7dbf5feb1fd9906381bb93f40baf56
timvisee <tim@visee.me>
2023-10-13 15:25:08 +0200

    Validate move shard, source and target address must not be the same

--- lib/collection/src/operations/cluster_ops.rs
+++ lib/collection/src/operations/cluster_ops.rs
@@ -1,6 +1,8 @@
+use std::borrow::Cow;
+
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use validator::Validate;
+use validator::{Validate, ValidationError, ValidationErrors};
 
 use crate::shards::shard::{PeerId, ShardId};
 
@@ -56,12 +58,31 @@ pub struct AbortTransferOperation {
     pub abort_transfer: MoveShard,
 }
 
-#[derive(Debug, Deserialize, Serialize, JsonSchema, Validate, Clone)]
+#[derive(Debug, Deserialize, Serialize, JsonSchema, Clone)]
 #[serde(rename_all = "snake_case")]
 pub struct MoveShard {
     pub shard_id: ShardId,
     pub to_peer_id: PeerId,
     pub from_peer_id: PeerId,
+}
+
+impl Validate for MoveShard {
+    fn validate(&self) -> Result<(), ValidationErrors> {
+        // Custom struct validator: source and target peer may not be the same
+        if self.to_peer_id == self.from_peer_id {
+            let mut errors = ValidationErrors::new();
+            errors.add("to_peer_id", {
+                let mut error = ValidationError::new("must_not_match");
+                error.add_param(Cow::from("value"), &self.to_peer_id.to_string());
+                error.add_param(Cow::from("other_field"), &"from_peer_id");
+                error.add_param(Cow::from("other_value"), &self.from_peer_id.to_string());
+                error
+            });
+            return Err(errors);
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize, JsonSchema, Validate, Clone)]
