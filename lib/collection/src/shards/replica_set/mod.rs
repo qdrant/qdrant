6dfb2ba76fac775c14bcfe23a3977a6b48152a2c
Roman Titov <ffuugoo@users.noreply.github.com>
2023-10-13 16:40:49 +0200

    Reorder items in `replica_set/mod.rs` (18/19)

--- lib/collection/src/shards/replica_set/mod.rs
+++ lib/collection/src/shards/replica_set/mod.rs
@@ -49,8 +49,6 @@ use crate::shards::shard_config::ShardConfig;
 use crate::shards::shard_trait::ShardOperation;
 use crate::shards::telemetry::ReplicaSetTelemetry;
 
-pub type ChangePeerState = Arc<dyn Fn(PeerId, ShardId) + Send + Sync>;
-
 const REPLICA_STATE_FILE: &str = "replica_state.json";
 
 //    │    Collection Created
@@ -115,6 +113,8 @@ pub struct ShardReplicaSet {
     /// Lock to serialized write operations on the replicaset when a write ordering is used.
     write_ordering_lock: Mutex<()>,
 }
+
+pub type ChangePeerState = Arc<dyn Fn(PeerId, ShardId) + Send + Sync>;
 
 impl ShardReplicaSet {
     /// Create a new fresh replica set, no previous state is expected.
