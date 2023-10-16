630769d7bb4c8adcbf59ef28b85bf71b8d75ccbd
timvisee <tim@visee.me>
2023-10-16 15:20:27 +0200

    During shard snapshot transfer, use REST port from config

--- lib/collection/src/shards/channel_service.rs
+++ lib/collection/src/shards/channel_service.rs
@@ -2,25 +2,29 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use api::grpc::transport_channel_pool::TransportChannelPool;
+use http::uri::Scheme;
 use tonic::transport::Uri;
+use url::Url;
 
+use crate::operations::types::{CollectionError, CollectionResult};
 use crate::shards::shard::PeerId;
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct ChannelService {
     // Shared with consensus_state
     pub id_to_address: Arc<parking_lot::RwLock<HashMap<PeerId, Uri>>>,
     pub channel_pool: Arc<TransportChannelPool>,
+    /// Port at which the public REST API is exposed.
+    pub rest_port: u16,
 }
 
 impl ChannelService {
-    pub fn new(
-        id_to_address: Arc<parking_lot::RwLock<HashMap<PeerId, Uri>>>,
-        channel_pool: Arc<TransportChannelPool>,
-    ) -> Self {
+    /// Construct a new channel service with the given REST port.
+    pub fn new(rest_port: u16) -> Self {
         Self {
-            id_to_address,
-            channel_pool,
+            id_to_address: Default::default(),
+            channel_pool: Default::default(),
+            rest_port,
         }
     }
 
@@ -28,6 +32,42 @@ impl ChannelService {
         let removed = self.id_to_address.write().remove(&peer_id);
         if let Some(uri) = removed {
             self.channel_pool.drop_pool(&uri).await;
+        }
+    }
+
+    /// Get the REST address for a given peer.
+    pub fn rest_address(&self, peer_id: PeerId) -> CollectionResult<Url> {
+        let local_peer_uri = self
+            .id_to_address
+            .read()
+            .get(&peer_id)
+            .cloned()
+            .ok_or_else(|| {
+                CollectionError::service_error(format!(
+                    "Cannot determine REST address, peer {peer_id} not found in cluster",
+                ))
+            })?;
+        Ok(Url::parse(&format!(
+            "{}://{}:{}",
+            local_peer_uri.scheme().unwrap_or(&Scheme::HTTP),
+            local_peer_uri.host().ok_or_else(|| {
+                CollectionError::service_error(
+                    "Cannot determine REST address, peer {peer_id} has unknown host",
+                )
+            })?,
+            self.rest_port,
+        ))
+        .expect("Malformed URL"))
+    }
+}
+
+#[cfg(test)]
+impl Default for ChannelService {
+    fn default() -> Self {
+        Self {
+            id_to_address: Default::default(),
+            channel_pool: Default::default(),
+            rest_port: 6333,
         }
     }
 }
