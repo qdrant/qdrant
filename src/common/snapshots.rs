65e79e45739b7d03dc77b68d66d32be5c96bb94b
Tim Visée <tim+github@visee.me>
2023-10-26 17:13:08 +0200

    Clean up downloaded snapshot after snapshot recovery (#2846)
    * Clean up remote shard snapshot after recovery
    
    * Clean up remote collection snapshot after recovery
    
    * Improve logging
    
    * Refactor file cleaner into TempPath, fix double delete
    
    This refactoring is done to better match the structures we use from
    `tempfile`. It also has a `TempPath` structure, but it only supports
    files.
    
    Also fixes an issue where we'd try to delete a file twice when
    `delete()` was called, because the drop handler tried to delete a second
    time.
    
    * Use TempFile interface to also clear downloaded snapshots on error
    
    * Replace our `TempPath` with `tempfile::TempPath`
    
    * Fix doc
    
    * Remove clone

--- src/common/snapshots.rs
+++ src/common/snapshots.rs
@@ -66,7 +66,7 @@ pub async fn recover_shard_snapshot(
 
     let download_dir = toc.snapshots_download_tempdir()?;
 
-    let snapshot_path = match snapshot_location {
+    let (snapshot_path, snapshot_temp_path) = match snapshot_location {
         ShardSnapshotLocation::Url(url) => {
             if !matches!(url.scheme(), "http" | "https") {
                 let description = format!(
@@ -76,13 +76,15 @@ pub async fn recover_shard_snapshot(
 
                 return Err(StorageError::bad_input(description));
             }
-            snapshots::download::download_snapshot(url, download_dir.path()).await?
+            let (snapshot_path, snapshot_temp_path) =
+                snapshots::download::download_snapshot(url, download_dir.path()).await?;
+            (snapshot_path, snapshot_temp_path)
         }
 
         ShardSnapshotLocation::Path(path) => {
             let snapshot_path = collection.get_shard_snapshot_path(shard_id, path).await?;
             check_shard_snapshot_file_exists(&snapshot_path)?;
-            snapshot_path
+            (snapshot_path, None)
         }
     };
 
@@ -94,6 +96,13 @@ pub async fn recover_shard_snapshot(
         snapshot_priority,
     )
     .await?;
+
+    // Remove snapshot after recovery if downloaded
+    if let Some(path) = snapshot_temp_path {
+        if let Err(err) = path.close() {
+            log::error!("Failed to remove downloaded shards snapshot after recovery: {err}");
+        }
+    }
 
     Ok(())
 }
