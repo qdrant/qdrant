1ae1f483d4c0e1a6a5b0e114d3207c4b7ac57e62
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-11-09 09:57:43 +0100

    Benchmark sparse filter plain search (#2947)
    * Sparse filter plain search
    
    * code review

--- lib/segment/benches/sparse_index_search.rs
+++ lib/segment/benches/sparse_index_search.rs
@@ -3,44 +3,128 @@ mod prof;
 
 use std::sync::atomic::AtomicBool;
 
+use common::types::PointOffsetType;
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 use segment::fixtures::sparse_fixtures::fixture_sparse_index_ram;
-use segment::index::VectorIndex;
+use segment::index::{PayloadIndex, VectorIndex};
+use segment::types::PayloadSchemaType::Keyword;
+use segment::types::{Condition, FieldCondition, Filter, Payload};
+use serde_json::json;
 use sparse::common::sparse_vector_fixture::random_sparse_vector;
+use tempfile::Builder;
 
-const NUM_VECTORS: usize = 10000;
-const MAX_SPARSE_DIM: usize = 512;
+const NUM_VECTORS: usize = 50_000;
+const MAX_SPARSE_DIM: usize = 30_000;
+const TOP: usize = 10;
 
 fn sparse_vector_index_search_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("sparse-vector-search-group");
 
     let stopped = AtomicBool::new(false);
     let mut rnd = StdRng::seed_from_u64(42);
-    let sparse_vector_index =
-        fixture_sparse_index_ram(&mut rnd, NUM_VECTORS, MAX_SPARSE_DIM, &stopped);
 
-    let mut result_size = 0;
-    let mut query_count = 0;
+    let payload_dir = Builder::new().prefix("payload_dir").tempdir().unwrap();
+    let storage_dir = Builder::new().prefix("storage_dir").tempdir().unwrap();
+    let index_dir = Builder::new().prefix("index_dir").tempdir().unwrap();
 
-    group.bench_function("sparse-index-search", |b| {
+    let sparse_vector_index = fixture_sparse_index_ram(
+        &mut rnd,
+        NUM_VECTORS,
+        MAX_SPARSE_DIM,
+        payload_dir.path(),
+        storage_dir.path(),
+        index_dir.path(),
+        &stopped,
+    );
+
+    // adding payload on field
+    let field_name = "field";
+    let field_value = "important value";
+    let payload: Payload = json!({
+        field_name: field_value,
+    })
+    .into();
+
+    // all points have the same payload
+    let mut payload_index = sparse_vector_index.payload_index.borrow_mut();
+    for idx in 0..NUM_VECTORS {
+        payload_index
+            .assign(idx as PointOffsetType, &payload)
+            .unwrap();
+    }
+    drop(payload_index);
+
+    // shared query vector
+    let sparse_vector = random_sparse_vector(&mut rnd, MAX_SPARSE_DIM);
+    eprintln!("sparse_vector size = {:#?}", sparse_vector.values.len());
+    let query_vector = sparse_vector.into();
+
+    group.bench_function("inverted-index", |b| {
         b.iter(|| {
-            let sparse_vector = random_sparse_vector(&mut rnd, MAX_SPARSE_DIM);
-            let query_vector = sparse_vector.into();
-            result_size += sparse_vector_index
-                .search(&[&query_vector], None, 1, None, &stopped)
-                .unwrap()
-                .len();
+            let results = sparse_vector_index
+                .search(&[&query_vector], None, TOP, None, &stopped)
+                .unwrap();
 
-            query_count += 1;
+            assert_eq!(results[0].len(), TOP);
         })
     });
 
-    eprintln!(
-        "result_size / query_count = {:#?}",
-        result_size / query_count
-    );
+    // filter by field
+    let filter = Filter::new_must(Condition::Field(FieldCondition::new_match(
+        field_name,
+        field_value.to_owned().into(),
+    )));
+
+    group.bench_function("inverted-index-filtered-plain", |b| {
+        b.iter(|| {
+            let results = sparse_vector_index
+                .search(&[&query_vector], Some(&filter), TOP, None, &stopped)
+                .unwrap();
+
+            assert_eq!(results[0].len(), TOP);
+        })
+    });
+
+    group.bench_function("plain-storage", |b| {
+        b.iter(|| {
+            let results = sparse_vector_index
+                .search_plain(&[&query_vector], &filter, TOP, &stopped)
+                .unwrap();
+
+            assert_eq!(results[0].len(), TOP);
+        })
+    });
+
+    let mut payload_index = sparse_vector_index.payload_index.borrow_mut();
+
+    // create payload field index
+    payload_index
+        .set_indexed(field_name, Keyword.into())
+        .unwrap();
+
+    drop(payload_index);
+
+    group.bench_function("inverted-index-filtered-payload-index", |b| {
+        b.iter(|| {
+            let results = sparse_vector_index
+                .search(&[&query_vector], Some(&filter), TOP, None, &stopped)
+                .unwrap();
+
+            assert_eq!(results[0].len(), TOP);
+        })
+    });
+
+    group.bench_function("payload-index", |b| {
+        b.iter(|| {
+            let results = sparse_vector_index
+                .search_plain(&[&query_vector], &filter, TOP, &stopped)
+                .unwrap();
+
+            assert_eq!(results[0].len(), TOP);
+        })
+    });
 
     group.finish();
 }
--- lib/segment/src/fixtures/sparse_fixtures.rs
+++ lib/segment/src/fixtures/sparse_fixtures.rs
@@ -8,7 +8,6 @@ use rand::Rng;
 use sparse::common::sparse_vector_fixture::random_sparse_vector;
 use sparse::index::inverted_index::inverted_index_ram::InvertedIndexRam;
 use sparse::index::inverted_index::InvertedIndex;
-use tempfile::Builder;
 
 use crate::common::operation_error::OperationResult;
 use crate::common::rocksdb_wrapper::{open_db, DB_VECTOR_CF};
@@ -24,12 +23,10 @@ use crate::vector_storage::VectorStorage;
 /// Helper to open a test sparse vector index
 pub fn fixture_open_sparse_index<I: InvertedIndex>(
     index_dir: &Path,
+    payload_dir: &Path,
+    storage_dir: &Path,
     num_vectors: usize, // used to size the id tracker
 ) -> OperationResult<SparseVectorIndex<I>> {
-    // temp dirs
-    let payload_dir = Builder::new().prefix("payload_dir").tempdir().unwrap();
-    let storage_dir = Builder::new().prefix("storage_dir").tempdir().unwrap();
-
     // setup
     let id_tracker = Arc::new(AtomicRefCell::new(FixtureIdTracker::new(num_vectors)));
     let payload_storage = InMemoryPayloadStorage::default();
@@ -37,12 +34,12 @@ pub fn fixture_open_sparse_index<I: InvertedIndex>(
     let payload_index = StructPayloadIndex::open(
         wrapped_payload_storage,
         id_tracker.clone(),
-        payload_dir.path(),
+        payload_dir,
         true,
     )?;
     let wrapped_payload_index = Arc::new(AtomicRefCell::new(payload_index));
 
-    let db = open_db(storage_dir.path(), &[DB_VECTOR_CF]).unwrap();
+    let db = open_db(storage_dir, &[DB_VECTOR_CF]).unwrap();
     let vector_storage = open_simple_sparse_vector_storage(db, DB_VECTOR_CF, Distance::Dot)?;
 
     let sparse_vector_index: SparseVectorIndex<I> = SparseVectorIndex::open(
@@ -60,10 +57,13 @@ pub fn fixture_sparse_index_ram<R: Rng + ?Sized>(
     rnd: &mut R,
     num_vectors: usize,
     max_dim: usize,
+    payload_dir: &Path,
+    storage_dir: &Path,
+    index_dir: &Path,
     stopped: &AtomicBool,
 ) -> SparseVectorIndex<InvertedIndexRam> {
-    let index_dir = Builder::new().prefix("index_dir").tempdir().unwrap();
-    let mut sparse_vector_index = fixture_open_sparse_index(index_dir.path(), num_vectors).unwrap();
+    let mut sparse_vector_index =
+        fixture_open_sparse_index(index_dir, payload_dir, storage_dir, num_vectors).unwrap();
     let mut borrowed_storage = sparse_vector_index.vector_storage.borrow_mut();
 
     // add points to storage
