e386f5869545d959b862aec9baa4549a1e8e57ec
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2023-11-17 14:01:08 +0100

    Remove sorted constraint on sparse vectors (#3030)
    * Remove sorted constraint on sparse vectors
    
    * make sure to sort the query as well

--- lib/sparse/src/common/sparse_vector.rs
+++ lib/sparse/src/common/sparse_vector.rs
@@ -9,7 +9,6 @@ use crate::common::types::{DimId, DimWeight};
 /// - indices to be unique
 /// - indices and values to be the same length
 /// - indices and values to be non-empty
-/// - indices to be sorted TODO(sparse) this is not validated yet
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
 pub struct SparseVector {
     pub indices: Vec<DimId>,
@@ -23,9 +22,39 @@ impl SparseVector {
         Ok(vector)
     }
 
+    /// Sort this vector by indices.
+    ///
+    /// Sorting is required for scoring and overlap checks.
+    pub fn sort_by_indices(&mut self) {
+        // do not sort if already sorted
+        if self.is_sorted() {
+            return;
+        }
+
+        let mut indexed_values: Vec<(u32, f32)> = self
+            .indices
+            .iter()
+            .zip(self.values.iter())
+            .map(|(&i, &v)| (i, v))
+            .collect();
+
+        // Sort the vector of tuples by indices
+        indexed_values.sort_by_key(|&(i, _)| i);
+
+        self.indices = indexed_values.iter().map(|&(i, _)| i).collect();
+        self.values = indexed_values.iter().map(|&(_, v)| v).collect();
+    }
+
+    /// Check if this vector is sorted by indices.
+    pub fn is_sorted(&self) -> bool {
+        self.indices.windows(2).all(|w| w[0] < w[1])
+    }
+
     /// Score this vector against another vector using dot product.
     /// Warning: Expects both vectors to be sorted by indices.
     pub fn score(&self, other: &SparseVector) -> f32 {
+        debug_assert!(self.is_sorted());
+        debug_assert!(other.is_sorted());
         let mut score = 0.0;
         let mut i = 0;
         let mut j = 0;
@@ -46,6 +75,8 @@ impl SparseVector {
     /// Check if this vector overlaps with another vector.
     /// Warning: Expects both vectors to be sorted by indices.
     pub fn overlaps(&self, other: &SparseVector) -> bool {
+        debug_assert!(self.is_sorted());
+        debug_assert!(other.is_sorted());
         let mut i = 0;
         let mut j = 0;
         while i < self.indices.len() && j < other.indices.len() {
@@ -90,9 +121,6 @@ impl Validate for SparseVector {
         }
         if self.indices.windows(2).any(|w| w[0] == w[1]) {
             errors.add("indices", ValidationError::new("must be unique"));
-        }
-        if self.indices.windows(2).any(|w| w[0] > w[1]) {
-            errors.add("indices", ValidationError::new("must be sorted"));
         }
 
         if errors.is_empty() {
@@ -147,7 +175,7 @@ mod tests {
         assert!(different_length.is_err());
 
         let not_sorted = SparseVector::new(vec![1, 3, 2], vec![1.0, 2.0, 3.0]);
-        assert!(not_sorted.is_err());
+        assert!(not_sorted.is_ok());
 
         let not_unique = SparseVector::new(vec![1, 2, 2], vec![1.0, 2.0, 3.0]);
         assert!(not_unique.is_err());
@@ -170,5 +198,13 @@ mod tests {
         let v1 = SparseVector::new(vec![3, 4, 5], vec![2.0, 3.0, 4.0]).unwrap();
         let v2 = SparseVector::new(vec![2, 3], vec![2.0, 3.0]).unwrap();
         assert!(v1.overlaps(&v2));
+    }
+
+    #[test]
+    fn sorting_test() {
+        let mut not_sorted = SparseVector::new(vec![1, 3, 2], vec![1.0, 2.0, 3.0]).unwrap();
+        assert!(!not_sorted.is_sorted());
+        not_sorted.sort_by_indices();
+        assert!(not_sorted.is_sorted());
     }
 }
