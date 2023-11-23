d3768f5cb1b2b4e33ac30cbcf6f95dc6052a5c46
Roman Titov <ffuugoo@users.noreply.github.com>
2023-11-23 13:21:05 +0100

    Only check for *initial/startup* shards readiness in `/readyz`

--- src/common/health.rs
+++ src/common/health.rs
@@ -1,12 +1,18 @@
+use std::collections::HashSet;
+use std::ops::DerefMut as _;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
 use collection::shards::replica_set::ReplicaState;
+use collection::shards::shard::ShardId;
+use collection::shards::CollectionId;
 use storage::content_manager::toc::TableOfContent;
+use tokio::sync::RwLock;
 
 pub struct Ready {
     toc: Arc<TableOfContent>,
     consensus_sync_instant: Instant,
+    initial_unhelathy_shards: RwLock<Option<HashSet<Replica>>>,
 }
 
 impl Ready {
@@ -21,6 +27,7 @@ impl Ready {
         Self {
             toc,
             consensus_sync_instant: Instant::now() + consensus_sync_timeout,
+            initial_unhelathy_shards: Default::default(),
         }
     }
 
@@ -29,8 +36,12 @@ impl Ready {
             return false;
         }
 
+        let is_first_run = self.initial_unhelathy_shards.read().await.is_some();
+
         let this_peer_id = self.toc.this_peer_id;
         let collections = self.toc.all_collections().await;
+
+        let mut shards = Vec::new();
 
         for collection in &collections {
             let state = match self.toc.get_collection(collection).await {
@@ -38,17 +49,56 @@ impl Ready {
                 Err(_) => continue,
             };
 
-            let ready = state
-                .shards
-                .iter()
-                .filter_map(|(_, state)| state.replicas.get(&this_peer_id).copied())
-                .all(|state| matches!(state, ReplicaState::Active | ReplicaState::Listener)); // TODO: `ReplicaState::Initializing`?
-
-            if !ready {
-                return false;
+            for (&shard, info) in state.shards.iter() {
+                if let Some(&state) = info.replicas.get(&this_peer_id) {
+                    shards.push((collection.clone(), shard, state));
+                }
             }
         }
 
-        true
+        let mut initial_unhealthy_shards = self.initial_unhelathy_shards.write().await;
+
+        match initial_unhealthy_shards.deref_mut() {
+            None => {
+                let unhealthy_shards =
+                    shards.into_iter().filter_map(|(collection, shard, state)| {
+                        if !matches!(state, ReplicaState::Active | ReplicaState::Listener) {
+                            Some(Replica::new(collection, shard))
+                        } else {
+                            None
+                        }
+                    });
+
+                initial_unhealthy_shards
+                    .insert(unhealthy_shards.collect())
+                    .is_empty()
+            }
+
+            Some(initial_unhealthy_shards) => {
+                for (collection, shard, state) in shards {
+                    let replica = Replica::new(collection, shard);
+
+                    if matches!(state, ReplicaState::Active | ReplicaState::Listener) {
+                        initial_unhealthy_shards.remove(&replica);
+                    } else if is_first_run {
+                        initial_unhealthy_shards.insert(replica);
+                    }
+                }
+
+                initial_unhealthy_shards.is_empty()
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Replica {
+    collection: CollectionId,
+    shard: ShardId,
+}
+
+impl Replica {
+    pub fn new(collection: CollectionId, shard: ShardId) -> Self {
+        Self { collection, shard }
     }
 }
