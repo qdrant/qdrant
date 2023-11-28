18478503e3da830d949cdded63a7fd5694edf9d0
rafael <22560219+rmuller-ml@users.noreply.github.com>
2023-11-27 21:44:04 -0500

    add trigram tokenizer

--- lib/segment/src/data_types/text_index.rs
+++ lib/segment/src/data_types/text_index.rs
@@ -15,6 +15,7 @@ pub enum TokenizerType {
     #[default]
     Word,
     Multilingual,
+    Trigram,
 }
 
 #[derive(Default, Debug, Deserialize, Serialize, JsonSchema, Clone, Copy, PartialEq, Hash, Eq)]
