95a32159a798593b0964906615b7dfceb58089db
timvisee <tim@visee.me>
2023-12-01 16:18:12 +0100

    Dynamically scale rayon CPU count based on CPU size

--- lib/segment/src/index/hnsw_index/mod.rs
+++ lib/segment/src/index/hnsw_index/mod.rs
@@ -14,15 +14,25 @@ mod tests;
 
 /// Default number of CPUs for HNSW graph building and optimization tasks in general.
 ///
+/// Dynamic based on CPU size.
+///
 /// Even on high-CPU systems, a value higher than 16 is discouraged. It will most likely not
 /// improve performance and is more likely to cause disconnected HNSW graphs.
 /// Will be less if currently available CPU budget is lower.
-const MAX_AUTO_RAYON_THREADS: usize = 8;
+const fn thread_count_for_cpu(num_cpu: usize) -> usize {
+    if num_cpu <= 48 {
+        8
+    } else if num_cpu <= 64 {
+        12
+    } else {
+        16
+    }
+}
 
 pub fn max_rayon_threads(max_indexing_threads: usize) -> usize {
     if max_indexing_threads == 0 {
         let num_cpu = common::cpu::get_num_cpus();
-        num_cpu.clamp(1, MAX_AUTO_RAYON_THREADS)
+        num_cpu.clamp(1, thread_count_for_cpu(num_cpu))
     } else {
         max_indexing_threads
     }
