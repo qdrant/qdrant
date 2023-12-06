3fc8a1727d8969c2f69a4c6cfb78b717d0a51ebe
generall <andrey@vasnetsov.com>
2023-12-06 14:45:57 +0100

    fix mutable->immutable optimization

--- lib/collection/src/collection_manager/optimizers/indexing_optimizer.rs
+++ lib/collection/src/collection_manager/optimizers/indexing_optimizer.rs
@@ -165,14 +165,16 @@ impl IndexingOptimizer {
                             {
                                 let vector_dim =
                                     read_segment.vector_dim(sparse_vector_name).unwrap_or(0);
-                                let is_indexed = sparse_vector_data.is_indexed();
+
+                                let is_index_immutable = !sparse_vector_data.is_index_immutable();
+
                                 let is_on_disk = sparse_vector_data.is_index_on_disk();
                                 let storage_size = point_count * vector_dim * VECTOR_ELEMENT_SIZE;
 
                                 let is_big_for_index = storage_size >= indexing_threshold_kb;
                                 let is_big_for_mmap = storage_size >= mmap_threshold_kb;
 
-                                let optimize_for_index = is_big_for_index && !is_indexed;
+                                let optimize_for_index = is_big_for_index && !is_index_immutable;
                                 let optimize_for_mmap = if let Some(on_disk_config) =
                                     sparse_vector_config.index.and_then(|x| x.on_disk)
                                 {
