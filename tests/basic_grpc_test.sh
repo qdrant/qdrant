4d41158641cc44b07dc5f1404245c900bcfe4879
keremnc <me@keremc.com>
2023-12-08 18:21:35 -0800

    undo script change

--- tests/basic_grpc_test.sh
+++ tests/basic_grpc_test.sh
@@ -6,8 +6,8 @@ set -ex
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
-# QDRANT_HOST='localhost:6334'
-QDRANT_HOST='host.docker.internal:6334'
+QDRANT_HOST='localhost:6334'
+
 docker_grpcurl="docker run --rm --network=host -v ${PWD}/lib/api/src/grpc/proto:/proto fullstorydev/grpcurl -plaintext -import-path /proto -proto qdrant.proto"
 
 $docker_grpcurl -d '{
