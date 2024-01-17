6ba22a040b825bee93bb1e74dd4e4d4611bee927
Albert Safin <xzfcpw@gmail.com>
2024-01-17 09:10:55 +0000

    Rename OpenAPI struct names

--- lib/segment/src/types.rs
+++ lib/segment/src/types.rs
@@ -1324,7 +1324,9 @@ impl From<Vec<IntPayloadType>> for MatchExcept {
 }
 
 /// Range filter request
-#[derive(Debug, Deserialize, Serialize, JsonSchema, Default, Clone, PartialEq)]
+#[macro_rules_attribute::macro_rules_derive(crate::common::macros::schemars_rename_generics)]
+#[derive_args(<FloatPayloadType> => "Range", <DateTimePayloadType> => "RangeDatetime")]
+#[derive(Debug, Deserialize, Serialize, Default, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub struct Range<T> {
     /// point.key < range.lt
