94af8fb041ea5bc5da14c2a93dac220e42169b4d
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-01-18 18:07:59 +0530

    This adds error to status code for StorageError::AlreadyExists using tonic::Code::AlreadyExists

--- lib/storage/src/content_manager/conversions.rs
+++ lib/storage/src/content_manager/conversions.rs
@@ -18,6 +18,7 @@ pub fn error_to_status(error: StorageError) -> tonic::Status {
         StorageError::BadRequest { .. } => tonic::Code::InvalidArgument,
         StorageError::Locked { .. } => tonic::Code::FailedPrecondition,
         StorageError::Timeout { .. } => tonic::Code::DeadlineExceeded,
+        StorageError::AlreadyExists { .. } => tonic::Code::AlreadyExists,
     };
     tonic::Status::new(error_code, format!("{error}"))
 }
