56171c60874942376fb7b106402fe55e83ea8f2d
Tim Visée <tim+github@visee.me>
2024-02-05 14:08:02 +0100

    Dynamic CPU saturation integration (#3115)
    * Integrate dynamic CPU saturation with public API changes
    
    This reverts commit 08930fefa940729064f649c1f7ec0bef53daad66.
    
    * Call num_cpus only once

--- lib/collection/tests/integration/common/mod.rs
+++ lib/collection/tests/integration/common/mod.rs
@@ -30,7 +30,7 @@ pub const TEST_OPTIMIZERS_CONFIG: OptimizersConfig = OptimizersConfig {
     memmap_threshold: None,
     indexing_threshold: Some(50_000),
     flush_interval_sec: 30,
-    max_optimization_threads: 2,
+    max_optimization_threads: Some(2),
 };
 
 #[cfg(test)]
