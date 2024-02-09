90f24db2cb40464d1596ffa075baac6b0658d570
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-02-09 18:56:37 +0000

    more readable for

--- lib/segment/src/vector_storage/chunked_vectors.rs
+++ lib/segment/src/vector_storage/chunked_vectors.rs
@@ -81,7 +81,7 @@ impl<T: Copy + Clone + Default> ChunkedVectors<T> {
             self.chunks.try_set_capacity(chunks_len)?;
             // Allocate new chunks in range [chunks.len()..chunks_len - 1).
             // All this new chunks will be fully allocated.
-            for _ in self.chunks.len() + 1..chunks_len {
+            for _ in self.chunks.len()..chunks_len - 1 {
                 let mut chunk = Vec::new();
                 chunk.try_set_capacity_exact(desired_capacity)?;
                 chunk.resize_with(desired_capacity, T::default);
