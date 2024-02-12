cade9e4e74c063d9939609f57c70e4e05729e199
timvisee <tim@visee.me>
2024-02-12 12:57:45 +0100

    Use single error branch for unsupported shard types

--- lib/collection/src/shards/shard.rs
+++ lib/collection/src/shards/shard.rs
@@ -151,18 +151,15 @@ impl Shard {
     pub async fn shard_recovery_point(&self) -> CollectionResult<RecoveryPoint> {
         match self {
             Self::Local(local_shard) => Ok(local_shard.shard_recovery_point().await),
-            Self::Proxy(_) => Err(CollectionError::service_error(
-                "Proxy shards do not support recovery point",
-            )),
             Self::ForwardProxy(proxy_shard) => {
                 Ok(proxy_shard.wrapped_shard.shard_recovery_point().await)
             }
-            Self::QueueProxy(_) => Err(CollectionError::service_error(
-                "Queue proxy shards do not support recovery point",
-            )),
-            Self::Dummy(_) => Err(CollectionError::service_error(
-                "Dummy shards do not support recovery point",
-            )),
+            Self::Proxy(_) | Self::QueueProxy(_) | Self::Dummy(_) => {
+                Err(CollectionError::service_error(format!(
+                    "Recovery point not supported on {}",
+                    self.variant_name(),
+                )))
+            }
         }
     }
 }
