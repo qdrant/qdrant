375e08d70b1c5f9b27bdc7063cb2c7c4882ad868
Luis Cossío <luis.cossio@outlook.com>
2024-02-14 16:49:50 -0300

    fix default opeapi file path

--- tests/openapi/openapi_integration/helpers/settings.py
+++ tests/openapi/openapi_integration/helpers/settings.py
@@ -3,7 +3,7 @@ import os
 import schemathesis
 
 ROOT_DIR = os.path.dirname(__file__)
-OPENAPI_FILE = os.environ.get("OPENAPI_FILE", os.path.join(os.path.dirname(ROOT_DIR), '../..', 'openapi-merged.yaml'))
+OPENAPI_FILE = os.environ.get("OPENAPI_FILE", os.path.join(os.path.dirname(ROOT_DIR), '../../../openapi', 'openapi-merged.yaml'))
 
 SCHEMA = schemathesis.from_file(open(OPENAPI_FILE))
 QDRANT_HOST = os.environ.get("QDRANT_HOST", "http://localhost:6333")
