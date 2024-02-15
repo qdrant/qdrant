040552a4f3c7786ae051a2cee26084633524a1eb
timvisee <tim@visee.me>
2024-02-15 16:44:46 +0100

    Keep some clocks locked across multiple iterations

--- lib/collection/src/wal_delta.rs
+++ lib/collection/src/wal_delta.rs
@@ -1089,6 +1089,9 @@ mod tests {
             .take(node_count)
             .collect::<Vec<_>>();
 
+        // A list of clocks we don't release for some iterations
+        let mut kept_clocks = vec![];
+
         // 25 times:
         // - insert random number of operations on all nodes
         // - randomly kill a node (or rather, mark as killed)
@@ -1122,6 +1125,12 @@ mod tests {
                     let mut operation = operation.clone();
                     wal.lock_and_write(&mut operation).await.unwrap();
                     clock.advance_to(operation.clock_tag.unwrap().clock_tick);
+                }
+
+                // Maybe keep the clock for some iterations
+                let keep_clock_for = rng.gen_range(0..3);
+                if keep_clock_for > 0 {
+                    kept_clocks.push((keep_clock_for, clock));
                 }
             }
 
@@ -1163,6 +1172,12 @@ mod tests {
                         .await
                         .unwrap();
                     clock.advance_to(operation.clock_tag.unwrap().clock_tick);
+                }
+
+                // Maybe keep the clock for some iterations
+                let keep_clock_for = rng.gen_range(0..10);
+                if keep_clock_for > 0 {
+                    kept_clocks.push((keep_clock_for, clock));
                 }
             }
 
@@ -1209,6 +1224,12 @@ mod tests {
                         "all WALs must have the same entries",
                     );
                 });
+
+            // Release some kept clocks
+            kept_clocks.retain(|(mut keep_for, _)| {
+                keep_for -= 1;
+                keep_for > 0
+            });
         }
 
         for (wal, _) in wals {
