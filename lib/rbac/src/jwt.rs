413b87a95910bd09cc4b209b80f4d85f0db3aca4
Luis Cossío <luis.cossio@qdrant.com>
2024-03-06 10:56:07 -0300

    RBAC: Initial implementation for decoding `Claims` (#3778)
    * initial implementation for RBAC structures
    
    * add tests, make exp optional
    
    * use a single bool for write access
    
    * revert qdrant.desktop edit

---
+++ lib/rbac/src/jwt.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Claims {
+    /// Expiration time (seconds since UNIX epoch)
+    pub exp: Option<u64>,
+
+    /// Write access, default is false. Read access is always enabled
+    pub w: Option<bool>,
+}
