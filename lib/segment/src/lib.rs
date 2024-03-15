ca825dfb31075561024464d176942409a3d653c5
jn <nxqd.inbox+rhacker@gmail.com>
2024-03-15 15:32:33 +0700

    Remove unused tests

--- lib/segment/src/lib.rs
+++ lib/segment/src/lib.rs
@@ -20,11 +20,3 @@ pub mod vector_storage;
 #[macro_use]
 extern crate num_derive;
 extern crate core;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
