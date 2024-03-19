caa03125ed5b361ba47a8d2350f2053ca5b49c5d
Tim Visée <tim+github@visee.me>
2024-03-19 14:50:13 +0100

    Switch to Recovery state, deprecated PartialSnapshot, for Qdrant 1.9 (#3847)
    * Deprecate PartialSnapshot replica set state
    
    * Improve logging
    
    * Update gRPC docs
    
    * Remove now obsolete migration TODO

--- lib/collection/src/collection/shard_transfer.rs
+++ lib/collection/src/collection/shard_transfer.rs
@@ -81,9 +81,8 @@ impl Collection {
 
             let initial_state = match shard_transfer.method.unwrap_or_default() {
                 ShardTransferMethod::StreamRecords => ReplicaState::Partial,
-                // TODO(1.9): switch into recovery state instead
                 ShardTransferMethod::Snapshot | ShardTransferMethod::WalDelta => {
-                    ReplicaState::PartialSnapshot
+                    ReplicaState::Recovery
                 }
             };
 
