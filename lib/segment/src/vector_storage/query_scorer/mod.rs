77ef7b473041e1f89030b760639f2ff25fe5838a
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-03-21 10:31:57 +0100

    colbert query scorers (#3713)
    * colbert query scorers
    
    fix build after rebase
    
    * review remarks

--- lib/segment/src/vector_storage/query_scorer/mod.rs
+++ lib/segment/src/vector_storage/query_scorer/mod.rs
@@ -1,7 +1,12 @@
 use common::types::{PointOffsetType, ScoreType};
 
+use crate::data_types::vectors::MultiDenseVector;
+use crate::spaces::metric::Metric;
+
 pub mod custom_query_scorer;
 pub mod metric_query_scorer;
+pub mod multi_custom_query_scorer;
+pub mod multi_metric_query_scorer;
 pub mod sparse_custom_query_scorer;
 
 pub trait QueryScorer<TVector: ?Sized> {
@@ -10,4 +15,20 @@ pub trait QueryScorer<TVector: ?Sized> {
     fn score(&self, v2: &TVector) -> ScoreType;
 
     fn score_internal(&self, point_a: PointOffsetType, point_b: PointOffsetType) -> ScoreType;
+}
+
+/// Colbert MaxSim metric, metric for multi-dense vectors
+/// https://arxiv.org/pdf/2112.01488.pdf, figure 1
+pub fn score_multi<TMetric: Metric>(
+    multidense_a: &MultiDenseVector,
+    multidense_b: &MultiDenseVector,
+) -> ScoreType {
+    let mut sum = 0.0;
+    for dense_a in multidense_a.iter() {
+        sum += multidense_b
+            .iter()
+            .map(|dense_b| TMetric::similarity(dense_a, dense_b))
+            .fold(0.0, |a, b| if a > b { a } else { b });
+    }
+    sum
 }
