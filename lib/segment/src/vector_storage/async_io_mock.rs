d032e7d16785e40bb364feb8c0199aa144f945ca
generall <andrey@vasnetsov.com>
2024-03-23 23:33:51 +0100

    fix UringReader on non-linux platform

--- lib/segment/src/vector_storage/async_io_mock.rs
+++ lib/segment/src/vector_storage/async_io_mock.rs
@@ -1,14 +1,19 @@
 use std::fs::File;
 
 use crate::common::operation_error::OperationResult;
+use crate::vector_storage::primitive::PrimitiveVectorElement;
 
 // This is a mock implementation of the async_io module for those platforms that don't support io_uring.
 #[allow(dead_code)]
-pub struct UringReader;
+pub struct UringReader<T: PrimitiveVectorElement> {
+    _phantom: std::marker::PhantomData<T>,
+}
 
 #[allow(dead_code)]
-impl UringReader {
+impl<T: PrimitiveVectorElement> UringReader<T> {
     pub fn new(_file: File, _raw_size: usize, _header_size: usize) -> OperationResult<Self> {
-        Ok(Self {})
+        Ok(Self {
+            _phantom: std::marker::PhantomData,
+        })
     }
 }
