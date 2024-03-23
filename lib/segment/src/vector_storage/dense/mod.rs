4fe71e5fe83cf3630e0853277b713e2f7215f1aa
generall <andrey@vasnetsov.com>
2024-03-23 22:03:23 +0100

    fmt

--- lib/segment/src/vector_storage/dense/mod.rs
+++ lib/segment/src/vector_storage/dense/mod.rs
@@ -2,5 +2,5 @@ pub mod appendable_mmap_dense_vector_storage;
 mod dynamic_mmap_flags;
 pub mod memmap_dense_vector_storage;
 pub mod mmap_dense_vectors;
-pub mod simple_dense_vector_storage;
 mod primitive;
+pub mod simple_dense_vector_storage;
--- lib/segment/src/vector_storage/dense/primitive.rs
+++ lib/segment/src/vector_storage/dense/primitive.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
+
 use crate::common::operation_error::OperationResult;
 use crate::data_types::named_vectors::CowVector;
 use crate::data_types::vectors::{VectorElementType, VectorRef};
 
 pub trait PrimitiveVectorElement:
-Copy + Clone + Default + Serialize + for<'a> Deserialize<'a>
+    Copy + Clone + Default + Serialize + for<'a> Deserialize<'a>
 {
     fn from_vector_ref(vector: VectorRef) -> OperationResult<&[Self]>;
 
