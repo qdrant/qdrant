439b0b9116663b1b07b1b96e95d21225211e5499
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-03-27 23:33:24 +0000

    nearest heap

--- lib/segment/src/index/hnsw_index/gpu/shaders/build_shaders.bat
+++ lib/segment/src/index/hnsw_index/gpu/shaders/build_shaders.bat
@@ -3,6 +3,7 @@ glslc test_links.comp -o test_links.spv -O --target-spv=spv1.3 --target-env=vulk
 glslc test_searcher.comp -o test_searcher.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
 glslc test_greedy_searcher.comp -o test_greedy_searcher.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
 glslc test_heuristic.comp -o test_heuristic.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
+glslc test_nearest_heap.comp -o test_nearest_heap.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
 glslc run_requests.comp -o run_requests.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
 glslc update_entries.comp -o update_entries.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
 glslc builder_profile_helper.comp -o builder_profile_helper.spv -O --target-spv=spv1.3 --target-env=vulkan1.3
