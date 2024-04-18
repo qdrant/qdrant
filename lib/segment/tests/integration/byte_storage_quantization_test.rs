9632520b6b3eff8d23cd1b45e976f8ec6fc27cd8
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-04-18 13:06:11 +0000

    exact true

--- lib/segment/tests/integration/byte_storage_quantization_test.rs
+++ lib/segment/tests/integration/byte_storage_quantization_test.rs
@@ -331,7 +331,6 @@ fn test_byte_storage_binary_quantization_hnsw(
                         rescore: Some(true),
                         ..Default::default()
                     }),
-                    exact: true,
                     ..Default::default()
                 }),
                 &stopped,
@@ -350,6 +349,7 @@ fn test_byte_storage_binary_quantization_hnsw(
                         ignore: true,
                         ..Default::default()
                     }),
+                    exact: true,
                     ..Default::default()
                 }),
                 &stopped,
