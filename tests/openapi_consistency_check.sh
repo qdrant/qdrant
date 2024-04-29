d40a51fe13854ee039992184d96abfe8eda77f60
Luis Cossío <luis.cossio@outlook.com>
2024-04-29 10:20:38 -0400

    fix number of apis checker

--- tests/openapi_consistency_check.sh
+++ tests/openapi_consistency_check.sh
@@ -35,8 +35,8 @@ fi
 # Cleanup
 rm -f ./docs/redoc/master/.diff.openapi.json
 
-NUMBER_OF_APIS=$(cat ./docs/redoc/master/openapi.json | jq '.paths | length')
-EXPECTED_NUMBER_OF_APIS=52
+NUMBER_OF_APIS=$(cat ./docs/redoc/master/openapi.json | jq '[.paths[] | length] | add')
+EXPECTED_NUMBER_OF_APIS=65
 
 if [ "$NUMBER_OF_APIS" -ne "$EXPECTED_NUMBER_OF_APIS" ]; then
     echo "ERROR: It looks like the total number of APIs has changed."
