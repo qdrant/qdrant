1970562deaecdd4333c5ef30059ac014d2f1f46e
Luis Cossío <luis.cossio@qdrant.com>
2024-04-29 15:06:06 -0400

    Issues API: Event-driven issue framework (#4086)
    * update issues crate from unindexed-field-issue-integration branch
    
    * add EventBroker, update Dashboard::solve_by_filter
    
    * Update outdated comment
    
    * better example use
    
    * use composite `Code` type to remove inverted index
    
    * rename `EventBroker::notify` to `EventBroker::publish`
    
    * remove `publish_async`
    
    * review fixes
    
    * rename `Code::from_issue` to `Code::of`

--- lib/common/issues/src/lib.rs
+++ lib/common/issues/src/lib.rs
@@ -1,51 +1,60 @@
-//! # Issues
-//!
-//! A logger-like module for logging issues and solutions to them.
-//!
-//! ## Usage
-//!
-//! ```
-//! use issues;
-//! use issues::{Issue, Solution};
-//!
-//! pub(crate) struct DummyIssue {
-//!   pub code: String,
-//! }
-//!
-//! impl DummyIssue {
-//!     pub fn new(code: impl Into<String>) -> Self {
-//!         Self { code: code.into() }
-//!     }
-//! }
-//!
-//! impl Issue for DummyIssue {
-//!     fn code(&self) -> String {
-//!         self.code.clone()
-//!     }
-//!
-//!     fn description(&self) -> String {
-//!         "".to_string()
-//!     }
-//!
-//!     fn solution(&self) -> Solution {
-//!         Solution::None
-//!     }
-//! }
-//!
-//! // Submit an issue
-//! issues::submit(DummyIssue::new("issue1"));
-//! assert!(!issues::submit(DummyIssue::new("issue1"))); // can't submit the same issue twice
-//!
-//! // Solve an issue
-//! issues::solve("issue1"); // returns true if the issue was solved
-//! assert!(issues::submit(DummyIssue::new("issue1"))); // Now we can submit it again
-//! ```
+#![doc = r#"# Issues
 
+A logger-like module for logging issues and solutions to them.
+
+## Usage
+
+```
+use issues;
+use issues::{Issue, Solution};
+
+pub(crate) struct DummyIssue {
+    pub distinctive: String,
+}
+
+impl DummyIssue {
+    pub fn new(distinctive: impl Into<String>) -> Self {
+        Self {
+            distinctive: distinctive.into(),
+        }
+    }
+}
+
+impl Issue for DummyIssue {
+    fn distinctive(&self) -> &str {
+        &self.distinctive
+    }
+
+    fn name() -> &'static str {
+        "DUMMY"
+    }
+
+    fn description(&self) -> String {
+        "".to_string()
+    }
+
+    fn solution(&self) -> Solution {
+        Solution::Refactor("".to_string())
+    }
+}
+
+// Submit an issue
+issues::submit(DummyIssue::new("issue1"));
+assert!(!issues::submit(DummyIssue::new("issue1"))); // can't submit the same issue twice
+
+// Solve an issue
+issues::solve(DummyIssue::new("issue1").code()); // returns true if the issue was solved
+assert!(issues::submit(DummyIssue::new("issue1"))); // Now we can submit it again
+```"#]
+
+pub mod broker;
 mod dashboard;
 mod issue;
 pub mod problems;
 mod solution;
+pub(crate) mod typemap;
 
-pub use dashboard::{all_issues, clear, solve, submit};
+pub use broker::EventBroker;
+pub use dashboard::{all_issues, clear, solve, solve_by_filter, submit};
 pub use issue::{Issue, IssueRecord};
-pub use solution::Solution;
+pub use solution::{Action, ImmediateSolution, Solution};
