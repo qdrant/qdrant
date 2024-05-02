97c107f21b8dbd1cb7190ecc732ff38f7cdd248f
Andrey Vasnetsov <andrey@vasnetsov.com>
2024-05-02 20:33:23 +0200

    bump version to v1.9.1 (#4160)

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Qdrant version string
-pub const QDRANT_VERSION_STRING: &str = "1.9.0";
+pub const QDRANT_VERSION_STRING: &str = "1.9.1";
 
 lazy_static! {
     /// Current Qdrant semver version
