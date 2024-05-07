60c4c028747e7b269eb81f358965b6360af5b4ef
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2024-05-07 17:31:59 +0900

    Refactor get_appropriate_chunk_size function to adjust chunk size based on service limits and file size

--- lib/collection/src/operations/snapshot_storage_ops.rs
+++ lib/collection/src/operations/snapshot_storage_ops.rs
@@ -62,6 +62,40 @@ pub async fn get_snapshot_description(
     })
 }
 
+/// This function adjusts the chunk size based on service limits and the total size of the data to be uploaded.
+/// Note:
+///
+/// * Amazon S3: https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html
+///     partsize: min 5 MB, max 5 GB, up to 10,000 parts.
+/// * Google Cloud Storage: https://cloud.google.com/storage/quotas?hl=ja#objects
+///     partsize: min 5 MB, max 5 GB, up to 10,000 parts.
+/// * Azure Storage: https://learn.microsoft.com/en-us/rest/api/storageservices/put-blob?tabs=microsoft-entra-id#remarks
+///     <TODO> It looks like Azure Storage has different limits for different service versions.
+pub async fn get_appropriate_chunk_size(local_source_path: &Path) -> CollectionResult<usize> {
+    const DEFAULT_CHUNK_SIZE: usize = 50 * 1024 * 1024;
+    const MAX_PART_NUMBER: usize = 10000;
+    const MAX_UPLOAD_SIZE: usize = 5 * 1024 * 1024 * 1024;
+
+    let file_meta = tokio::fs::metadata(local_source_path).await?;
+    let file_size = file_meta.len() as usize;
+
+    // check if the file size exceeds the maximum upload size
+    if file_size > MAX_UPLOAD_SIZE {
+        return Err(CollectionError::service_error(format!(
+            "File size exceeds the maximum upload size: {}",
+            MAX_UPLOAD_SIZE
+        )));
+    }
+
+    // check if the file size exceeds the maximum part number
+    // if so, adjust the chunk size to fit the maximum part number
+    if file_size > DEFAULT_CHUNK_SIZE * MAX_PART_NUMBER {
+        let chunk_size = (file_size - 1 / MAX_PART_NUMBER) + 1; // ceil(file_size / MAX_PART_NUMBER)
+        return Ok(chunk_size);
+    }
+    Ok(DEFAULT_CHUNK_SIZE)
+}
+
 pub async fn multipart_upload(
     client: &dyn object_store::ObjectStore,
     source_path: &Path,
@@ -73,11 +107,11 @@ pub async fn multipart_upload(
         .await
         .map_err(|e| CollectionError::service_error(format!("Failed to put multipart: {}", e)))?;
 
-    const CHUNK_SIZE: usize = 5 * 1024 * 1024;
-    let mut write = WriteMultipart::new_with_chunk_size(upload, CHUNK_SIZE);
+    let chunk_size: usize = get_appropriate_chunk_size(source_path).await?;
+    let mut write = WriteMultipart::new_with_chunk_size(upload, chunk_size);
     let file = File::open(source_path)?;
     let mut reader = BufReader::new(file);
-    let mut buffer = vec![0u8; CHUNK_SIZE];
+    let mut buffer = vec![0u8; chunk_size];
     while let Ok(bytes_read) = reader.read(&mut buffer) {
         if bytes_read == 0 {
             break;
