34f7f8ec2d8455c0b3fe0f1fb134907ba7816a08
Tim Visée <tim+github@visee.me>
2024-05-10 09:11:27 +0200

    Bump version to 1.9.2 (#4207)

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Qdrant version string
-pub const QDRANT_VERSION_STRING: &str = "1.9.1";
+pub const QDRANT_VERSION_STRING: &str = "1.9.2";
 
 lazy_static! {
     /// Current Qdrant semver version
