7352ac2fb37b70eb19729ec17a770f6cb4c4403d
Roman Titov <ffuugoo@users.noreply.github.com>
2024-05-13 11:17:41 +0200

    Simplify `deseriailze_logger_config` test

--- src/tracing/test.rs
+++ src/tracing/test.rs
@@ -1,18 +1,26 @@
+use std::collections::HashSet;
+
 use serde_json::json;
-use tracing_subscriber::fmt;
 
 use super::*;
 
 #[test]
 fn deseriailze_logger_config() {
-    let config = deserialize_config(config());
+    let json = json!({
+        "log_level": "debug",
+        "span_events": ["new", "close"],
+        "color": "enable",
+    });
+
+    let config = deserialize_config(json);
 
     let expected = LoggerConfig {
         default: default::Config {
-            log_level: Some("debug".to_string()),
-            span_events: Some(config::SpanEvent::from_fmt_span(
-                fmt::format::FmtSpan::NEW | fmt::format::FmtSpan::CLOSE,
-            )),
+            log_level: Some("debug".into()),
+            span_events: Some(HashSet::from_iter([
+                config::SpanEvent::New,
+                config::SpanEvent::Close,
+            ])),
             color: Some(config::Color::Enable),
         },
     };
@@ -22,12 +30,4 @@ fn deseriailze_logger_config() {
 
 fn deserialize_config(json: serde_json::Value) -> LoggerConfig {
     serde_json::from_value(json).unwrap()
-}
-
-fn config() -> serde_json::Value {
-    json!({
-        "log_level": "debug",
-        "span_events": ["new", "close"],
-        "color": "enable",
-    })
 }
