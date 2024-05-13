52034128d46b665a0a853cc3954c86666d35a7fd
Roman Titov <ffuugoo@users.noreply.github.com>
2024-05-13 11:30:33 +0200

    Add some simple unit-tests

--- src/tracing/test.rs
+++ src/tracing/test.rs
@@ -28,6 +28,24 @@ fn deseriailze_logger_config() {
     assert_eq!(config, expected);
 }
 
+#[test]
+fn deserialize_empty_config() {
+    let config = deserialize_config(json!({}));
+    assert_eq!(config, LoggerConfig::default());
+}
+
+#[test]
+fn deseriailze_config_with_explicit_nulls() {
+    let json = json!({
+        "log_level": null,
+        "span_events": null,
+        "color": null,
+    });
+
+    let config = deserialize_config(json);
+    assert_eq!(config, LoggerConfig::default());
+}
+
 fn deserialize_config(json: serde_json::Value) -> LoggerConfig {
     serde_json::from_value(json).unwrap()
 }
