b334d9fd9079c3eb1bec22bf3d33724eff8a35bf
Kamyar Salahi <kam.salahi@gmail.com>
2024-05-13 14:19:21 -0700

    Half-precision float vector metrics (#4122)
    * Adding half-precision floating point SIMD-optimized implementation for vector distance metrics.
    
    * Primitives adjustment
    
    * Remove ds store
    
    * Load assembly only for neon
    
    * Fixing linter errors
    
    * Adding float16 type
    
    * Addressing f16 comments
    
    * Refactoring and adding benchmarks
    
    * Renaming simd functions
    
    * Cleaning openapi
    
    * Merging in changes to dev
    
    * Fixing linter error
    
    * fix clippy
    
    * disable float16 feature in API
    
    ---------
    
    Co-authored-by: generall <andrey@vasnetsov.com>

---
+++ lib/segment/src/spaces/metric_f16/mod.rs
@@ -0,0 +1,12 @@
+pub mod simple_dot;
+pub mod simple_euclid;
+pub mod simple_manhattan;
+
+#[cfg(target_arch = "x86_64")]
+pub mod avx;
+
+#[cfg(target_arch = "aarch64")]
+pub mod neon;
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub mod sse;
