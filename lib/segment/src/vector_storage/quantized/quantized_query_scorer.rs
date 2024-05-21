903698833f029b04d98bf50382288965ed9e8abd
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-05-21 17:40:41 +0200

    quantization scorer with multivector support (#4289)

--- lib/segment/src/vector_storage/quantized/quantized_query_scorer.rs
+++ lib/segment/src/vector_storage/quantized/quantized_query_scorer.rs
@@ -2,9 +2,10 @@ use std::borrow::Cow;
 use std::marker::PhantomData;
 
 use common::types::{PointOffsetType, ScoreType};
+use itertools::Itertools;
 
 use crate::data_types::primitive::PrimitiveVectorElement;
-use crate::data_types::vectors::{DenseVector, TypedDenseVector};
+use crate::data_types::vectors::{DenseVector, MultiDenseVector};
 use crate::spaces::metric::Metric;
 use crate::types::QuantizationConfig;
 use crate::vector_storage::query_scorer::QueryScorer;
@@ -15,10 +16,10 @@ where
     TMetric: Metric<TElement>,
     TEncodedVectors: quantization::EncodedVectors<TEncodedQuery>,
 {
-    original_query: TypedDenseVector<TElement>,
     query: TEncodedQuery,
     quantized_data: &'a TEncodedVectors,
     metric: PhantomData<TMetric>,
+    element: PhantomData<TElement>,
 }
 
 impl<'a, TElement, TMetric, TEncodedQuery, TEncodedVectors>
@@ -43,14 +44,46 @@ where
         let query = quantized_data.encode_query(&original_query_prequantized);
 
         Self {
-            original_query: original_query.into_owned(),
             query,
             quantized_data,
             metric: PhantomData,
+            element: PhantomData,
+        }
+    }
+
+    #[allow(unused)]
+    pub fn new_multi(
+        raw_query: MultiDenseVector,
+        quantized_data: &'a TEncodedVectors,
+        quantization_config: &QuantizationConfig,
+    ) -> Self {
+        let slices = raw_query.multi_vectors();
+        let query = slices
+            .into_iter()
+            .flat_map(|inner_vector| {
+                let inner_preprocessed = TMetric::preprocess(inner_vector.to_vec());
+                let inner_converted =
+                    TElement::slice_from_float_cow(Cow::Owned(inner_preprocessed));
+                let inner_prequantized = TElement::quantization_preprocess(
+                    quantization_config,
+                    TMetric::distance(),
+                    inner_converted.as_ref(),
+                )
+                .into_owned();
+                inner_prequantized.into_iter()
+            })
+            .collect_vec();
+
+        let query = quantized_data.encode_query(&query);
+
+        Self {
+            query,
+            quantized_data,
+            metric: PhantomData,
+            element: PhantomData,
         }
     }
 }
-
 impl<TElement, TMetric, TEncodedQuery, TEncodedVectors> QueryScorer<[TElement]>
     for QuantizedQueryScorer<'_, TElement, TMetric, TEncodedQuery, TEncodedVectors>
 where
@@ -62,12 +95,8 @@ where
         self.quantized_data.score_point(&self.query, idx)
     }
 
-    fn score(&self, v2: &[TElement]) -> ScoreType {
-        debug_assert!(
-            false,
-            "This method is not expected to be called for quantized scorer"
-        );
-        TMetric::similarity(&self.original_query, v2)
+    fn score(&self, _v2: &[TElement]) -> ScoreType {
+        unimplemented!("This method is not expected to be called for quantized scorer");
     }
 
     fn score_internal(&self, point_a: PointOffsetType, point_b: PointOffsetType) -> ScoreType {
