87c8ff120d8efe872c6f5770b8735e850d6c7491
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-05-29 21:29:11 +0200

    mmap for quantized multivector offsets

--- lib/segment/src/vector_storage/quantized/quantized_multivector_storage.rs
+++ lib/segment/src/vector_storage/quantized/quantized_multivector_storage.rs
@@ -9,36 +9,90 @@ use serde::{Deserialize, Serialize};
 
 use crate::common::mmap_type::MmapSlice;
 use crate::common::operation_error::OperationResult;
-use crate::data_types::vectors::TypedMultiDenseVectorRef;
+use crate::data_types::vectors::{TypedMultiDenseVectorRef, VectorElementType};
 use crate::types::{MultiVectorComparator, MultiVectorConfig};
 
 #[derive(Clone, Copy, Debug, Default, Serialize, Deserialize, PartialEq)]
 pub struct MultivectorOffset {
-    pub offset: PointOffsetType,
+    pub start: PointOffsetType,
     pub count: PointOffsetType,
 }
 
-pub struct QuantizedMultivectorStorage<TEncodedQuery, QuantizedStorage>
+pub trait MultivectorOffsetsStorage: Sized {
+    fn load(path: &Path) -> OperationResult<Self>;
+
+    fn save(&self, path: &Path) -> OperationResult<()>;
+
+    fn get_offset(&self, idx: PointOffsetType) -> MultivectorOffset;
+}
+
+impl MultivectorOffsetsStorage for Vec<MultivectorOffset> {
+    fn load(path: &Path) -> OperationResult<Self> {
+        let offsets_file = std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(false)
+            .open(path)?;
+        let offsets_mmap = unsafe { MmapMut::map_mut(&offsets_file) }?;
+        let mut offsets_mmap_type =
+            unsafe { MmapSlice::<MultivectorOffset>::try_from(offsets_mmap)? };
+        Ok(offsets_mmap_type.deref_mut().iter().copied().collect())
+    }
+
+    fn save(&self, path: &Path) -> OperationResult<()> {
+        create_offsets_file_from_iter(path, self.len(), self.iter().copied())
+    }
+
+    fn get_offset(&self, idx: PointOffsetType) -> MultivectorOffset {
+        self[idx as usize]
+    }
+}
+
+impl MultivectorOffsetsStorage for MmapSlice<MultivectorOffset> {
+    fn load(path: &Path) -> OperationResult<Self> {
+        let offsets_file = std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(false)
+            .open(path)?;
+        let offsets_mmap = unsafe { MmapMut::map_mut(&offsets_file) }?;
+        unsafe { Ok(MmapSlice::<MultivectorOffset>::try_from(offsets_mmap)?) }
+    }
+
+    fn save(&self, _path: &Path) -> OperationResult<()> {
+        // mmap is already saved
+        Ok(())
+    }
+
+    fn get_offset(&self, idx: PointOffsetType) -> MultivectorOffset {
+        self[idx as usize]
+    }
+}
+
+pub struct QuantizedMultivectorStorage<TEncodedQuery, QuantizedStorage, TMultivectorOffsetsStorage>
 where
     TEncodedQuery: Sized,
     QuantizedStorage: EncodedVectors<TEncodedQuery>,
+    TMultivectorOffsetsStorage: MultivectorOffsetsStorage,
 {
     quantized_storage: QuantizedStorage,
-    offsets: Vec<MultivectorOffset>,
+    offsets: TMultivectorOffsetsStorage,
     dim: usize,
     multi_vector_config: MultiVectorConfig,
     encoded_query: PhantomData<TEncodedQuery>,
 }
 
-impl<TEncodedQuery, QuantizedStorage> QuantizedMultivectorStorage<TEncodedQuery, QuantizedStorage>
+impl<TEncodedQuery, QuantizedStorage, TMultivectorOffsetsStorage>
+    QuantizedMultivectorStorage<TEncodedQuery, QuantizedStorage, TMultivectorOffsetsStorage>
 where
     TEncodedQuery: Sized,
     QuantizedStorage: EncodedVectors<TEncodedQuery>,
+    TMultivectorOffsetsStorage: MultivectorOffsetsStorage,
 {
     pub fn new(
         dim: usize,
         quantized_storage: QuantizedStorage,
-        offsets: Vec<MultivectorOffset>,
+        offsets: TMultivectorOffsetsStorage,
         multi_vector_config: MultiVectorConfig,
     ) -> Self {
         Self {
@@ -56,24 +110,7 @@ where
         meta_path: &Path,
         offsets_path: &Path,
     ) -> OperationResult<()> {
-        offsets_path.parent().map(std::fs::create_dir_all);
-
-        let offsets_file_size = self.offsets.len() * std::mem::size_of::<MultivectorOffset>();
-        let offsets_file = std::fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            // Don't truncate because we explicitly set the length later
-            .truncate(false)
-            .open(offsets_path)?;
-        offsets_file.set_len(offsets_file_size as u64)?;
-
-        let offsets_mmap = unsafe { MmapMut::map_mut(&offsets_file) }?;
-        let mut offsets_mmap_type =
-            unsafe { MmapSlice::<MultivectorOffset>::try_from(offsets_mmap)? };
-        offsets_mmap_type.deref_mut().copy_from_slice(&self.offsets);
-        offsets_mmap_type.flusher()()?;
-
+        self.offsets.save(offsets_path)?;
         Ok(self.quantized_storage.save(data_path, meta_path)?)
     }
 
@@ -84,37 +121,30 @@ where
         vector_parameters: &VectorParameters,
         multi_vector_config: &MultiVectorConfig,
     ) -> OperationResult<Self> {
-        let offsets_file = std::fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(false)
-            .open(offsets_path)?;
-        let offsets_mmap = unsafe { MmapMut::map_mut(&offsets_file) }?;
-        let mut offsets_mmap_type =
-            unsafe { MmapSlice::<MultivectorOffset>::try_from(offsets_mmap)? };
-        let offsets = offsets_mmap_type.deref_mut().iter().copied().collect();
-
         Ok(Self {
             dim: vector_parameters.dim,
             quantized_storage: QuantizedStorage::load(data_path, meta_path, vector_parameters)?,
-            offsets,
+            offsets: TMultivectorOffsetsStorage::load(offsets_path)?,
             multi_vector_config: *multi_vector_config,
             encoded_query: PhantomData,
         })
     }
 
     /// Custom `score_max_similarity` implementation for quantized vectors
-    fn score_point_max_similarity(&self, query: &Vec<TEncodedQuery>, vector_index: u32) -> f32 {
-        let vectors_count = self.offsets[vector_index as usize].count;
-        let vectors_offset = self.offsets[vector_index as usize].offset;
+    fn score_point_max_similarity(
+        &self,
+        query: &Vec<TEncodedQuery>,
+        vector_index: PointOffsetType,
+    ) -> ScoreType {
+        let offset = self.offsets.get_offset(vector_index);
         let mut sum = 0.0;
         for inner_query in query {
             let mut max_sim = ScoreType::NEG_INFINITY;
             // manual `max_by` for performance
-            for i in 0..vectors_count {
+            for i in 0..offset.count {
                 let sim = self
                     .quantized_storage
-                    .score_point(inner_query, vectors_offset + i);
+                    .score_point(inner_query, offset.start + i);
                 if sim > max_sim {
                     max_sim = sim;
                 }
@@ -126,19 +156,21 @@ where
     }
 
     /// Custom `score_max_similarity` implementation for quantized vectors
-    fn score_internal_max_similarity(&self, vector_a_index: u32, vector_b_index: u32) -> f32 {
-        let vector_a_count = self.offsets[vector_a_index as usize].count;
-        let vector_b_count = self.offsets[vector_b_index as usize].count;
-        let vector_a_offset = self.offsets[vector_a_index as usize].offset;
-        let vector_b_offset = self.offsets[vector_b_index as usize].offset;
+    fn score_internal_max_similarity(
+        &self,
+        vector_a_index: PointOffsetType,
+        vector_b_index: PointOffsetType,
+    ) -> ScoreType {
+        let offset_a = self.offsets.get_offset(vector_a_index);
+        let offset_b = self.offsets.get_offset(vector_b_index);
         let mut sum = 0.0;
-        for a in 0..vector_a_count {
+        for a in 0..offset_a.count {
             let mut max_sim = ScoreType::NEG_INFINITY;
             // manual `max_by` for performance
-            for b in 0..vector_b_count {
+            for b in 0..offset_b.count {
                 let sim = self
                     .quantized_storage
-                    .score_internal(vector_a_offset + a, vector_b_offset + b);
+                    .score_internal(offset_a.start + a, offset_b.start + b);
                 if sim > max_sim {
                     max_sim = sim;
                 }
@@ -150,11 +182,12 @@ where
     }
 }
 
-impl<TEncodedQuery, QuantizedStorage> EncodedVectors<Vec<TEncodedQuery>>
-    for QuantizedMultivectorStorage<TEncodedQuery, QuantizedStorage>
+impl<TEncodedQuery, QuantizedStorage, TMultivectorOffsetsStorage> EncodedVectors<Vec<TEncodedQuery>>
+    for QuantizedMultivectorStorage<TEncodedQuery, QuantizedStorage, TMultivectorOffsetsStorage>
 where
     TEncodedQuery: Sized,
     QuantizedStorage: EncodedVectors<TEncodedQuery>,
+    TMultivectorOffsetsStorage: MultivectorOffsetsStorage,
 {
     // TODO(colbert): refactor `EncodedVectors` to support multi vector storage after quantization migration
     fn save(&self, _data_path: &Path, _meta_path: &Path) -> std::io::Result<()> {
@@ -172,7 +205,7 @@ where
         )
     }
 
-    fn encode_query(&self, query: &[f32]) -> Vec<TEncodedQuery> {
+    fn encode_query(&self, query: &[VectorElementType]) -> Vec<TEncodedQuery> {
         let multi_vector = TypedMultiDenseVectorRef {
             dim: self.dim,
             flattened_vectors: query,
@@ -183,15 +216,42 @@ where
             .collect()
     }
 
-    fn score_point(&self, query: &Vec<TEncodedQuery>, i: u32) -> f32 {
+    fn score_point(&self, query: &Vec<TEncodedQuery>, i: PointOffsetType) -> ScoreType {
         match self.multi_vector_config.comparator {
             MultiVectorComparator::MaxSim => self.score_point_max_similarity(query, i),
         }
     }
 
-    fn score_internal(&self, i: u32, j: u32) -> f32 {
+    fn score_internal(&self, i: PointOffsetType, j: PointOffsetType) -> ScoreType {
         match self.multi_vector_config.comparator {
             MultiVectorComparator::MaxSim => self.score_internal_max_similarity(i, j),
         }
     }
+}
+
+pub(super) fn create_offsets_file_from_iter(
+    path: &Path,
+    count: usize,
+    iter: impl Iterator<Item = MultivectorOffset>,
+) -> OperationResult<()> {
+    path.parent().map(std::fs::create_dir_all);
+
+    let offsets_file_size = count * std::mem::size_of::<MultivectorOffset>();
+    let offsets_file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        // Don't truncate because we explicitly set the length later
+        .truncate(false)
+        .open(path)?;
+    offsets_file.set_len(offsets_file_size as u64)?;
+
+    let offsets_mmap = unsafe { MmapMut::map_mut(&offsets_file) }?;
+    let mut offsets_mmap_type = unsafe { MmapSlice::<MultivectorOffset>::try_from(offsets_mmap)? };
+    let offsets_mut: &mut [MultivectorOffset] = offsets_mmap_type.deref_mut();
+    for (dst, src) in offsets_mut.iter_mut().zip(iter) {
+        *dst = src;
+    }
+    offsets_mmap_type.flusher()()?;
+    Ok(())
 }
