2604d7ce6ceba151ce97dc795bfdeb10c3167972
timvisee <tim@visee.me>
2024-05-30 11:24:24 +0200

    Add basic steps in resharding driver

--- lib/collection/src/shards/resharding/driver.rs
+++ lib/collection/src/shards/resharding/driver.rs
@@ -30,6 +30,66 @@ pub async fn drive_resharding(
     _channel_service: ChannelService,
     _temp_dir: &Path,
 ) -> CollectionResult<bool> {
+    // Stage 1: init
+    if !check_init() {
+        stage_init()?;
+    }
+
+    // Stage 2: init
+    if !check_migrate_points() {
+        stage_migrate_points()?;
+    }
+
+    // Stage 3: replicate to match replication factor
+    if !check_replicate_replication_factor() {
+        stage_replicate_replication_factor()?;
+    }
+
+    // ...
+
     todo!("implement driver steps here");
+
     Ok(true)
+}
+
+/// Stage 1: init
+///
+/// Check whether we need to initialize the resharding process.
+fn check_init() -> bool {
+    todo!()
+}
+
+/// Stage 1: init
+///
+/// Do initialize the resharding process.
+fn stage_init() -> CollectionResult<()> {
+    todo!()
+}
+
+/// Stage 2: migrate points
+///
+/// Check whether we need to migrate points into the new shard.
+fn check_migrate_points() -> bool {
+    todo!()
+}
+
+/// Stage 2: migrate points
+///
+/// Do migrate points into the new shard.
+fn stage_migrate_points() -> CollectionResult<()> {
+    todo!()
+}
+
+/// Stage 3: replicate to match replication factor
+///
+/// Check whether we need to replicate to match replication factor.
+fn check_replicate_replication_factor() -> bool {
+    todo!()
+}
+
+/// Stage 3: replicate to match replication factor
+///
+/// Do replicate replicate to match replication factor.
+fn stage_replicate_replication_factor() -> CollectionResult<()> {
+    todo!()
 }
