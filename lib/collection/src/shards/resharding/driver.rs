9420d2851289ccb42bab19abf8dbe459e7ce5575
timvisee <tim@visee.me>
2024-05-31 17:00:33 +0200

    Add simple resharding driver state

--- lib/collection/src/shards/resharding/driver.rs
+++ lib/collection/src/shards/resharding/driver.rs
@@ -1,15 +1,88 @@
+use std::collections::HashMap;
 use std::path::Path;
 use std::sync::Arc;
 
 use parking_lot::Mutex;
+use serde::{Deserialize, Serialize};
 
 use super::tasks_pool::ReshardTaskProgress;
 use super::ReshardKey;
 use crate::operations::types::CollectionResult;
 use crate::shards::channel_service::ChannelService;
+use crate::shards::shard::{PeerId, ShardId};
 use crate::shards::shard_holder::LockedShardHolder;
 use crate::shards::transfer::ShardTransferConsensus;
 use crate::shards::CollectionId;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DriverState {
+    key: ReshardKey,
+
+    /// State of each peer we know about
+    peers: HashMap<PeerId, Stage>,
+
+    /// List of shard IDs successfully migrated to the new shard.
+    migrated_shard_ids: Vec<u32>,
+}
+
+impl DriverState {
+    pub fn new(key: ReshardKey) -> Self {
+        Self {
+            key,
+            peers: HashMap::new(),
+            migrated_shard_ids: vec![],
+        }
+    }
+
+    /// Check whether all peers have reached at least the given stage
+    fn all_peers_reached(&self, stage: Stage) -> bool {
+        self.peers.values().all(|peer_stage| peer_stage >= &stage)
+    }
+
+    /// Bump the state of all peers to at least the given stage.
+    fn bump_all_peers_to(&mut self, stage: Stage) {
+        self.peers
+            .values_mut()
+            .for_each(|peer_stage| *peer_stage = stage.max(*peer_stage));
+    }
+
+    /// List the shard IDs we still need to migrate.
+    pub fn shard_ids_to_migrate(&self) -> Vec<ShardId> {
+        self.source_shard_ids()
+            .filter(|shard_id| !self.migrated_shard_ids.contains(shard_id))
+            .collect()
+    }
+
+    /// Get all the shard IDs which points are sourced from.
+    pub fn source_shard_ids(&self) -> impl Iterator<Item = ShardId> {
+        0..self.key.shard_id
+    }
+}
+
+/// State of each node while resharding
+///
+/// Important: the states in this enum are ordered, from beginning to end!
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+#[serde(rename_all = "snake_case")]
+#[allow(non_camel_case_types)]
+enum Stage {
+    #[serde(rename = "init_start")]
+    S1_InitStart,
+    #[serde(rename = "init_end")]
+    S1_InitEnd,
+    #[serde(rename = "migrate_points_start")]
+    S2_MigratePointsStart,
+    #[serde(rename = "migrate_points_end")]
+    S2_MigratePointsEnd,
+    #[serde(rename = "replicate")]
+    S3_Replicate,
+    #[serde(rename = "commit_hash_ring")]
+    S4_CommitHashring,
+    #[serde(rename = "propagate_deletes")]
+    S5_PropagateDeletes,
+    #[serde(rename = "finalize")]
+    S6_Finalize,
+}
 
 /// Drive the resharding on the target node based on the given configuration
 ///
@@ -23,7 +96,7 @@ use crate::shards::CollectionId;
 /// This function is cancel safe.
 #[allow(clippy::too_many_arguments)]
 pub async fn drive_resharding(
-    _reshard_key: ReshardKey,
+    reshard_key: ReshardKey,
     _progress: Arc<Mutex<ReshardTaskProgress>>,
     _shard_holder: Arc<LockedShardHolder>,
     _consensus: &dyn ShardTransferConsensus,
@@ -31,9 +104,11 @@ pub async fn drive_resharding(
     _channel_service: ChannelService,
     _temp_dir: &Path,
 ) -> CollectionResult<bool> {
+    let mut state = DriverState::new(reshard_key);
+
     // Stage 1: init
-    if !check_init() {
-        stage_init()?;
+    if !completed_init(&state) {
+        stage_init(&mut state)?;
     }
 
     // Stage 2: init
@@ -65,15 +140,16 @@ pub async fn drive_resharding(
 /// Stage 1: init
 ///
 /// Check whether we need to initialize the resharding process.
-fn check_init() -> bool {
-    todo!()
+fn completed_init(state: &DriverState) -> bool {
+    state.all_peers_reached(Stage::S1_InitEnd)
 }
 
 /// Stage 1: init
 ///
 /// Do initialize the resharding process.
-fn stage_init() -> CollectionResult<()> {
-    todo!()
+fn stage_init(state: &mut DriverState) -> CollectionResult<()> {
+    state.bump_all_peers_to(Stage::S1_InitEnd);
+    todo!();
 }
 
 /// Stage 2: migrate points
