7cf84108c731d0cbcd761a21c224052173067f41
xzfc <5121426+xzfc@users.noreply.github.com>
2024-05-31 09:51:37 +0000

    Improve tools/*.sh (#4365)
    * Speedup tools/generate_openapi_models.sh
    
    * Improve tools/generate_grpc_docs.sh

--- tools/generate_openapi_models.sh
+++ tools/generate_openapi_models.sh
@@ -6,40 +6,45 @@ set -e
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
+# Fallback to dockerized utilities if they are not installed locally
+
+if command -v ytt &>/dev/null; then
+    sh_with_ytt() { sh -c "$1"; }
+else
+    sh_with_ytt() { docker run --rm -v "${PWD}":/workspace --entrypoint sh gerritk/ytt -c "$1"; }
+fi
+
+if ! yq --version 2>&1 | grep -q mikefarah; then
+    yq() { docker run --rm -v "${PWD}":/workdir mikefarah/yq "$@"; }
+fi
+
 # Apply `ytt` template engine to obtain OpenAPI definitions for REST endpoints
 
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-shards.ytt.yaml > ./openapi/openapi-shards.yaml
-
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-service.ytt.yaml > ./openapi/openapi-service.yaml
-
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-cluster.ytt.yaml > ./openapi/openapi-cluster.yaml
-
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-collections.ytt.yaml > ./openapi/openapi-collections.yaml
-
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-snapshots.ytt.yaml > ./openapi/openapi-snapshots.yaml
-
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-shard-snapshots.ytt.yaml > ./openapi/openapi-shard-snapshots.yaml
-
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-points.ytt.yaml > ./openapi/openapi-points.yaml
-
-docker run --rm -v "${PWD}":/workspace gerritk/ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-main.ytt.yaml > ./openapi/openapi-main.yaml
+sh_with_ytt '
+    set -e
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-shards.ytt.yaml > ./openapi/openapi-shards.yaml
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-service.ytt.yaml > ./openapi/openapi-service.yaml
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-cluster.ytt.yaml > ./openapi/openapi-cluster.yaml
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-collections.ytt.yaml > ./openapi/openapi-collections.yaml
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-snapshots.ytt.yaml > ./openapi/openapi-snapshots.yaml
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-shard-snapshots.ytt.yaml > ./openapi/openapi-shard-snapshots.yaml
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-points.ytt.yaml > ./openapi/openapi-points.yaml
+    ytt -f ./openapi/openapi.lib.yml -f ./openapi/openapi-main.ytt.yaml > ./openapi/openapi-main.yaml
+'
 
 # Generates models from internal service structures
 cargo run --package qdrant --bin schema_generator > ./openapi/schemas/AllDefinitions.json
 
-(
-    cd tools/schema2openapi/
-    docker build . --tag schema2openapi
-)
+docker build tools/schema2openapi --tag schema2openapi
 
 docker run --rm \
-    -v "$(pwd)/openapi/schemas/AllDefinitions.json:/app/schema.json" \
+    -v "$PWD/openapi/schemas/AllDefinitions.json:/app/schema.json" \
     schema2openapi | sed -e 's%#/definitions/%#/components/schemas/%g' >./openapi/models.json
 
-docker run --rm -i mikefarah/yq eval -P - <./openapi/models.json > ./openapi/models.yaml
+yq eval -o yaml -P ./openapi/models.json > ./openapi/models.yaml
 
 # Merge all *.yaml files together into a single-file OpenAPI definition
-docker run --rm -v "${PWD}":/workdir mikefarah/yq eval-all '. as $item ireduce ({}; . *+ $item)' \
+yq eval-all '. as $item ireduce ({}; . *+ $item)' \
   ./openapi/openapi-shards.yaml \
   ./openapi/openapi-service.yaml \
   ./openapi/openapi-cluster.yaml \
@@ -52,6 +57,6 @@ docker run --rm -v "${PWD}":/workdir mikefarah/yq eval-all '. as $item ireduce (
 
 docker run --rm -v "${PWD}"/openapi:/spec redocly/openapi-cli:v1.0.0-beta.88 lint openapi-merged.yaml
 
-docker run --rm -i mikefarah/yq eval -o=json - <./openapi/openapi-merged.yaml | jq > ./openapi/openapi-merged.json
+yq eval -o=json ./openapi/openapi-merged.yaml | jq > ./openapi/openapi-merged.json
 
 cp ./openapi/openapi-merged.json ./docs/redoc/master/openapi.json
