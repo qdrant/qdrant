ebc05b7c2107c591c9ce5f15fe8eb252f1b04f6a
Albert Safin <xzfcpw@gmail.com>
2024-05-31 00:50:21 +0000

    Improve tools/generate_grpc_docs.sh

--- tools/generate_grpc_docs.sh
+++ tools/generate_grpc_docs.sh
@@ -6,28 +6,27 @@ set -e
 cd "$(dirname "$0")/../"
 
 # Create a temporary directory and store its name in a variable.
-TEMPD=$(mktemp -d)
+TEMPD=$(mktemp -d -t qdrant_docs.XXXXXXXXXX)
+trap 'rm -rf -- "$TEMPD"' EXIT
 
-cp -r $(pwd)/lib/api/src/grpc/proto/* ${TEMPD}
+cp -r "$PWD"/lib/api/src/grpc/proto/* "$TEMPD"
 
 # Do not generate docs for internal services
-rm ${TEMPD}/'collections_internal_service.proto'
-rm ${TEMPD}/'points_internal_service.proto'
-rm ${TEMPD}/'shard_snapshots_service.proto'
-rm ${TEMPD}/'raft_service.proto'
+rm "$TEMPD/collections_internal_service.proto"
+rm "$TEMPD/points_internal_service.proto"
+rm "$TEMPD/shard_snapshots_service.proto"
+rm "$TEMPD/raft_service.proto"
 
-cat ${TEMPD}/qdrant.proto \
+cat "$TEMPD/qdrant.proto" \
   | grep -v 'collections_internal_service.proto' \
   | grep -v 'points_internal_service.proto' \
   | grep -v 'shard_snapshots_service.proto' \
   | grep -v 'raft_service.proto'\
-   > ${TEMPD}/qdrant.proto.tmp
-mv ${TEMPD}/qdrant.proto.tmp ${TEMPD}/qdrant.proto
+   > "$TEMPD/qdrant.proto.tmp"
+mv "$TEMPD/qdrant.proto.tmp" "$TEMPD/qdrant.proto"
 
 docker run --rm \
-  -v $(pwd)/docs/grpc:/out \
-  -v ${TEMPD}:/protos \
+  -u "$(id -u):$(id -g)" \
+  -v "$PWD/docs/grpc":/out \
+  -v "$TEMPD":/protos \
   pseudomuto/protoc-gen-doc --doc_opt=markdown,docs.md
-
-# <https://github.com/pseudomuto/protoc-gen-doc/issues/383>
-sudo chown -R "$USER:$(id -g -n)" $(pwd)/docs/grpc
