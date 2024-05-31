9a0d7572bacd1b7e3ee6f12a0b8f56b793e80a0d
Luis Cossío <luis.cossio@outlook.com>
2024-05-31 12:37:38 -0400

    cargo fmt + use helpers::time

--- src/actix/api/mod.rs
+++ src/actix/api/mod.rs
@@ -4,6 +4,7 @@ pub mod count_api;
 pub mod debug_api;
 pub mod discovery_api;
 pub mod issues_api;
+pub mod query_api;
 pub mod read_params;
 pub mod recommend_api;
 pub mod retrieve_api;
@@ -12,7 +13,6 @@ pub mod service_api;
 pub mod shards_api;
 pub mod snapshot_api;
 pub mod update_api;
-pub mod query_api;
 
 use common::validation::validate_collection_name;
 use serde::Deserialize;
--- src/actix/api/query_api.rs
+++ src/actix/api/query_api.rs
@@ -1,13 +1,14 @@
-use actix_web::{post, web::{self, Json, Path, Query}, Responder};
+use actix_web::web::{self, Json, Path, Query};
+use actix_web::{post, Responder};
 use api::rest::QueryRequest;
 use collection::operations::shard_selector_internal::ShardSelectorInternal;
 use itertools::Itertools;
 use storage::dispatcher::Dispatcher;
-use tokio::time::Instant;
 
-use crate::actix::{auth::ActixAccess, helpers::process_response};
-
-use super::{read_params::ReadParams, CollectionPath};
+use super::read_params::ReadParams;
+use super::CollectionPath;
+use crate::actix::auth::ActixAccess;
+use crate::actix::helpers;
 
 #[post("/collections/{name}/points/query")]
 async fn query_points(
@@ -17,33 +18,34 @@ async fn query_points(
     params: Query<ReadParams>,
     ActixAccess(access): ActixAccess,
 ) -> impl Responder {
-    let timing = Instant::now();
+    helpers::time(async move {
+        let QueryRequest {
+            internal: query_request,
+            shard_key,
+        } = request.into_inner();
 
-    let QueryRequest {
-        internal: query_request,
-        shard_key,
-    } = request.into_inner();
+        let shard_selection = match shard_key {
+            None => ShardSelectorInternal::All,
+            Some(shard_keys) => shard_keys.into(),
+        };
 
-    let shard_selection = match shard_key {
-        None => ShardSelectorInternal::All,
-        Some(shard_keys) => shard_keys.into(),
-    };
-
-    let response = dispatcher.toc(&access).query(
-        &collection.name,
-        query_request.into(),
-        params.consistency,
-        shard_selection,
-        access,
-        // TODO(universal-query): add params.timeout()
-    )
+        dispatcher
+            .toc(&access)
+            .query(
+                &collection.name,
+                query_request.into(),
+                params.consistency,
+                shard_selection,
+                access,
+                // TODO(universal-query): add params.timeout()
+            )
+            .await
+            .map(|scored_points| {
+                scored_points
+                    .into_iter()
+                    .map(api::rest::ScoredPoint::from)
+                    .collect_vec()
+            })
+    })
     .await
-    .map(|scored_points| {
-        scored_points
-            .into_iter()
-            .map(api::rest::ScoredPoint::from)
-            .collect_vec()
-    });
-
-    process_response(response, timing)
 }
