d35319c663af1abe50b083a5ea862cb243892db5
Luis Cossío <luis.cossio@qdrant.com>
2024-06-10 11:27:28 -0400

    universal-query: validate REST query request (#4388)
    Implements `Validate` for REST-facing `QueryRequest`

--- lib/api/src/rest/validate.rs
+++ lib/api/src/rest/validate.rs
@@ -1,6 +1,8 @@
-use validator::Validate;
+use common::validation::validate_multi_vector;
+use validator::{Validate, ValidationError};
 
 use super::schema::{BatchVectorStruct, Vector, VectorStruct};
+use super::{Fusion, OrderByInterface, Query, QueryInterface, RecommendInput, VectorInput};
 use crate::rest::NamedVectorStruct;
 
 impl Validate for VectorStruct {
@@ -39,6 +41,88 @@ impl Validate for NamedVectorStruct {
             NamedVectorStruct::Default(_) => Ok(()),
             NamedVectorStruct::Dense(_) => Ok(()),
             NamedVectorStruct::Sparse(v) => v.validate(),
+        }
+    }
+}
+
+impl Validate for QueryInterface {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        match self {
+            QueryInterface::Nearest(vector) => vector.validate(),
+            QueryInterface::Query(query) => query.validate(),
+        }
+    }
+}
+
+impl Validate for Query {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        match self {
+            Query::Nearest(vector) => vector.validate(),
+            Query::Recommend(recommend) => recommend.validate(),
+            Query::Discover(discover) => discover.validate(),
+            Query::Context(context) => context.validate(),
+            Query::Fusion(fusion) => fusion.validate(),
+            Query::OrderBy(order_by) => order_by.validate(),
+        }
+    }
+}
+
+impl Validate for VectorInput {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        match self {
+            VectorInput::Id(_id) => Ok(()),
+            VectorInput::DenseVector(_dense) => Ok(()),
+            VectorInput::SparseVector(sparse) => sparse.validate(),
+            VectorInput::MultiDenseVector(multi) => validate_multi_vector(multi),
+        }
+    }
+}
+
+impl Validate for RecommendInput {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        let no_positives = self
+            .positives
+            .as_ref()
+            .map(|p| p.is_empty())
+            .unwrap_or(true);
+        let no_negatives = self
+            .positives
+            .as_ref()
+            .map(|n| n.is_empty())
+            .unwrap_or(true);
+
+        if no_positives && no_negatives {
+            let mut errors = validator::ValidationErrors::new();
+            errors.add(
+                "positives, negatives",
+                ValidationError::new(
+                    "At least one positive or negative vector/id must be provided",
+                ),
+            );
+            return Err(errors);
+        }
+
+        for item in self.iter() {
+            item.validate()?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Validate for Fusion {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        match self {
+            Fusion::Rrf => Ok(()),
+        }
+    }
+}
+
+impl Validate for OrderByInterface {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        match self {
+            OrderByInterface::Key(_key) => Ok(()), // validated during parsing
+            OrderByInterface::Struct(order_by) => order_by.validate(),
         }
     }
 }
