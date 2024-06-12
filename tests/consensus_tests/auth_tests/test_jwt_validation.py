118721987dca6eeef8506fe525667614aec10815
Luis Cossío <luis.cossio@qdrant.com>
2024-06-12 10:00:03 -0400

    universal-query: test payload claim (#4451)
    Adds jwt access tests for the payload claim in queries

--- tests/consensus_tests/auth_tests/test_jwt_validation.py
+++ tests/consensus_tests/auth_tests/test_jwt_validation.py
@@ -2,9 +2,21 @@ import pytest
 import requests
 from consensus_tests import fixtures
 
-from .utils import API_KEY_HEADERS, REST_URI, SECRET, encode_jwt
+from .utils import API_KEY_HEADERS, READ_ONLY_API_KEY, REST_URI, SECRET, encode_jwt
 
 COLL_NAME = "jwt_test_collection"
+
+
+@pytest.fixture(scope="module")
+def berlin_token():
+    return encode_jwt(
+        {
+            "access": [
+                {"collection": COLL_NAME, "access": "r", "payload": {"city": "Berlin"}}
+            ]
+        },
+        SECRET,
+    )
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -103,20 +115,14 @@ def test_value_exists_claim():
     fixtures.drop_collection(REST_URI, validation_collection, headers=API_KEY_HEADERS)
 
 
-def test_payload_filters_queries():
-    token = encode_jwt(
-        {"access": [{"collection": COLL_NAME, "access": "r", "payload": {"city": "Berlin"}}]},
-        SECRET,
-    )
-
-    # With scroll
+def test_payload_filters_scroll(berlin_token):
     res = requests.post(
         f"{REST_URI}/collections/{COLL_NAME}/points/scroll",
         json={
             "limit": 100,
             "with_payload": True,
         },
-        headers={"Authorization": f"Bearer {token}"},
+        headers={"Authorization": f"Bearer {berlin_token}"},
     )
     res.raise_for_status()
 
@@ -126,7 +132,8 @@ def test_payload_filters_queries():
 
     assert cities_in_payload == {"Berlin"}
 
-    # With search
+
+def test_payload_filters_search(berlin_token):
     res = requests.post(
         f"{REST_URI}/collections/{COLL_NAME}/points/search",
         json={
@@ -134,19 +141,22 @@ def test_payload_filters_queries():
             "limit": 100,
             "with_payload": True,
         },
-        headers={"Authorization": f"Bearer {token}"},
+        headers={"Authorization": f"Bearer {berlin_token}"},
     )
     res.raise_for_status()
 
-    cities_in_payload = set(point["payload"].get("city") for point in res.json()["result"])
+    cities_in_payload = set(
+        point["payload"].get("city") for point in res.json()["result"]
+    )
 
     assert cities_in_payload == {"Berlin"}
 
-    # With count
+
+def test_payload_filters_count(berlin_token):
     res = requests.post(
         f"{REST_URI}/collections/{COLL_NAME}/points/count",
         json={},
-        headers={"Authorization": f"Bearer {token}"},
+        headers={"Authorization": f"Bearer {berlin_token}"},
     )
     res.raise_for_status()
 
@@ -158,3 +168,123 @@ def test_payload_filters_queries():
     res_expected.raise_for_status()
 
     assert res.json()["result"] == res_expected.json()["result"]
+
+
+@pytest.mark.parametrize(
+    "prefetch",
+    [
+        None,
+        {"query": [0.1, 0.2, 0.3, 0.4]},
+        [{"query": [0.1, 0.2, 0.3, 0.4]}, {"limit": 100}],
+    ],
+)
+def test_payload_filters_query(berlin_token, prefetch):
+    # Check that there are more cities in the payload
+    res = requests.post(
+        f"{REST_URI}/collections/{COLL_NAME}/points/query",
+        json={
+            "prefetch": prefetch,
+            "query": [0.1, 0.2, 0.3, 0.4],
+            "limit": 100,
+            "with_payload": True,
+        },
+        headers=API_KEY_HEADERS,
+    )
+
+    cities_in_payload = set(
+        point["payload"].get("city") for point in res.json()["result"]
+    )
+
+    assert len(cities_in_payload) > 1
+
+    # Now check that there is only Berlin with the payload claim in the token
+    res = requests.post(
+        f"{REST_URI}/collections/{COLL_NAME}/points/query",
+        json={
+            "prefetch": prefetch,
+            "query": [0.1, 0.2, 0.3, 0.4],
+            "limit": 100,
+            "with_payload": True,
+        },
+        headers={"Authorization": f"Bearer {berlin_token}"},
+    )
+
+    cities_in_payload = set(
+        point["payload"].get("city") for point in res.json()["result"]
+    )
+
+    assert cities_in_payload == {"Berlin"}
+
+
+def test_payload_claim_fails_with_ids_in_recommend(berlin_token):
+    res = requests.post(
+        f"{REST_URI}/collections/{COLL_NAME}/points/recommend",
+        json={"positive": [1], "limit": 10},
+        headers={"Authorization": f"Bearer {berlin_token}"},
+    )
+    assert res.status_code == 403, res.json()
+    assert (
+        res.json()["status"]["error"]
+        == 'Forbidden: This operation is not allowed when "payload" restriction is present for collection jwt_test_collection'
+    )
+
+
+@pytest.mark.parametrize("target", [1, None])
+@pytest.mark.parametrize("context", [None, [{"positive": 1, "negative": 3}]])
+def test_payload_claim_fails_with_ids_in_discover(berlin_token, target, context):
+    if not target and not context:
+        return
+
+    res = requests.post(
+        f"{REST_URI}/collections/{COLL_NAME}/points/discover",
+        json={"target": target, "context": context, "limit": 10},
+        headers={"Authorization": f"Bearer {berlin_token}"},
+    )
+    assert res.status_code == 403, res.json()
+    assert (
+        res.json()["status"]["error"]
+        == 'Forbidden: This operation is not allowed when "payload" restriction is present for collection jwt_test_collection'
+    )
+
+
+@pytest.mark.parametrize(
+    "query",
+    [
+        1,
+        {"nearest": 1},
+        {"recommend": {"positive": [1]}},
+        {"recommend": {"negative": [1]}},
+        {"recommend": {"negative": [1], "strategy": "best_score"}},
+        {"discover": {"target": 1, "context": [{"positive": 1, "negative": 2}]}},
+        {"context": [{"positive": 1, "negative": 2}]},
+    ],
+)
+@pytest.mark.parametrize("in_prefetch", [False, True])
+def test_payload_claim_fails_with_ids_in_query(berlin_token, query, in_prefetch):
+    if in_prefetch:
+        res = requests.post(
+            f"{REST_URI}/collections/{COLL_NAME}/points/query",
+            json={
+                "prefetch": {"query": query},
+                "query": [0.1, 0.2, 0.3, 0.4],
+                "limit": 100,
+                "with_payload": True,
+            },
+            headers={"Authorization": f"Bearer {berlin_token}"},
+        )
+    else:
+        res = requests.post(
+            f"{REST_URI}/collections/{COLL_NAME}/points/query",
+            json={
+                "query": query,
+                "limit": 100,
+                "with_payload": True,
+            },
+            headers={"Authorization": f"Bearer {berlin_token}"},
+        )
+
+    assert res.status_code == 403, res.json()
+    assert (
+        res.json()["status"]["error"]
+        == 'Forbidden: This operation is not allowed when "payload" restriction is present for collection jwt_test_collection'
+    )
