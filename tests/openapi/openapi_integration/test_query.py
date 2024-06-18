7189f9815886ca0952e1f20d05d5b4227e07c33d
Luis Cossío <luis.cossio@qdrant.com>
2024-06-18 10:27:24 -0400

    use get instead of indexing (#4496)

--- tests/openapi/openapi_integration/test_query.py
+++ tests/openapi/openapi_integration/test_query.py
@@ -283,5 +283,5 @@ def test_basic_rrf():
     # rrf order is not deterministic with same scores, so we need to sort by id
     for expected, result in zip(sorted(rrf_expected, key=get_id), sorted(rrf_result, key=get_id)):
         assert expected["id"] == result["id"]
-        assert expected["payload"] == result["payload"]
+        assert expected.get("payload") == result.get("payload")
         assert isclose(expected["score"], result["score"], rel_tol=1e-5)
