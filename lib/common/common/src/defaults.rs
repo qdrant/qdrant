7bd4c8e07055df1b136b569c7a7e0296d3fa6cdd
timvisee <tim@visee.me>
2024-06-25 13:47:52 +0200

    Bump version to 1.9.7

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Qdrant version string
-pub const QDRANT_VERSION_STRING: &str = "1.9.6";
+pub const QDRANT_VERSION_STRING: &str = "1.9.7";
 
 lazy_static! {
     /// Current Qdrant semver version
--- tools/missed_cherry_picks.sh
+++ tools/missed_cherry_picks.sh
@@ -8,7 +8,7 @@
 set -euo pipefail
 
 # Ignore all commits upto and including this commit hash on dev
-IGNORE_UPTO=15de581d15530ba1b07988dcc6767a5039979e63
+IGNORE_UPTO=b0b21fc0a8707d5581f33f97162892eb613790da
 
 # Fetch latest branch info from remote
 git fetch -q origin master
