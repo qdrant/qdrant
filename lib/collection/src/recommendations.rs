8cab109978f59f6ef23937e72d1dad56e52da259
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-06-25 14:15:15 +0200

    Avg multivector (#4542)
    * avg multivector
    
    * integration test
    
    * remove unwrap

--- lib/collection/src/recommendations.rs
+++ lib/collection/src/recommendations.rs
@@ -5,8 +5,8 @@ use std::time::Duration;
 use api::rest::RecommendStrategy;
 use itertools::Itertools;
 use segment::data_types::vectors::{
-    DenseVector, NamedQuery, NamedVectorStruct, Vector, VectorElementType, VectorRef,
-    DEFAULT_VECTOR_NAME,
+    DenseVector, NamedQuery, NamedVectorStruct, TypedMultiDenseVector, Vector, VectorElementType,
+    VectorRef, DEFAULT_VECTOR_NAME,
 };
 use segment::types::{
     Condition, ExtendedPointId, Filter, HasIdCondition, PointIdType, ScoredPoint,
@@ -33,8 +33,10 @@ use crate::operations::types::{
 fn avg_vectors<'a>(vectors: impl IntoIterator<Item = VectorRef<'a>>) -> CollectionResult<Vector> {
     let mut avg_dense = DenseVector::default();
     let mut avg_sparse = SparseVector::default();
+    let mut avg_multi: Option<TypedMultiDenseVector<VectorElementType>> = None;
     let mut dense_count = 0;
     let mut sparse_count = 0;
+    let mut multi_count = 0;
     for vector in vectors {
         match vector {
             VectorRef::Dense(vector) => {
@@ -51,36 +53,48 @@ fn avg_vectors<'a>(vectors: impl IntoIterator<Item = VectorRef<'a>>) -> Collecti
                 sparse_count += 1;
                 avg_sparse = vector.combine_aggregate(&avg_sparse, |v1, v2| v1 + v2);
             }
-            VectorRef::MultiDense(_) => {
-                // TODO(colbert)
-                return Err(CollectionError::bad_input(
-                    "MultiDenseVector is not supported".to_owned(),
+            VectorRef::MultiDense(vector) => {
+                multi_count += 1;
+                avg_multi = Some(avg_multi.map_or_else(
+                    || vector.to_owned(),
+                    |mut avg_multi| {
+                        avg_multi
+                            .flattened_vectors
+                            .extend_from_slice(vector.flattened_vectors);
+                        avg_multi
+                    },
                 ));
             }
         }
     }
 
-    match (dense_count, sparse_count) {
+    match (dense_count, sparse_count, multi_count) {
         // TODO(sparse): what if vectors iterator is empty? We added CollectionError::BadRequest,
         // but it's not clear if it's the best solution.
         // Currently it's hard to return an zeroed vector, because we don't know its type: dense or sparse.
-        (0, 0) => Err(CollectionError::bad_input(
+        (0, 0, 0) => Err(CollectionError::bad_input(
             "Positive vectors should not be empty with `average` strategy".to_owned(),
         )),
-        (_, 0) => {
+        (_, 0, 0) => {
             for item in &mut avg_dense {
                 *item /= dense_count as VectorElementType;
             }
-            Ok(avg_dense.into())
+            Ok(Vector::from(avg_dense))
         }
-        (0, _) => {
+        (0, _, 0) => {
             for item in &mut avg_sparse.values {
                 *item /= sparse_count as VectorElementType;
             }
-            Ok(avg_sparse.into())
+            Ok(Vector::from(avg_sparse))
         }
-        (_, _) => Err(CollectionError::bad_input(
-            "Can't average dense and sparse vectors together".to_owned(),
+        (0, 0, _) => match avg_multi {
+            Some(avg_multi) => Ok(Vector::from(avg_multi)),
+            None => Err(CollectionError::bad_input(
+                "Positive vectors should not be empty with `average` strategy".to_owned(),
+            )),
+        },
+        (_, _, _) => Err(CollectionError::bad_input(
+            "Can't average vectors with different types".to_owned(),
         )),
     }
 }
@@ -98,8 +112,13 @@ fn merge_positive_and_negative_avg(positive: Vector, negative: Vector) -> Collec
         (Vector::Sparse(positive), Vector::Sparse(negative)) => Ok(positive
             .combine_aggregate(&negative, |pos, neg| pos + pos - neg)
             .into()),
+        (Vector::MultiDense(mut positive), Vector::MultiDense(negative)) => {
+            // merge positive and negative vectors as concatenated vectors with negative vectors negated
+            positive.flattened_vectors.extend(negative.flattened_vectors.into_iter().map(|x| -x));
+            Ok(Vector::MultiDense(positive))
+        },
         _ => Err(CollectionError::bad_input(
-            "Positive and negative vectors should be of the same type, either all dense or all sparse".to_owned(),
+            "Positive and negative vectors should be of the same type, either all dense or all sparse or all multi".to_owned(),
         )),
     }
 }
---
+++ tests/openapi/openapi_integration/test_recommendation_multivector.py
@@ -0,0 +1,213 @@
+import pytest
+
+from .helpers.collection_setup import drop_collection
+from .helpers.helpers import request_with_validation
+
+collection_name = "test_recommend_multivector"
+
+@pytest.fixture(autouse=True)
+def setup(on_disk_vectors):
+    multivector_collection_setup(collection_name=collection_name, on_disk_vectors=on_disk_vectors)
+    yield
+    drop_collection(collection_name=collection_name)
+
+def multivector_collection_setup(
+        collection_name='test_collection',
+        on_disk_vectors=False):
+    drop_collection(collection_name=collection_name)
+
+    response = request_with_validation(
+        api='/collections/{collection_name}',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        body={
+            "vectors": {
+                "size": 2,
+                "distance": "Dot",
+                "on_disk": on_disk_vectors,
+                "multivector_config": {
+                    "comparator": "max_sim"
+                }
+            },
+        }
+    )
+    assert response.ok
+
+    # batch upsert
+    response = request_with_validation(
+        api='/collections/{collection_name}/points',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        query_params={'wait': 'true'},
+        body={
+            "points": [
+                {
+                    "id": 1,
+                    "vector": [
+                        [-1, 1],
+                        [-2, 2],
+                        [-3, 3]
+                    ]
+
+                },
+                {
+                    "id": 2,
+                    "vector": [
+                        [-1.5, 1.5],
+                        [-2.5, 2.5],
+                        [-3.5, 3.5]
+                    ]
+                },
+                {
+                    "id": 3,
+                    "vector": [
+                        [-1.7, 1.7],
+                        [-2.7, 2.7],
+                        [-3.7, 3.7]
+                    ]
+                },
+            ]
+        }
+    )
+    assert response.ok
+
+    response = request_with_validation(
+        api='/collections/{collection_name}',
+        method="GET",
+        path_params={'collection_name': collection_name},
+    )
+    assert response.ok
+
+def test_multi_default_is_avg_vector():
+    params = {
+        "positive": [[1, 2]],
+        "negative": [[3, 4]],
+        "exact": True,
+        "limit": 10,
+    }
+
+    default_response = request_with_validation(
+        api="/collections/{collection_name}/points/recommend",
+        method="POST",
+        path_params={"collection_name": collection_name},
+        body={
+            **params,
+        },
+    )
+    assert default_response.ok
+
+    # we should only get 3 because we don't have more
+    assert len(default_response.json()["result"]) == 3
+
+    avg_response = request_with_validation(
+        api="/collections/{collection_name}/points/recommend",
+        method="POST",
+        path_params={"collection_name": collection_name},
+        body={
+            **params,
+            "strategy": "average_vector",
+        },
+    )
+    assert avg_response.ok
+    assert len(avg_response.json()["result"]) == 3
+
+    assert default_response.json()["result"] == avg_response.json()["result"]
+
+
+def test_multi_single_vs_batch():
+    # Bunch of valid examples
+    params_list = [
+        {
+            "positive": [1],
+            "negative": [3],
+            "limit": 1,
+        },
+        {
+            # no negative because it's optional with this strategy
+            "negative": [1, 2],
+            "exact": True,
+            "strategy": "best_score",
+            "limit": 1,
+        },
+        {
+            "positive": [1],
+            "negative": [],
+            "exact": True,
+            "strategy": "average_vector",
+            "limit": 1,
+        },
+    ]
+
+    batch_response = request_with_validation(
+        api="/collections/{collection_name}/points/recommend/batch",
+        method="POST",
+        path_params={"collection_name": collection_name},
+        body={"searches": params_list},
+    )
+
+    assert batch_response.ok
+    assert len(batch_response.json()["result"]) == len(params_list)
+
+    # Compare against sequential single searches
+    for i, params in enumerate(params_list):
+        single_response = request_with_validation(
+            api="/collections/{collection_name}/points/recommend",
+            method="POST",
+            path_params={"collection_name": collection_name},
+            body=params,
+        )
+        assert single_response.ok
+        assert single_response.json()["result"] == batch_response.json()["result"][i]
+
+
+def test_multi_without_positives():
+    def req_with_positives(positive, strategy=None):
+        if strategy is None:
+            strat_dict = {}
+        else:
+            strat_dict = {"strategy": strategy}
+
+        return request_with_validation(
+            api="/collections/{collection_name}/points/recommend",
+            method="POST",
+            path_params={"collection_name": collection_name},
+            body={
+                "positive": positive,
+                **strat_dict,
+                "limit": 2,
+            },
+        )
+
+    # Assert this is valid
+    response = req_with_positives([[1, 2]])
+    assert response.ok
+
+    # But all these are not
+    response = req_with_positives([[]])
+    assert response.status_code == 400
+
+    response = req_with_positives([[]], "average_vector")
+    assert response.status_code == 400
+
+    # Also no negative and no positive is invalid with best_score
+    response = req_with_positives([], "best_score")
+    assert response.status_code == 400
+
+
+def test_multi_best_score_works_with_only_negatives():
+    response = request_with_validation(
+        api="/collections/{collection_name}/points/recommend",
+        method="POST",
+        path_params={"collection_name": collection_name},
+        body={
+            "negative": [[1, 2]],
+            "strategy": "best_score",
+            "limit": 5,
+        },
+    )
+    assert response.ok
+    assert len(response.json()["result"]) == 3
+
+    # All scores should be negative
+    for result in response.json()["result"]:
+        assert result["score"] < 0
