c88f10c19fbeff3ebf5231e5fa42481f90d49fe6
tellet-q <166374656+tellet-q@users.noreply.github.com>
2024-06-29 22:40:18 +0200

    Add test for OOD during indexing (#4267)
    * Add test for OOD during indexing
    
    * Start indexing right after 1st OOD message
    
    * Only send search request after insert loop
    
    * Fail early when encountering out-of-storage during optimization (#4578)
    
    * fs4@0.8.4
    
    * fail early on low storage
    
    * move `dir_size` to `common`
    
    * move the ood bailout to `SegmentOptimizer::optimized_segment_builder`
    
    * drop dead code
    
    * move dir_size to common subcrate
    
    ---------
    
    Co-authored-by: generall <andrey@vasnetsov.com>
    
    ---------
    
    Co-authored-by: xhjkl <xhjkl@users.noreply.github.com>
    Co-authored-by: generall <andrey@vasnetsov.com>

--- lib/collection/src/collection_manager/optimizers/segment_optimizer.rs
+++ lib/collection/src/collection_manager/optimizers/segment_optimizer.rs
@@ -4,6 +4,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
 use common::cpu::CpuPermit;
+use common::disk::dir_size;
 use io::storage_version::StorageVersion;
 use itertools::Itertools;
 use parking_lot::{Mutex, RwLock, RwLockUpgradableReadGuard};
@@ -118,6 +119,10 @@ pub trait SegmentOptimizer {
         // }
         let mut bytes_count_by_vector_name = HashMap::new();
 
+        // Counting up how much space do the segments being optimized actually take on the fs.
+        // If there was at least one error while reading the size, this will be `None`.
+        let mut space_occupied = Some(0u64);
+
         for segment in optimizing_segments {
             let segment = match segment {
                 LockedSegment::Original(segment) => segment,
@@ -133,6 +138,28 @@ pub trait SegmentOptimizer {
                 let vector_size = locked_segment.available_vectors_size_in_bytes(&vector_name)?;
                 let size = bytes_count_by_vector_name.entry(vector_name).or_insert(0);
                 *size += vector_size;
+            }
+
+            space_occupied = space_occupied
+                .and_then(|x| dir_size(locked_segment.data_path()).ok().map(|y| x + y));
+        }
+
+        let space_needed = space_occupied.map(|x| 2 * x);
+        let space_available = fs4::available_space(self.temp_path()).ok();
+
+        match (space_available, space_needed) {
+            (Some(space_available), Some(space_needed)) => {
+                if space_available < space_needed {
+                    return Err(CollectionError::service_error(
+                        "Not enough space available for optimization".to_string(),
+                    ));
+                }
+            }
+            _ => {
+                log::warn!(
+                    "Could not estimate available storage space in `{}`; will try optimizing anyway",
+                    self.name()
+                );
             }
         }
 
@@ -511,9 +538,8 @@ pub trait SegmentOptimizer {
             proxy_ids
         };
 
-        check_process_stopped(stopped).map_err(|error| {
+        check_process_stopped(stopped).inspect_err(|_| {
             self.handle_cancellation(&segments, &proxy_ids, &tmp_segment);
-            error
         })?;
 
         // ---- SLOW PART -----
