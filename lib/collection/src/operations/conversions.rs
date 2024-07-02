10beec7c1d707c03809a80dc8febee55c4d3be70
timvisee <tim@visee.me>
2024-07-02 16:36:28 +0200

    Expose resharding operations in cluster info

--- lib/collection/src/operations/conversions.rs
+++ lib/collection/src/operations/conversions.rs
@@ -1543,12 +1543,11 @@ impl From<CollectionClusterInfo> for api::grpc::qdrant::CollectionClusterInfoRes
                 .into_iter()
                 .map(|shard| shard.into())
                 .collect(),
-            // TODO(resharding): enable on release:
-            // resharding_operations: value
-            //     .resharding_operations
-            //     .into_iter()
-            //     .map(|info| info.into())
-            //     .collect(),
+            resharding_operations: value
+                .resharding_operations
+                .into_iter()
+                .map(|info| info.into())
+                .collect(),
         }
     }
 }
