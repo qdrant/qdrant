4c6caf840256f825e034058685af5d45d4ed0648
timvisee <tim@visee.me>
2024-07-03 10:38:48 +0200

    Add resharding replica balancing test

--- tests/consensus_tests/test_resharding.py
+++ tests/consensus_tests/test_resharding.py
@@ -62,3 +62,54 @@ def test_resharding(tmp_path: pathlib.Path):
         for uri in peer_api_uris:
             assert check_collection_local_shards_count(uri, COLLECTION_NAME, shard_count)
             assert get_collection_local_shards_point_count(uri, COLLECTION_NAME) == num_points
+
+
+# Test resharding shard balancing.
+#
+# Sets up a 3 node cluster and a collection with 1 shard and 2 replicas.
+# Performs resharding 7 times and asserts the shards replicas are evenly
+# balanced across all nodes.
+#
+# In this case the replicas are balanced on the second and third node. The first
+# node has all shards because we explicitly set it as shard target all the time.
+def test_resharding_balance(tmp_path: pathlib.Path):
+    assert_project_root()
+
+    # Prevent optimizers messing with point counts
+    env={
+        "QDRANT__STORAGE__OPTIMIZERS__MAX_OPTIMIZATION_THREADS": "0",
+    }
+
+    peer_api_uris, _peer_dirs, _bootstrap_uri = start_cluster(tmp_path, 3, None, extra_env=env)
+    first_peer_id = get_cluster_info(peer_api_uris[0])['peer_id']
+
+    # Create collection, insert points
+    create_collection(peer_api_uris[0], shard_number=1, replication_factor=2)
+    wait_collection_exists_and_active_on_all_peers(
+        collection_name=COLLECTION_NAME,
+        peer_api_uris=peer_api_uris
+    )
+    upsert_random_points(peer_api_uris[0], 100)
+
+    # Reshard 5 times in sequence
+    for _shard_count in range(2, 7):
+        # Start resharding
+        r = requests.post(
+            f"{peer_api_uris[0]}/collections/{COLLECTION_NAME}/cluster", json={
+                "start_resharding": {
+                    "peer_id": first_peer_id,
+                    "shard_key": None,
+                }
+            })
+        assert_http_ok(r)
+
+        # Wait for resharding operation to start and stop
+        wait_for_collection_resharding_operations_count(peer_api_uris[0], COLLECTION_NAME, 1)
+        wait_for_collection_resharding_operations_count(peer_api_uris[0], COLLECTION_NAME, 0)
+
+    # We must end up with:
+    # - 6 shards on first node, it was the resharding target
+    # - 3 shards on the other two nodes, 6 replicas spread over 2 nodes
+    assert check_collection_local_shards_count(peer_api_uris[0], COLLECTION_NAME, 6)
+    for uri in peer_api_uris[1:]:
+        assert check_collection_local_shards_count(uri, COLLECTION_NAME, 3)
