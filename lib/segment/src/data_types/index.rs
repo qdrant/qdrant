8187864cf58010980feed5df105fa4f9a5daaf4c
Albert Safin <xzfcpw@gmail.com>
2024-07-03 19:32:39 +0000

    Formatting

--- lib/segment/src/data_types/index.rs
+++ lib/segment/src/data_types/index.rs
@@ -15,8 +15,10 @@ pub enum IntegerIndexType {
 pub struct IntegerIndexParams {
     // Required for OpenAPI schema without anonymous types, versus #[serde(tag = "type")]
     pub r#type: IntegerIndexType,
+
     /// If true - support direct lookups.
     pub lookup: bool,
+
     /// If true - support ranges filters.
     pub range: bool,
 }
@@ -45,13 +47,17 @@ pub enum TokenizerType {
 pub struct TextIndexParams {
     // Required for OpenAPI schema without anonymous types, versus #[serde(tag = "type")]
     pub r#type: TextIndexType,
+
     #[serde(default)]
     pub tokenizer: TokenizerType,
+
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub min_token_len: Option<usize>,
+
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub max_token_len: Option<usize>,
+
+    /// If true, lowercase all tokens. Default: true.
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    /// If true, lowercase all tokens. Default: true
     pub lowercase: Option<bool>,
 }
