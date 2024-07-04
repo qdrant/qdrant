fae4b46349e0dcfaf94dea3a1358b690aa2297d4
timvisee <tim@visee.me>
2024-07-04 15:51:21 +0200

    Test resharding with concurrent updates

--- tests/consensus_tests/test_resharding.py
+++ tests/consensus_tests/test_resharding.py
@@ -146,3 +146,130 @@ def test_resharding_balance(tmp_path: pathlib.Path):
     assert check_collection_local_shards_count(peer_api_uris[0], COLLECTION_NAME, 6)
     for uri in peer_api_uris[1:]:
         assert check_collection_local_shards_count(uri, COLLECTION_NAME, 3)
+
+
+# Test resharding with concurrent updates.
+#
+# This performs resharding a few times while sending point updates to all peers
+# concurrently. At the end of the whole process it asserts the expected point
+# count.
+#
+# The concurrent update tasks consist of:
+# - 3 threads upserting new points on all peers
+# - 1 threads updating existing points on the first peer
+# - 2 threads deleting points on the first two peers
+def test_resharding_concurrent_updates(tmp_path: pathlib.Path):
+    assert_project_root()
+
+    num_points = 1000
+    num_upserts = 100
+    num_deletes = 33
+
+    # Prevent optimizers messing with point counts
+    env={
+        "QDRANT__STORAGE__OPTIMIZERS__INDEXING_THRESHOLD_KB": "0",
+    }
+
+    peer_api_uris, _peer_dirs, _bootstrap_uri = start_cluster(tmp_path, 3, None, extra_env=env)
+    first_peer_id = get_cluster_info(peer_api_uris[0])['peer_id']
+
+    # Create collection, insert points
+    create_collection(peer_api_uris[0], shard_number=1, replication_factor=3)
+    wait_collection_exists_and_active_on_all_peers(
+        collection_name=COLLECTION_NAME,
+        peer_api_uris=peer_api_uris,
+    )
+    upsert_random_points(peer_api_uris[0], num_points)
+
+    sleep(1)
+
+    # Assert node shard and point sum count
+    for uri in peer_api_uris:
+        assert check_collection_local_shards_count(uri, COLLECTION_NAME, 1)
+        assert check_collection_local_shards_point_count(uri, COLLECTION_NAME, num_points)
+
+    # During resharding, keep pushing updates into the collection
+    update_tasks = [
+        # Upsert new points on all peers
+        run_in_background(upsert_points_throttled, peer_api_uris[0], COLLECTION_NAME, start=10000, end=10000 + num_upserts),
+        run_in_background(upsert_points_throttled, peer_api_uris[1], COLLECTION_NAME, start=20000, end=20000 + num_upserts),
+        run_in_background(upsert_points_throttled, peer_api_uris[2], COLLECTION_NAME, start=30000, end=30000 + num_upserts),
+        # Update existing points on the first peer
+        run_in_background(upsert_points_throttled, peer_api_uris[0], COLLECTION_NAME, start=0, end=num_upserts),
+        # Delete points on the first two peers
+        run_in_background(delete_points_throttled, peer_api_uris[0], COLLECTION_NAME, start=500, end=500 + num_deletes),
+        run_in_background(delete_points_throttled, peer_api_uris[1], COLLECTION_NAME, start=600, end=600 + num_deletes),
+    ]
+
+    # Reshard 3 times in sequence
+    for shard_count in range(2, 5):
+        # Start resharding
+        r = requests.post(
+            f"{peer_api_uris[0]}/collections/{COLLECTION_NAME}/cluster", json={
+                "start_resharding": {
+                    "peer_id": first_peer_id,
+                    "shard_key": None,
+                }
+            })
+        assert_http_ok(r)
+
+        # Wait for resharding operation to start and stop
+        wait_for_collection_resharding_operations_count(peer_api_uris[0], COLLECTION_NAME, 1)
+        for uri in peer_api_uris:
+            wait_for_collection_resharding_operations_count(uri, COLLECTION_NAME, 0)
+
+        # Assert node shard count
+        for uri in peer_api_uris:
+            assert check_collection_local_shards_count(uri, COLLECTION_NAME, shard_count)
+
+    # Wait for background updates to finish
+    for task in update_tasks:
+        while task.is_alive():
+            pass
+
+    # Assert node shard and point sum count
+    # Expects base points + 3x upserts - 2x deletes
+    expected_points = num_points + num_upserts * 3 - num_deletes * 2
+    for uri in peer_api_uris:
+        assert check_collection_local_shards_point_count(uri, COLLECTION_NAME, expected_points)
+
+
+def run_in_background(run, *args, **kwargs):
+    p = multiprocessing.Process(target=run, args=args, kwargs=kwargs)
+    p.start()
+    return p
+
+
+def upsert_points_throttled(peer_url, collection_name, start=0, end=None):
+    batch_size = 3
+    offset = start
+
+    while True:
+        count = min(end - offset, batch_size) if end is not None else batch_size
+        if count <= 0:
+            return
+
+        upsert_random_points(peer_url, count, collection_name, offset=offset)
+        offset += count
+
+        sleep(random.uniform(0.4, 0.6))
+
+
+def delete_points_throttled(peer_url, collection_name, start=0, end=None):
+    batch_size = 3
+    offset = start
+
+    while True:
+        count = min(end - offset, batch_size) if end is not None else batch_size
+        if count <= 0:
+            return
+
+        r = requests.post(
+            f"{peer_url}/collections/{collection_name}/points/delete?wait=true", json={
+                "points": list(range(offset, offset + count)),
+            }
+        )
+        assert_http_ok(r)
+        offset += count
+
+        sleep(random.uniform(0.4, 0.6))
