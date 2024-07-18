74a0d0fe9086710ddf389d28b03c51ce4ac44215
timvisee <tim@visee.me>
2024-07-18 17:00:21 +0200

    In resharding driver, make list of participating shards generic

--- lib/collection/src/shards/resharding/driver.rs
+++ lib/collection/src/shards/resharding/driver.rs
@@ -38,8 +38,8 @@ pub(super) struct DriverState {
     key: ReshardKey,
     /// Stage each peer is currently in
     peers: HashMap<PeerId, Stage>,
-    /// List of shard IDs that must be migrated into the new shard
-    source_shard_ids: HashSet<ShardId>,
+    /// List of shard IDs that participate in the resharding process
+    shard_ids: HashSet<ShardId>,
     /// List of shard IDs successfully migrated to the new shard
     pub migrated_shards: Vec<ShardId>,
     /// List of shard IDs in which we successfully deleted migrated points
@@ -47,14 +47,14 @@ pub(super) struct DriverState {
 }
 
 impl DriverState {
-    pub fn new(key: ReshardKey, source_shard_ids: HashSet<ShardId>, peers: &[PeerId]) -> Self {
+    pub fn new(key: ReshardKey, shard_ids: HashSet<ShardId>, peers: &[PeerId]) -> Self {
         Self {
             key,
             peers: peers
                 .iter()
                 .map(|peer_id| (*peer_id, Stage::default()))
                 .collect(),
-            source_shard_ids,
+            shard_ids,
             migrated_shards: vec![],
             deleted_shards: vec![],
         }
@@ -94,21 +94,29 @@ impl DriverState {
             .for_each(|peer_stage| *peer_stage = next_stage.max(*peer_stage));
     }
 
-    /// List the shard IDs we still need to migrate.
+    /// List the shard ID pairs we still need to migrate
     pub fn shards_to_migrate(&self) -> impl Iterator<Item = ShardId> + '_ {
-        self.source_shards()
-            .filter(|shard_id| !self.migrated_shards.contains(shard_id))
+        self.shards()
+            // Exlude current resharding shard, and already migrated shards
+            .filter(|shard_id| {
+                *shard_id != self.key.shard_id && !self.migrated_shards.contains(shard_id)
+            })
     }
 
     /// List the shard IDs in which we still need to propagate point deletions.
     pub fn shards_to_delete(&self) -> impl Iterator<Item = ShardId> + '_ {
-        self.source_shards()
-            .filter(|shard_id| !self.deleted_shards.contains(shard_id))
+        self.shards()
+            // Exlude current resharding shard, and already deleted shards
+            .filter(|shard_id| {
+                *shard_id != self.key.shard_id && !self.deleted_shards.contains(shard_id)
+            })
     }
 
-    /// Get all the shard IDs which points are sourced from.
-    pub fn source_shards(&self) -> impl Iterator<Item = ShardId> + '_ {
-        self.source_shard_ids.iter().copied()
+    /// Get all shard IDs which are participating in this resharding process.
+    ///
+    /// Includes the newly added or to be removed shard.
+    fn shards(&self) -> impl Iterator<Item = ShardId> + '_ {
+        self.shard_ids.iter().copied()
     }
 
     /// Describe the current stage and state in a human readable string.
@@ -211,9 +219,7 @@ pub async fn drive_resharding(
 
     // Load or initialize resharding state
     let init_state = || {
-        let mut shard_ids = hash_ring.unique_nodes();
-        shard_ids.remove(&reshard_key.shard_id);
-
+        let shard_ids = hash_ring.unique_nodes();
         DriverState::new(reshard_key.clone(), shard_ids, &consensus.peers())
     };
     let state: PersistedState = if can_resume {
@@ -226,7 +232,7 @@ pub async fn drive_resharding(
 
     log::debug!(
         "Resharding {collection_id}:{to_shard_id} from shards {:?}",
-        state.read().source_shards().collect::<Vec<_>>(),
+        state.read().shards().collect::<Vec<_>>(),
     );
 
     // Stage 1: init
