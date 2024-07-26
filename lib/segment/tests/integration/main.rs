bdd961ddcf32041837cd02bbabbce7c68588ec29
Albert Safin <xzfcpw@gmail.com>
2024-07-26 06:17:50 +0000

    Tidy up integration tests

--- lib/segment/tests/integration/main.rs
+++ lib/segment/tests/integration/main.rs
@@ -1,23 +1,21 @@
-#![cfg(test)]
-
-pub mod batch_search_test;
+mod batch_search_test;
 mod byte_storage_hnsw_test;
-pub mod byte_storage_quantization_test;
-pub mod disbalanced_vectors_test;
-pub mod exact_search_test;
-pub mod fail_recovery_test;
-pub mod filtering_context_check;
-pub mod filtrable_hnsw_test;
-pub mod fixtures;
-pub mod hnsw_discover_test;
-pub mod hnsw_quantized_search_test;
+mod byte_storage_quantization_test;
+mod disbalanced_vectors_test;
+mod exact_search_test;
+mod fail_recovery_test;
+mod filtering_context_check;
+mod filtrable_hnsw_test;
+mod fixtures;
+mod hnsw_discover_test;
+mod hnsw_quantized_search_test;
 mod multivector_filtrable_hnsw_test;
 mod multivector_hnsw_test;
 mod multivector_quantization_test;
-pub mod nested_filtering_test;
-pub mod payload_index_test;
-pub mod scroll_filtering_test;
-pub mod segment_builder_test;
-pub mod segment_tests;
+mod nested_filtering_test;
+mod payload_index_test;
+mod scroll_filtering_test;
+mod segment_builder_test;
+mod segment_tests;
 mod sparse_discover_test;
 mod sparse_vector_index_search_tests;
