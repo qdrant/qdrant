c1f7dea5843699da715289e9cb8fd25ad55e3f75
timvisee <tim@visee.me>
2024-08-12 15:36:10 +0200

    Bump version to 1.11.0

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Qdrant version string
-pub const QDRANT_VERSION_STRING: &str = "1.10.1";
+pub const QDRANT_VERSION_STRING: &str = "1.11.0";
 
 lazy_static! {
     /// Current Qdrant semver version
