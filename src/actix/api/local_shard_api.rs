03db56a6919262dc3cc9de9e466ffeceac80b505
Roman Titov <ffuugoo@users.noreply.github.com>
2024-08-15 16:43:12 +0200

    Use current hashring for `expected_shard_id` instead of explicit hashring

--- src/actix/api/local_shard_api.rs
+++ src/actix/api/local_shard_api.rs
@@ -1,8 +1,6 @@
-use std::collections::HashSet;
 use std::sync::Arc;
 
 use actix_web::{post, web, Responder};
-use collection::hash_ring::{self, HashRing, HashRingFilter};
 use collection::operations::point_ops::{PointIdsList, PointsSelector};
 use collection::operations::shard_selector_internal::ShardSelectorInternal;
 use collection::operations::types::{
@@ -10,8 +8,9 @@ use collection::operations::types::{
 };
 use collection::shards::shard::ShardId;
 use segment::types::{Condition, ExtendedPointId, Filter};
+use storage::content_manager::errors::{StorageError, StorageResult};
 use storage::dispatcher::Dispatcher;
-use validator::Validate;
+use storage::rbac::{Access, AccessRequirements};
 
 use super::update_api::UpdateParam;
 use crate::actix::api::read_params::ReadParams;
@@ -67,6 +66,20 @@ async fn scroll_points(
             hash_ring_filter,
         } = request.into_inner();
 
+        let hash_ring_filter = match hash_ring_filter {
+            Some(filter) => get_hash_ring_filter(
+                &dispatcher,
+                &access,
+                &path.collection,
+                AccessRequirements::new(),
+                filter.expected_shard_id,
+            )
+            .await?
+            .into(),
+
+            None => None,
+        };
+
         request.filter = merge_with_optional_filter(request.filter.take(), hash_ring_filter);
 
         dispatcher
@@ -98,6 +111,20 @@ async fn count_points(
             hash_ring_filter,
         } = request.into_inner();
 
+        let hash_ring_filter = match hash_ring_filter {
+            Some(filter) => get_hash_ring_filter(
+                &dispatcher,
+                &access,
+                &path.collection,
+                AccessRequirements::new(),
+                filter.expected_shard_id,
+            )
+            .await?
+            .into(),
+
+            None => None,
+        };
+
         request.filter = merge_with_optional_filter(request.filter.take(), hash_ring_filter);
 
         points::do_count_points(
@@ -126,6 +153,15 @@ async fn delete_points(
         let path = path.into_inner();
         let selector = selector.into_inner();
 
+        let filter = get_hash_ring_filter(
+            &dispatcher,
+            &access,
+            &path.collection,
+            AccessRequirements::new().write().whole(),
+            selector.hash_ring_filter.expected_shard_id,
+        )
+        .await?;
+
         let mut points = Vec::new();
         let mut next_offset = Some(ExtendedPointId::NumId(0));
 
@@ -133,7 +169,7 @@ async fn delete_points(
             let scroll = ScrollRequestInternal {
                 limit: Some(1000),
                 offset: Some(current_offset),
-                filter: Some(selector.filter.clone()),
+                filter: Some(filter.clone()),
                 ..Default::default()
             };
 
@@ -188,126 +224,60 @@ struct CollectionShard {
     shard: ShardId,
 }
 
+#[derive(Clone, Debug, serde::Deserialize)]
+struct WithFilter<T> {
+    #[serde(flatten)]
+    request: T,
+    #[serde(default)]
+    hash_ring_filter: Option<SerdeHelper>,
+}
+
+#[derive(Clone, Debug, serde::Deserialize)]
+struct Selector {
+    hash_ring_filter: SerdeHelper,
+}
+
+#[derive(Clone, Debug, serde::Deserialize)]
+struct SerdeHelper {
+    expected_shard_id: ShardId,
+}
+
+async fn get_hash_ring_filter(
+    dispatcher: &Dispatcher,
+    access: &Access,
+    collection: &str,
+    reqs: AccessRequirements,
+    expected_shard_id: ShardId,
+) -> StorageResult<Filter> {
+    let pass = access.check_collection_access(collection, reqs)?;
+
+    let shard_holder = dispatcher
+        .toc(access)
+        .get_collection(&pass)
+        .await?
+        .shards_holder();
+
+    let hash_ring_filter = shard_holder
+        .read()
+        .await
+        .hash_ring_filter(expected_shard_id)
+        .ok_or_else(|| {
+            StorageError::bad_request(format!(
+                "shard {expected_shard_id} does not exist in collection {collection}"
+            ))
+        })?;
+
+    let condition = Condition::CustomIdChecker(Arc::new(hash_ring_filter));
+    let filter = Filter::new_must(condition);
+
+    Ok(filter)
+}
+
 fn merge_with_optional_filter(filter: Option<Filter>, hash_ring: Option<Filter>) -> Option<Filter> {
     match (filter, hash_ring) {
         (Some(filter), Some(hash_ring)) => hash_ring.merge_owned(filter).into(),
         (Some(filter), None) => filter.into(),
         (None, Some(hash_ring)) => hash_ring.into(),
         _ => None,
-    }
-}
-
-#[derive(Clone, Debug, serde::Deserialize)]
-struct WithFilter<T> {
-    #[serde(flatten)]
-    request: T,
-    #[serde(default, deserialize_with = "deserialize_optional_filter")]
-    hash_ring_filter: Option<Filter>,
-}
-
-#[derive(Clone, Debug, serde::Deserialize)]
-struct Selector {
-    #[serde(default, deserialize_with = "deserialize_filter")]
-    filter: Filter,
-}
-
-fn deserialize_optional_filter<'de, D>(deserializer: D) -> Result<Option<Filter>, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    let helper: Option<SerdeHelper> = serde::Deserialize::deserialize(deserializer)?;
-
-    let Some(helper) = helper else {
-        return Ok(None);
-    };
-
-    helper.validate().map_err(serde::de::Error::custom)?;
-    Ok(Some(helper.into()))
-}
-
-fn deserialize_filter<'de, D>(deserializer: D) -> Result<Filter, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    let helper: SerdeHelper = serde::Deserialize::deserialize(deserializer)?;
-    helper.validate().map_err(serde::de::Error::custom)?;
-    Ok(helper.into())
-}
-
-#[derive(Clone, Debug, serde::Deserialize)]
-struct SerdeHelper {
-    #[serde(default)]
-    scale: Scale,
-    shard_ids: HashSet<ShardId>,
-    expected_shard_id: ShardId,
-}
-
-impl validator::Validate for SerdeHelper {
-    fn validate(&self) -> Result<(), validator::ValidationErrors> {
-        use validator::{ValidationError, ValidationErrors};
-
-        let mut errors = ValidationErrors::new();
-
-        if let Scale::Fair(0) = self.scale {
-            errors.add("scale", ValidationError::new("fair scale can't be 0"));
-        }
-
-        let Some(&max_shard_id) = self.shard_ids.iter().max() else {
-            errors.add("shard_ids", ValidationError::new("can't be empty"));
-            return Err(errors);
-        };
-
-        if (0..max_shard_id).any(|shard_id| !self.shard_ids.contains(&shard_id)) {
-            // TODO(resharding): Is this true for custom sharding? 🤔
-            errors.add(
-                "shard_ids",
-                ValidationError::new("all shard ids must be sequential"),
-            );
-        }
-
-        if !self.shard_ids.contains(&self.expected_shard_id) {
-            errors.add("expected_shard_id", ValidationError::new(""));
-        }
-
-        if errors.is_empty() {
-            Ok(())
-        } else {
-            Err(errors)
-        }
-    }
-}
-
-impl From<SerdeHelper> for Filter {
-    fn from(helper: SerdeHelper) -> Self {
-        Filter::new_must(Condition::CustomIdChecker(Arc::new(HashRingFilter::from(
-            helper,
-        ))))
-    }
-}
-
-impl From<SerdeHelper> for HashRingFilter {
-    fn from(helper: SerdeHelper) -> Self {
-        let mut ring = match helper.scale {
-            Scale::Raw => HashRing::raw(),
-            Scale::Fair(scale) => HashRing::fair(scale),
-        };
-
-        for shard_id in helper.shard_ids {
-            ring.add(shard_id);
-        }
-
-        HashRingFilter::new(ring, helper.expected_shard_id)
-    }
-}
-
-#[derive(Clone, Debug, serde::Deserialize)]
-enum Scale {
-    Raw,
-    Fair(u32),
-}
-
-impl Default for Scale {
-    fn default() -> Self {
-        Self::Fair(hash_ring::HASH_RING_SHARD_SCALE)
     }
 }
