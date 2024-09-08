1202f8eb0ac7fd575c7ec167aa6ed47f682b113f
generall <andrey@vasnetsov.com>
2024-09-08 17:49:44 +0200

    impl ops for MmapInvertedIndex

--- lib/segment/src/index/field_index/full_text_index/mmap_inverted_index/inverted_index_ops.rs
+++ lib/segment/src/index/field_index/full_text_index/mmap_inverted_index/inverted_index_ops.rs
@@ -1,6 +1,6 @@
 use common::types::PointOffsetType;
 
-use crate::index::field_index::full_text_index::inverted_index::ParsedQuery;
+use crate::index::field_index::full_text_index::inverted_index::{ParsedQuery, TokenId};
 use crate::index::field_index::full_text_index::mmap_inverted_index::MmapInvertedIndex;
 use crate::index::field_index::full_text_index::postings_iterator::intersect_compressed_postings_iterator;
 
@@ -50,5 +50,49 @@ impl MmapInvertedIndex {
             move |idx: PointOffsetType| !self.deleted_points.get(idx as usize).unwrap_or(true);
 
         intersect_compressed_postings_iterator(postings, filter)
+    }
+
+    pub fn values_is_empty(&self, point_id: PointOffsetType) -> bool {
+        if self.point_to_tokens_count.len() <= point_id as usize {
+            return true;
+        }
+        self.point_to_tokens_count[point_id as usize] == 0
+    }
+
+    pub fn values_count(&self, point_id: PointOffsetType) -> usize {
+        if self.point_to_tokens_count.len() <= point_id as usize {
+            return 0;
+        }
+        self.point_to_tokens_count[point_id as usize]
+    }
+
+    pub fn check_match(&self, parsed_query: &ParsedQuery, point_id: PointOffsetType) -> bool {
+        if parsed_query.tokens.contains(&None) {
+            return false;
+        }
+        // check presence of the document
+        if self.deleted_points.get(point_id as usize).unwrap_or(true) {
+            return false;
+        }
+
+        // Check that all tokens are in document
+        parsed_query
+            .tokens
+            .iter()
+            // unwrap crash safety: all tokens exist in the vocabulary if it passes the above check
+            .all(|query_token| {
+                self.postings
+                    .get(query_token.unwrap())
+                    .map(|postings| postings.contains(&point_id))
+                    .unwrap_or(false)
+            })
+    }
+
+    pub fn vocab_with_postings_len_iter(&self) -> impl Iterator<Item = (&str, usize)> + '_ {
+        self.vocab.iter().filter_map(|(token, posting_idx)| {
+            self.postings
+                .get(posting_idx[0] as TokenId)
+                .map(|posting| (token, posting.len()))
+        })
     }
 }
