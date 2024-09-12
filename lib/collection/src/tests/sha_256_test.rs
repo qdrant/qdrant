3624b4f2eb20c4383e2f7be82f03aa66e6fa5dd8
Tim Visée <tim+github@visee.me>
2024-09-12 11:17:11 +0200

    Make computing snapshot checksums faster in debug (#5064)
    * In debug builds on Unix, use sha256sum binary because it is faster
    
    * Add test to ensure sha256sum produces the same hash
    
    * Apply suggestions from code review
    
    Co-authored-by: Roman Titov <ffuugoo@users.noreply.github.com>
    
    * Only use sha256sum on Linux platforms
    
    * Remove hash through binary implementation
    
    * Enable release optimizations for sha2 crate in debug builds
    
    ---------
    
    Co-authored-by: Roman Titov <ffuugoo@users.noreply.github.com>

--- lib/collection/src/tests/sha_256_test.rs
+++ lib/collection/src/tests/sha_256_test.rs
@@ -11,7 +11,7 @@ async fn test_sha_256_digest() -> std::io::Result<()> {
     let result_hash = hash_file(file.path()).await?;
     assert_eq!(
         result_hash,
-        "735e3ec1b05d901d07e84b1504518442aba2395fe3f945a1c962e81a8e152b2d"
+        "735e3ec1b05d901d07e84b1504518442aba2395fe3f945a1c962e81a8e152b2d",
     );
     Ok(())
 }
