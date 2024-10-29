157cccff0c8f09c124b6ff8ec9c33dd3abd04b6b
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-10-29 02:14:23 +0100

    fix compression tests

--- lib/segment/src/index/hnsw_index/links_compressor.rs
+++ lib/segment/src/index/hnsw_index/links_compressor.rs
@@ -1,5 +1,3 @@
-use crate::common::operation_error::{OperationError, OperationResult};
-
 pub struct DecompressIterator<T: Iterator<Item = u8>> {
     data: T,
     delta: u64,
@@ -16,11 +14,12 @@ impl<T: Iterator<Item = u8>> Iterator for DecompressIterator<T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut link: u64 = 0;
-        #[allow(for_loops_over_fallibles)]
+        let mut shift = 0;
         while let Some(byte) = self.data.next() {
             let byte = byte as u64;
-            link |= (link << 7) | (byte & 127);
-            if byte < 128 {
+            link |= (byte & 0x7F) << shift;
+            shift += 7;
+            if byte < 0x80 {
                 let result = link + self.delta;
                 self.delta = result;
                 return Some(result);
@@ -33,50 +32,73 @@ impl<T: Iterator<Item = u8>> Iterator for DecompressIterator<T> {
 pub fn estimate_compressed_size(data: &[u64]) -> usize {
     let mut delta = 0;
     data.iter()
-        .map(|&v| {
-            if v < delta {
+        .map(|&orig| {
+            if orig < delta {
                 panic!("Links are not sorted");
             }
 
-            let mut v = v - delta;
-            delta = v;
+            let mut v = orig - delta;
+            delta = orig;
+
             let mut bytes_count = 0;
-            while v > 0 {
+            loop {
                 bytes_count += 1;
                 v >>= 7;
+                if v == 0 {
+                    break;
+                }
             }
-            if bytes_count == 0 {
-                1
-            } else {
-                bytes_count
-            }
+            bytes_count
         })
         .sum()
 }
 
 pub fn compress(data: &[u64], dst: &mut Vec<u8>) {
     let mut delta = 0;
-    for &v in data {
-        if v < delta {
+    for &orig in data {
+        if orig < delta {
             panic!("Links are not sorted");
         }
 
-        let mut v = v - delta;
-        delta = v;
+        let mut v = orig - delta;
+        delta = orig;
 
         loop {
-            let byte = (v & 127) as u8;
-
-            v >>= 7;
-            if v > 0 {
-                dst.push(128 | byte);
+            let byte = (v & 0x7F) as u8;
+            if v > 0x7F {
+                dst.push(0x80 | byte);
             } else {
                 dst.push(byte);
             }
-
+            v >>= 7;
             if v == 0 {
                 break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compression() {
+        let data = vec![0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        //let data = vec![500, 1_000, 9_000, 1_000_000, 6_000_000, 7_000_000_000, 7_000_000_001, 7_000_000_001];
+        let mut compressed = Vec::new();
+        compress(&data, &mut compressed);
+        let decompressed = DecompressIterator::new(compressed.iter().copied());
+        let decompressed_data: Vec<_> = decompressed.collect();
+        assert_eq!(data, decompressed_data);
+    }
+
+    #[test]
+    fn test_estimate_compressed_size() {
+        let data = vec![0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let compressed_size = estimate_compressed_size(&data);
+        let mut compressed = Vec::new();
+        compress(&data, &mut compressed);
+        assert_eq!(compressed_size, compressed.len());
     }
 }
