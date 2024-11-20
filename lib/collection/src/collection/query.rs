e85044881fcdf3a911eef2a3de165503b3a134f0
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-11-20 23:19:51 +0530

    fix: clippy  usize -> Duration conversion

--- lib/collection/src/collection/query.rs
+++ lib/collection/src/collection/query.rs
@@ -219,9 +219,9 @@ impl Collection {
         // update timeout
         let timeout = timeout.map(|timeout| {
             // avoid flooring the timeout to 0
-            Duration::from_secs_f32(
-                timeout.saturating_sub(start.elapsed()).as_secs_f32().ceil() as usize
-            )
+            let remaining_secs =
+                timeout.saturating_sub(start.elapsed()).as_secs_f32().ceil() as usize;
+            Duration::from_secs(remaining_secs as u64)
         });
 
         // Check we actually fetched all referenced vectors from the resolver requests
