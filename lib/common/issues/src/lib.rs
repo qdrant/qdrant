3ce4035df33f9911c96942c168034ebf2005ac37
Andrey Vasnetsov <andrey@vasnetsov.com>
2024-12-09 15:29:11 +0100

    allow to read related issues with collection-level access (#5609)
    * allow to read related issues with collection-level access
    
    * refactor to use requirements
    
    * patch test
    
    ---------
    
    Co-authored-by: Luis Cossío <luis.cossio@outlook.com>

--- lib/common/issues/src/lib.rs
+++ lib/common/issues/src/lib.rs
@@ -6,6 +6,8 @@ mod solution;
 pub(crate) mod typemap;
 
 pub use broker::{add_subscriber, publish};
-pub use dashboard::{all_issues, clear, solve, solve_by_filter, submit, Code};
+pub use dashboard::{
+    all_collection_issues, all_issues, clear, solve, solve_by_filter, submit, Code,
+};
 pub use issue::{Issue, IssueRecord};
 pub use solution::{Action, ImmediateSolution, Solution};
--- lib/common/issues/src/problems/too_many_collections.rs
+++ lib/common/issues/src/problems/too_many_collections.rs
@@ -12,6 +12,10 @@ impl Issue for TooManyCollections {
         "TOO_MANY_COLLECTIONS"
     }
 
+    fn related_collection(&self) -> Option<String> {
+        None
+    }
+
     fn description(&self) -> String {
         "It looks like you have too many collections.\nIf your architecture creates collections programmatically, it's probably better to restructure your solution into a fixed number of them. \nLearn more here: https://qdrant.tech/documentation/guides/multiple-partitions/".to_string()
     }
