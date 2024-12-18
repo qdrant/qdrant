8b4e798948d8d2ccbcef79107b61ed4e360507e5
Luis Cossío <luis.cossio@qdrant.com>
2024-12-18 17:17:47 -0300

    Enable resetting `max_optimization_threads` to automatic (#5634)
    * Allow max-optimization-thread config to be set to null on update
    
    * add new field. Support both, but prepare for deprecation.
    
    * better openapi
    
    * only introduce change in OptimizersConfigDiff
    
    * move to `api::rest::schema`
    
    * update openapi
    
    * improve test, fix diff to config conversion
    
    * upd grpc docs
    
    * clippy
    
    * remove schemars from common common
    
    ---------
    
    Co-authored-by: Gulshan Kumar <kumargu@amazon.com>

--- lib/collection/src/operations/config_diff.rs
+++ lib/collection/src/operations/config_diff.rs
@@ -1,6 +1,7 @@
 use std::hash::Hash;
 use std::num::NonZeroU32;
 
+use api::rest::MaxOptimizationThreads;
 use merge::Merge;
 use schemars::JsonSchema;
 use segment::types::{
@@ -162,9 +163,9 @@ pub struct OptimizersConfigDiff {
     pub flush_interval_sec: Option<u64>,
     /// Max number of threads (jobs) for running optimizations per shard.
     /// Note: each optimization job will also use `max_indexing_threads` threads by itself for index building.
-    /// If null - have no limit and choose dynamically to saturate CPU.
+    /// If "auto" - have no limit and choose dynamically to saturate CPU.
     /// If 0 - no optimization threads, optimizations will be disabled.
-    pub max_optimization_threads: Option<usize>,
+    pub max_optimization_threads: Option<MaxOptimizationThreads>,
 }
 
 impl std::hash::Hash for OptimizersConfigDiff {
@@ -200,7 +201,29 @@ impl DiffConfig<HnswConfig> for HnswConfigDiff {}
 
 impl DiffConfig<HnswConfigDiff> for HnswConfigDiff {}
 
-impl DiffConfig<OptimizersConfig> for OptimizersConfigDiff {}
+impl DiffConfig<OptimizersConfig> for OptimizersConfigDiff {
+    fn update(self, config: &OptimizersConfig) -> CollectionResult<OptimizersConfig>
+    where
+        Self: Sized + Serialize + DeserializeOwned + Merge,
+    {
+        Ok(OptimizersConfig {
+            deleted_threshold: self.deleted_threshold.unwrap_or(config.deleted_threshold),
+            vacuum_min_vector_number: self
+                .vacuum_min_vector_number
+                .unwrap_or(config.vacuum_min_vector_number),
+            default_segment_number: self
+                .default_segment_number
+                .unwrap_or(config.default_segment_number),
+            max_segment_size: self.max_segment_size.or(config.max_segment_size),
+            memmap_threshold: self.memmap_threshold.or(config.memmap_threshold),
+            indexing_threshold: self.indexing_threshold.or(config.indexing_threshold),
+            flush_interval_sec: self.flush_interval_sec.unwrap_or(config.flush_interval_sec),
+            max_optimization_threads: self
+                .max_optimization_threads
+                .map_or(config.max_optimization_threads, From::from),
+        })
+    }
+}
 
 impl DiffConfig<WalConfig> for WalConfigDiff {}
 
@@ -333,6 +356,7 @@ impl Validate for QuantizationConfigDiff {
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
     use segment::types::{Distance, HnswConfig};
 
     use super::*;
@@ -386,6 +410,31 @@ mod tests {
             serde_json::from_str(r#"{ "indexing_threshold": 10000 }"#).unwrap();
         let new_config = update.update(&base_config).unwrap();
         assert_eq!(new_config.indexing_threshold, Some(10000))
+    }
+
+    #[rstest]
+    #[case::number(r#"{ "max_optimization_threads": 5 }"#, Some(5))]
+    #[case::auto(r#"{ "max_optimization_threads": "auto" }"#, None)]
+    #[case::null(r#"{ "max_optimization_threads": null }"#, Some(1))] // no effect
+    #[case::nothing("{  }", Some(1))] // no effect
+    #[should_panic]
+    #[case::other(r#"{ "max_optimization_threads": "other" }"#, Some(1))]
+    fn test_set_optimizer_threads(#[case] json_diff: &str, #[case] expected: Option<usize>) {
+        let base_config = OptimizersConfig {
+            deleted_threshold: 0.9,
+            vacuum_min_vector_number: 1000,
+            default_segment_number: 10,
+            max_segment_size: None,
+            memmap_threshold: None,
+            indexing_threshold: Some(50_000),
+            flush_interval_sec: 30,
+            max_optimization_threads: Some(1),
+        };
+
+        let update: OptimizersConfigDiff = serde_json::from_str(json_diff).unwrap();
+        let new_config = update.update(&base_config).unwrap();
+
+        assert_eq!(new_config.max_optimization_threads, expected);
     }
 
     #[test]
