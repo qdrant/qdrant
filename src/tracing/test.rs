40083f6641dfc9e80890aaa9d3da4c1182bb6d58
Gulshan <kumar.gulshan2192@gmail.com>
2024-12-20 17:57:49 +0530

    Support for JSON logging (#5602)
    * Support for json logging
    
    * Rename enum name and verbose matches
    
    * Use snake case formatting for log format variants
    
    ---------
    
    Co-authored-by: Gulshan Kumar <kumargu@amazon.com>
    Co-authored-by: Tim Visée <tim+github@visee.me>

--- src/tracing/test.rs
+++ src/tracing/test.rs
@@ -5,7 +5,7 @@ use serde_json::json;
 use super::*;
 
 #[test]
-fn deseriailze_logger_config() {
+fn deserialize_logger_config() {
     let json = json!({
         "log_level": "debug",
         "span_events": ["new", "close"],
@@ -29,6 +29,50 @@ fn deseriailze_logger_config() {
                 config::SpanEvent::Close,
             ])),
             color: Some(config::Color::Explicit(true)),
+            log_format: None,
+        },
+
+        on_disk: on_disk::Config {
+            enabled: Some(true),
+            log_file: Some("/logs/qdrant".into()),
+            log_level: Some("tracing".into()),
+            span_events: Some(HashSet::from([
+                config::SpanEvent::New,
+                config::SpanEvent::Close,
+            ])),
+        },
+    };
+
+    assert_eq!(config, expected);
+}
+
+#[test]
+fn deserialize_json_logger_config() {
+    let json = json!({
+        "log_level": "debug",
+        "span_events": ["new", "close"],
+        "color": true,
+        "log_format": "json",
+
+        "on_disk": {
+            "enabled": true,
+            "log_file": "/logs/qdrant",
+            "log_level": "tracing",
+            "span_events": ["new", "close"],
+        }
+    });
+
+    let config = deserialize_config(json);
+
+    let expected = LoggerConfig {
+        default: default::Config {
+            log_level: Some("debug".into()),
+            span_events: Some(HashSet::from([
+                config::SpanEvent::New,
+                config::SpanEvent::Close,
+            ])),
+            color: Some(config::Color::Explicit(true)),
+            log_format: Some(default::LogFormat::Json),
         },
 
         on_disk: on_disk::Config {
@@ -63,6 +107,7 @@ fn deseriailze_config_with_explicit_nulls() {
         "log_level": null,
         "span_events": null,
         "color": null,
+        "log_format": null,
 
         "on_disk": {
             "enabled": null,
