253362fcc212dd01b5dcfa7579b2dcdd6c8e5474
Gulshan <kumar.gulshan2192@gmail.com>
2024-12-20 17:57:49 +0530

    Support for JSON logging (#5602)
    * Support for json logging
    
    * Rename enum name and verbose matches
    
    * Use snake case formatting for log format variants
    
    ---------
    
    Co-authored-by: Gulshan Kumar <kumargu@amazon.com>
    Co-authored-by: Tim Visée <tim+github@visee.me>

--- src/tracing/default.rs
+++ src/tracing/default.rs
@@ -2,10 +2,18 @@ use std::collections::HashSet;
 
 use common::ext::OptionExt;
 use serde::{Deserialize, Serialize};
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::{filter, fmt, registry};
+use tracing_subscriber::fmt::{self};
+use tracing_subscriber::{filter, registry, Layer};
 
 use super::*;
+
+#[derive(Default, Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum LogFormat {
+    #[default]
+    Text,
+    Json,
+}
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Deserialize, Serialize)]
 #[serde(default)]
@@ -13,6 +21,7 @@ pub struct Config {
     pub log_level: Option<String>,
     pub span_events: Option<HashSet<config::SpanEvent>>,
     pub color: Option<config::Color>,
+    pub log_format: Option<LogFormat>,
 }
 
 impl Config {
@@ -21,39 +30,56 @@ impl Config {
             log_level,
             span_events,
             color,
+            log_format,
         } = other;
 
         self.log_level.replace_if_some(log_level);
         self.span_events.replace_if_some(span_events);
         self.color.replace_if_some(color);
+        self.log_format.replace_if_some(log_format);
     }
 }
 
-#[rustfmt::skip] // `rustfmt` formats this into unreadable single line
-pub type Logger<S> = filter::Filtered<
-    Option<fmt::Layer<S>>,
-    filter::EnvFilter,
-    S,
->;
+pub type Logger<S> =
+    filter::Filtered<Option<Box<dyn Layer<S> + Send + Sync>>, filter::EnvFilter, S>;
 
 pub fn new_logger<S>(config: &Config) -> Logger<S>
 where
     S: tracing::Subscriber + for<'span> registry::LookupSpan<'span>,
 {
-    let layer = new_layer(config);
+    let layer: Box<dyn Layer<S> + Send + Sync> = match config.log_format {
+        Some(LogFormat::Json) => Box::new(new_layer_with_json(config)),
+        Some(LogFormat::Text) | None => Box::new(new_layer(config)),
+    };
     let filter = new_filter(config);
     Some(layer).with_filter(filter)
 }
 
-pub fn new_layer<S>(config: &Config) -> fmt::Layer<S>
+pub fn new_layer<S>(config: &Config) -> Box<dyn Layer<S> + Send + Sync>
 where
     S: tracing::Subscriber + for<'span> registry::LookupSpan<'span>,
 {
-    fmt::Layer::default()
-        .with_span_events(config::SpanEvent::unwrap_or_default_config(
-            &config.span_events,
-        ))
-        .with_ansi(config.color.unwrap_or_default().to_bool())
+    Box::new(
+        fmt::Layer::default()
+            .with_span_events(config::SpanEvent::unwrap_or_default_config(
+                &config.span_events,
+            ))
+            .with_ansi(config.color.unwrap_or_default().to_bool()),
+    )
+}
+
+pub fn new_layer_with_json<S>(config: &Config) -> Box<dyn Layer<S> + Send + Sync>
+where
+    S: tracing::Subscriber + for<'span> registry::LookupSpan<'span>,
+{
+    Box::new(
+        fmt::Layer::default()
+            .json()
+            .with_span_events(config::SpanEvent::unwrap_or_default_config(
+                &config.span_events,
+            ))
+            .with_ansi(config.color.unwrap_or_default().to_bool()),
+    )
 }
 
 pub fn new_filter(config: &Config) -> filter::EnvFilter {
