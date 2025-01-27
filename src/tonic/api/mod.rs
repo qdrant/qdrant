4ac345e7b4fcc584646800e37bae4b4e3901009c
Roman Titov <ffuugoo@users.noreply.github.com>
2025-01-27 18:30:14 +0100

    Split `tonic::api::points_common` module (#5865)

--- src/tonic/api/mod.rs
+++ src/tonic/api/mod.rs
@@ -1,11 +1,13 @@
 pub mod collections_api;
-mod collections_common;
 pub mod collections_internal_api;
 pub mod points_api;
-mod points_common;
 pub mod points_internal_api;
 pub mod raft_api;
 pub mod snapshots_api;
+
+mod collections_common;
+mod query_common;
+mod update_common;
 
 use collection::operations::validation;
 use tonic::Status;
