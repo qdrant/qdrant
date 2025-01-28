182dbdced201a732e088e5ba624c4a4e4b871bdd
KShivendu <kshivendu1@gmail.com>
2025-01-28 16:23:26 +0530

    Try generating coverage one by one

---
+++ coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WORKSPACE_CRATES=$(cargo metadata --format-version 1 | jq -r '.workspace_members[]' | awk -F '/' '{print $NF}' | cut -d '#' -f 1)
+
+for CRATE in $WORKSPACE_CRATES; do
+    if [ "$CRATE" == "qdrant" ]; then
+        continue
+    fi
+
+    echo "Testing crate with coverage: $CRATE"
+    cargo llvm-cov nextest --profile ci --jobs=1 -p "$CRATE"
+done
