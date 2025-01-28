f1da895bc7efb87bf6c0f2422f6a629d95a0c9e9
KShivendu <kshivendu1@gmail.com>
2025-01-28 19:40:01 +0530

    Improve coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-WORKSPACE_CRATES=($(cargo metadata --format-version 1 | jq -r '.workspace_members[] | split("/") | .[-1] | split("#")[0]' | sort))
-EXPECTED_WORKSPACE_CRATES=("api" "blob_store" "cancel" "collection" "common" "dataset" "gpu" "io" "issues" "memory" "qdrant" "quantization" "segment" "sparse" "storage")
+PACKAGES=($(cargo metadata --format-version 1 | jq -r '.workspace_members[] | split("/") | .[-1] | split("#")[0]' | sort))
+EXPECTED_PACKAGES=("api" "blob_store" "cancel" "collection" "common" "dataset" "gpu" "io" "issues" "memory" "qdrant" "quantization" "segment" "sparse" "storage")
 
-if [ "${WORKSPACE_CRATES[*]}" != "${EXPECTED_WORKSPACE_CRATES[*]}" ]; then
-    echo "Workspace crates have changed. Please update coverage.sh to update the expected and whitelisted ones."
+if [ "${PACKAGES[*]}" != "${EXPECTED_PACKAGES[*]}" ]; then
+    echo "Workspace packages have changed. Please update the expected and whitelisted packages in coverage.sh"
     exit 1
 fi
 
-WHITELISTED_WORKSPACE_CRATES=("api" "common" "segment" "blob_store")
-REPORT_DIR="target/llvm-cov/crate-reports"
+WHITELISTED_PACKAGES=("api" "blob_store" "collection" "common" "segment" "storage") # ToDo: Use block list instead?
+# EMPTY_PACKAGES=("cancel" "dataset" "gpu" "io")
+# IGNORED_PACKAGES=("issues" "memory" "qdrant" "quantization" "sparse")
 
-echo "All workspace crates: ${WORKSPACE_CRATES[*]}"
-echo "Whitelisted workspace crates: ${WHITELISTED_WORKSPACE_CRATES[*]}"
+REPORT_DIR="target/llvm-cov/package-reports"
+
+echo "All workspace packages: ${PACKAGES[*]}"
+echo "Whitelisted workspace packages: ${WHITELISTED_PACKAGES[*]}"
 
 mkdir -p "$REPORT_DIR"
 
 LCOV_COMMAND_ARGS=""
 
-for CRATE in "${WHITELISTED_WORKSPACE_CRATES[@]}"; do
-    if [ "$CRATE" == "qdrant" ]; then
+for PACKAGE in "${WHITELISTED_PACKAGES[@]}"; do
+    if [ "$PACKAGE" == "qdrant" ]; then
         continue
     fi
 
-    echo "Testing crate with coverage: $CRATE"
+    echo "Testing PACKAGE with coverage: $PACKAGE"
     # Profile "ci" is configured in .config/nextest.toml
-    cargo llvm-cov --no-clean nextest --profile ci -p "$CRATE" --lcov --output-path "$REPORT_DIR/$CRATE.info"
+    cargo llvm-cov --no-clean nextest --profile ci -p "$PACKAGE" --lcov --output-path "$REPORT_DIR/$PACKAGE.info"
 
-    LCOV_COMMAND_ARGS="${LCOV_COMMAND_ARGS} -a $REPORT_DIR/$CRATE.info"
+    LCOV_COMMAND_ARGS="${LCOV_COMMAND_ARGS} -a $REPORT_DIR/$PACKAGE.info"
 done
 
 if [ -n "$LCOV_COMMAND_ARGS" ]; then
