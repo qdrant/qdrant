03630a9fecb9ba4d07d7db7b5e46ba6cc574980f
KShivendu <kshivendu1@gmail.com>
2025-01-28 20:01:54 +0530

    Prepare for review

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-PACKAGES=($(cargo metadata --format-version 1 | jq -r '.workspace_members[] | split("/") | .[-1] | split("#")[0]' | sort))
-EXPECTED_PACKAGES=("api" "blob_store" "cancel" "collection" "common" "dataset" "gpu" "io" "issues" "memory" "qdrant" "quantization" "segment" "sparse" "storage")
+RUN_PER_PACKAGE=${RUN_PER_PACKAGE:-"false"}
 
-if [ "${PACKAGES[*]}" != "${EXPECTED_PACKAGES[*]}" ]; then
-    echo "Workspace packages have changed. Please update the expected and whitelisted packages in coverage.sh"
-    exit 1
+if [ "$RUN_PER_PACKAGE" == "false" ]; then
+    # Run for the entire workspace in one shot. This assumes we have enough memory
+    cargo llvm-cov nextest --profile ci --workspace --html
+    exit 0
 fi
 
-WHITELISTED_PACKAGES=("qdrant" "api" "blob_store" "collection" "common" "segment" "storage")
-# EMPTY_PACKAGES=("cancel" "dataset" "gpu" "io")
-# IGNORED_PACKAGES=("issues" "memory" "quantization" "sparse")
-
+PACKAGES=($(cargo metadata --format-version 1 | jq -r '.workspace_members[] | split("/") | .[-1] | split("#")[0]' | sort))
 REPORT_DIR="target/llvm-cov/package-reports"
 
-echo "All workspace packages: ${PACKAGES[*]}"
-echo "Whitelisted workspace packages: ${WHITELISTED_PACKAGES[*]}"
+echo "Workspace packages: ${PACKAGES[*]}"
 
 mkdir -p "$REPORT_DIR"
 
 LCOV_COMMAND_ARGS=""
 
-for PACKAGE in "${WHITELISTED_PACKAGES[@]}"; do
+for PACKAGE in "${PACKAGES[@]}"; do
     echo "Testing PACKAGE with coverage: $PACKAGE"
     # Profile "ci" is configured in .config/nextest.toml
     cargo llvm-cov --no-clean nextest --profile ci -p "$PACKAGE" --lcov --output-path "$REPORT_DIR/$PACKAGE.info"
