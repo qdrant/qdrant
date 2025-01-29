cb813e8ef3eb853ef5012597ecc9e6299fd88a9f
timvisee <tim@visee.me>
2025-01-29 17:08:41 +0100

    Add debug assertion for last applied index and first entry index

--- lib/storage/src/content_manager/consensus_manager.rs
+++ lib/storage/src/content_manager/consensus_manager.rs
@@ -788,6 +788,7 @@ impl<C: CollectionContainer> ConsensusManager<C> {
             return Ok(false);
         };
 
+        debug_assert!(last_applied_index == 0 || last_applied_index >= first_entry.index - 1);
         if last_applied_index.saturating_sub(first_entry.index) < min_entries_to_compact {
             return Ok(false);
         }
