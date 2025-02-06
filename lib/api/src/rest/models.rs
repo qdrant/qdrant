b4b5395a0e67c887d259f1554193811e3238f789
jojii <jojii@gmx.net>
2025-02-06 15:14:39 +0100

    Add payload index metric to api and telemetry

--- lib/api/src/rest/models.rs
+++ lib/api/src/rest/models.rs
@@ -55,6 +55,7 @@ pub struct HardwareUsage {
     pub cpu: usize,
     pub payload_io_read: usize,
     pub payload_io_write: usize,
+    pub payload_index_io_read: usize,
     pub vector_io_read: usize,
     pub vector_io_write: usize,
 }
