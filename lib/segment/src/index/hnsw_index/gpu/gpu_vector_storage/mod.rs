a6f4769ae3077dd9fb4d45318760bddfc66a75e0
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2025-02-15 00:34:59 +0000

    32-bit vector element

--- lib/segment/src/index/hnsw_index/gpu/gpu_vector_storage/mod.rs
+++ lib/segment/src/index/hnsw_index/gpu/gpu_vector_storage/mod.rs
@@ -28,7 +28,7 @@ use crate::vector_storage::{
     DenseVectorStorage, MultiVectorStorage, VectorStorage, VectorStorageEnum,
 };
 
-pub const ELEMENTS_PER_SUBGROUP: usize = 4;
+pub const VECTOR_STORAGE_ELEMENT_SIZE: usize = 4;
 pub const UPLOAD_CHUNK_SIZE: usize = 64 * 1024 * 1024;
 pub const STORAGES_COUNT: usize = 4;
 
@@ -773,7 +773,7 @@ impl GpuVectorStorage {
         // We need to keep this alignment for bq case that's why we have `max` here.
         // But TBH even CPU emulator has 8 subgroups to that we don't expect in practice
         // that we will have less than 16 bytes alignment.
-        let alignment = std::cmp::max(device.subgroup_size() * ELEMENTS_PER_SUBGROUP, 16);
+        let alignment = std::cmp::max(device.subgroup_size() * VECTOR_STORAGE_ELEMENT_SIZE, 16);
         dim.next_multiple_of(alignment)
     }
 
