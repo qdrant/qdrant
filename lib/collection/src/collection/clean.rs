0e6aa45b4f3de0707dbe70169690ac1749b6a841
jojii <jojii@gmx.net>
2025-03-06 13:00:45 +0100

    Clarifying why we don't measure shard cleanup

--- lib/collection/src/collection/clean.rs
+++ lib/collection/src/collection/clean.rs
@@ -257,6 +257,9 @@ async fn clean_task(
     shard_id: ShardId,
     sender: Sender<ShardCleanStatus>,
 ) -> Result<(), CollectionError> {
+    // Do not measure the hardware usage of these deletes as clean the shard is always considered an internal operation
+    // users should not be billed for.
+
     let mut offset = None;
     let mut deleted_points = 0;
 
