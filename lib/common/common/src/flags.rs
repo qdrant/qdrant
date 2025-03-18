d8404cfb3cd9e5263284888767ede45e59a41dfe
timvisee <tim@visee.me>
2025-03-18 16:40:26 +0100

    Feature flag use of new shard key format

--- lib/common/common/src/flags.rs
+++ lib/common/common/src/flags.rs
@@ -6,4 +6,12 @@ use serde::Deserialize;
 pub static FEATURE_FLAGS: OnceLock<FeatureFlags> = OnceLock::new();
 
 #[derive(Default, Debug, Deserialize, Clone, Copy)]
-pub struct FeatureFlags {}
+pub struct FeatureFlags {
+    /// Whether to use the new format to persist shard keys
+    ///
+    /// The old format fails to persist shard key numbers correctly, converting them into strings on
+    /// load. While this is false, the new format is only used if any shard key is a number.
+    // TODO(1.14): set to true, remove other branches in code, and remove this flag
+    #[serde(default)]
+    pub use_new_shard_key_mapping_format: bool,
+}
