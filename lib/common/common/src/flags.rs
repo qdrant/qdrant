f30b79ef94649f7bacf25817a112deed8622b79b
Jojii <15957865+JojiiOfficial@users.noreply.github.com>
2025-03-19 10:14:44 +0100

    Prevent unwrapping everywhere; Single place for initialization (#6198)

--- lib/common/common/src/flags.rs
+++ lib/common/common/src/flags.rs
@@ -3,7 +3,7 @@ use std::sync::OnceLock;
 use serde::Deserialize;
 
 /// Global feature flags, normally initialized when starting Qdrant.
-pub static FEATURE_FLAGS: OnceLock<FeatureFlags> = OnceLock::new();
+static FEATURE_FLAGS: OnceLock<FeatureFlags> = OnceLock::new();
 
 #[derive(Default, Debug, Deserialize, Clone, Copy)]
 pub struct FeatureFlags {
@@ -14,4 +14,24 @@ pub struct FeatureFlags {
     // TODO(1.14): set to true, remove other branches in code, and remove this flag
     #[serde(default)]
     pub use_new_shard_key_mapping_format: bool,
+}
+
+/// Initializes the global feature flags with `flags`. Must only be called once at
+/// startup or otherwise throws a warning and discards the values.
+pub fn init_feature_flags(flags: &FeatureFlags) {
+    let res = FEATURE_FLAGS.set(*flags);
+    if res.is_err() {
+        log::warn!("Feature flags already initialized!");
+    }
+}
+
+/// Returns the configured global feature flags.
+pub fn feature_flags() -> FeatureFlags {
+    if let Some(flags) = FEATURE_FLAGS.get() {
+        return *flags;
+    }
+
+    // They should always be initialized.
+    log::warn!("Feature flags not initialized!");
+    FeatureFlags::default()
 }
