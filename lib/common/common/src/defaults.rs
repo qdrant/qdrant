1d286412fe47c54c1c225bdea133185c2a81fc0f
Tim Visée <tim+github@visee.me>
2025-03-21 12:33:34 +0100

    Bump dev version to 1.13.6-dev (#6219)

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Qdrant version string
-pub const QDRANT_VERSION_STRING: &str = "1.13.5-dev";
+pub const QDRANT_VERSION_STRING: &str = "1.13.6-dev";
 
 lazy_static! {
     /// Current Qdrant semver version
