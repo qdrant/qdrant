247c58ea975d28131e043665290433726f84ac06
Luis Cossío <luis.cossio@qdrant.com>
2025-04-04 11:08:21 -0300

    Only implement verification for `CollectionQuery` types (#6319)
    * verify CollectionQuery's Prefetch
    
    * remove implementation for REST query
    
    * remove implementation for REST query groups

--- lib/collection/src/operations/verification/query.rs
+++ lib/collection/src/operations/verification/query.rs
@@ -1,109 +1,11 @@
-use api::rest::{Prefetch, QueryGroupsRequestInternal, QueryRequestInternal};
 use segment::types::StrictModeConfig;
 
 use super::StrictModeVerification;
 use crate::collection::Collection;
 use crate::operations::types::{CollectionError, CollectionResult};
 use crate::operations::universal_query::collection_query::{
-    CollectionQueryGroupsRequest, CollectionQueryRequest, Query,
+    CollectionPrefetch, CollectionQueryGroupsRequest, CollectionQueryRequest, Query,
 };
-
-impl StrictModeVerification for QueryRequestInternal {
-    async fn check_custom(
-        &self,
-        collection: &Collection,
-        strict_mode_config: &StrictModeConfig,
-    ) -> Result<(), crate::operations::types::CollectionError> {
-        if let Some(prefetch) = &self.prefetch {
-            for prefetch in prefetch {
-                prefetch
-                    .check_strict_mode(collection, strict_mode_config)
-                    .await?;
-            }
-        }
-
-        Ok(())
-    }
-
-    fn query_limit(&self) -> Option<usize> {
-        self.limit
-    }
-
-    fn indexed_filter_read(&self) -> Option<&segment::types::Filter> {
-        self.filter.as_ref()
-    }
-
-    fn indexed_filter_write(&self) -> Option<&segment::types::Filter> {
-        None
-    }
-
-    fn request_exact(&self) -> Option<bool> {
-        None
-    }
-
-    fn request_search_params(&self) -> Option<&segment::types::SearchParams> {
-        self.params.as_ref()
-    }
-}
-
-impl StrictModeVerification for Prefetch {
-    async fn check_custom(
-        &self,
-        collection: &Collection,
-        strict_mode_config: &StrictModeConfig,
-    ) -> Result<(), crate::operations::types::CollectionError> {
-        // Prefetch.prefetch is of type Prefetch (recursive type)
-        if let Some(prefetch) = &self.prefetch {
-            for prefetch in prefetch {
-                Box::pin(prefetch.check_strict_mode(collection, strict_mode_config)).await?;
-            }
-        }
-
-        Ok(())
-    }
-
-    fn query_limit(&self) -> Option<usize> {
-        self.limit
-    }
-
-    fn indexed_filter_read(&self) -> Option<&segment::types::Filter> {
-        self.filter.as_ref()
-    }
-
-    fn indexed_filter_write(&self) -> Option<&segment::types::Filter> {
-        None
-    }
-
-    fn request_exact(&self) -> Option<bool> {
-        None
-    }
-
-    fn request_search_params(&self) -> Option<&segment::types::SearchParams> {
-        self.params.as_ref()
-    }
-}
-
-impl StrictModeVerification for QueryGroupsRequestInternal {
-    fn query_limit(&self) -> Option<usize> {
-        self.group_request.limit
-    }
-
-    fn indexed_filter_read(&self) -> Option<&segment::types::Filter> {
-        self.filter.as_ref()
-    }
-
-    fn indexed_filter_write(&self) -> Option<&segment::types::Filter> {
-        None
-    }
-
-    fn request_exact(&self) -> Option<bool> {
-        None
-    }
-
-    fn request_search_params(&self) -> Option<&segment::types::SearchParams> {
-        self.params.as_ref()
-    }
-}
 
 impl Query {
     fn check_strict_mode(
@@ -141,10 +43,56 @@ impl StrictModeVerification for CollectionQueryRequest {
         collection: &Collection,
         strict_mode_config: &StrictModeConfig,
     ) -> CollectionResult<()> {
+        // CollectionPrefetch.prefetch is of type CollectionPrefetch (recursive type)
+        for prefetch in &self.prefetch {
+            Box::pin(prefetch.check_strict_mode(collection, strict_mode_config)).await?;
+        }
+
         if let Some(query) = self.query.as_ref() {
             // check for unindexed fields in formula
             query.check_strict_mode(collection, strict_mode_config)?
         }
+
+        Ok(())
+    }
+
+    fn query_limit(&self) -> Option<usize> {
+        Some(self.limit)
+    }
+
+    fn indexed_filter_read(&self) -> Option<&segment::types::Filter> {
+        self.filter.as_ref()
+    }
+
+    fn indexed_filter_write(&self) -> Option<&segment::types::Filter> {
+        None
+    }
+
+    fn request_exact(&self) -> Option<bool> {
+        None
+    }
+
+    fn request_search_params(&self) -> Option<&segment::types::SearchParams> {
+        self.params.as_ref()
+    }
+}
+
+impl StrictModeVerification for CollectionPrefetch {
+    async fn check_custom(
+        &self,
+        collection: &Collection,
+        strict_mode_config: &StrictModeConfig,
+    ) -> Result<(), crate::operations::types::CollectionError> {
+        // CollectionPrefetch.prefetch is of type CollectionPrefetch (recursive type)
+        for prefetch in &self.prefetch {
+            Box::pin(prefetch.check_strict_mode(collection, strict_mode_config)).await?;
+        }
+
+        if let Some(query) = self.query.as_ref() {
+            // check for unindexed fields in formula
+            query.check_strict_mode(collection, strict_mode_config)?
+        }
+
         Ok(())
     }
 
