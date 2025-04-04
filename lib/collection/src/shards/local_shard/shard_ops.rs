e64563e860c7e20f0d9dea6160955c66431a8acd
timvisee <tim@visee.me>
2025-04-04 17:35:47 +0200

    Parallelize large segment search batches

--- lib/collection/src/shards/local_shard/shard_ops.rs
+++ lib/collection/src/shards/local_shard/shard_ops.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -253,17 +254,66 @@ impl ShardOperation for LocalShard {
         timeout: Option<Duration>,
         hw_measurement_acc: HwMeasurementAcc,
     ) -> CollectionResult<Vec<ShardQueryResponse>> {
-        let planned_query = PlannedQuery::try_from(requests.as_ref().to_owned())?;
+        // Chunk requests for parallelism in certain scenarios
+        //
+        // Deeper down, each segment gets its own dedicated search thread. If this shard has just
+        // one segment, all requests will be executed on a single thread.
+        //
+        // To prevent this from being a bottleneck if we have a lot of requests, we can chunk the
+        // requests into multiple searches to allow more parallelism.
+        //
+        // We don't create smaller chunks than MIN_BATCH_SIZE to ensure our 'filter reuse
+        // optimization' can be properly used.
+        const MIN_CHUNK_SIZE: usize = 10;
+        let chunk_size = {
+            let count = requests.len();
+            if count <= MIN_CHUNK_SIZE {
+                // If number of requests is less than minimum batch size, create one chunk
+                count
+            } else {
+                let segment_count = max(self.segments.read().len().saturating_sub(1), 1);
+                if count <= segment_count {
+                    // If number of requests is less than segment count, create one chunk
+                    // Each request is also already parallelized over each segment
+                    count
+                } else {
+                    // Determine CPU count per segment, create up to that amount of chunks to parallelize
+                    // Each request is also already parallelized over each segment
+                    let cpus_per_segment = max(common::cpu::get_num_cpus() / segment_count, 1);
+                    max(count.div_ceil(cpus_per_segment), MIN_CHUNK_SIZE)
+                }
+            }
+        };
+
+        let mut rate_limit_cost = 0;
+
+        let chunk_futures = requests
+            .chunks(chunk_size)
+            .map(|r| {
+                let planned_query = PlannedQuery::try_from(r.to_vec())?;
+                rate_limit_cost += planned_query.searches.len() + planned_query.scrolls.len();
+                let future = self.do_planned_query(
+                    planned_query,
+                    search_runtime_handle,
+                    timeout,
+                    hw_measurement_acc.clone(),
+                );
+
+                CollectionResult::Ok(future)
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
         // Check read rate limiter before proceeding
-        let cost = planned_query.searches.len() + planned_query.scrolls.len();
-        self.check_read_rate_limiter(cost, &hw_measurement_acc, "query_batch")?;
-        self.do_planned_query(
-            planned_query,
-            search_runtime_handle,
-            timeout,
-            hw_measurement_acc,
-        )
-        .await
+        self.check_read_rate_limiter(rate_limit_cost, &hw_measurement_acc, "query_batch")?;
+
+        // Drive all searches and concatenate chunks again
+        let results = futures::future::try_join_all(chunk_futures)
+            .await?
+            .into_iter()
+            .flatten()
+            .collect();
+
+        Ok(results)
     }
 
     /// This call is rate limited by the read rate limiter.
