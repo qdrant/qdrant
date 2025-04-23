b5a3d58a44896712f3f3afb7d336b743846d38ec
KShivendu <kshivendu1@gmail.com>
2025-04-23 11:45:02 +0530

    include mode in profraw filename

--- tests/integration-tests.sh
+++ tests/integration-tests.sh
@@ -6,9 +6,10 @@ set -ex
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
+MODE=$1
 QDRANT_HOST='localhost:6333'
 export QDRANT__SERVICE__GRPC_PORT="6334"
-export LLVM_PROFILE_FILE="./target/llvm-cov-target/qdrant-integration.profraw" # Avoiding -%p for now
+export LLVM_PROFILE_FILE="./target/llvm-cov-target/qdrant-openapi-$MODE-%m.profraw"
 
 if [ "$COVERAGE" == "1" ]; then
   QDRANT_EXECUTABLE="./target/llvm-cov-target/debug/qdrant"
@@ -16,7 +17,6 @@ else
   QDRANT_EXECUTABLE="./target/debug/qdrant"
 fi
 
-MODE=$1
 # Enable distributed mode on demand
 if [ "$MODE" == "distributed" ]; then
   export QDRANT__CLUSTER__ENABLED="true"
