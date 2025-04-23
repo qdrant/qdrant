91183cc4aa185bf5dabbc231ea176b852b1f6967
KShivendu <kshivendu1@gmail.com>
2025-04-23 13:02:00 +0530

    split coverage workflow into parallel jobs

--- tools/unit-test-coverage.sh
+++ tools/unit-test-coverage.sh
@@ -7,10 +7,20 @@
 # If using locally, occasionally run `cargo llvm-cov clean` to avoid bloating `target/llvm-cov-target` dir with .profraw files
 
 RUN_PER_PACKAGE=${RUN_PER_PACKAGE:-"false"}
+OUTPATH_PATH="unit-test-coverage.lcov"
 
 if [ "$RUN_PER_PACKAGE" == "false" ]; then
     # Run for the entire workspace in one shot. This assumes that the machine has enough memory
-    cargo llvm-cov --no-clean nextest --profile ci --workspace --lcov --output-path "lcov.info"
+    cargo nextest run --no-run --profile ci --workspace --target-dir target/llvm-cov-target --verbose # only build, don't run
+
+    # export LLVM_PROFILE_FILE_NAME="target/llvm-cov-target/qdrant-unit-%m.profraw" # Merge all profraw files into ones
+    # RUSTFLAGS="-C instrument-coverage" cargo build --features "service_debug data-consistency_check" --locked --target-dir target/llvm-cov-target
+    # Before cargo run tests/nextest will need to build the project with LLVM coverage instrumentation
+    # However, surprisingly llvm-cov passes LLVM_PROFILE_FILE_NAME with %p by default and that generates thousands of small files during build phase
+    # Best solution is that llvm-cov should avoid passing this env var in the build step (or introduce a dedicated build step without this)
+    # But in the meantime, I'm using this workaround of using %m to merge all profraw files into one (includes data from build + test phase)
+    cargo llvm-cov --no-clean nextest --profile ci --workspace --verbose
+    cargo llvm-cov report --lcov --output-path "$OUTPATH_PATH"
     cargo llvm-cov report --html
     exit 0
 fi
@@ -39,5 +49,5 @@ for PACKAGE in "${PACKAGES[@]}"; do
 done
 
 if [ -n "$LCOV_COMMAND_ARGS" ]; then
-    lcov $LCOV_COMMAND_ARGS --output-file unit-test-lcov.info
+    lcov $LCOV_COMMAND_ARGS --output-file "$OUTPATH_PATH"
 fi
