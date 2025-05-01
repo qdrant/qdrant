317ad0f0ef8147fbad10548b1bc9880804157b50
timvisee <tim@visee.me>
2025-05-01 12:14:54 +0200

    Link to pull request

--- lib/collection/src/shards/local_shard/search.rs
+++ lib/collection/src/shards/local_shard/search.rs
@@ -44,6 +44,7 @@ impl LocalShard {
             // Don't batch if we have more segments than search threads
             // Not a perfect condition, but it helps to prevent consuming a lot of search threads
             // if the number of segments is large
+            // See: <https://github.com/qdrant/qdrant/pull/6478>
             true
         } else {
             false
