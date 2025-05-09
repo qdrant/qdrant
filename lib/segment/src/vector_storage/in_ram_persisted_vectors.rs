d6a0d3caa7f28492d907e6026943317cd475a713
generall <andrey@vasnetsov.com>
2025-05-09 13:24:03 +0200

    implement get_many_sequential

--- lib/segment/src/vector_storage/in_ram_persisted_vectors.rs
+++ lib/segment/src/vector_storage/in_ram_persisted_vectors.rs
@@ -97,8 +97,7 @@ impl<T: Sized + Copy + Clone + Default + 'static> ChunkedVectorStorage<T>
 
     #[inline]
     fn get_many_sequential(&self, key: VectorOffsetType, count: usize) -> Option<&[T]> {
-        // No optimization for sequential access
-        self.mmap_storage.get_many(key, count)
+        self.mmap_storage.get_many_sequential(key, count)
     }
 
     #[inline]
