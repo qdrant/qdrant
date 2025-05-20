5d1763a169262a1701d27db7825799a3534bcc04
timvisee <tim@visee.me>
2025-05-20 12:12:51 +0200

    Resolve TODOs

--- lib/segment/src/spaces/simple_avx.rs
+++ lib/segment/src/spaces/simple_avx.rs
@@ -8,14 +8,11 @@ use crate::data_types::vectors::{DenseVector, VectorElementType};
 #[target_feature(enable = "avx")]
 #[target_feature(enable = "fma")]
 #[allow(clippy::missing_safety_doc)]
-#[allow(unused_unsafe)] // TODO remove once we can bump the MSRV to 1.87 (cargo-chef)
 pub unsafe fn hsum256_ps_avx(x: __m256) -> f32 {
-    unsafe {
-        let x128: __m128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));
-        let x64: __m128 = _mm_add_ps(x128, _mm_movehl_ps(x128, x128));
-        let x32: __m128 = _mm_add_ss(x64, _mm_shuffle_ps(x64, x64, 0x55));
-        _mm_cvtss_f32(x32)
-    }
+    let x128: __m128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));
+    let x64: __m128 = _mm_add_ps(x128, _mm_movehl_ps(x128, x128));
+    let x32: __m128 = _mm_add_ss(x64, _mm_shuffle_ps(x64, x64, 0x55));
+    _mm_cvtss_f32(x32)
 }
 
 #[target_feature(enable = "avx")]
