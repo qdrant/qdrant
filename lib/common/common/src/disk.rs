c6d2379074d429aea1d0cf89533e1dcab7518438
Tim Visée <tim+github@visee.me>
2025-06-06 17:09:14 +0200

    Migrate away from RocksDB based sparse vector storage (#6607)
    * Reformat
    
    * Add function to find appendable sparse storage files without opening it
    
    * Actively migrate away from RocksDB based sparse vector storage
    
    * Add test for simple sparse vector storage migration
    
    * Fix test comments
    
    * Also migrate vector deletes for sparse storage
    
    * Use walkdir to list files

--- lib/common/common/src/disk.rs
+++ lib/common/common/src/disk.rs
@@ -1,5 +1,7 @@
 use std::path::{Path, PathBuf};
 
+use walkdir::WalkDir;
+
 /// How many bytes a directory takes.
 pub fn dir_size(path: impl Into<PathBuf>) -> std::io::Result<u64> {
     fn dir_size(mut dir: std::fs::ReadDir) -> std::io::Result<u64> {
@@ -28,14 +30,11 @@ pub fn list_files(dir: impl AsRef<Path>) -> std::io::Result<Vec<PathBuf>> {
     }
 
     let mut files = Vec::new();
-    for entry in dir.read_dir()? {
+    for entry in WalkDir::new(dir).min_depth(1).follow_links(true) {
         let entry = entry?;
-        let file_type = entry.file_type()?;
+        let file_type = entry.file_type();
         if file_type.is_file() || file_type.is_symlink() {
-            files.push(entry.path());
-        } else {
-            debug_assert!(file_type.is_dir(), "path is expected to be a dir");
-            files.extend(list_files(entry.path())?);
+            files.push(entry.into_path());
         }
     }
 
