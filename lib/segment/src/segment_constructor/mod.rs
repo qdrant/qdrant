d629781844698746c8d7679793b8da2ab02c93e1
timvisee <tim@visee.me>
2025-06-09 13:07:25 +0200

    Only expose simple segment constructor in tests

--- lib/segment/src/segment_constructor/mod.rs
+++ lib/segment/src/segment_constructor/mod.rs
@@ -3,6 +3,7 @@ mod batched_reader;
 mod rocksdb_builder;
 pub mod segment_builder;
 mod segment_constructor_base;
+#[cfg(any(test, feature = "testing"))]
 pub mod simple_segment_constructor;
 
 pub use segment_constructor_base::*;
