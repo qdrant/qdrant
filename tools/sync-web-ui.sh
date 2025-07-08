9102c888a137938a347741af7b1a513d8bc166f2
Andrey Vasnetsov <andrey@vasnetsov.com>
2025-07-08 09:54:23 +0200

    add retries to curl (#6820)

--- tools/sync-web-ui.sh
+++ tools/sync-web-ui.sh
@@ -8,7 +8,7 @@ OPENAPI_FILE=${OPENAPI_DIR:-"./docs/redoc/master/openapi.json"}
 # Download `dist.zip` from the latest release of https://github.com/qdrant/qdrant-web-ui and unzip given folder
 
 # Get latest dist.zip, assume jq is installed
-DOWNLOAD_LINK=$(curl --silent "https://api.github.com/repos/qdrant/qdrant-web-ui/releases/latest" | jq -r '.assets[] | select(.name=="dist-qdrant.zip") | .browser_download_url')
+DOWNLOAD_LINK=$(curl --retry 5 --retry-all-errors --silent "https://api.github.com/repos/qdrant/qdrant-web-ui/releases/latest" | jq -r '.assets[] | select(.name=="dist-qdrant.zip") | .browser_download_url')
 
 if command -v wget &> /dev/null
 then
