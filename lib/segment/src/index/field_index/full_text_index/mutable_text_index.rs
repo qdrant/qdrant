62ce6b86d1baa614557e1738eb3ee9082282a18f
timvisee <tim@visee.me>
2025-07-08 14:19:33 +0200

    Use weak references to Gridstore in mutable payload index flushers

--- lib/segment/src/index/field_index/full_text_index/mutable_text_index.rs
+++ lib/segment/src/index/field_index/full_text_index/mutable_text_index.rs
@@ -242,9 +242,18 @@ impl MutableFullTextIndex {
             #[cfg(feature = "rocksdb")]
             Storage::RocksDb(db_wrapper) => db_wrapper.flusher(),
             Storage::Gridstore(Some(store)) => {
-                let store = store.clone();
+                let store = Arc::downgrade(store);
                 Box::new(move || {
-                    store.read().flush().map_err(|err| {
+                    store
+                        .upgrade()
+                        .ok_or_else(|| {
+                            OperationError::service_error(
+                                "Failed to flush mutable full text index, backing Gridstore storage is already dropped",
+                            )
+                        })?
+                        .read()
+                        .flush()
+                        .map_err(|err| {
                         OperationError::service_error(format!(
                             "Failed to flush mutable full text index gridstore: {err}"
                         ))
