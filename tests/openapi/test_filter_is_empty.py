a0c270089c7cda650e40d3f24cb17bb2ad373f7d
timvisee <tim@visee.me>
2025-07-16 10:15:23 +0200

    Add test to catch bug
    Bug: <https://github.com/qdrant/qdrant/issues/6880>

---
+++ tests/openapi/test_filter_is_empty.py
@@ -0,0 +1,119 @@
+import pytest
+
+from .helpers.collection_setup import multivec_collection_setup, drop_collection
+from .helpers.helpers import request_with_validation
+
+
+@pytest.fixture(autouse=True, scope="module")
+def setup(collection_name):
+    create_collection(collection_name)
+    yield
+    drop_collection(collection_name=collection_name)
+
+
+def create_collection(collection_name):
+    drop_collection(collection_name)
+
+    response = request_with_validation(
+        api='/collections/{collection_name}',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        body={
+            "vectors": {
+                "size": 3,
+                "distance": "Dot",
+            },
+        }
+    )
+    assert response.ok
+
+    response = request_with_validation(
+        api='/collections/{collection_name}',
+        method="GET",
+        path_params={'collection_name': collection_name},
+    )
+    assert response.ok
+
+    response = request_with_validation(
+        api='/collections/{collection_name}/index',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        query_params={'wait': 'true'},
+        body={
+            "field_name": "a",
+            "field_schema": "keyword",
+        }
+    )
+    assert response.ok
+
+    response = request_with_validation(
+        api='/collections/{collection_name}/index',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        query_params={'wait': 'true'},
+        body={
+            "field_name": "b",
+            "field_schema": "keyword",
+        }
+    )
+    assert response.ok
+
+    response = request_with_validation(
+        api='/collections/{collection_name}/points',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        query_params={'wait': 'true'},
+        body={
+            "points": [
+                {
+                    "id": 1,
+                    "vector": [0.05, 0.61, 0.76],
+                    "payload": {"a": "foo"}
+                },
+                {
+                    "id": 2,
+                    "vector": [0.19, 0.81, 0.75],
+                    "payload": {"a": "foo"}
+                },
+                {
+                    "id": 3,
+                    "vector": [0.36, 0.55, 0.47],
+                    "payload": {"a": "foo"}
+                },
+            ]
+        }
+    )
+    assert response.ok
+
+
+def test_filter_is_empty(collection_name):
+    """
+    Bug: <https://github.com/qdrant/qdrant/issues/6880>
+    """
+
+    response = request_with_validation(
+        api='/collections/{collection_name}/points/query',
+        method="POST",
+        path_params={'collection_name': collection_name},
+        body={
+            "filter": {
+                "must": [
+                    {
+                        "key": "a",
+                        "match": { "value": "foo" }
+                    },
+                    {
+                        "is_empty": {
+                            "key": "b"
+                        }
+                    }
+                ]
+            },
+            "query": [0.1, 0.1, 0.1],
+            "limit": 2,
+        }
+    )
+    assert response.ok
+
+    json = response.json()
+    assert len(json['result']['points']) == 2, json
