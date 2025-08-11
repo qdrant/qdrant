293855fa12d3455bbe3f85690a91ff1d4596c90f
timvisee <tim@visee.me>
2025-08-11 14:37:28 +0200

    Bump dev version to v1.15.3-dev

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Qdrant version string
-pub const QDRANT_VERSION_STRING: &str = "1.15.2-dev";
+pub const QDRANT_VERSION_STRING: &str = "1.15.3-dev";
 
 lazy_static! {
     /// Current Qdrant semver version
