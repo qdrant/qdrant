a0c69680d10ee1c1ced1d8e3a1e437db4b09d36c
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2025-09-02 17:00:30 +0200

    add comment

--- lib/collection/src/collection_manager/optimizers/config_mismatch_optimizer.rs
+++ lib/collection/src/collection_manager/optimizers/config_mismatch_optimizer.rs
@@ -184,6 +184,8 @@ impl ConfigMismatchOptimizer {
                                     let target_quantization_appendable = target_quantization
                                         .map(|q| q.supports_appendable())
                                         .unwrap_or(false);
+                                    // If segment is unindexed, only appendable quantization is applied.
+                                    // So that we check if any config is appendable to avoid infinity loop here.
                                     let unindexed_changed = vector_data_quantization_appendable
                                         || target_quantization_appendable;
                                     (vector_data.quantization_config.is_some()
