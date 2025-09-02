981386403038f247ea6fd671f166bf7c56a039f6
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2025-09-02 12:54:12 +0200

    Chain nested locks for proxy segments (#7196)
    * Chain nested locks for proxy segments
    
    * Remove get_cloned function on locked segment
    
    ---------
    
    Co-authored-by: timvisee <tim@visee.me>

--- lib/collection/src/collection_manager/tests/mod.rs
+++ lib/collection/src/collection_manager/tests/mod.rs
@@ -241,8 +241,8 @@ fn test_upsert_points_in_smallest_segment() {
 
     // Segment 1 and 2 are over capacity, we expect to have the new points in segment 3
     {
-        let segment3 = segments.read().get(sid3).unwrap().get_cloned();
-        let segment3_read = segment3.read();
+        let segment3 = segments.read();
+        let segment3_read = segment3.get(sid3).unwrap().get().read();
         for point_id in 1000..1010 {
             assert!(segment3_read.has_point(point_id.into()));
         }
