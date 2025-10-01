2fbbeb06f540d992e913284785373e136c755060
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2025-10-01 10:31:36 +0200

    enable some our trace logs

--- tests/consensus_tests/utils.py
+++ tests/consensus_tests/utils.py
@@ -94,7 +94,7 @@ def get_env(p2p_port: int, grpc_port: int, http_port: int) -> Dict[str, str]:
     env["QDRANT__CLUSTER__P2P__PORT"] = str(p2p_port)
     env["QDRANT__SERVICE__HTTP_PORT"] = str(http_port)
     env["QDRANT__SERVICE__GRPC_PORT"] = str(grpc_port)
-    env["QDRANT__LOG_LEVEL"] = "DEBUG,raft::raft=info"
+    env["QDRANT__LOG_LEVEL"] = "TRACE,raft::raft=info,actix_http=info,tonic=info,want=info,mio=info"
     env["QDRANT__SERVICE__HARDWARE_REPORTING"] = "true"
 
     if is_coverage_mode():
