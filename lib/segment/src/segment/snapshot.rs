3866ccd0810ce691fde2d9ed4d5621d2475d09fa
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2025-10-10 10:46:32 +0200

    juggle with booleans

--- lib/segment/src/segment/snapshot.rs
+++ lib/segment/src/segment/snapshot.rs
@@ -36,7 +36,7 @@ impl SnapshotEntry for Segment {
         log::debug!("Taking snapshot of segment {segment_id}");
 
         // force flush segment to capture latest state
-        self.flush(false, true)?;
+        self.flush(true, true)?;
 
         let include_files = match manifest {
             None => HashSet::new(),
