3701d4326c57100084a515ee4f74a2447971bb4d
generall <andrey@vasnetsov.com>
2025-10-19 21:23:08 +0200

    implement filter condition

--- lib/edge/python/src/types/filter/condition.rs
+++ lib/edge/python/src/types/filter/condition.rs
@@ -1,17 +1,20 @@
 use pyo3::{FromPyObject, IntoPyObject};
 use segment::types::Condition;
 
+use crate::types::PyFilter;
 use crate::types::filter::field_condition::PyFieldCondition;
 
 #[derive(Clone, Debug, IntoPyObject, FromPyObject)]
 pub enum PyCondition {
-    FieldCondition(PyFieldCondition),
+    Field(PyFieldCondition),
+    Filter(PyFilter),
 }
 
 impl From<PyCondition> for Condition {
     fn from(value: PyCondition) -> Self {
         match value {
-            PyCondition::FieldCondition(field_condition) => Condition::Field(field_condition.0),
+            PyCondition::Field(field_condition) => Condition::Field(field_condition.0),
+            PyCondition::Filter(filter) => Condition::Filter(filter.0),
         }
     }
 }
@@ -20,14 +23,14 @@ impl From<Condition> for PyCondition {
     fn from(value: Condition) -> Self {
         match value {
             Condition::Field(field_condition) => {
-                PyCondition::FieldCondition(PyFieldCondition(field_condition))
+                PyCondition::Field(PyFieldCondition(field_condition))
             }
             Condition::IsEmpty(_) => todo!(),
             Condition::IsNull(_) => todo!(),
             Condition::HasId(_) => todo!(),
             Condition::HasVector(_) => todo!(),
             Condition::Nested(_) => todo!(),
-            Condition::Filter(_) => todo!(),
+            Condition::Filter(filter) => PyCondition::Filter(PyFilter(filter)),
             Condition::CustomIdChecker(_) => todo!(),
         }
     }
