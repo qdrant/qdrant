2598306ab2249b580349edb68ec41b74c12ac3ea
generall <andrey@vasnetsov.com>
2025-10-24 09:31:27 +0200

    remove more tests

--- tests/consensus_tests/auth_tests/test_jwt_validation.py
+++ tests/consensus_tests/auth_tests/test_jwt_validation.py
@@ -9,18 +9,6 @@ OTHER_COLL_NAME = "jwt_other_test_collection"
 
 
 @pytest.fixture(scope="module")
-def berlin_token():
-    return encode_jwt(
-        {
-            "access": [
-                {"collection": COLL_NAME, "access": "r", "payload": {"city": "Berlin"}}
-            ]
-        },
-        SECRET,
-    )
-
-
-@pytest.fixture(scope="module")
 def collection_readonly_token():
     return encode_jwt(
         {
@@ -128,120 +116,6 @@ def test_value_exists_claim():
     fixtures.drop_collection(REST_URI, validation_collection, headers=API_KEY_HEADERS)
 
 
-def test_payload_filters_scroll(berlin_token):
-    res = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/scroll",
-        json={
-            "limit": 100,
-            "with_payload": True,
-        },
-        headers={"Authorization": f"Bearer {berlin_token}"},
-    )
-    res.raise_for_status()
-
-    cities_in_payload = set(
-        point["payload"].get("city") for point in res.json()["result"]["points"]
-    )
-
-    assert cities_in_payload == {"Berlin"}
-
-
-def test_payload_filters_search(berlin_token):
-    res = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/search",
-        json={
-            "vector": [1, 2, 3, 4],
-            "limit": 100,
-            "with_payload": True,
-        },
-        headers={"Authorization": f"Bearer {berlin_token}"},
-    )
-    res.raise_for_status()
-
-    cities_in_payload = set(
-        point["payload"].get("city") for point in res.json()["result"]
-    )
-
-    assert cities_in_payload == {"Berlin"}
-
-
-def test_payload_filters_count(berlin_token):
-    res = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/count",
-        json={},
-        headers={"Authorization": f"Bearer {berlin_token}"},
-    )
-    res.raise_for_status()
-
-    res_expected = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/count",
-        json={"filter": {"must": [{"key": "city", "match": {"value": "Berlin"}}]}},
-        headers=API_KEY_HEADERS,
-    )
-    res_expected.raise_for_status()
-
-    assert res.json()["result"] == res_expected.json()["result"]
-
-
-@pytest.mark.parametrize(
-    "prefetch",
-    [
-        None,
-        {"query": [0.1, 0.2, 0.3, 0.4]},
-        [{"query": [0.1, 0.2, 0.3, 0.4]}, {"limit": 100}],
-    ],
-)
-def test_payload_filters_query(berlin_token, prefetch):
-    # Check that there are more cities in the payload
-    res = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/query",
-        json={
-            "prefetch": prefetch,
-            "query": [0.1, 0.2, 0.3, 0.4],
-            "limit": 100,
-            "with_payload": True,
-        },
-        headers=API_KEY_HEADERS,
-    )
-
-    cities_in_payload = set(
-        point["payload"].get("city") for point in res.json()["result"]["points"]
-    )
-
-    assert len(cities_in_payload) > 1
-
-    # Now check that there is only Berlin with the payload claim in the token
-    res = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/query",
-        json={
-            "prefetch": prefetch,
-            "query": [0.1, 0.2, 0.3, 0.4],
-            "limit": 100,
-            "with_payload": True,
-        },
-        headers={"Authorization": f"Bearer {berlin_token}"},
-    )
-
-    cities_in_payload = set(
-        point["payload"].get("city") for point in res.json()["result"]["points"]
-    )
-
-    assert cities_in_payload == {"Berlin"}
-
-
-def test_payload_claim_fails_with_ids_in_recommend(berlin_token):
-    res = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/recommend",
-        json={"positive": [1], "limit": 10},
-        headers={"Authorization": f"Bearer {berlin_token}"},
-    )
-    assert res.status_code == 403, res.json()
-    assert (
-        res.json()["status"]["error"]
-        == 'Forbidden: This operation is not allowed when "payload" restriction is present for collection jwt_test_collection'
-    )
-
-
 def test_recommend_collection_lookup(collection_readonly_token):
     # delete other collection if exists
     fixtures.drop_collection(REST_URI, OTHER_COLL_NAME, headers=API_KEY_HEADERS)
@@ -278,8 +152,8 @@ def test_recommend_collection_lookup(collection_readonly_token):
     )
     assert res.status_code == 403, res.json()
     assert (
-        res.json()["status"]["error"]
-        == f"Forbidden: Access to collection {OTHER_COLL_NAME} is required"
+            res.json()["status"]["error"]
+            == f"Forbidden: Access to collection {OTHER_COLL_NAME} is required"
     )
 
 
@@ -328,8 +202,8 @@ def test_query_recommendation_collection_lookup(collection_readonly_token):
     )
     assert res.status_code == 403, res.json()
     assert (
-        res.json()["status"]["error"]
-        == f"Forbidden: Access to collection {OTHER_COLL_NAME} is required"
+            res.json()["status"]["error"]
+            == f"Forbidden: Access to collection {OTHER_COLL_NAME} is required"
     )
 
     # query nested recommendation with points from other collection should fail
@@ -354,67 +228,6 @@ def test_query_recommendation_collection_lookup(collection_readonly_token):
     )
     assert res.status_code == 403, res.json()
     assert (
-        res.json()["status"]["error"]
-        == f"Forbidden: Access to collection {OTHER_COLL_NAME} is required"
-    )
-
-
-@pytest.mark.parametrize("target", [1, None])
-@pytest.mark.parametrize("context", [None, [{"positive": 1, "negative": 3}]])
-def test_payload_claim_fails_with_ids_in_discover(berlin_token, target, context):
-    if not target and not context:
-        return
-
-    res = requests.post(
-        f"{REST_URI}/collections/{COLL_NAME}/points/discover",
-        json={"target": target, "context": context, "limit": 10},
-        headers={"Authorization": f"Bearer {berlin_token}"},
-    )
-    assert res.status_code == 403, res.json()
-    assert (
-        res.json()["status"]["error"]
-        == 'Forbidden: This operation is not allowed when "payload" restriction is present for collection jwt_test_collection'
-    )
-
-
-@pytest.mark.parametrize(
-    "query",
-    [
-        1,
-        {"nearest": 1},
-        {"recommend": {"positive": [1]}},
-        {"recommend": {"negative": [1]}},
-        {"recommend": {"negative": [1], "strategy": "best_score"}},
-        {"discover": {"target": 1, "context": [{"positive": 1, "negative": 2}]}},
-        {"context": [{"positive": 1, "negative": 2}]},
-    ],
-)
-@pytest.mark.parametrize("in_prefetch", [False, True])
-def test_payload_claim_fails_with_ids_in_query(berlin_token, query, in_prefetch):
-    if in_prefetch:
-        res = requests.post(
-            f"{REST_URI}/collections/{COLL_NAME}/points/query",
-            json={
-                "prefetch": {"query": query},
-                "query": [0.1, 0.2, 0.3, 0.4],
-                "limit": 100,
-                "with_payload": True,
-            },
-            headers={"Authorization": f"Bearer {berlin_token}"},
-        )
-    else:
-        res = requests.post(
-            f"{REST_URI}/collections/{COLL_NAME}/points/query",
-            json={
-                "query": query,
-                "limit": 100,
-                "with_payload": True,
-            },
-            headers={"Authorization": f"Bearer {berlin_token}"},
-        )
-
-    assert res.status_code == 403, res.json()
-    assert (
-        res.json()["status"]["error"]
-        == 'Forbidden: This operation is not allowed when "payload" restriction is present for collection jwt_test_collection'
+            res.json()["status"]["error"]
+            == f"Forbidden: Access to collection {OTHER_COLL_NAME} is required"
     )
