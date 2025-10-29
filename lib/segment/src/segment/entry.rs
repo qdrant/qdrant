0fc3c3542bcf32e4e4638eeec40250e209e5afda
timvisee <tim@visee.me>
2025-10-29 09:53:46 +0100

    Get vector data checked to prevent panic

--- lib/segment/src/segment/entry.rs
+++ lib/segment/src/segment/entry.rs
@@ -67,7 +67,11 @@ impl SegmentEntry for Segment {
         query_context: &SegmentQueryContext,
     ) -> OperationResult<Vec<Vec<ScoredPoint>>> {
         check_query_vectors(vector_name, query_vectors, &self.segment_config)?;
-        let vector_data = &self.vector_data[vector_name];
+        let vector_data = &self.vector_data.get(vector_name).ok_or_else(|| {
+            OperationError::VectorNameNotExists {
+                received_name: vector_name.to_owned(),
+            }
+        })?;
         let vector_query_context = query_context.get_vector_context(vector_name);
         let internal_results = vector_data.vector_index.borrow().search(
             query_vectors,
@@ -319,7 +323,6 @@ impl SegmentEntry for Segment {
         point_id: PointIdType,
         hw_counter: &HardwareCounterCell,
     ) -> OperationResult<Option<VectorInternal>> {
-        check_vector_name(vector_name, &self.segment_config)?;
         let internal_id = self.lookup_internal_id(point_id)?;
         let vector_opt = self.vector_by_offset(vector_name, internal_id, hw_counter)?;
         Ok(vector_opt)
@@ -454,7 +457,11 @@ impl SegmentEntry for Segment {
 
     fn available_vectors_size_in_bytes(&self, vector_name: &VectorName) -> OperationResult<usize> {
         check_vector_name(vector_name, &self.segment_config)?;
-        let vector_data = &self.vector_data[vector_name];
+        let vector_data = self.vector_data.get(vector_name).ok_or_else(|| {
+            OperationError::VectorNameNotExists {
+                received_name: vector_name.to_owned(),
+            }
+        })?;
         let size = vector_data
             .vector_index
             .borrow()
