df0c11eece0bdd501b53baf50fe39702e1c61c6d
timvisee <tim@visee.me>
2025-10-29 12:52:19 +0100

    Improve minimum active replica count in case of resharding

--- src/common/metrics.rs
+++ src/common/metrics.rs
@@ -1,5 +1,5 @@
 use api::rest::models::HardwareUsage;
-use collection::shards::telemetry::OptimizerRunCounters;
+use collection::shards::replica_set::ReplicaState;
 use itertools::Itertools;
 use prometheus::TextEncoder;
 use prometheus::proto::{Counter, Gauge, LabelPair, Metric, MetricFamily, MetricType};
@@ -197,12 +197,42 @@ impl MetricsProvider for CollectionsTelemetry {
                 .shards
                 .iter()
                 .flatten()
-                // Ignore shards in resharding state.
-                .filter(|shard| !shard.replicate_states.values().any(|i| i.is_resharding()))
+                // While resharding up, some (shard) replica sets may still be incomplete during
+                // the resharding process. In that case we don't want to consider these replica
+                // sets at all in the active replica calculation. This is fine because searches nor
+                // searches don't depend on them being available yet.
+                //
+                // More specifically:
+                // - in stage 2 (migrate points) of resharding up we don't rely on the replica
+                //   to be available yet. In this stage, these replicas will have the `Resharding`
+                //   state.
+                // - in stage 3 (replicate) of resharding up we activate the the replica and
+                //   replicate to match the configuration replication factor. From this point on we
+                //   do rely on the replica to be available. Now one replica will be `Active`, and
+                //   the other replicas will be in a transfer state. No replica will have `Resharding`
+                //   state.
+                //
+                // So, during stage 2 of resharding up we don't want to adjust the minimum number
+                // of active replicas downwards. During stage 3 we do want it to affect the minimum
+                // available replica number. It will be 1 for some time until replication transfers
+                // complete.
+                //
+                // To ignore a (shard) replica set that is in stage 2 of resharding up, we simply
+                // check if any of it's replicas is in `Resharding` state.
+                .filter(|shard| {
+                    !shard
+                        .replicate_states
+                        .values()
+                        .any(|i| matches!(i, ReplicaState::Resharding))
+                })
                 .map(|shard| {
                     shard
                         .replicate_states
                         .values()
+                        // While resharding down, all the replicas that we keep will get the
+                        // `ReshardingScaleDown` state for a period of time. We simply consider
+                        // these replicas to be active. The `is_active` function already accounts
+                        // this.
                         .filter(|state| state.is_active())
                         .count()
                 })
@@ -218,17 +248,6 @@ impl MetricsProvider for CollectionsTelemetry {
                 total_min_active_replicas = total_min_active_replicas.min(min);
                 total_max_active_replicas = total_max_active_replicas.max(max);
             }
-
-            // Sum the optimization triggers over all shards of this collection.
-            let optimizer_triggers = collection
-                .shards
-                .iter()
-                .flatten()
-                .filter_map(|i| i.local.as_ref())
-                .map(|i| i.optimizations.runs)
-                .fold(OptimizerRunCounters::default(), |total, state| {
-                    total + state
-                });
         }
 
         metrics.push(metric_family(
