c1e5b17ddea035df3ee3e7b486edb96068886474
Jojii <15957865+JojiiOfficial@users.noreply.github.com>
2025-10-29 16:48:06 +0100

    Dead shards in /metrics (#7310)
    * Dead shards in /metrics
    
    * Update src/common/metrics.rs
    
    Co-authored-by: Tim Visée <tim+github@visee.me>
    
    ---------
    
    Co-authored-by: Tim Visée <tim+github@visee.me>

--- src/common/metrics.rs
+++ src/common/metrics.rs
@@ -166,6 +166,7 @@ impl MetricsProvider for CollectionsTelemetry {
                 CollectionTelemetryEnum::Full(c) => c.count_vectors(),
             })
             .sum::<usize>();
+
         metrics.push(metric_family(
             "collections_total",
             "number of collections",
@@ -192,6 +193,8 @@ impl MetricsProvider for CollectionsTelemetry {
 
         // Vectors excluded from index-only requests.
         let mut indexed_only_excluded = vec![];
+
+        let mut total_dead_shards = 0;
 
         for collection in self.collections.iter().flatten() {
             let collection = match collection {
@@ -290,6 +293,13 @@ impl MetricsProvider for CollectionsTelemetry {
                     &[("id", &collection.id), ("vector", name)],
                 ))
             }
+
+            total_dead_shards += collection
+                .shards
+                .iter()
+                .flatten()
+                .filter(|i| i.replicate_states.values().any(|state| !state.is_active()))
+                .count();
         }
 
         if !indexed_only_excluded.is_empty() {
@@ -340,6 +350,13 @@ impl MetricsProvider for CollectionsTelemetry {
             "approximate amount of vectors per collection",
             MetricType::GAUGE,
             vectors_per_collection,
+        ));
+
+        metrics.push(metric_family(
+            "dead_shards_total",
+            "total amount of shard replicas in non-active state",
+            MetricType::GAUGE,
+            vec![gauge(total_dead_shards as f64, &[])],
         ));
     }
 }
