e15a54f91b1dbbbb98dab58c549ddad84222f67e
Roman Titov <ffuugoo@users.noreply.github.com>
2025-10-29 15:41:43 +0100

    Cleanup `edge_py::types::filter::condition` module

--- lib/edge/python/src/types/point_id.rs
+++ lib/edge/python/src/types/point_id.rs
@@ -7,12 +7,17 @@ use pyo3::prelude::*;
 use segment::types::PointIdType;
 use uuid::Uuid;
 
-#[derive(Copy, Clone, Debug, Into)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Into)]
 #[repr(transparent)]
 pub struct PyPointId(pub PointIdType);
 
 impl PyPointId {
     pub fn into_rust_vec(point_ids: Vec<PyPointId>) -> Vec<PointIdType> {
+        // `PyPointId` has transparent representation, so transmuting is safe
+        unsafe { mem::transmute(point_ids) }
+    }
+
+    pub fn into_rust_set(point_ids: ahash::HashSet<PyPointId>) -> ahash::HashSet<PointIdType> {
         // `PyPointId` has transparent representation, so transmuting is safe
         unsafe { mem::transmute(point_ids) }
     }
