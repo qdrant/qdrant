4085c79c7670535ef15990e8ba04925a19d13d62
Roman Titov <ffuugoo@users.noreply.github.com>
2025-11-13 12:14:52 +0100

    Restrict `/logger` API
    - check access token
    - updating logger configuration requires `manage` access
    - on-disk log file path can only be set in config file,
      it can't be updated through `/logger` API

--- src/tracing/on_disk.rs
+++ src/tracing/on_disk.rs
@@ -71,14 +71,16 @@ where
     }
 
     let Some(log_file) = &config.log_file else {
-        return Err(anyhow::format_err!("log file is not specified"));
+        return Err(anyhow::format_err!(
+            "log file is not specified (it can only be specified in the config file)"
+        ));
     };
 
     let writer = fs::OpenOptions::new()
         .create(true)
         .append(true)
         .open(log_file)
-        .with_context(|| format!("failed to open {log_file} log-file"))?;
+        .with_context(|| format!("failed to open log file {log_file}"))?;
 
     let layer = fmt::Layer::default()
         .with_writer(Mutex::new(io::BufWriter::with_capacity(
