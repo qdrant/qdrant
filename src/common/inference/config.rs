f47d664a83b098d9c192108be29f8d273693cef6
Daniel Boros <dancixx@gmail.com>
2025-11-25 10:33:11 +0100

    chore: cleanup timeout from inference config

--- src/common/inference/config.rs
+++ src/common/inference/config.rs
@@ -3,20 +3,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct InferenceConfig {
     pub address: Option<String>,
-    #[serde(default = "default_inference_timeout")]
-    pub timeout: u64,
     pub token: Option<String>,
-}
-
-const fn default_inference_timeout() -> u64 {
-    10
 }
 
 impl InferenceConfig {
     pub fn new(address: Option<String>) -> Self {
         Self {
             address,
-            timeout: default_inference_timeout(),
             token: None,
         }
     }
@@ -26,7 +19,6 @@ impl Default for InferenceConfig {
     fn default() -> Self {
         Self {
             address: None,
-            timeout: default_inference_timeout(),
             token: None,
         }
     }
