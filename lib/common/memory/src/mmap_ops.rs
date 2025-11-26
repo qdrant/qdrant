1cba724293298add10a9b15ef32601301cf2df04
timvisee <tim@visee.me>
2025-11-26 14:33:05 +0100

    Import LazyLock

--- lib/common/memory/src/mmap_ops.rs
+++ lib/common/memory/src/mmap_ops.rs
@@ -1,5 +1,6 @@
 use std::mem::{align_of, size_of};
 use std::path::Path;
+use std::sync::LazyLock;
 use std::{io, mem, ptr};
 
 use fs_err as fs;
@@ -15,7 +16,7 @@ pub const TEMP_FILE_EXTENSION: &str = "tmp";
 /// Some environments corrupt data on the file system if multiple memory maps are opened on the
 /// same piece of data. This variable allows disabling the use of multiple memory maps at runtime.
 /// An example of such environment is Docker on Windows with a mount into Windows.
-pub static MULTI_MMAP_IS_SUPPORTED: std::sync::LazyLock<bool> = std::sync::LazyLock::new(|| {
+pub static MULTI_MMAP_IS_SUPPORTED: LazyLock<bool> = LazyLock::new(|| {
     let supported = std::env::var_os("QDRANT_NO_MULTI_MMAP").is_none_or(|val| val.is_empty());
     if !supported {
         log::warn!("QDRANT_NO_MULTI_MMAP is set, you may see reduced performance");
