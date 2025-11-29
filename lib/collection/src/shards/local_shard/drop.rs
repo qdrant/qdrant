962ea4fe989a25e765da48d6d5ae7a9c6ddbaa30
generall <andrey@vasnetsov.com>
2025-11-29 02:19:17 +0100

    explain when it is ok to shutdown

--- lib/collection/src/shards/local_shard/drop.rs
+++ lib/collection/src/shards/local_shard/drop.rs
@@ -4,23 +4,20 @@ impl Drop for LocalShard {
     fn drop(&mut self) {
         let already_stopped = self.blocking_ask_workers_to_stop();
 
+        // Normally we expect, that LocalShard should be explicitly stopped in asynchronous
+        // runtime before being dropped.
+        // We want this because:
+        // - We don't want a conflict of background tasks.
+        // - We might want to delete files, and can't have other threads working with them.
+        //
+        // However.
+        // It is not possible to explicitly catch all cases of LocalShard being dropped.
+        // For example, then the service is shutdown by interruption, we might not have a
+        // deep enough control over the shutdown sequence.
+        // So we have to assume, that it is fine to not await for explicit shutdown in some cases.
+        // But we still want to call explicit shutdown on all removes and internal operations.
         if !already_stopped {
             log::debug!("Dropping LocalShard while it is not already stopped");
         }
-
-        // #[cfg(any(debug_assertions, feature = "staging"))]
-        // if !already_stopped {
-        //     eprintln!(
-        //         "LocalShard::drop {:?}",
-        //         std::backtrace::Backtrace::capture()
-        //     );
-        //     panic!(
-        //         "Dropping LocalShard which was not already stopped. Waiting for graceful shutdown."
-        //     );
-        // }
-        //
-        // if !already_stopped {
-        //     log::error!("Dropping LocalShard which was not already stopped.");
-        // }
     }
 }
