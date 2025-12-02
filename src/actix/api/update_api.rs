32a9f06da31df285038145278e5c8eb496139b67
Daniel Boros <dancixx@gmail.com>
2025-12-02 17:55:07 +0100

    feat: add timeout to update params

--- src/actix/api/update_api.rs
+++ src/actix/api/update_api.rs
@@ -50,7 +50,7 @@ async fn upsert_points(
     let timing = Instant::now();
 
     // Update operation doesn't have timeout yet
-    let inference_params = InferenceParams::new(inference_token, None);
+    let inference_params = InferenceParams::new(inference_token, params.timeout);
 
     let result_with_usage = do_upsert_points(
         StrictModeCheckedTocProvider::new(&dispatcher),
@@ -130,7 +130,7 @@ async fn update_vectors(
     );
     let timing = Instant::now();
 
-    let inference_params = InferenceParams::new(inference_token, None);
+    let inference_params = InferenceParams::new(inference_token, params.timeout);
 
     let res = do_update_vectors(
         StrictModeCheckedTocProvider::new(&dispatcher),
@@ -342,7 +342,7 @@ async fn update_batch(
     );
 
     // Update operation doesn't have timeout yet
-    let inference_params = InferenceParams::new(inference_token.clone(), None);
+    let inference_params = InferenceParams::new(inference_token.clone(), params.timeout);
 
     let timing = Instant::now();
 
