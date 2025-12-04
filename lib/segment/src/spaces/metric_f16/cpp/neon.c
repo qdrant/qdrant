741b8614c14d11540c0cdae471f73c1d90d3c8de
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2025-12-04 18:22:29 +0100

    remove PC_VER check

--- lib/segment/src/spaces/metric_f16/cpp/neon.c
+++ lib/segment/src/spaces/metric_f16/cpp/neon.c
@@ -1,4 +1,3 @@
-#if !defined PC_VER
 #include <arm_neon.h>
 
 #if defined(_MSC_VER) || defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
@@ -133,5 +132,4 @@ float32_t manhattanDist_half_4x4(const float16_t* pSrcA, const float16_t* pSrcB,
 
     return manhattanDistance;
 }
-#endif
 #endif
