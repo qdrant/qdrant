5d62d783fe8cb0057ca4b70510e3356738ea752b
Roman Titov <ffuugoo@users.noreply.github.com>
2025-12-10 23:34:58 +0100

    Implement `__repr__` for `PyExpressionInterface`

--- lib/edge/python/src/types/formula/expression_interface.rs
+++ lib/edge/python/src/types/formula/expression_interface.rs
@@ -1,6 +1,9 @@
+use std::fmt;
+
 use bytemuck::TransparentWrapper;
 use pyo3::prelude::*;
 
+use crate::repr::*;
 use crate::*;
 
 #[pyclass(name = "Expression")]
@@ -83,6 +86,72 @@ pub enum PyExpressionInterface {
     },
 }
 
+impl Repr for PyExpressionInterface {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let (repr, fields): (_, &[(_, &dyn Repr)]) = match self {
+            PyExpressionInterface::Constant { val } => ("Constant", &[("val", val)]),
+            PyExpressionInterface::Variable { var } => ("Variable", &[("var", var)]),
+            PyExpressionInterface::Condition { cond } => ("Condition", &[("cond", cond)]),
+
+            PyExpressionInterface::GeoDistance { origin, to } => {
+                ("GeoDistance", &[("origin", origin), ("to", to)])
+            }
+
+            PyExpressionInterface::Datetime { date_time } => {
+                ("Datetime", &[("date_time", date_time)])
+            }
+
+            PyExpressionInterface::DatetimeKey { path } => ("DatetimeKey", &[("path", path)]),
+            PyExpressionInterface::Mult { exprs } => ("Mult", &[("exprs", exprs)]),
+            PyExpressionInterface::Sum { exprs } => ("Sum", &[("exprs", exprs)]),
+            PyExpressionInterface::Neg { expr } => ("Neg", &[("expr", expr)]),
+
+            PyExpressionInterface::Div {
+                left,
+                right,
+                by_zero_default,
+            } => (
+                "Div",
+                &[
+                    ("left", left),
+                    ("right", right),
+                    ("by_zero_default", by_zero_default),
+                ],
+            ),
+
+            PyExpressionInterface::Sqrt { expr } => ("Sqrt", &[("expr", expr)]),
+
+            PyExpressionInterface::Pow { base, exponent } => {
+                ("Pow", &[("base", base), ("exponent", exponent)])
+            }
+
+            PyExpressionInterface::Exp { expr } => ("Exp", &[("expr", expr)]),
+            PyExpressionInterface::Log10 { expr } => ("Log10", &[("expr", expr)]),
+            PyExpressionInterface::Ln { expr } => ("Ln", &[("expr", expr)]),
+            PyExpressionInterface::Abs { expr } => ("Abs", &[("expr", expr)]),
+
+            PyExpressionInterface::Decay {
+                kind,
+                x,
+                target,
+                midpoint,
+                scale,
+            } => (
+                "Decay",
+                &[
+                    ("kind", kind),
+                    ("x", x),
+                    ("target", target),
+                    ("midpoint", midpoint),
+                    ("scale", scale),
+                ],
+            ),
+        };
+
+        f.complex_enum::<Self>(repr, fields)
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Boxed<T>(Box<T>);
 
@@ -131,5 +200,11 @@ where
 
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
         self.into_inner().into_pyobject(py)
+    }
+}
+
+impl<T: Repr> Repr for Boxed<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
     }
 }
