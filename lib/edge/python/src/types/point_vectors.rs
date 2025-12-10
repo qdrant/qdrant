db46478db575e972cf9139842ac438218160c553
Roman Titov <ffuugoo@users.noreply.github.com>
2025-12-10 18:06:43 +0100

    Implement `__repr__` for `PyPointVectors`

--- lib/edge/python/src/types/point_vectors.rs
+++ lib/edge/python/src/types/point_vectors.rs
@@ -1,14 +1,18 @@
-use bytemuck::TransparentWrapper as _;
+use std::fmt;
+
+use bytemuck::TransparentWrapper;
 use derive_more::Into;
 use pyo3::{pyclass, pymethods};
 use segment::types::PointIdType;
 use shard::operations::point_ops::VectorStructPersisted;
 use shard::operations::vector_ops::PointVectorsPersisted;
 
+use crate::repr::*;
 use crate::types::{PyPointId, PyVector};
 
 #[pyclass(name = "PointVectors")]
-#[derive(Clone, Debug, Into)]
+#[derive(Clone, Debug, Into, TransparentWrapper)]
+#[repr(transparent)]
 pub struct PyPointVectors(pub PointVectorsPersisted);
 
 #[pymethods]
@@ -22,12 +26,22 @@ impl PyPointVectors {
     }
 
     #[getter]
-    fn id(&self) -> PyPointId {
+    pub fn id(&self) -> PyPointId {
         PyPointId(self.0.id)
     }
 
     #[getter]
-    fn vector(&self) -> &PyVector {
+    pub fn vector(&self) -> &PyVector {
         PyVector::wrap_ref(&self.0.vector)
+    }
+
+    pub fn __repr__(&self) -> String {
+        self.repr()
+    }
+}
+
+impl Repr for PyPointVectors {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.class::<Self>(&[("id", &self.id()), ("vector", &self.vector())])
     }
 }
