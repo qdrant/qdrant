d2c5d8afc007f4d1bc0e4c59fd2063cc9a2faec2
Luis Cossío <luis.cossio@outlook.com>
2025-12-12 16:56:20 -0300

    filter collections during telemetry collection

--- src/common/telemetry.rs
+++ src/common/telemetry.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use api::grpc;
+use ahash::HashSet;
 use collection::operations::verification::new_unchecked_verification_pass;
 use collection::telemetry::CollectionTelemetry;
 use common::types::{DetailsLevel, TelemetryDetail};
@@ -84,6 +85,7 @@ impl TelemetryCollector {
         &self,
         access: &Access,
         detail: TelemetryDetail,
+        only_collections: Option<HashSet<String>>,
         timeout: Option<Duration>,
     ) -> StorageResult<TelemetryData> {
         let timeout = timeout.unwrap_or(DEFAULT_TELEMETRY_TIMEOUT);
@@ -104,6 +106,7 @@ impl TelemetryCollector {
                     CollectionsTelemetry::collect(
                         detail,
                         &access_collection,
+                        only_collections,
                         &toc,
                         timeout,
                         &is_stopped,
