87034fc41c659e14c2a62d3f306382d2cf6dfa7e
Ivan Boldyrev <ivan.boldyrev@qdrant.com>
2025-12-28 16:16:53 +0100

    Qdrant content-aware pool, initial version

---
+++ lib/common/pool/src/lib.rs
@@ -0,0 +1,264 @@
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::hash::Hash;
+use std::panic::{UnwindSafe, catch_unwind};
+use std::sync::Arc;
+use std::thread::JoinHandle;
+
+use parking_lot::{Condvar, Mutex};
+
+// Defines ordering in which tasks are added.
+// Tasks which added first are executed first unless something blocks them.
+// Smaller task ID means higher priority.
+type TaskId = usize;
+
+// The UnwindSafe bound may be removed if we handle panics with recreating a new thread.
+pub type Task = Box<dyn FnOnce() + Send + UnwindSafe + 'static>;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum OperationMode {
+    Shared,
+    Exclusive,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum GroupMode {
+    Shared(usize), // number of scheduled or running shared tasks
+    Exclusive,
+}
+
+pub struct Pool<Id> {
+    _threads: Vec<JoinHandle<()>>,
+    tasks: Arc<Mutex<PoolTasks<Id>>>,
+    // TODO termination flag
+}
+
+struct PoolTasks<GroupId> {
+    next_available_priority: TaskId,
+    ready_to_run_tasks: BinaryHeap<RevQueuePair<TaskId, ReadyToRunTask<GroupId>>>,
+    waiting_tasks: HashMap<GroupId, TaskGroup>,
+}
+
+impl<GroupId> Default for PoolTasks<GroupId> {
+    fn default() -> Self {
+        Self {
+            next_available_priority: Default::default(),
+            ready_to_run_tasks: Default::default(),
+            waiting_tasks: Default::default(),
+        }
+    }
+}
+
+struct ReadyToRunTask<GroupId> {
+    group_id: GroupId,
+    task: Task,
+    // TODO needed only to check correctness.
+    mode: OperationMode,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct RevQueuePair<K, V>(Reverse<K>, V);
+
+impl<K, V> RevQueuePair<K, V> {
+    fn new(key: K, val: V) -> Self {
+        Self(Reverse(key), val)
+    }
+}
+
+impl<K: PartialEq, V> PartialEq for RevQueuePair<K, V> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl<K: Eq, V> Eq for RevQueuePair<K, V> {}
+
+impl<K: PartialOrd, V> PartialOrd for RevQueuePair<K, V> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl<K: Ord, V> Ord for RevQueuePair<K, V> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+#[derive(Default)]
+struct TaskGroup {
+    // ready_to_run: current_mode != Some(Exclusive)
+    current_mode: Option<GroupMode>,
+    // it is implicitely ordered by the priority
+    waiting_tasks: VecDeque<(OperationMode, TaskId, Task)>,
+}
+
+impl TaskGroup {
+    fn is_empty(&self) -> bool {
+        let has_running = match self.current_mode {
+            Some(GroupMode::Shared(count)) => count > 0, // TODO can it be 0 here?
+            Some(GroupMode::Exclusive) => true,
+            None => false,
+        };
+        self.waiting_tasks.is_empty() && !has_running
+    }
+
+    fn try_get_next_runnable_task(&mut self) -> Option<(OperationMode, TaskId, Task)> {
+        if let Some(&(mode, _task_id, ref _task)) = self.waiting_tasks.front() {
+            match (self.current_mode.as_ref(), mode) {
+                (None, _) => {
+                    // no running tasks, can run anything
+                    let (_, task_id, task) = self.waiting_tasks.pop_front().unwrap();
+                    self.current_mode = Some(match mode {
+                        OperationMode::Shared => GroupMode::Shared(1),
+                        OperationMode::Exclusive => GroupMode::Exclusive,
+                    });
+                    Some((mode, task_id, task))
+                }
+                (Some(GroupMode::Shared(count)), OperationMode::Shared) => {
+                    // can run another shared task
+                    let (_, task_id, task) = self.waiting_tasks.pop_front().unwrap();
+                    self.current_mode = Some(GroupMode::Shared(count + 1));
+                    Some((mode, task_id, task))
+                }
+                _ => {
+                    // cannot run the next task
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+
+    fn task_is_complete<GroupId: Clone>(
+        &mut self,
+        complete_task_operation_mode: OperationMode,
+        group_id: &GroupId,
+        ready_to_run_tasks: &mut BinaryHeap<RevQueuePair<TaskId, ReadyToRunTask<GroupId>>>,
+    ) {
+        match self.current_mode.as_mut() {
+            Some(GroupMode::Shared(count)) => {
+                assert_eq!(complete_task_operation_mode, OperationMode::Shared);
+                *count = count.checked_sub(1).expect("shared task count underflow");
+                if *count == 0 {
+                    self.current_mode = None;
+                    self.refill_ready_to_run_tasks(group_id, ready_to_run_tasks);
+                }
+            }
+            Some(GroupMode::Exclusive) => {
+                assert_eq!(complete_task_operation_mode, OperationMode::Exclusive);
+                self.current_mode = None;
+                self.refill_ready_to_run_tasks(group_id, ready_to_run_tasks);
+            }
+            None => {
+                panic!("task_is_complete called when no tasks are running");
+            }
+        }
+    }
+
+    fn refill_ready_to_run_tasks<GroupId: Clone>(
+        &mut self,
+        group_id: &GroupId,
+        ready_to_run_tasks: &mut BinaryHeap<RevQueuePair<TaskId, ReadyToRunTask<GroupId>>>,
+    ) {
+        // the state is checked and updated by the try_get_next_runnable_task call
+        while let Some((mode, task_id, task)) = self.try_get_next_runnable_task() {
+            let ready_task = ReadyToRunTask::<GroupId> {
+                group_id: group_id.clone(),
+                task,
+                mode,
+            };
+            ready_to_run_tasks.push(RevQueuePair::new(task_id, ready_task));
+        }
+    }
+}
+
+impl<Id: Clone + Hash + Eq + Send + 'static> Pool<Id> {
+    pub fn new(threads: usize) -> Self {
+        let wait_for_jobs = Arc::<Condvar>::default();
+        let tasks = Arc::<Mutex<PoolTasks<Id>>>::default();
+        let threads = (0..threads)
+            .map(|i| {
+                let wait_for_jobs = wait_for_jobs.clone();
+                let tasks = tasks.clone();
+                std::thread::Builder::new()
+                    .name(format!("qdant-pool-worker-{}", i + 1))
+                    .spawn(move || {
+                        thread_worker(wait_for_jobs, tasks);
+                    })
+                    .expect("failed to spawn thread")
+            })
+            .collect();
+        Self {
+            _threads: threads,
+            tasks,
+        }
+    }
+}
+
+impl<Id: Clone + Eq + Hash> Pool<Id> {
+    pub fn submit(&self, group_id: Id, mode: OperationMode, task: Task) {
+        let mut guard = self.tasks.lock();
+
+        let task_id = guard.next_available_priority;
+        guard.next_available_priority += 1;
+
+        let PoolTasks {
+            waiting_tasks,
+            ready_to_run_tasks,
+            ..
+        } = &mut *guard;
+
+        let task_group = waiting_tasks.entry(group_id.clone()).or_default();
+        task_group.waiting_tasks.push_back((mode, task_id, task));
+        task_group.refill_ready_to_run_tasks(&group_id, ready_to_run_tasks);
+    }
+}
+
+fn thread_worker<Id: Eq + Hash + Clone>(
+    wait_for_jobs: Arc<Condvar>,
+    tasks: Arc<Mutex<PoolTasks<Id>>>,
+) {
+    // TODO restoring a thread if old one panics, or handling the panic.
+    loop {
+        let ready_to_run_task = {
+            let mut guard = tasks.lock();
+            loop {
+                if let Some(pair) = guard.ready_to_run_tasks.pop() {
+                    let ready_to_run_task = pair.1;
+                    break ready_to_run_task;
+                } else {
+                    // The mutex is released while waiting for the condvar, and other threads can proceed.
+                    wait_for_jobs.wait(&mut guard);
+                }
+            }
+        };
+
+        let _result = catch_unwind(ready_to_run_task.task);
+        // TODO report the panic if any, or refactor everything, let it panic and create a new thread.
+
+        {
+            let mut guard = tasks.lock();
+            let PoolTasks {
+                waiting_tasks,
+                ready_to_run_tasks,
+                ..
+            } = &mut *guard;
+
+            let group = waiting_tasks
+                .get_mut(&ready_to_run_task.group_id)
+                .expect("missing task group");
+            group.task_is_complete(
+                ready_to_run_task.mode,
+                &ready_to_run_task.group_id,
+                // refill ready to run tasks if possible
+                ready_to_run_tasks,
+            );
+            if group.is_empty() {
+                // Includes both running, queued and waiting tasks.
+                waiting_tasks.remove(&ready_to_run_task.group_id);
+            }
+        }
+    }
+}
