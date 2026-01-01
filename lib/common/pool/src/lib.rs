dde4f5e3df1b105e79bed877ba435af423650350
Ivan Boldyrev <ivan.boldyrev@qdrant.com>
2026-01-01 11:10:12 +0100

    Refactor thread_worker to a module

--- lib/common/pool/src/lib.rs
+++ lib/common/pool/src/lib.rs
@@ -7,9 +7,11 @@ use std::sync::atomic::AtomicBool;
 use std::thread::JoinHandle;
 
 use parking_lot::{Condvar, Mutex};
+use thread::thread_worker;
 
 #[cfg(feature = "tokio")]
 mod async_pool;
+mod thread;
 
 // Defines ordering in which tasks are added.
 // Tasks which added first are executed first unless something blocks them.
@@ -279,50 +281,6 @@ impl KeyTaskGroup {
             };
             ready_to_run_tasks.push(RevQueuePair::new(task_id, (task_info, task)));
             condvar.notify_one();
-        }
-    }
-}
-fn thread_worker<GroupId: Eq + Hash + Clone>(
-    wait_for_jobs: Arc<Condvar>,
-    tasks: Arc<Mutex<PoolTasks<GroupId>>>,
-    terminate: Arc<AtomicBool>,
-) {
-    // TODO restoring a thread if old one panics, or handling the panic.
-
-    // Loop over tasks.
-    loop {
-        let (task_info, task) = {
-            let mut guard = tasks.lock();
-            // Loop over condvar wait attempts.
-            loop {
-                // Both Mutex::lock and Condvar::wait should impose a total ordering per se, so Relaxed ordering here is
-                // sufficient.
-                //
-                // TODO does it apply to `parking_lot` too? (At least it uses Aquire and Release internally.) If not,
-                // the thread may end up waiting for the condvar infinitely.
-                if terminate.load(std::sync::atomic::Ordering::Relaxed) {
-                    return;
-                }
-                if let Some(ready_to_run_task) = guard.get_next_task() {
-                    break ready_to_run_task;
-                } else {
-                    // The mutex is released while waiting for the condvar, and other threads can proceed.
-                    wait_for_jobs.wait(&mut guard);
-                }
-            }
-        };
-
-        let _result = catch_unwind(task);
-        // TODO report the panic if any, or refactor everything, let it panic and create a new thread.
-
-        if terminate.load(std::sync::atomic::Ordering::Relaxed) {
-            return;
-        }
-
-        {
-            let mut guard = tasks.lock();
-            guard.complete_task(&task_info, wait_for_jobs.as_ref());
-            // TODO here lock is released just to be reacquired in the next iteration.
         }
     }
 }
