8889aeef5a6e71ec6043865cbe59923a0172602f
generall <andrey@vasnetsov.com>
2026-01-03 21:07:27 +0100

    method to request snapshot manifest

--- lib/edge/src/snapshots.rs
+++ lib/edge/src/snapshots.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use segment::common::operation_error::OperationResult;
+use segment::data_types::manifest::SnapshotManifest;
 use shard::segment_holder::SegmentHolder;
 
 use crate::Shard;
@@ -8,5 +9,9 @@ use crate::Shard;
 impl Shard {
     pub fn unpack_snapshot(snapshot_path: &Path, target_path: &Path) -> OperationResult<()> {
         SegmentHolder::unpack_snapshot(snapshot_path, target_path)
+    }
+
+    pub fn manifest(&self) -> OperationResult<SnapshotManifest> {
+        self.segments.read().snapshot_manifest()
     }
 }
