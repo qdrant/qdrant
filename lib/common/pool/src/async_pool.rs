ff20414256031aaf078e52df0963868fd69ae233
Ivan Boldyrev <ivan.boldyrev@qdrant.com>
2026-01-06 18:22:08 +0100

    Do not handle panics at all

--- lib/common/pool/src/async_pool.rs
+++ lib/common/pool/src/async_pool.rs
@@ -1,6 +1,4 @@
-use std::any::Any;
 use std::hash::Hash;
-use std::panic::{UnwindSafe, catch_unwind};
 
 use tokio::sync::oneshot;
 
@@ -30,40 +28,35 @@ where
         &self,
         group_id: GroupId,
         mode: OperationMode,
-        task: impl FnOnce() -> R + Send + UnwindSafe + 'static,
+        task: impl FnOnce() -> R + Send + 'static,
     ) -> Result<R, AsyncTaskError> {
         let (oneshot_receiver, oneshot_task) = async_wrap(task);
         self.inner.submit(group_id, mode, oneshot_task);
         match oneshot_receiver.await {
-            Ok(Ok(value)) => Ok(value),
-            Ok(Err(panic)) => Err(AsyncTaskError::Panicked(panic)),
+            Ok(value) => Ok(value),
             Err(_) => Err(AsyncTaskError::Canceled),
         }
     }
 
     pub async fn submit_uncontended<R: Send + 'static>(
         &self,
-        task: impl FnOnce() -> R + Send + UnwindSafe + 'static,
+        task: impl FnOnce() -> R + Send + 'static,
     ) -> Result<R, AsyncTaskError> {
         let (oneshot_receiver, oneshot_task) = async_wrap(task);
         self.inner.submit_uncontended(oneshot_task);
         match oneshot_receiver.await {
-            Ok(Ok(value)) => Ok(value),
-            Ok(Err(panic)) => Err(AsyncTaskError::Panicked(panic)),
+            Ok(value) => Ok(value),
             Err(_) => Err(AsyncTaskError::Canceled),
         }
     }
 }
 
 fn async_wrap<R: Send + 'static>(
-    task: impl FnOnce() -> R + Send + UnwindSafe + 'static,
-) -> (
-    oneshot::Receiver<Result<R, Box<dyn Any + Send + 'static>>>,
-    Task,
-) {
+    task: impl FnOnce() -> R + Send + 'static,
+) -> (oneshot::Receiver<R>, Task) {
     let (oneshot_sender, oneshot_receiver) = oneshot::channel();
     let oneshot_task = move || {
-        let result = catch_unwind(task);
+        let result = task();
         let _ = oneshot_sender.send(result);
     };
     (oneshot_receiver, Box::new(oneshot_task))
