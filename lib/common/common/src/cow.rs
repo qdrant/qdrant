b464c6e112b9dcf4805627bd26fb4053e749cb82
xzfc <xzfcpw@gmail.com>
2026-01-28 13:10:26 +0000

    Generalize BorrowCow

--- lib/common/common/src/cow.rs
+++ lib/common/common/src/cow.rs
@@ -1,53 +1,32 @@
-//! [`std::borrow::Cow`]-like enums.
-//!
-//! # Comparison table
-//!
-//! | Type                 | Borrowed | Owned                   |
-//! | -------------------- | -------- | ----------------------- |
-//! | [`Cow<'a, T>`]       | `&'a T`  | `<T as ToOwned>::Owned` |
-//! | [`BoxCow<'a, T>`]    | `&'a T`  | `Box<T>`                |
-//! | [`SimpleCow<'a, T>`] | `&'a T`  | `T`                     |
-//!
-//! [`Cow<'a, T>`]: std::borrow::Cow
+use std::borrow::Borrow;
 use std::ops::Deref;
+use std::sync::Arc;
 
-pub enum BoxCow<'a, T: ?Sized> {
-    Borrowed(&'a T),
-    Owned(Box<T>),
+pub type ArcCow<'a, T> = BorrowCow<'a, T, Arc<T>>;
+pub type BoxCow<'a, T> = BorrowCow<'a, T, Box<T>>;
+pub type SimpleCow<'a, T> = BorrowCow<'a, T, T>;
+
+/// [`std::borrow::Cow`]-like enum, but based on [`Borrow`] instead of [`ToOwned`].
+pub enum BorrowCow<'a, Borrowed: ?Sized, Owned: Borrow<Borrowed>> {
+    Borrowed(&'a Borrowed),
+    Owned(Owned),
 }
 
-impl<'a, T: ?Sized> BoxCow<'a, T> {
+impl<'a, B: ?Sized, O: Borrow<B>> BorrowCow<'a, B, O> {
+    #[inline(always)]
     pub fn as_borrowed(&'a self) -> Self {
-        match self {
-            BoxCow::Borrowed(v) => BoxCow::Borrowed(v),
-            BoxCow::Owned(v) => BoxCow::Borrowed(v.as_ref()),
-        }
+        Self::Borrowed(self.deref())
     }
 }
 
-impl<T: ?Sized> Deref for BoxCow<'_, T> {
-    type Target = T;
+impl<B: ?Sized, O: Borrow<B>> Deref for BorrowCow<'_, B, O> {
+    type Target = B;
 
+    #[inline(always)]
     fn deref(&self) -> &Self::Target {
         match self {
-            BoxCow::Borrowed(t) => t,
-            BoxCow::Owned(t) => t,
-        }
-    }
-}
-
-pub enum SimpleCow<'a, T> {
-    Borrowed(&'a T),
-    Owned(T),
-}
-
-impl<T> Deref for SimpleCow<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            SimpleCow::Borrowed(v) => v,
-            SimpleCow::Owned(v) => v,
+            BorrowCow::Borrowed(t) => t,
+            BorrowCow::Owned(t) => t.borrow(),
         }
     }
 }
