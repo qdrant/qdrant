040151e26dfcb65ca7756dd0c140dbe3cecdd080
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2026-01-29 15:05:20 +0100

    basic test

---
+++ tests/openapi/test_update_queue.py
@@ -0,0 +1,83 @@
+import pytest
+
+from .helpers.helpers import request_with_validation
+from .helpers.collection_setup import basic_collection_setup, drop_collection
+
+  
+@pytest.fixture(autouse=True)
+def setup(on_disk_vectors, collection_name):
+    basic_collection_setup(collection_name=collection_name, on_disk_vectors=on_disk_vectors)
+    yield
+    drop_collection(collection_name=collection_name)
+
+
+def get_queue_info(collection_name):
+    response = request_with_validation(
+        api='/collections/{collection_name}',
+        method="GET",
+        path_params={'collection_name': collection_name},
+    )
+    assert response.ok
+        
+    return response.json()["result"]["update_queue"]
+    
+
+def test_queue_last_applied_seq(collection_name):
+    response = request_with_validation(
+        api='/collections/{collection_name}',
+        method="GET",
+        path_params={'collection_name': collection_name},
+    )
+    assert response.ok
+        
+    queue_info = get_queue_info(collection_name)
+    
+    assert queue_info["length"] == 0
+    assert queue_info["last_applied_seq"] == 0
+    
+    # first update
+    response = request_with_validation(
+        api='/collections/{collection_name}/points',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        query_params={'wait': 'true'},
+        body={
+            "points": [
+                {
+                    "id": 1,
+                    "vector": [0.05, 0.61, 0.76, 0.74],
+                    "payload": {}
+                }
+            ]}        
+    )
+    assert response.ok
+    
+    queue_info = get_queue_info(collection_name)
+    
+    # wait=true so ack. after application
+    assert queue_info["length"] == 0
+    
+    assert queue_info["last_applied_seq"] == 1
+    
+    # second update
+    response = request_with_validation(
+        api='/collections/{collection_name}/points',
+        method="PUT",
+        path_params={'collection_name': collection_name},
+        query_params={'wait': 'true'},
+        body={
+            "points": [
+                {
+                    "id": 1,
+                    "vector": [0.05, 0.61, 0.76, 0.74],
+                    "payload": {}
+                }
+            ]}        
+    )
+    assert response.ok
+    
+    queue_info = get_queue_info(collection_name)
+    # wait=true so ack. after application
+    assert queue_info["length"] == 0
+    
+    assert queue_info["last_applied_seq"] == 2
