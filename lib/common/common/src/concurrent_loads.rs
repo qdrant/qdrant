96c89b993d4fd1b0fc028ccf2d28f0654192a018
timvisee <tim@visee.me>
2026-02-04 14:37:15 +0100

    Disallow zero values

--- lib/common/common/src/concurrent_loads.rs
+++ lib/common/common/src/concurrent_loads.rs
@@ -1,5 +1,7 @@
 //! Configuration for concurrent load limits at collection, shard, and segment levels.
 
+use std::num::NonZeroUsize;
+
 use serde::{Deserialize, Serialize};
 
 const DEFAULT_MAX_CONCURRENT_COLLECTION_LOADS: usize = 1;
@@ -11,28 +13,31 @@ const DEFAULT_MAX_CONCURRENT_SEGMENT_LOADS: usize = 8;
 pub struct ConcurrentLoadConfig {
     /// Maximum number of collections to load concurrently.
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub max_concurrent_collection_loads: Option<usize>,
+    pub max_concurrent_collection_loads: Option<NonZeroUsize>,
     /// Maximum number of shards to load concurrently when loading a collection.
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub max_concurrent_shard_loads: Option<usize>,
+    pub max_concurrent_shard_loads: Option<NonZeroUsize>,
     /// Maximum number of segments to load concurrently when loading a shard.
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub max_concurrent_segment_loads: Option<usize>,
+    pub max_concurrent_segment_loads: Option<NonZeroUsize>,
 }
 
 impl ConcurrentLoadConfig {
     pub fn get_concurrent_collections(&self) -> usize {
         self.max_concurrent_collection_loads
+            .map(NonZeroUsize::get)
             .unwrap_or(DEFAULT_MAX_CONCURRENT_COLLECTION_LOADS)
     }
 
     pub fn get_concurrent_shards(&self) -> usize {
         self.max_concurrent_shard_loads
+            .map(NonZeroUsize::get)
             .unwrap_or(DEFAULT_MAX_CONCURRENT_SHARD_LOADS)
     }
 
     pub fn get_concurrent_segments(&self) -> usize {
         self.max_concurrent_segment_loads
+            .map(NonZeroUsize::get)
             .unwrap_or(DEFAULT_MAX_CONCURRENT_SEGMENT_LOADS)
     }
 }
