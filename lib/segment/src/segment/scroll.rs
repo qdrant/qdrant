069c06a0f8a11dec8bd63622cf8ed9d465f5cede
Ivan Boldyrev <ivan.boldyrev@qdrant.com>
2026-02-06 18:17:30 +0100

    Copy some fields to `PayloadIndexInfo`
    Make some fields accessible under `RwLock`, allowing (potentially) do
    payload index operations with interior mutability.
    
    Tests are broken and do not compile.

--- lib/segment/src/segment/scroll.rs
+++ lib/segment/src/segment/scroll.rs
@@ -23,7 +23,8 @@ impl Segment {
         hw_counter: &HardwareCounterCell,
     ) -> bool {
         let query_cardinality = {
-            let payload_index = self.payload_index.borrow();
+            let payload_index_info = self.payload_index_info.read();
+            let payload_index = payload_index_info.payload_index.borrow();
             payload_index.estimate_cardinality(filter, hw_counter)
         };
 
@@ -67,7 +68,9 @@ impl Segment {
         is_stopped: &AtomicBool,
         hw_counter: &HardwareCounterCell,
     ) -> Vec<PointIdType> {
-        let payload_index = self.payload_index.borrow();
+        let payload_index_info = self.payload_index_info.read();
+        let payload_index = payload_index_info.payload_index.borrow();
+
         let filter_context = payload_index.filter_context(condition, hw_counter);
         self.id_tracker
             .borrow()
@@ -100,7 +103,9 @@ impl Segment {
         is_stopped: &AtomicBool,
         hw_counter: &HardwareCounterCell,
     ) -> Vec<PointIdType> {
-        let payload_index = self.payload_index.borrow();
+        let payload_index_info = self.payload_index_info.read();
+        let payload_index = payload_index_info.payload_index.borrow();
+
         let id_tracker = self.id_tracker.borrow();
         let cardinality_estimation = payload_index.estimate_cardinality(condition, hw_counter);
 
