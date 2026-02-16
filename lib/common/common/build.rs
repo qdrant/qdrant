4e188a453361506c56abdfb391af5ff320f69bb6
xzfc <xzfcpw@gmail.com>
2026-02-16 21:48:31 +0000

    Move lib/common/{io,memory}/* -> lib/common/common/*
    - Mmap-related items are grouped into `common::mmap` sub-module:
      - `memory/src/chunked_utils.rs`      -> `common/src/mmap/chunked.rs`
      - `memory/src/madvise.rs`            -> `common/src/mmap/advice.rs`
      - `memory/src/mmap_ops.rs`           -> `common/src/mmap/ops.rs`
      - `memory/src/mmap_type_readonly.rs` -> `common/src/mmap/mmap_readonly.rs`
      - `memory/src/mmap_type.rs`          -> `common/src/mmap/mmap_rw.rs`
    - Filesystem-related items are grouped into `common::fs` sub-module:
      - `common/src/fs.rs`          -> `common/src/fs/sync.rs`
      - `io/src/file_operations.rs` -> `common/src/fs/ops.rs`
      - `io/src/move_files.rs`      -> `common/src/fs/move.rs`
      - `io/src/safe_delete.rs`     -> `common/src/fs/safe_delete.rs`
      - `memory/src/checkfs.rs`     -> `common/src/fs/check.rs`
      - `memory/src/fadvise.rs`     -> `common/src/fs/fadvise.rs`
    - Rest is moved straight into `common`:
      - `io/src/storage_version.rs` -> `common/src/storage_version.rs`
    
    The old `io` and `memory` are now hollow crates that re-export items
    from `common`. These hollow crates will be removed in next commits.

--- lib/common/common/build.rs
+++ lib/common/common/build.rs
@@ -1,6 +1,38 @@
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
     // Required for tango benchmarks, see:
     // https://github.com/bazhenov/tango/blob/v0.6.0/README.md#getting-started
     println!("cargo:rustc-link-arg-benches=-rdynamic");
-    println!("cargo:rerun-if-changed=build.rs");
+
+    // Matches all platforms that have `nix::fcntl::posix_fadvise` function.
+    // https://github.com/nix-rust/nix/blob/v0.29.0/src/fcntl.rs#L35-L42
+    println!("cargo:rustc-check-cfg=cfg(posix_fadvise_supported)");
+    if matches!(
+        std::env::var("CARGO_CFG_TARGET_OS").unwrap().as_str(),
+        "linux" | "freebsd" | "android" | "fuchsia" | "emscripten" | "wasi"
+    ) || matches!(
+        std::env::var("CARGO_CFG_TARGET_ENV").unwrap().as_str(),
+        "uclibc"
+    ) {
+        println!("cargo:rustc-cfg=posix_fadvise_supported")
+    }
+
+    // Matches all platforms, that have `nix::sys::statfs::statfs` function.
+    // https://github.com/nix-rust/nix/blob/v0.29.0/src/sys/mod.rs#L131
+    println!("cargo:rustc-check-cfg=cfg(fs_type_check_supported)");
+    if matches!(
+        std::env::var("CARGO_CFG_TARGET_OS").unwrap().as_str(),
+        "linux"
+            | "freebsd"
+            | "android"
+            | "openbsd"
+            | "ios"
+            | "macos"
+            | "watchos"
+            | "tvos"
+            | "visionos"
+    ) {
+        println!("cargo:rustc-cfg=fs_type_check_supported")
+    }
 }
--- lib/common/memory/src/lib.rs
+++ lib/common/memory/src/lib.rs
@@ -1,7 +1,36 @@
-pub mod checkfs;
-pub mod chunked_utils;
-pub mod fadvise;
-pub mod madvise;
-pub mod mmap_ops;
-pub mod mmap_type;
-pub mod mmap_type_readonly;
+pub mod checkfs {
+    pub use common::fs::{FsCheckResult, check_fs_info, check_mmap_functionality};
+}
+
+pub mod chunked_utils {
+    pub use common::mmap::UniversalMmapChunk;
+    pub use common::mmap::chunked::{chunk_name, create_chunk, read_mmaps};
+}
+
+pub mod fadvise {
+    pub use common::fs::{OneshotFile, clear_disk_cache};
+}
+
+pub mod madvise {
+    pub use common::mmap::advice::{get_global, madvise, set_global, will_need_multiple_pages};
+    pub use common::mmap::{Advice, AdviceSetting, Madviseable};
+}
+
+pub mod mmap_ops {
+    pub use common::mmap::{
+        MULTI_MMAP_IS_SUPPORTED, MULTI_MMAP_SUPPORT_CHECK_RESULT, TEMP_FILE_EXTENSION,
+        create_and_ensure_length, open_read_mmap, open_write_mmap,
+    };
+    #[expect(deprecated)]
+    pub use common::mmap::{
+        transmute_from_u8, transmute_from_u8_to_slice, transmute_to_u8, transmute_to_u8_slice,
+    };
+}
+
+pub mod mmap_type {
+    pub use common::mmap::{Error, MmapBitSlice, MmapFlusher, MmapSlice, MmapType};
+}
+
+pub mod mmap_type_readonly {
+    pub use common::mmap::{MmapSliceReadOnly, MmapTypeReadOnly};
+}
