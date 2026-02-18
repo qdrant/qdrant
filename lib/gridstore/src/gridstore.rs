e16030825578eb3ba75bd63cbf696710edf30b64
Tim Visée <tim+github@visee.me>
2026-02-18 16:29:04 +0100

    Don't lock Gridstore bitmask for full duration of flush (#8169)

--- lib/gridstore/src/gridstore.rs
+++ lib/gridstore/src/gridstore.rs
@@ -617,8 +617,7 @@ impl<V> Gridstore<V> {
                 return Err(GridstoreError::FlushCancelled);
             };
 
-            let mut bitmask_guard = bitmask.upgradable_read();
-            bitmask_guard.flush()?;
+            bitmask.read().flush()?;
             for page in pages.read().iter() {
                 page.flush()?;
             }
@@ -629,21 +628,25 @@ impl<V> Gridstore<V> {
                 // Nothing to do flush here
                 return Ok(());
             }
+
             // Update all free blocks in the bitmask
-            bitmask_guard.with_upgraded(|guard| {
-                for (page_id, pointer_group) in
-                    &old_pointers.into_iter().chunk_by(|pointer| pointer.page_id)
-                {
-                    let local_ranges = pointer_group.map(|pointer| {
-                        let start = pointer.block_offset;
-                        let end = pointer.block_offset
-                            + Self::blocks_for_value(pointer.length as usize, block_size_bytes);
-                        start as usize..end as usize
-                    });
-                    guard.mark_blocks_batch(page_id, local_ranges, false);
-                }
-            });
-            bitmask_guard.flush()?;
+            {
+                let mut bitmask_guard = bitmask.upgradable_read();
+                bitmask_guard.with_upgraded(|guard| {
+                    for (page_id, pointer_group) in
+                        &old_pointers.into_iter().chunk_by(|pointer| pointer.page_id)
+                    {
+                        let local_ranges = pointer_group.map(|pointer| {
+                            let start = pointer.block_offset;
+                            let end = pointer.block_offset
+                                + Self::blocks_for_value(pointer.length as usize, block_size_bytes);
+                            start as usize..end as usize
+                        });
+                        guard.mark_blocks_batch(page_id, local_ranges, false);
+                    }
+                });
+                bitmask_guard.flush()?;
+            }
 
             // Keep the guard till the end of the flush to prevent concurrent drop/flushes
             drop(is_alive_flush_guard);
