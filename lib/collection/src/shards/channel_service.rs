8268dc38092cfcd240ff71c7edaa2c6ee9ffba9e
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2026-02-18 13:10:24 +0100

    log peers without metadata

--- lib/collection/src/shards/channel_service.rs
+++ lib/collection/src/shards/channel_service.rs
@@ -181,6 +181,13 @@ impl ChannelService {
 
         // Ensure there aren't more peer addresses than metadata
         if id_to_address.len() > id_to_metadata.len() {
+            let peers_without_metadata: HashMap<_, _> = id_to_address
+                .iter()
+                .filter(|(id, _m)| !id_to_metadata.contains_key(id))
+                .collect();
+            log::info!(
+                "Not all peers at version:{version} because there are peers without metadata {peers_without_metadata:?}"
+            );
             return false;
         }
 
