8fb7adea98b86bbcc8421866ac5c9cee88b0b9a5
xzfc <5121426+xzfc@users.noreply.github.com>
2026-02-20 18:02:20 +0000

    Move `github.com/qdrant/wal` into `lib/wal` (#8185)
    * Copy github.com/qdrant/wal into lib/wal
    
    Commit: c07fb56ebc8120ebe4e3c602d31ce98f356f4676 2026-02-18
    
    * Clean cruft
    
    * Integrate lib/wal into workspace
    
    * Cargo fmt
    
    * Fix clippy warnings
    
    * Adhere to our conventions
    
    * Fix codespell warnings

---
+++ lib/wal/benches/benchmark.rs
@@ -0,0 +1,54 @@
+use std::hint::black_box;
+
+use crc::{CRC_32_ISCSI, Crc};
+use criterion::{Criterion, criterion_group, criterion_main};
+use rand::RngCore;
+
+pub const CASTAGNOLI: Crc<u32> = Crc::<u32>::new(&CRC_32_ISCSI);
+
+pub fn criterion_benchmark_4k(c: &mut Criterion) {
+    let mut buffer = [0u8; 8192];
+    let mut rng = rand::rng();
+    rng.fill_bytes(&mut buffer);
+
+    let mut group = c.benchmark_group("8k");
+    group.throughput(criterion::Throughput::Bytes(8192));
+    group.bench_function("crc", |b| {
+        b.iter(|| {
+            let mut digest = CASTAGNOLI.digest();
+            digest.update(&buffer);
+            black_box(digest.finalize());
+        })
+    });
+
+    group.bench_function("crc32c", |b| {
+        b.iter(|| {
+            black_box(crc32c::crc32c(&buffer));
+        })
+    });
+}
+
+pub fn criterion_benchmark_1024k(c: &mut Criterion) {
+    let mut buffer = [0u8; 1048576];
+    let mut rng = rand::rng();
+    rng.fill_bytes(&mut buffer);
+
+    let mut group = c.benchmark_group("1M");
+    group.throughput(criterion::Throughput::Bytes(1048576));
+    group.bench_function("crc", |b| {
+        b.iter(|| {
+            let mut digest = CASTAGNOLI.digest();
+            digest.update(&buffer);
+            black_box(digest.finalize());
+        })
+    });
+
+    group.bench_function("crc32c", |b| {
+        b.iter(|| {
+            black_box(crc32c::crc32c(&buffer));
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark_4k, criterion_benchmark_1024k);
+criterion_main!(benches);
---
+++ lib/wal/tests/process_crash.rs
@@ -0,0 +1,69 @@
+//! Simulates a process crash while writing to a write ahead log segment.
+//!
+//! Steps:
+//!
+//! 1. The test process creates a seed and a temporary directory
+//! 2. The test process creates a subprocess with the seed and directory as
+//!    arguments.
+//! 3. The subprocess creates a segment in the directory, and appends entries to
+//!    the segment based on the seed.
+//! 4. The subprocess aborts without closing resources or cleaning up state.
+//! 5. The test process reads the entries from the segment, ensuring that no
+//!    corruption or data loss has occurred.
+
+use std::collections::HashMap;
+use std::{env, process};
+
+use rand::RngCore;
+use tempfile::Builder;
+use wal::test_utils::EntryGenerator;
+
+const SEGMENT_CAPACITY: usize = 32 * 1024 * 1024;
+const EXIT_CODE: i32 = 42;
+
+#[test]
+fn process_crash() {
+    let vars: HashMap<String, String> = env::vars().collect();
+    if vars.contains_key("SEED") && vars.contains_key("SEGMENT_PATH") {
+        let seed: usize = vars["SEED"].parse().unwrap();
+        let path = vars["SEGMENT_PATH"].clone();
+        subprocess(path, seed);
+    } else {
+        test()
+    }
+}
+
+fn test() {
+    let tempdir = Builder::new().prefix("segment").tempdir().unwrap();
+    let seed: usize = rand::rng().next_u32() as usize;
+    let path = tempdir.path().join("segment");
+
+    println!("Spawning subprocess; path: {path:?}; seed: {seed}");
+
+    let exit_code = process::Command::new(env::current_exe().unwrap())
+        .env("SEED", format!("{seed}"))
+        .env("SEGMENT_PATH", format!("{}", path.display()))
+        .status()
+        .unwrap()
+        .code()
+        .unwrap();
+
+    assert_eq!(EXIT_CODE, exit_code);
+
+    let segment = wal::Segment::open(path).unwrap();
+    let entries = EntryGenerator::with_seed_and_segment_capacity(seed, SEGMENT_CAPACITY);
+
+    for (i, entry) in entries.into_iter().enumerate() {
+        assert_eq!(&entry[..], &*segment.entry(i).unwrap());
+    }
+}
+
+fn subprocess(path: String, seed: usize) {
+    let mut segment = wal::Segment::create(path, SEGMENT_CAPACITY).unwrap();
+
+    for entry in EntryGenerator::with_seed_and_segment_capacity(seed, SEGMENT_CAPACITY) {
+        segment.append(&entry).expect("segment capacity exhausted");
+    }
+
+    process::exit(EXIT_CODE);
+}
