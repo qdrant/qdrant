648bca2367de0ee539ca77f28bc6d5e096588b2a
krapcys1-maker <krapcys1@gmail.com>
2026-02-23 13:05:56 +0200

    gpu: use c_char pointers for Vulkan names (#8177)

--- lib/gpu/src/instance.rs
+++ lib/gpu/src/instance.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::ffi::CString;
+use std::ffi::{CString, c_char};
 use std::ops::Deref;
 use std::sync::Arc;
 
@@ -141,7 +141,7 @@ impl Instance {
             .iter()
             .filter_map(|s| CString::new(s.clone().into_bytes()).ok())
             .collect();
-        let extension_names_raw: Vec<*const i8> = extensions_cstr
+        let extension_names_raw: Vec<*const c_char> = extensions_cstr
             .iter()
             .map(|raw_name| raw_name.as_ptr())
             .collect();
@@ -154,7 +154,7 @@ impl Instance {
             .iter()
             .filter_map(|s| CString::new(s.clone().into_bytes()).ok())
             .collect();
-        let layers_raw: Vec<*const i8> = layers_cstr
+        let layers_raw: Vec<*const c_char> = layers_cstr
             .iter()
             .map(|raw_name| raw_name.as_ptr())
             .collect();
