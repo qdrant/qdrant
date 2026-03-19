590ebd700045898240140516071eae9d09e62c8f
Daniel Boros <dancixx@gmail.com>
2026-03-19 11:34:47 +0100

    fix: linter

--- lib/uio-client/build.rs
+++ lib/uio-client/build.rs
@@ -1,9 +1,7 @@
 fn main() -> std::io::Result<()> {
-    tonic_build::configure()
-        .out_dir("src/generated/")
-        .compile(
-            &["../api/src/grpc/proto/storage_read_service.proto"],
-            &["../api/src/grpc/proto"],
-        )?;
+    tonic_build::configure().out_dir("src/generated/").compile(
+        &["../api/src/grpc/proto/storage_read_service.proto"],
+        &["../api/src/grpc/proto"],
+    )?;
     Ok(())
 }
