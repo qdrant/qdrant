07d5da054f395e43402239a3ced29a003cfb4070
xzfc <xzfcpw@gmail.com>
2026-03-19 16:40:20 +0000

    generic_consts: doc comments

--- lib/common/common/src/generic_consts.rs
+++ lib/common/common/src/generic_consts.rs
@@ -1,3 +1,30 @@
+//! Named consts to use as generic parameters.
+//!
+//! ```ignore
+//! // Not obvious:
+//! fn read<const IS_SEQUENTIAL: bool>(&self);
+//! foo.read::<true>();
+//!
+//! // Better:
+//! fn read<P: AccessPattern>(&self);
+//! foo.read::<Sequential>();
+//! ```
+//!
+//!
+//! Ideally, we should use regular enums instead of traits and unit structs from
+//! this module:
+//! ```ignore
+//! enum AccessPattern { Random, Sequential }
+//! fn read<const P: AccessPattern>(&self);
+//! foo.read::<AccessPattern::Sequential>();
+//! ```
+//! But until <https://github.com/rust-lang/rust/issues/95174> is resolved, we
+//! have to use this workaround.
+
+/// A hint describing the pattern in which the caller retrieves items by IDs.
+/// Affects the performance. The implementation might enable read-ahead aka
+/// pre-fetch for sequential access.
+/// [`Random`] is like `MADV_RANDOM`. [`Sequential`] is like `MADV_SEQUENTIAL`.
 pub trait AccessPattern: Copy {
     const IS_SEQUENTIAL: bool;
 }
