fc778993e9ac7b3bcb0d4c9df4f15c4c54b74bab
Roman Titov <ffuugoo@users.noreply.github.com>
2026-03-24 19:00:25 +0100

    Rename `mmap_file` to `mmap`

--- lib/common/common/src/universal_io/mod.rs
+++ lib/common/common/src/universal_io/mod.rs
@@ -5,7 +5,7 @@ pub mod file_ops;
 #[cfg(target_os = "linux")]
 pub mod io_uring;
 pub mod local_file_ops;
-pub mod mmap_file;
+pub mod mmap;
 pub mod mmap_universal;
 pub mod read;
 pub mod read_only;
@@ -19,7 +19,7 @@ pub use self::error::UniversalIoError;
 pub use self::file_ops::UniversalReadFileOps;
 #[cfg(target_os = "linux")]
 pub use self::io_uring::*;
-pub use self::mmap_file::*;
+pub use self::mmap::*;
 pub use self::mmap_universal::*;
 pub use self::read::UniversalRead;
 pub use self::read_only::*;
