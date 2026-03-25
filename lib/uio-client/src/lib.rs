76cab3ad8794a229e419a584afd3622fec198844
Daniel Boros <dancixx@gmail.com>
2026-03-26 00:10:06 +0100

    feat: add tests

--- lib/uio-client/src/lib.rs
+++ lib/uio-client/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod generated;
 pub mod remote_read;
 
+#[cfg(test)]
+mod tests;
+
 pub use generated::qdrant::storage_read_client::StorageReadClient;
 pub use remote_read::{RemoteClient, RemoteUniversalRead};
