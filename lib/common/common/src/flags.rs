1adc4f4207320127e6ba930cc2420dfc5dfd6cb2
xzfc <xzfcpw@gmail.com>
2026-03-27 20:55:07 +0000

    Remove leftover rocksdb-related code
    Removed:
    - Cargo.toml: rocksdb cargo feature flag and dependencies.
    - code: rocksdb-related items that was not under feature flag.
    - flags.rs: rocksdb-related qdrant feature flags.
    
    Disabled:
    - Some benchmarks because these do not compile now.

--- lib/common/common/src/flags.rs
+++ lib/common/common/src/flags.rs
@@ -14,52 +14,10 @@ pub struct FeatureFlags {
     /// Note that this will only be applied to all flags when passed into [`init_feature_flags`].
     all: bool,
 
-    /// Skip usage of RocksDB in new immutable payload indices.
-    ///
-    /// First implemented in Qdrant 1.13.5.
-    /// Enabled by default in Qdrant 1.14.1.
-    pub payload_index_skip_rocksdb: bool,
-
-    /// Skip usage of RocksDB in new mutable payload indices.
-    ///
-    /// First implemented in Qdrant 1.15.0.
-    /// Enabled by default in Qdrant 1.16.0.
-    pub payload_index_skip_mutable_rocksdb: bool,
-
-    /// Skip usage of RocksDB in new payload storages.
-    ///
-    /// On-disk payload storages never use Gridstore.
-    ///
-    /// First implemented in Qdrant 1.15.0.
-    /// Enabled by default in Qdrant 1.16.0.
-    pub payload_storage_skip_rocksdb: bool,
-
     /// Use incremental HNSW building.
     ///
     /// Enabled by default in Qdrant 1.14.1.
     pub incremental_hnsw_building: bool,
-
-    /// Migrate RocksDB based ID trackers into file based ID tracker on start.
-    ///
-    /// Enabled by default in Qdrant 1.15.0.
-    pub migrate_rocksdb_id_tracker: bool,
-
-    /// Migrate RocksDB based vector storages into new format on start.
-    ///
-    /// Enabled by default in Qdrant 1.16.1.
-    pub migrate_rocksdb_vector_storage: bool,
-
-    /// Migrate RocksDB based payload storages into new format on start.
-    ///
-    /// Enabled by default in Qdrant 1.16.1.
-    pub migrate_rocksdb_payload_storage: bool,
-
-    /// Migrate RocksDB based payload indices into new format on start.
-    ///
-    /// Rebuilds a new payload index from scratch.
-    ///
-    /// Enabled by default in Qdrant 1.16.1.
-    pub migrate_rocksdb_payload_indices: bool,
 
     /// Use appendable quantization in appendable plain segments.
     ///
@@ -76,14 +34,7 @@ impl Default for FeatureFlags {
     fn default() -> FeatureFlags {
         FeatureFlags {
             all: false,
-            payload_index_skip_rocksdb: true,
-            payload_index_skip_mutable_rocksdb: true,
-            payload_storage_skip_rocksdb: true,
             incremental_hnsw_building: true,
-            migrate_rocksdb_id_tracker: true,
-            migrate_rocksdb_vector_storage: true,
-            migrate_rocksdb_payload_storage: true,
-            migrate_rocksdb_payload_indices: true,
             appendable_quantization: true,
             single_file_mmap_vector_storage: false,
         }
@@ -102,28 +53,14 @@ impl FeatureFlags {
 pub fn init_feature_flags(mut flags: FeatureFlags) {
     let FeatureFlags {
         all,
-        payload_index_skip_rocksdb,
-        payload_index_skip_mutable_rocksdb,
-        payload_storage_skip_rocksdb,
         incremental_hnsw_building,
-        migrate_rocksdb_id_tracker,
-        migrate_rocksdb_vector_storage,
-        migrate_rocksdb_payload_storage,
-        migrate_rocksdb_payload_indices,
         appendable_quantization,
         single_file_mmap_vector_storage,
     } = &mut flags;
 
     // If all is set, explicitly set all feature flags
     if *all {
-        *payload_index_skip_rocksdb = true;
-        *payload_index_skip_mutable_rocksdb = true;
-        *payload_storage_skip_rocksdb = true;
         *incremental_hnsw_building = true;
-        *migrate_rocksdb_id_tracker = true;
-        *migrate_rocksdb_vector_storage = true;
-        *migrate_rocksdb_payload_storage = true;
-        *migrate_rocksdb_payload_indices = true;
         *appendable_quantization = true;
         *single_file_mmap_vector_storage = true;
     }
