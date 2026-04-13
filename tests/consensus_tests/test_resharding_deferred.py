7371f6a585c0cea7e7bc3cc61f1ad52ccfa7e07e
Kyamran Shakhaev <shkyga@yandex.ru>
2026-04-13 21:12:06 +0300

    Review fixes

--- tests/consensus_tests/test_resharding_deferred.py
+++ tests/consensus_tests/test_resharding_deferred.py
@@ -307,8 +307,8 @@ def test_resharding_transfer_deferred_points(tmp_path: pathlib.Path, direction: 
                     }
                 },
             )
-        assert_http_ok(resp)
-        time.sleep(1)
+            assert_http_ok(resp)
+            time.sleep(1)
 
     # Complete resharding: commit hash rings and finish.
     resp = commit_read_hashring(peer_api_uris[0])
