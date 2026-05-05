e7bc1e2b7533cd0cbe9bfcf0d6303b0451bfd288
generall <andrey@vasnetsov.com>
2026-05-06 01:03:15 +0200

    deduplicate get_vector_opt

--- lib/segment/src/vector_storage/multi_dense/read_only/chunked_vector_storage.rs
+++ lib/segment/src/vector_storage/multi_dense/read_only/chunked_vector_storage.rs
@@ -1,17 +1,16 @@
-use std::borrow::Cow;
-
 use common::bitvec::{BitSlice, BitVec};
 use common::generic_consts::AccessPattern;
 use common::types::PointOffsetType;
 use common::universal_io::UniversalReadFamily;
 
-use crate::data_types::named_vectors::{CowMultiVector, CowVector};
+use crate::data_types::named_vectors::CowVector;
 use crate::data_types::primitive::PrimitiveVectorElement;
-use crate::data_types::vectors::{TypedMultiDenseVector, TypedMultiDenseVectorRef};
 use crate::types::{Distance, VectorStorageDatatype};
+use crate::vector_storage::VectorStorageRead;
 use crate::vector_storage::chunked_vectors::ChunkedVectorsRead;
-use crate::vector_storage::multi_dense::appendable_mmap_multi_dense_vector_storage::MultivectorMmapOffset;
-use crate::vector_storage::{VectorOffset, VectorOffsetType, VectorStorageRead};
+use crate::vector_storage::multi_dense::appendable_mmap_multi_dense_vector_storage::{
+    MultivectorMmapOffset, read_multi_vector,
+};
 
 #[derive(Debug)]
 pub struct ReadOnlyChunkedMultiDenseVectorStorage<T: PrimitiveVectorElement, S: UniversalReadFamily>
@@ -51,26 +50,8 @@ impl<T: PrimitiveVectorElement, S: UniversalReadFamily> VectorStorageRead
     }
 
     fn get_vector_opt<P: AccessPattern>(&self, key: PointOffsetType) -> Option<CowVector<'_>> {
-        self.offsets
-            .get::<P>(key as VectorOffsetType)
-            .and_then(|mmap_offset| {
-                let mmap_offset = mmap_offset.first().expect("mmap_offset must not be empty");
-                self.vectors
-                    .get_many::<P>(mmap_offset.offset(), mmap_offset.multi_vector_count())
-            })
-            .map(|flattened_vectors| {
-                let multi = match flattened_vectors {
-                    Cow::Borrowed(slice) => CowMultiVector::Borrowed(TypedMultiDenseVectorRef {
-                        flattened_vectors: slice,
-                        dim: self.vectors.dim(),
-                    }),
-                    Cow::Owned(vec) => CowMultiVector::Owned(TypedMultiDenseVector {
-                        flattened_vectors: vec,
-                        dim: self.vectors.dim(),
-                    }),
-                };
-                CowVector::MultiDense(T::into_float_multivector(multi))
-            })
+        read_multi_vector::<T, P, _, _>(&self.offsets, &self.vectors, key)
+            .map(|multi| CowVector::MultiDense(T::into_float_multivector(multi)))
     }
 
     fn is_deleted_vector(&self, key: PointOffsetType) -> bool {
