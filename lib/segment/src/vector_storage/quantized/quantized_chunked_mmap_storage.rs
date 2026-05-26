d4a34a119bf4437e52195778549f92f8f5d68f94
Daniel Boros <dancixx@gmail.com>
2026-05-26 16:19:56 +0200

    fix: is_in_ram_or_mmap for S3

--- lib/segment/src/vector_storage/quantized/quantized_chunked_mmap_storage.rs
+++ lib/segment/src/vector_storage/quantized/quantized_chunked_mmap_storage.rs
@@ -78,12 +78,11 @@ impl quantization::EncodedStorage for QuantizedChunkedMmapStorage {
 
         match StorageType::storage_kind() {
             UniversalKind::IoUring |
-            UniversalKind::DiskCache => false,
+            UniversalKind::DiskCache | UniversalKind::S3 |
+            UniversalKind::Gcs |
+            UniversalKind::Azure => false,
             UniversalKind::SimpleDiskCache | // FIXME: only `true` if it was entirely prefilled
             UniversalKind::Mmap => true,
-            UniversalKind::S3 |
-            UniversalKind::Gcs |
-            UniversalKind::Azure => todo!(),
         }
     }
 
