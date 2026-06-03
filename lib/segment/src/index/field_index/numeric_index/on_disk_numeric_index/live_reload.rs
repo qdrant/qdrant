f3969f4501cd2bb1c68e296deb220f5801536a0f
Daniel Boros <dancixx@gmail.com>
2026-06-03 18:33:59 +0200

    feat: implement LiveReload for numeric index

--- lib/segment/src/index/field_index/numeric_index/on_disk_numeric_index/live_reload.rs
+++ lib/segment/src/index/field_index/numeric_index/on_disk_numeric_index/live_reload.rs
@@ -5,12 +5,21 @@ use common::universal_io::UniversalRead;
 use crate::common::operation_error::OperationResult;
 use crate::index::field_index::LiveReload;
 use crate::index::field_index::numeric_index::Encodable;
+<<<<<<< HEAD
 use crate::index::field_index::numeric_index::on_disk_numeric_index::OnDiskNumericIndex;
 use crate::index::field_index::numeric_point::Numericable;
 use crate::index::field_index::on_disk_point_to_values::StoredValue;
 
 impl<T: Encodable + Numericable + Default + StoredValue + 'static, S: UniversalRead> LiveReload
     for OnDiskNumericIndex<T, S>
+=======
+use crate::index::field_index::numeric_index::universal_numeric_index::UniversalNumericIndex;
+use crate::index::field_index::numeric_point::Numericable;
+use crate::index::field_index::stored_point_to_values::StoredValue;
+
+impl<T: Encodable + Numericable + Default + StoredValue + 'static, S: UniversalRead> LiveReload
+    for UniversalNumericIndex<T, S>
+>>>>>>> a1e2bdd1a (feat: implement LiveReload for numeric index)
 {
     type Fs = S::Fs;
 
