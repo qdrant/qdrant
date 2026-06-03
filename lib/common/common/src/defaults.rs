531eb2df69306cb033749b215d2a683720fcedc8
timvisee <tim@visee.me>
2026-06-03 15:27:28 +0200

    Bump dev version to 1.18.3-dev

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -7,7 +7,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Qdrant version string
-pub const QDRANT_VERSION_STRING: &str = "1.18.2-dev";
+pub const QDRANT_VERSION_STRING: &str = "1.18.3-dev";
 
 /// Current Qdrant semver version
 pub static QDRANT_VERSION: LazyLock<Version> =
