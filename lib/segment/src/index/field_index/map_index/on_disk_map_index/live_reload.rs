7d0d583dd87c1818b2ac496c87e753e8bb90973c
Luis Cossío <luis.cossio@outlook.com>
2026-06-08 13:00:47 -0400

    use in ReadOnlyMapIndex

--- lib/segment/src/index/field_index/map_index/on_disk_map_index/live_reload.rs
+++ lib/segment/src/index/field_index/map_index/on_disk_map_index/live_reload.rs
@@ -1,5 +1,6 @@
 use common::counter::hardware_counter::HardwareCounterCell;
 use common::persisted_hashmap::Key;
+use common::sorted_slice::SortedSlice;
 use common::types::PointOffsetType;
 use common::universal_io::UniversalRead;
 
@@ -18,8 +19,8 @@ where
     fn live_reload(
         &mut self,
         _fs: &S::Fs,
-        deleted_points: &[PointOffsetType],
-        _new_points: &[PointOffsetType],
+        deleted_points: &SortedSlice<'_, PointOffsetType>,
+        _new_points: &SortedSlice<'_, PointOffsetType>,
         _hw_counter: &HardwareCounterCell,
     ) -> OperationResult<()> {
         // No on-disk state is changing when we live-reload, as
