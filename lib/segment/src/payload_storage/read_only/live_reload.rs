cb608a4e05cb6eaee39a53609285b20a0c478153
Luis Cossío <luis.cossio@outlook.com>
2026-07-30 15:29:00 -0400

    adjust existing trait implementations

--- lib/segment/src/payload_storage/read_only/live_reload.rs
+++ lib/segment/src/payload_storage/read_only/live_reload.rs
@@ -1,18 +1,18 @@
 use common::counter::hardware_counter::HardwareCounterCell;
 use common::sorted_slice::SortedSlice;
 use common::types::PointOffsetType;
-use common::universal_io::UniversalRead;
+use common::universal_io::{UniversalRead, UniversalReadFs};
 
 use super::ReadOnlyPayloadStorage;
 use crate::common::live_reload::LiveReload;
 use crate::common::operation_error::OperationResult;
 
 impl<S: UniversalRead> LiveReload for ReadOnlyPayloadStorage<S> {
-    type Fs = S::Fs;
+    type File = S;
 
-    fn live_reload(
+    fn live_reload<Fs: UniversalReadFs<File = S>>(
         &mut self,
-        fs: &S::Fs,
+        fs: &Fs,
         _deleted_points: &SortedSlice<'_, PointOffsetType>,
         _new_points: &SortedSlice<'_, PointOffsetType>,
         _hw_counter: &HardwareCounterCell,
