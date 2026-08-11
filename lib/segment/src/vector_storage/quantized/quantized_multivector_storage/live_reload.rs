c24f0784ac720e26f8ebaf6959a88317256f71df
Luis Cossío <luis.cossio@outlook.com>
2026-08-11 19:38:34 -0400

    impl live_preload for read-only quantized vectors

--- lib/segment/src/vector_storage/quantized/quantized_multivector_storage/live_reload.rs
+++ lib/segment/src/vector_storage/quantized/quantized_multivector_storage/live_reload.rs
@@ -1,7 +1,7 @@
 use common::counter::hardware_counter::HardwareCounterCell;
 use common::sorted_slice::SortedSlice;
 use common::types::PointOffsetType;
-use common::universal_io::{UniversalRead, UniversalReadFs};
+use common::universal_io::{CachedReadFs, UniversalRead, UniversalReadFs};
 
 use super::MultivectorOffsetsStorageChunkedRead;
 use crate::common::operation_error::OperationResult;
@@ -9,6 +9,10 @@ use crate::index::field_index::LiveReload;
 
 impl<S: UniversalRead> LiveReload for MultivectorOffsetsStorageChunkedRead<S> {
     type File = S;
+
+    fn live_preload<Fs: CachedReadFs<File = S>>(&self, fs: &Fs) -> OperationResult<()> {
+        self.data.live_preload(fs)
+    }
 
     /// Pick up offsets a writer appended to the chunked backing.
     fn live_reload<Fs: UniversalReadFs<File = S>>(
