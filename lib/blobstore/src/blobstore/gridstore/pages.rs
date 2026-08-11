961a714090d823269b0c1c1bfe4573a0fc954fd5
Luis Cossío <luis.cossio@outlook.com>
2026-08-11 18:05:18 -0400

    support UnchangedOpen in `live_reload`

--- lib/blobstore/src/blobstore/gridstore/pages.rs
+++ lib/blobstore/src/blobstore/gridstore/pages.rs
@@ -8,7 +8,7 @@ use common::generic_consts::AccessPattern;
 use common::maybe_uninit::assume_init_vec;
 use common::mmap::{Advice, AdviceSetting};
 use common::universal_io::{
-    CachedReadFs, FileIndex, Flusher, OpenOptions, Populate, ReadPipeline, ReadRange,
+    CachedReadFs, FileIndex, Flusher, OkUnchanged, OpenOptions, Populate, ReadPipeline, ReadRange,
     UniversalRead, UniversalReadFs, UniversalWrite, UserData,
 };
 use itertools::Either;
@@ -496,10 +496,20 @@ impl<S: UniversalRead> Pages<S> {
         let next_page_id = num_pages as PageId;
 
         if num_pages > 0 {
-            // Re-attach the last page, which should have the latest data.
-            self.pages.pop();
-            let page_path = self.page_path((num_pages - 1) as PageId);
-            self.attach_page(fs, &page_path, populate)?;
+            // Refresh the last page, which should have the latest data.
+            let last_page_id = num_pages - 1;
+            let page_path = self.page_path(last_page_id as PageId);
+            let changed = fs
+                .open(
+                    &page_path,
+                    page_open_options(populate, self.writeable),
+                    Default::default(),
+                )
+                // data might have not changed.
+                .ok_unchanged()?;
+            if let Some(page) = changed {
+                self.pages[last_page_id] = page;
+            }
         }
 
         for page_id in next_page_id.. {
