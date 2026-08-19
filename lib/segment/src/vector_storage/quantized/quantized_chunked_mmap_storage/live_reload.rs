88c161f232aa1d7d3499c0a93b2dc5ce2673f952
Luis Cossío <luis.cossio@outlook.com>
2026-08-19 15:09:05 -0400

    [LiveReload] Preload all vector storages (#10228)
    * impl live_preload for read-only vector storages
    
    * impl live_preload for read-only quantized vectors

--- lib/segment/src/vector_storage/quantized/quantized_chunked_mmap_storage/live_reload.rs
+++ lib/segment/src/vector_storage/quantized/quantized_chunked_mmap_storage/live_reload.rs
@@ -1,7 +1,7 @@
 use common::counter::hardware_counter::HardwareCounterCell;
 use common::sorted_slice::SortedSlice;
 use common::types::PointOffsetType;
-use common::universal_io::{UniversalRead, UniversalReadFs};
+use common::universal_io::{CachedReadFs, UniversalRead, UniversalReadFs};
 
 use crate::common::operation_error::OperationResult;
 use crate::index::field_index::LiveReload;
@@ -9,6 +9,10 @@ use crate::vector_storage::quantized::quantized_chunked_mmap_storage::QuantizedC
 
 impl<S: UniversalRead> LiveReload for QuantizedChunkedStorageRead<S> {
     type File = S;
+
+    fn live_preload<Fs: CachedReadFs<File = S>>(&self, fs: &Fs) -> OperationResult<()> {
+        self.data.live_preload(fs)
+    }
 
     /// Pick up quantized vectors a writer appended to the chunked backing.
     fn live_reload<Fs: UniversalReadFs<File = S>>(
--- lib/segment/src/vector_storage/read_only/live_reload.rs
+++ lib/segment/src/vector_storage/read_only/live_reload.rs
@@ -1,7 +1,7 @@
 use common::counter::hardware_counter::HardwareCounterCell;
 use common::sorted_slice::SortedSlice;
 use common::types::PointOffsetType;
-use common::universal_io::{UniversalRead, UniversalReadFs};
+use common::universal_io::{CachedReadFs, UniversalRead, UniversalReadFs};
 
 use super::VectorStorageReadEnum;
 use crate::common::live_reload::LiveReload;
@@ -9,6 +9,24 @@ use crate::common::operation_error::OperationResult;
 
 impl<S: UniversalRead> LiveReload for VectorStorageReadEnum<S> {
     type File = S;
+
+    fn live_preload<Fs: CachedReadFs<File = S>>(&self, fs: &Fs) -> OperationResult<()> {
+        match self {
+            VectorStorageReadEnum::Dense(s) => s.live_preload(fs),
+            VectorStorageReadEnum::DenseByte(s) => s.live_preload(fs),
+            VectorStorageReadEnum::DenseHalf(s) => s.live_preload(fs),
+            VectorStorageReadEnum::DenseChunked(s) => s.live_preload(fs),
+            VectorStorageReadEnum::DenseChunkedByte(s) => s.live_preload(fs),
+            VectorStorageReadEnum::DenseChunkedHalf(s) => s.live_preload(fs),
+            VectorStorageReadEnum::MultiDenseChunked(s) => s.live_preload(fs),
+            VectorStorageReadEnum::MultiDenseChunkedByte(s) => s.live_preload(fs),
+            VectorStorageReadEnum::MultiDenseChunkedHalf(s) => s.live_preload(fs),
+            VectorStorageReadEnum::DenseTurbo(s) => s.live_preload(fs),
+            VectorStorageReadEnum::DenseTurboChunked(s) => s.live_preload(fs),
+            VectorStorageReadEnum::MultiDenseTurbo(s) => s.live_preload(fs),
+            VectorStorageReadEnum::Sparse(s) => s.live_preload(fs),
+        }
+    }
 
     fn live_reload<Fs: UniversalReadFs<File = S>>(
         &mut self,
